2715a3f59c87bfe8b0b68851ec054b5304c72443
matthew de haast <matt@fynbos.dev>
2021-08-20 12:04:05 +0000

    feat(infra): initializing the AWS infrastructure

---
+++ infra/aws/live/logs/baseline/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	secure_baseline "gitlab.com/fynbos/infra/aws/modules/secure-baseline"
+)
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		rootStack, err := pulumi.NewStackReference(ctx, "fynbos/aws-root/baseline", nil)
+		if err != nil {
+			return err
+		}
+		ctKMSKeyArn := rootStack.GetOutput(pulumi.String("cloudtrailKMSKeyArn"))
+		ctS3BucketName := rootStack.GetOutput(pulumi.String("cloudtrailS3BucketName"))
+
+		// Configure cloudtrail
+		err = secure_baseline.NewCloudtrailBaseline(ctx, "logs-ct", &secure_baseline.CloudtrailBaselineArgs{
+			KmsKeyId:     pulumi.Sprintf("%s", ctKMSKeyArn),
+			S3BucketName: pulumi.Sprintf("%s", ctS3BucketName),
+		})
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+}
