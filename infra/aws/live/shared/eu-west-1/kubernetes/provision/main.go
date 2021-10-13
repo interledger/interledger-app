2d5119eed8422506642674b42de648863a86b76d
Donovan Changfoot <don@fynbos.dev>
2021-10-13 15:36:38 +0200

    feat(infra): provision eks cluster with one managed node group
    An eks cluster is provisioned in eu-west-1. The control plane is
    deployed in an AWS managed VPC. We then deploy 1 managed node group in
    our own VPC.
    
    The end state we want our EKS cluster in is to have the cluster endpoint
    only accessible from within our VPC. However, pulumi needs access to the
    cluster to configure the `aws-auth` configmap and VPC CNI add on.
    We therefore need to first provision the cluster with public access to
    the cluster endpoint. This will allow us to configure the cluster.
    Once this is done we then disable the public access.

---
+++ infra/aws/live/shared/eu-west-1/kubernetes/provision/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"os"
+
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
+	k8s "gitlab.com/fynbos/infra/aws/modules/kubernetes"
+	"gitlab.com/fynbos/infra/aws/modules/utils"
+)
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		clusterName := "live-eu-west-1-eks-cluster"
+		// Setting this to `true` will publically expose the clusters admin endpoint.
+		// We do this so that we can configure the cluster afterwhich we will disable
+		// the public access.
+		allowPublicConfiguration := os.Getenv("ALLOW_PUBLIC_CONFIGURATION") == "true"
+		fynbosConf := config.New(ctx, "fynbos")
+		awsAccountId := fynbosConf.Get("accountId")
+		vpcStack, err := pulumi.NewStackReference(ctx, "fynbos/aws-shared-euwest1-networking/main", nil)
+		if err != nil { return err }
+
+		vpcId := vpcStack.GetIDOutput(pulumi.String("vpcId"))
+		publicSubnetIds := utils.StringArrayOutputFromStack(vpcStack, "publicSubnets")
+		privateSubnetIds := utils.StringArrayOutputFromStack(vpcStack, "privateSubnets")
+
+		roles, err := k8s.NewEksRoles(ctx, awsAccountId)
+		if err != nil { return err }
+		ctx.Export("adminRoleArn", roles.Admin.Arn)
+		ctx.Export("automationRoleArn", roles.Automation.Arn)
+
+		cluster, err := k8s.NewEksControlPlane(ctx, k8s.EksControlPlaneArgs{
+			Name: clusterName,
+			Version: "1.21",
+			VpcId: vpcId,
+			PublicSubnets: publicSubnetIds,
+			PrivateSubnets: privateSubnetIds,
+			AccountId: awsAccountId,
+			IamRoles: roles,
+			ExposeAdminEndpoint: allowPublicConfiguration,
+		})
+		if err != nil { return err }
+		ctx.Export("kubeconfig", cluster.Kubeconfig)
+		ctx.Export("oidcProvider", cluster.Core.OidcProvider())
+		ctx.Export("clusterEndpoint", cluster.Core.Endpoint())
+
+		_, err = k8s.NewManagedNodeGroup(ctx, k8s.ManagedNodeGroupArgs{
+			Name: "managed-ng-0",
+			Cluster: cluster,
+			NodeRole: roles.NodeGroup,
+			InstanceTypes: pulumi.StringArray{pulumi.String("t2.medium")},
+			MinSize: 0,
+			MaxSize: 2,
+			DesiredSize: 1,
+			SubnetIds: privateSubnetIds,
+		})
+
+		return nil
+	})
+}
