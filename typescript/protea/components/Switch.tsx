12f4a87ec6ca8fc3078d3e0dfbaf3bab9f76c672
Cairin Michie <cairinmichie@gmail.com>
2021-10-21 20:57:03 +0200

    feat(protea): implement Switch component
    A switch allows for toggling settings in the UI.

---
+++ typescript/protea/components/Switch.tsx
@@ -0,0 +1,31 @@
+import { FC, useState } from 'react'
+import { Switch as HeadlessSwitch } from '@headlessui/react'
+
+type ContainerProps = {
+  className?: string
+  enabled: boolean
+  onChange: any
+}
+
+export const Switch: FC<ContainerProps> = ({
+  children,
+  className,
+  enabled,
+  onChange
+}) => {
+  return (
+    <HeadlessSwitch
+      checked={enabled}
+      onChange={onChange}
+      className={`${enabled ? 'bg-primary' : 'bg-strong'}
+          inline-flex flex-shrink-0 h-[24px] w-[46px] p-[2px] rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-black`}
+    >
+      <span className='sr-only'>Test data</span>
+      <span
+        aria-hidden='true'
+        className={`${enabled ? 'translate-x-[22px]' : 'translate-x-0'}
+            pointer-events-none inline-block h-[20px] w-[20px] rounded-full bg-white shadow-lg transform transition ease-in-out duration-200`}
+      />
+    </HeadlessSwitch>
+  )
+}
