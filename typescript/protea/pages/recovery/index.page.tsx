47a8e77fcd24f2d29728f08c1d35fd4d7a726851
Cairin Michie <cairinmichie@gmail.com>
2021-10-26 16:36:22 +0200

    feat(protea): recovery page and form
    Basic styling for the form page, with server props.

--- typescript/protea/pages/recovery/index.page.tsx
+++ typescript/protea/pages/recovery/index.page.tsx
@@ -1,28 +1,28 @@
-import { useRouter } from 'next/router'
-import { NextPage } from 'next'
-import { useSession } from 'hooks'
-import { RecoveryForm } from './recoveryForm'
-import { Container, Routes } from 'components'
+import { GetServerSideProps, InferGetServerSidePropsType, NextPage } from 'next'
+import { RecoveryForm } from './RecoveryForm'
+import { Logo, Router, Routes } from 'components'
+import React from 'react'
+import { checkSession } from 'lib/kratos'
 
-const RecoveryPage: NextPage = () => {
-  const session = useSession()
-  const router = useRouter()
-
-  if (session) {
-    // TODO: user has valid session. what should we do?
-    // Kratos will return error nodes if there is a valid session
-    // and user is trying to recover.
-    router.push(Routes.organisation)
-    return null
-  }
-
+const RecoveryPage: NextPage<
+  InferGetServerSidePropsType<typeof getServerSideProps>
+> = () => {
   return (
-    <div className='relative overflow-hidden w-full'>
-      <Container className=' overflow-x-hidden'>
-        <RecoveryForm />
-      </Container>
-    </div>
+    <main className='flex flex-col items-start justify-center max-w-sm mx-auto h-screen px-4'>
+      <Router href={Routes.home} aria-label='Fynbos logo'>
+        <Logo className='h-8' />
+      </Router>
+      <h1 className='text-4xl font-display font-medium text-strong mt-6 mb-1 leading-normal'>
+        Recover your account
+      </h1>
+      <p className='text-medium mb-10'>
+        We’ll send you an email to change your password.
+      </p>
+      <RecoveryForm />
+    </main>
   )
 }
 
 export default RecoveryPage
+
+export const getServerSideProps: GetServerSideProps = checkSession
