1766f4a3584883126adc87dbeaf24b17ccc38d68
Cairin Michie <cairinmichie@gmail.com>
2021-10-26 16:36:22 +0200

    feat(protea): profile page and form
    Basic styling for the form page, with server props.

--- typescript/protea/pages/profile/index.page.tsx
+++ typescript/protea/pages/profile/index.page.tsx
@@ -1,24 +1,36 @@
-import { useRouter } from 'next/router'
-import { Container, Routes } from 'components'
-import { useSession } from 'hooks'
-import { ProfileForm } from './profileForm'
+import type { NextPage } from 'next'
+import { Routes, Dashboard } from 'components'
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
+import { getSession } from 'lib/kratos'
+import { RecoveryPasswordForm } from './RecoveryPasswordForm'
 
-const SettingsPage = () => {
-  const session = useSession()
-  const router = useRouter()
-
-  if (!session) {
-    router.push(Routes.login)
-    return null
-  }
-
+const OverviewPage: NextPage<
+  InferGetServerSidePropsType<typeof getServerSideProps>
+> = ({ session }) => {
   return (
-    <div className='relative overflow-hidden w-full'>
-      <Container className=' overflow-x-hidden'>
-        <ProfileForm />
-      </Container>
-    </div>
+    <Dashboard route={Routes.profile}>
+      <span className='text-4xl font-display font-medium'>Overview</span>
+      <br />
+      <span className='text-2xl font-normal font-display'>Personal info</span>
+      <br />
+      <span className='uppercase text-xs font-normal font-medium font-sans'>
+        password
+      </span>
+      <span className='text-base font-normal font-sans'>
+        &#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;
+      </span>
+      <br />
+      <span className='uppercase text-xs font-normal font-medium font-sans'>
+        email
+      </span>
+      <span className='text-base font-normal font-sans'>
+        {session?.identity.traits.email}
+      </span>
+      <RecoveryPasswordForm />
+    </Dashboard>
   )
 }
 
-export default SettingsPage
+export default OverviewPage
+
+export const getServerSideProps: GetServerSideProps = getSession
