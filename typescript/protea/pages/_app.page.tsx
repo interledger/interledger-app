c77227e59d116d2c52649e8ca8b55088d63cf3c0
Cairin Michie <cairinmichie@gmail.com>
2021-10-26 16:36:22 +0200

    feat(protea): move session out of _app
    This removes the auth context provider that was providing the session
    information to pages. And rather fetches the session data in
    getServerSideProps on each page.
    
    To achieve SSR, we need a second kratos client to be used server side.
    This is only because we can't resolve dns inside the container - the
    client itself does actually work both on the client and server.
    
    There are two default functions exposed for getServerSideProps:
    
    - checkSession: to be used on pages that shouldn't need auth, and should
                    be redirected away from when the user has an auth context.
                    e.g. login
    - getSession:   to be used on pages that should be authed, redirects to
                    login/signup if the user isn't authed.

--- typescript/protea/pages/_app.page.tsx
+++ typescript/protea/pages/_app.page.tsx
@@ -1,17 +1,8 @@
 import 'styles/main.css'
-import App, { AppProps as NextAppProps, AppContext } from 'next/app'
+import { AppProps as NextAppProps } from 'next/app'
 import Head from 'next/head'
-import { Session } from '@ory/kratos-client'
-import { AxiosError } from 'axios'
-import { AuthContext } from 'contexts/auth'
-import { kratos } from 'lib/kratos'
-import { Routes } from 'components'
 
-interface AppProps extends NextAppProps {
-  session: Session | undefined
-}
-
-function MyApp({ Component, pageProps, session }: AppProps) {
+function MyApp({ Component, pageProps }: NextAppProps) {
   const metaContent = {
     title: 'Fynbos',
     description: 'Connecting the Internet economy.'
@@ -46,73 +37,9 @@ function MyApp({ Component, pageProps, session }: AppProps) {
           content='minimum-scale=1, initial-scale=1, width=device-width'
         />
       </Head>
-      <AuthContext.Provider value={{ session }}>
-        <Component {...pageProps} />
-      </AuthContext.Provider>
+      <Component {...pageProps} />
     </>
   )
-}
-
-// This turns off automatic static optimization unless a page
-// explicitly specifies optimization. Every page is then SSR
-// where we fetch the session data from Kratos.
-MyApp.getInitialProps = async (appContext: AppContext) => {
-  const appProps = await App.getInitialProps(appContext)
-
-  if (process.env.KRATOS_ENABLED === 'true') {
-    // forward cookie to Kratos since this is now happening server side.
-    const cookie = appContext.ctx.req?.headers.cookie
-    const session = await kratos
-      .toSession(undefined, cookie)
-      .then((res) => res.data)
-      .catch((err: AxiosError) => {
-        switch (err.response?.status) {
-          case 403:
-          // This is a legacy error code thrown. See code 422 for
-          // more details.
-          case 422:
-            // This status code is returned when we are trying to
-            // validate a session which has not yet completed
-            // it's second factor
-            redirect(Routes.login + '?aal=aal2', appContext)
-            return undefined
-          case 401:
-            if (isProtected(appContext.ctx.pathname)) {
-              redirect(Routes.login, appContext)
-            }
-            return undefined
-        }
-
-        // Something else happened!
-        throw new Error(err.response?.status.toString())
-        return undefined
-      })
-  }
-
-  // Temp till we deploy Kratos in prod
-  if (appContext.ctx.pathname !== '' && appContext.ctx.pathname !== '/') {
-    redirect(Routes.home, appContext)
-    return undefined
-  }
-
-  return { ...appProps, session: undefined }
-}
-
-// TODO: pattern for protected routes
-const isProtected = (path: string): boolean => {
-  return path.includes(Routes.organisation)
-}
-
-const redirect = (path: string, appContext: AppContext): void => {
-  if (appContext.ctx.res) {
-    appContext.ctx.res?.writeHead(302, {
-      Location: path
-    })
-    appContext.ctx.res.end()
-    return
-  }
-
-  appContext.router.push(path)
 }
 
 export default MyApp
