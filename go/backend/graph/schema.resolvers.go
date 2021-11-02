efbbf48e0f6e204e8b87a80520c1b1ed7945da6c
Donovan Changfoot <don@fynbos.dev>
2021-11-02 16:20:09 +0200

    feat(backend): start graphql endpoints for organisation
    The service has been wired up to connect to cockroachdb so that the e2e
    flow can be tested for creating and getting organisations using graphql.
    The e2e flow spins up CockroachDb in a test container and serves the
    graphql endpoint using the go test http server. An actual graphql client
    is then used to make requests against the server and create/read
    organisations from the DB.
    
    Note that when connecting to the DB we read in the certs from file and
    build the connections string with the ssl inlined to side step file
    permission issues when it comes to deploy time.

--- go/backend/graph/schema.resolvers.go
+++ go/backend/graph/schema.resolvers.go
@@ -7,19 +7,33 @@ import (
 	"context"
 
 	"gitlab.com/fynbos/backend/graph/generated"
-	"gitlab.com/fynbos/backend/graph/model"
+	"gitlab.com/fynbos/backend/models"
 )
 
-func (r *queryResolver) Organisation(ctx context.Context) (*model.Organisation, error) {
-	org := &model.Organisation{
-		ID: "1",
-		Name: "My first organisation",
+func (r *mutationResolver) CreateOrganisation(ctx context.Context, name string) (*models.Organisation, error) {
+	org, err := r.Organisations.Create(name)
+	if err != nil {
+		return nil, err
 	}
 
 	return org, nil
 }
 
+func (r *queryResolver) Organisation(ctx context.Context, id string) (*models.Organisation, error) {
+	org, err := r.Organisations.Get(id)
+	// TODO: error handling
+	if err != nil {
+		return nil, err
+	}
+
+	return org, nil
+}
+
+// Mutation returns generated.MutationResolver implementation.
+func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
+
 // Query returns generated.QueryResolver implementation.
 func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 
+type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
--- go/go.mod
+++ go/go.mod
@@ -33,6 +33,7 @@ require (
 	github.com/hashicorp/errwrap v1.0.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.0 // indirect
 	github.com/hashicorp/golang-lru v0.5.1 // indirect
+	github.com/machinebox/graphql v0.2.2 // indirect
 	github.com/matryer/moq v0.0.0-20200106131100-75d0ddfc0007 // indirect
 	github.com/mitchellh/mapstructure v1.1.2 // indirect
 	github.com/moby/sys/mount v0.2.0 // indirect
