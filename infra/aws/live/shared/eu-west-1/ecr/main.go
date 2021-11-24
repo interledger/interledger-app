57f3236ac75ed85e86a0f1a8f2ea6708ece9de0a
Donovan Changfoot <don@fynbos.dev>
2021-11-24 17:11:01 +0200

    feat(ci): use buildkit to build images and cache from ecr
    Kaniko was tested to do the builds and push to ECR. However, it was
    found to be slow for multi-stage builds even with remote caching. Since
    the backend base image needs to be run for testing anyway, a custom
    docker image is built with docker-ecr-credential-helper baked in (
    new pulumi `ecr` project). This image is then used to build the images
    with BUILDKIT. Although the docker layer caching isn't working properly
    with BUILDKIT and ECR, it still has the same run time as Kaniko with
    caching.
    
    The custom image is used to avoid running custom userdata on the runner
    instance to install docker-ecr-credential-helper and mount it into
    all containers running on the instance. Instead, the manager only has
    docker-ecr-credential-helper installed allowing it to pull the image
    only when it's specified in the `.gitlab.yaml` files.
    
    refs: https://gist.github.com/dreampuf/a0d416a15299a2ac74a0a5cb8f2871c0

--- infra/aws/live/shared/eu-west-1/ecr/main.go
+++ infra/aws/live/shared/eu-west-1/ecr/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/pulumi/pulumi-docker/sdk/v3/go/docker"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
 
@@ -16,6 +17,20 @@ func main() {
 			return err
 		}
 
+		// used to store our custom docker image that has the aws credential helper.
+		dockerRepo, err := ecr.NewPrivateRepository(ctx, "docker", accountID)
+		if err != nil {
+			return err
+		}
+
+		image, err := docker.NewImage(ctx, "docker-ecr", &docker.ImageArgs{
+			Build:     &docker.DockerBuildArgs{Context: pulumi.String("./")},
+			ImageName: pulumi.String(accountID + ".dkr.ecr.eu-west-1.amazonaws.com/docker:19.03.12"),
+			Registry:  docker.ImageRegistryArgs{}, // use ECR credential helper
+		})
+
+		ctx.Export("dockerRepoUri", dockerRepo.RepositoryUri)
+		ctx.Export("dockerImage", image.ImageName)
 		ctx.Export("backendEcrRepoUri", backendEcrRepo.RepositoryUri)
 		return nil
 	})
