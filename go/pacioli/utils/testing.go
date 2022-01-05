74afacaaf858667a88f1df1828ae9fe50d0dedcf
Donovan Changfoot <don@fynbos.dev>
2022-01-05 18:35:35 +0200

    feat(pacioli): create accounts in TigerBeetle
    Tenants can create accounts in ledgers. The accounts inherit its `code`
    from an account category. This will be used later on when transfers
    are created to enforce transaction types. The `unit` needs to be
    assigned which represents the currency.
    
    UUIDs are used for account IDs and the string format is converted back
    into a `u128` so that it can be stored in TigerBeetle. The `ledgerID` is
    stored in the `userData` field of TigerBeetle so that it can be
    retrieved later to enforce tenant isolation.

--- go/pacioli/utils/testing.go
+++ go/pacioli/utils/testing.go
@@ -5,8 +5,10 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"path/filepath"
 	"runtime"
+	"time"
 
 	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/cockroachdb"
@@ -92,4 +94,76 @@ func TruncateDb(ctx context.Context, db *sqlx.DB) error {
 	const query = `SELECT 'TRUNCATE TABLE ' + Table_Schema + '.' + Table_Name from INFORMATION_SCHEMA.tables where table_type = 'base table'`
 	_, err := db.ExecContext(ctx, query)
 	return err
+}
+
+type TigerBeetleContainer struct {
+	testcontainers.Container
+	URI     string
+	DataDir string
+}
+
+func SetupTigerBeetle(ctx context.Context, clusterID uint32) (*TigerBeetleContainer, error) {
+	const (
+		TIGERBEETLE_PORT = "3000"
+		TIGERBEETLE_DIR  = "/var/lib/tigerbeetle"
+	)
+
+	dataDir, err := ioutil.TempDir("", "tb")
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println("Initializing TigerBeetle temporary data dir:" + dataDir)
+	initReq := testcontainers.ContainerRequest{
+		Image:        "tigerbeetle", // TODO: host image
+		ExposedPorts: []string{TIGERBEETLE_PORT},
+		WaitingFor:   wait.ForLog("info: initialized data file").WithPollInterval(1 * time.Second),
+		BindMounts: map[string]string{
+			TIGERBEETLE_DIR: dataDir,
+		},
+		Cmd: []string{
+			"init",
+			fmt.Sprintf("--cluster=%d", clusterID),
+			"--replica=0",
+			"--directory=" + TIGERBEETLE_DIR,
+		},
+	}
+	_, err = testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: initReq,
+		Started:          true,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println("Starting TigerBeetle test container.")
+	req := testcontainers.ContainerRequest{
+		Image:        "tigerbeetle", // TODO: host image
+		ExposedPorts: []string{TIGERBEETLE_PORT},
+		BindMounts: map[string]string{
+			TIGERBEETLE_DIR: dataDir,
+		},
+		WaitingFor: wait.ForLog(fmt.Sprintf("info: cluster=%d replica=0: listening on 0.0.0.0:3000", clusterID)).WithPollInterval(1 * time.Second),
+		Cmd: []string{
+			"start",
+			fmt.Sprintf("--cluster=%d", clusterID),
+			"--replica=0",
+			"--addresses=0.0.0.0:" + TIGERBEETLE_PORT,
+			"--directory=" + TIGERBEETLE_DIR,
+		},
+	}
+	container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	mappedPort, err := container.MappedPort(ctx, TIGERBEETLE_PORT)
+	if err != nil {
+		return nil, err
+	}
+
+	return &TigerBeetleContainer{Container: container, URI: fmt.Sprintf("0.0.0.0:%s", mappedPort.Port()), DataDir: dataDir}, nil
 }
