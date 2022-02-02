b51363b3b2bddb3e34d3b34d61e46accd3e08da2
Cairin Michie <cairinmichie@gmail.com>
2022-02-02 11:55:19 +0200

    feat(protea): update button component styles to match designs
    - Adds the outline variant
    - Adds icon prop
    - Changes to the pill shape and filled background

--- typescript/protea/components/Button.tsx
+++ typescript/protea/components/Button.tsx
@@ -1,17 +1,29 @@
 import React, { FC } from 'react'
 
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  icon?: React.ReactNode
+  outline?: boolean
+}
 
-export const Button: FC<ButtonProps> = ({ children, ...buttonProps }, ref) => {
+export const Button: FC<ButtonProps> = (
+  { children, icon, outline, ...buttonProps },
+  ref
+) => {
+  const activeClassNames = outline
+    ? 'bg-white ring-2 ring-base hover:ring-hover active:ring-active'
+    : 'bg-container-primary hover:bg-container-primary-hover active:bg-container-primary-active'
   return (
     <button
       {...buttonProps}
-      className={`h-12 max-w-max px-6 ${
+      className={`focus:outline-none flex h-10 items-center rounded-full font-display text-sm font-medium text-medium ${
+        icon ? 'pl-4 pr-6' : 'px-6'
+      } ${
         buttonProps.disabled
-          ? 'focus:outline-none cursor-not-allowed bg-strong text-medium'
-          : 'focus:outline-none cursor-pointer bg-primary focus:ring-2 focus:ring-focus'
+          ? 'cursor-not-allowed bg-disabled text-disabled'
+          : `cursor-pointer focus:ring-2 focus:ring-focus ${activeClassNames}`
       }`}
     >
+      {icon && <div className='mr-2'>{icon}</div>}
       {children}
     </button>
   )
