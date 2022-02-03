64f1a709fe884836fea27a7af9f0eec7dfdd577c
Cairin Michie <cairinmichie@gmail.com>
2022-02-03 14:40:51 +0200

    feat(protea): scaffold base set of pages with new nav

---
+++ typescript/protea/pages/transact/receive/index.page.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import {
+  NextPage,
+  InferGetServerSidePropsType,
+  GetServerSideProps,
+  GetServerSidePropsResult
+} from 'next'
+import { Routes, WalletLayout } from 'components'
+import { getSessionOrRedirect } from 'lib/kratos'
+
+const TransactReceivePage: NextPage<
+  InferGetServerSidePropsType<typeof getServerSideProps>
+> = () => {
+  return (
+    <WalletLayout backRoute={Routes.transact} header='Receive' hideNav>
+      {/* TODO insert content */}
+    </WalletLayout>
+  )
+}
+
+export default TransactReceivePage
+
+export const getServerSideProps: GetServerSideProps = async (
+  context
+): Promise<GetServerSidePropsResult<any>> => {
+  const session = await getSessionOrRedirect(context, true)
+  if (session && 'redirect' in session) {
+    return session
+  }
+
+  return {
+    props: {}
+  }
+}
