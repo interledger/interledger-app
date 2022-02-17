a64b2c36d85f393db1521e21ff77c2eb6f8686a9
Cairin Michie <cairinmichie@gmail.com>
2022-02-17 21:13:27 +0200

    refactor(protea): wallet layout component
    Refactored the wallet layout component to be a hidden layout route.
    Replaced some of the inner elements to work more dynamically with the
    layout route.

---
+++ typescript/protea/app/routes/__WalletLayout.tsx
@@ -0,0 +1,155 @@
+import { Link, NavLink, Outlet, useLoaderData } from 'remix'
+import type { LoaderFunction } from 'remix'
+import { route } from 'routes-gen'
+import React, { FC } from 'react'
+import {
+  ConnectIcon,
+  SettingsIcon,
+  TransactIcon,
+  WalletIcon,
+  Logo,
+  LogoIcon
+} from '~/components'
+
+export const loader: LoaderFunction = async ({ request }) => {
+  let url = new URL(request.url)
+  return (
+    // Allow us to show/hide the nav on certain pages.
+    ['/home', '/transact', '/connect', '/settings'].findIndex(
+      (val) => val === url.pathname
+    ) >= 0
+  )
+}
+
+export default function WalletLayout() {
+  const showNav = useLoaderData<boolean>()
+  return (
+    <div className='flex flex-col text-medium sm:flex-row'>
+      {showNav && (
+        <NavRail>
+          <NavList>
+            <Link to={route('/')} aria-label='Fynbos logo'>
+              <LogoIcon className='mx-auto mb-4 h-8' />
+            </Link>
+            <NavListItem icon={<WalletIcon />} to={route('/home')}>
+              Home
+            </NavListItem>
+            <NavListItem icon={<TransactIcon />} to={route('/transact')}>
+              Transact
+            </NavListItem>
+            <NavListItem icon={<ConnectIcon />} to={route('/connect')}>
+              Connect
+            </NavListItem>
+            <NavListItem icon={<SettingsIcon />} to={route('/settings')}>
+              Settings
+            </NavListItem>
+          </NavList>
+        </NavRail>
+      )}
+      {showNav && (
+        <NavDrawer>
+          <NavList>
+            <Link to={route('/')} aria-label='Fynbos logo'>
+              <Logo className='mb-6 ml-4 h-8' />
+            </Link>
+            <NavListItem icon={<WalletIcon />} to={route('/home')}>
+              Home
+            </NavListItem>
+            <NavListItem icon={<TransactIcon />} to={route('/transact')}>
+              Transact
+            </NavListItem>
+            <NavListItem icon={<ConnectIcon />} to={route('/connect')}>
+              Connect
+            </NavListItem>
+          </NavList>
+          <NavList>
+            <NavListItem icon={<SettingsIcon />} to={route('/settings')}>
+              Settings
+            </NavListItem>
+          </NavList>
+        </NavDrawer>
+      )}
+      <div className='w-full'>
+        {/* The header and body of the page should be inserted here by the page. */}
+        <Outlet />
+      </div>
+      {showNav && (
+        <NavBar>
+          <NavList>
+            <NavListItem icon={<WalletIcon />} to={route('/home')}>
+              Home
+            </NavListItem>
+            <NavListItem icon={<TransactIcon />} to={route('/transact')}>
+              Transact
+            </NavListItem>
+            <NavListItem icon={<ConnectIcon />} to={route('/connect')}>
+              Connect
+            </NavListItem>
+          </NavList>
+        </NavBar>
+      )}
+    </div>
+  )
+}
+
+const NavList: FC = ({ children }) => {
+  return (
+    <ul className='flex w-full space-x-4 sm:flex-col sm:space-x-0 sm:space-y-6 lg:space-y-2'>
+      {children}
+    </ul>
+  )
+}
+
+const NavBar: FC = ({ children }) => {
+  return (
+    <div className='sticky bottom-0 flex h-20 min-w-full select-none justify-between bg-container-primary p-4 pt-3 font-display text-xs sm:hidden'>
+      {children}
+    </div>
+  )
+}
+
+const NavRail: FC = ({ children }) => {
+  return (
+    <ul className='sticky top-0 hidden h-screen min-w-max select-none flex-col justify-between bg-container p-4 px-3 pt-4 font-display text-xs sm:flex lg:hidden'>
+      {children}
+    </ul>
+  )
+}
+
+const NavDrawer: FC = ({ children }) => {
+  return (
+    <ul className='sticky top-0 hidden h-screen min-w-max select-none flex-col justify-between bg-container p-4 px-3 pt-4 font-display text-base lg:flex'>
+      {children}
+    </ul>
+  )
+}
+
+type NavListItemProps = {
+  icon?: React.ReactNode
+  to: string
+}
+
+const NavListItem: FC<NavListItemProps> = ({ children, icon, to }) => {
+  return (
+    <NavLink prefetch='render' className='w-full' to={to}>
+      {({ isActive }) => (
+        <li
+          className={`flex w-full flex-col items-center justify-between space-y-1 rounded-full sm:w-14 sm:space-y-2 lg:h-12 lg:w-56 lg:flex-row lg:justify-start lg:space-y-0 lg:space-x-3 lg:px-4 lg:hover:bg-container-hover ${
+            isActive ? 'ring-focus lg:ring-2' : ''
+          }`}
+        >
+          <div
+            className={`flex h-8 w-16 items-center justify-center rounded-full sm:w-14 lg:h-6 lg:w-6 ${
+              isActive
+                ? 'border-2 border-focus text-primary lg:border-0'
+                : 'text-medium'
+            }`}
+          >
+            {icon}
+          </div>
+          <div>{children}</div>
+        </li>
+      )}
+    </NavLink>
+  )
+}
