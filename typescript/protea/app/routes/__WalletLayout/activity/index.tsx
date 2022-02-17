cb69fe6324bd1131ee9ba670b4b790904fd44bc3
Cairin Michie <cairinmichie@gmail.com>
2022-02-17 21:11:52 +0200

    refactor(protea): page scaffolding
    Refactors the pages to use data loaders (Remix) instead of
    getServerSideProps (Nextjs). Mostly moving things over to use remix's
    conventions. Some extra formatting that was missed before.

--- typescript/protea/app/routes/__WalletLayout/activity/index.tsx
+++ typescript/protea/app/routes/__WalletLayout/activity/index.tsx
@@ -1,43 +1,25 @@
-import React from 'react'
-import {
-  NextPage,
-  InferGetServerSidePropsType,
-  GetServerSideProps,
-  GetServerSidePropsResult
-} from 'next'
-import { FilterIcon, Routes, WalletLayout } from 'components'
-import { getSessionOrRedirect } from 'lib/kratos'
+import type { LoaderFunction } from 'remix'
+import { requireUserSession } from '~/lib/kratos'
 
-const ActivityPage: NextPage<
-  InferGetServerSidePropsType<typeof getServerSideProps>
-> = () => {
-  return (
-    <WalletLayout
-      backRoute={Routes.walletHome}
-      header='Activity'
-      hideNav
-      actionButton={{
-        text: 'Filter',
-        route: Routes.activityFilter,
-        icon: <FilterIcon />
-      }}
-    >
-      {/* TODO insert content */}
-    </WalletLayout>
-  )
+export const loader: LoaderFunction = async ({ request }) => {
+  return requireUserSession(request)
 }
 
-export default ActivityPage
-
-export const getServerSideProps: GetServerSideProps = async (
-  context
-): Promise<GetServerSidePropsResult<any>> => {
-  const session = await getSessionOrRedirect(context, true)
-  if (session && 'redirect' in session) {
-    return session
-  }
-
-  return {
-    props: {}
-  }
+export default function ActivityPage() {
+  return (
+    // <WalletLayout
+    //   backRoute={Routes.walletHome}
+    //   header='Activity'
+    //   hideNav
+    //   actionButton={{
+    //     text: 'Filter',
+    //     route: Routes.activityFilter,
+    //     icon: <FilterIcon />
+    //   }}
+    // >
+    {
+      /* TODO insert content */
+    }
+    // </WalletLayout>
+  )
 }
--- typescript/protea/app/routes/__WalletLayout/transact/index.tsx
+++ typescript/protea/app/routes/__WalletLayout/transact/index.tsx
@@ -1,34 +1,31 @@
 import React from 'react'
-import {
-  NextPage,
-  InferGetServerSidePropsType,
-  GetServerSideProps,
-  GetServerSidePropsResult
-} from 'next'
-import { Routes, WalletLayout } from 'components'
-import { getSessionOrRedirect } from 'lib/kratos'
+import { LoaderFunction, NavLink } from 'remix'
+import { route } from 'routes-gen'
+import { SettingsIcon } from '~/components'
+import { requireUserSession } from '~/lib/kratos'
 
-const TransactPage: NextPage<
-  InferGetServerSidePropsType<typeof getServerSideProps>
-> = () => {
-  return (
-    <WalletLayout route={Routes.transact} header='Transact' settings>
-      {/* TODO insert content */}
-    </WalletLayout>
-  )
+export const loader: LoaderFunction = async ({ request }) => {
+  return requireUserSession(request)
 }
 
-export default TransactPage
-
-export const getServerSideProps: GetServerSideProps = async (
-  context
-): Promise<GetServerSidePropsResult<any>> => {
-  const session = await getSessionOrRedirect(context, true)
-  if (session && 'redirect' in session) {
-    return session
-  }
-
-  return {
-    props: {}
-  }
+export default function TransactPage() {
+  return (
+    <>
+      {/* Header */}
+      <header className='sticky top-0 flex h-16 min-w-full select-none items-center justify-between bg-white p-4 text-medium'>
+        <div className='flex items-center justify-start font-display text-2xl font-medium'>
+          Home
+        </div>
+        <NavLink className='sm:hidden' to={route('/home')}>
+          <div className='-mr-3 p-3 text-medium'>
+            <SettingsIcon />
+          </div>
+        </NavLink>
+      </header>
+      {/* Body */}
+      <div className='mx-auto grid min-h-[calc(100vh-9rem)] w-full grid-cols-4 content-start gap-4 gap-y-2 overflow-y-auto p-4 pb-24 sm:max-w-lg sm:grid-cols-8 sm:px-0 lg:max-w-3xl lg:grid-cols-12 xl:max-w-4xl'>
+        Body
+      </div>
+    </>
+  )
 }
