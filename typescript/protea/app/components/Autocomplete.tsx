48c217f7bf6b88995799356bb97e1a23d8ac9e84
Cairin Michie <cairinmichie@gmail.com>
2022-03-03 16:20:32 +0200

    feat(protea): implement autocomplete component
    https://headlessui.dev/react/combobox

---
+++ typescript/protea/app/components/Autocomplete.tsx
@@ -0,0 +1,120 @@
+import { Combobox, Transition } from '@headlessui/react'
+import React, { Fragment, FC } from 'react'
+import { SelectIcon, CheckIcon } from '.'
+
+type AutocompleteOptions = {
+  id: string
+  name: string
+}
+
+interface AutocompleteProps {
+  id: string
+  // Override the `className` of the root `div` of the Input. Defaults to **min-w-full**.
+  className?: string
+  disabled?: boolean
+  // The label value.
+  label?: string
+  // The message from errors produced by form validation.
+  errorMessage?: string
+  value?: AutocompleteOptions
+  options: AutocompleteOptions[]
+  onChange(value: AutocompleteOptions): void
+  onQuery(query: string): void
+}
+
+/**
+ * Autocomplete component
+ * https://headlessui.dev/react/combobox
+ * NOTE: a hidden input is required to submit to remix.
+ * @param param0
+ * @returns
+ */
+export const Autocomplete: FC<AutocompleteProps> = ({
+  id,
+  className,
+  label,
+  errorMessage,
+  value,
+  onChange,
+  onQuery,
+  options,
+  disabled
+}) => {
+  return (
+    <div className={className || 'min-w-full'}>
+      <label
+        htmlFor={id}
+        className='ml-2 block text-sm font-medium text-medium'
+      >
+        {label}
+      </label>
+      <Combobox disabled={disabled} value={value} onChange={onChange}>
+        <div className='relative'>
+          <div className='mt-1 h-12 min-w-full rounded-xl ring-2 ring-base focus:outline-none focus:ring focus:ring-focus focus-visible:ring focus-visible:ring-focus'>
+            <div className='flex h-full items-center justify-between overflow-hidden rounded-xl'>
+              <Combobox.Input
+                autoComplete='off'
+                type='text'
+                className='w-full overflow-hidden border-none focus:ring-0 '
+                displayValue={(value: AutocompleteOptions) => value?.name}
+                onChange={(event) => onQuery(event.target.value)}
+              />
+              <Combobox.Button className='flex h-full items-center bg-container px-4'>
+                <SelectIcon />
+              </Combobox.Button>
+            </div>
+          </div>
+          <Transition
+            as={Fragment}
+            leave='transition ease-in duration-100'
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'
+            afterLeave={() => onQuery('')}
+          >
+            <Combobox.Options className='absolute mt-2 max-h-60 w-full overflow-auto rounded-xl bg-container py-1 text-base shadow-lg focus:outline-none sm:text-sm'>
+              {options.length > 0 &&
+                options.map((option, index) => (
+                  <Combobox.Option
+                    key={index}
+                    className={({ active }) =>
+                      `relative flex h-12 cursor-pointer select-none items-center justify-between pl-4 pr-3 ${
+                        active
+                          ? 'bg-container-hover text-strong'
+                          : 'text-medium'
+                      }`
+                    }
+                    value={option}
+                  >
+                    {({ selected }) => (
+                      <>
+                        <span
+                          className={`block truncate font-sans text-base font-normal`}
+                        >
+                          {option.name}
+                        </span>
+                        {selected && (
+                          <span className='flex text-primary'>
+                            <CheckIcon />
+                          </span>
+                        )}
+                      </>
+                    )}
+                  </Combobox.Option>
+                ))}
+              {options.length === 0 && (
+                <div className='relative flex h-12 select-none items-center justify-between pl-4 pr-3 text-medium'>
+                  Nothing found.
+                </div>
+              )}
+            </Combobox.Options>
+          </Transition>
+        </div>
+      </Combobox>
+      <div className='h-7 pt-2 pl-2'>
+        {errorMessage && <p className='text-sm text-error'>{errorMessage}</p>}
+      </div>
+    </div>
+  )
+}
+
+Autocomplete.displayName = 'Autocomplete'
