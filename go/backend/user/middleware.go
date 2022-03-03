0002cbf7ab4b6ef81bb74a05b7fe484a4f2c5201
Matt de Haast <matt@fynbos.dev>
2022-03-03 16:01:11 +0200

    refactor(backend): user auth
    refactor the user service and middleware to rather attempt to convert
    a cookie into a session. If it cant it will just return nothing to
    the context. Downstream resolvers can now decide how to handle auth
    
    Signed-off-by: Matt de Haast <matt@fynbos.dev>

--- go/backend/user/middleware.go
+++ go/backend/user/middleware.go
@@ -2,6 +2,7 @@ package user
 
 import (
 	"context"
+	"errors"
 	"net/http"
 )
 
@@ -10,21 +11,21 @@ func MakeMiddleware(us Service) func(http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			user, err := us.GetUser(*r)
 			if err != nil {
-				switch err.(type) {
-				case *NoCookieError:
-					http.Error(w, "Unauthorized.", http.StatusUnauthorized)
-					return
-				default:
-					http.Error(w, "Invalid cookie.", http.StatusForbidden)
+				if !errors.Is(err, ErrNoUserFound) {
+					http.Error(w, "error parsing session", http.StatusInternalServerError)
 					return
 				}
 			}
 
-			// put it in context
-			ctx := context.WithValue(r.Context(), userCtxKey, user)
+			// If no user don't add to context
+			if user != nil {
+				// put it in context
+				ctx := context.WithValue(r.Context(), userCtxKey, user)
 
-			// and call the next with our new context
-			r = r.WithContext(ctx)
+				// and call the next with our new context
+				r = r.WithContext(ctx)
+			}
+
 			next.ServeHTTP(w, r)
 		})
 	}
--- go/backend/user/service.go
+++ go/backend/user/service.go
@@ -2,6 +2,7 @@ package user
 
 import (
 	"context"
+	"errors"
 	"net/http"
 	"time"
 
@@ -11,6 +12,8 @@ import (
 // A private key for context that only this package can access. This is important
 // to prevent collisions between different context uses
 var userCtxKey = &contextKey{"user"}
+
+var ErrNoUserFound = errors.New("no user found")
 
 type contextKey struct {
 	name string
@@ -38,24 +41,27 @@ func NewService(kratosClient *kratos.APIClient) (Service, error) {
 // ForContext finds the user from the context. REQUIRES Middleware to have run.
 func (self *service) ForContext(ctx context.Context) (*User, error) {
 	raw, ok := ctx.Value(userCtxKey).(*User)
-	if !ok {
-		return nil, NoUserFoundError{}
+	if !ok || raw == nil {
+		return nil, ErrNoUserFound
 	}
 	return raw, nil
 }
 
-// This will call out to Kratos to check if the session is valid.
+// GetUser will either return the user
 func (self *service) GetUser(r http.Request) (*User, error) {
 	c, err := r.Cookie(self.kratosCookieName)
 	if err != nil || c == nil {
-		return nil, &NoCookieError{}
+		return nil, ErrNoUserFound
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(self.kratosTimeout))
+	ctx, cancel := context.WithTimeout(context.Background(), self.kratosTimeout)
 	defer cancel()
 
-	session, _, err := self.kratos.V0alpha2Api.ToSession(ctx).Cookie(self.kratosCookieName + "=" + c.Value).Execute()
+	session, resp, err := self.kratos.V0alpha2Api.ToSession(ctx).Cookie(self.kratosCookieName + "=" + c.Value).Execute()
 	if err != nil {
+		if resp.StatusCode == http.StatusUnauthorized {
+			return nil, nil
+		}
 		return nil, err
 	}
 	traits := session.Identity.Traits.(map[string]interface{})
@@ -74,23 +80,4 @@ type User struct {
 
 type traits struct {
 	Email string
-}
-
-// Error set
-type NoUserFoundError struct{}
-
-func (r NoUserFoundError) Error() string {
-	return "No user found."
-}
-
-type NoCookieError struct{}
-
-func (e NoCookieError) Error() string {
-	return "Cookie not found."
-}
-
-type InvalidCookieError struct{}
-
-func (e InvalidCookieError) Error() string {
-	return "Invalid cookie."
 }
