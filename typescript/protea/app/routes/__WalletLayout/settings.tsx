49e9a357a7fb31751d4bd2f4c53a73200e0d634d
Cairin Michie <cairinmichie@gmail.com>
2022-03-07 22:19:48 +0200

    feat(protea): basic settings page layout
    Adds simple design of the settings page with dummy data.

--- typescript/protea/app/routes/__WalletLayout/settings.tsx
+++ typescript/protea/app/routes/__WalletLayout/settings.tsx
@@ -1,5 +1,7 @@
-import { LoaderFunction, redirect, useLoaderData } from 'remix'
+import React from 'react'
+import { Link, LoaderFunction, redirect, useLoaderData } from 'remix'
 import { route } from 'routes-gen'
+import { BackIcon, NextIcon } from '~/components'
 import { requireUserSession } from '~/lib/kratos'
 
 export const loader: LoaderFunction = async ({ request }) => {
@@ -17,14 +19,55 @@ export const loader: LoaderFunction = async ({ request }) => {
 export default function SettingsPage() {
   const session = useLoaderData()
   return (
-    // <WalletLayout
-    //   route={Routes.settings}
-    //   backRoute={Routes.walletHome}
-    //   header='Settings'
-    //   hideNav
-    // >
-    <div>{session?.identity.traits.email}</div>
-    //   &#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;
-    // </WalletLayout>
+    <div className='w-full'>
+      {/* Header */}
+      <header className='sticky top-0 mx-auto flex h-16 w-full select-none items-center justify-start bg-white p-4 text-medium sm:min-w-full'>
+        <Link className='sm:hidden' to={route('/home')}>
+          <div className='-ml-3 p-3 text-medium'>
+            <BackIcon />
+          </div>
+        </Link>
+        <div className='flex items-center justify-start font-display text-2xl font-medium'>
+          Settings
+        </div>
+      </header>
+      {/* Body */}
+      <div className='mx-auto grid min-h-[calc(100vh-9rem)] w-full grid-cols-4 content-start gap-4 gap-y-2 overflow-y-auto p-4 pb-24 sm:max-w-lg sm:grid-cols-8 sm:px-0 lg:max-w-3xl lg:grid-cols-12 xl:max-w-4xl'>
+        {/* Activity item */}
+        <span className='col-span-full ml-4 font-display text-lg font-medium sm:col-span-6 sm:col-start-2 lg:col-start-4'>
+          Personal info
+        </span>
+        <div className='col-span-full flex items-center justify-between rounded-xl bg-container px-4 py-2 sm:col-span-6 sm:col-start-2 lg:col-start-4'>
+          <div className='flex flex-col'>
+            <span className='font-display text-xs font-medium'>Email</span>
+            <span className='font-sans text-base font-normal'>
+              {session?.identity.traits.email}
+            </span>
+          </div>
+          <NextIcon />
+        </div>
+        <div className='col-span-full flex items-center justify-between rounded-xl bg-container px-4 py-2 sm:col-span-6 sm:col-start-2 lg:col-start-4'>
+          <div className='flex flex-col'>
+            <span className='font-display text-xs font-medium'>Country</span>
+            <span className='font-sans text-base font-normal'>
+              South Africa
+            </span>
+          </div>
+          <NextIcon />
+        </div>
+        <span className='col-span-full ml-4 font-display text-lg font-medium sm:col-span-6 sm:col-start-2 lg:col-start-4'>
+          Security
+        </span>
+        <div className='col-span-full flex items-center justify-between rounded-xl bg-container px-4 py-2 sm:col-span-6 sm:col-start-2 lg:col-start-4'>
+          <div className='flex flex-col'>
+            <span className='font-display text-xs font-medium'>Password</span>
+            <span className='font-sans text-base font-normal'>
+              &#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;
+            </span>
+          </div>
+          <NextIcon />
+        </div>
+      </div>
+    </div>
   )
 }
