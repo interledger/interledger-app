0b68fe7caeee62b62db67728ee5e22c8505038fd
Adrian Hope-Bailie <adrian@fynbos.dev>
2022-03-30 20:23:56 +0000

    fix(protea): update link to Open Payments docs in footer

--- typescript/protea/app/components/Footer.tsx
+++ typescript/protea/app/components/Footer.tsx
@@ -17,7 +17,7 @@ export const Footer: FC = () => {
         <Router to={'https://interledger.org'} aria-label='Interledger'>
           Interledger
         </Router>
-        <Router to={'https://openpayments.dev'} aria-label='Open Payments'>
+        <Router to={'https://docs.openpayments.guide'} aria-label='Open Payments'>
           Open Payments
         </Router>
       </div>
