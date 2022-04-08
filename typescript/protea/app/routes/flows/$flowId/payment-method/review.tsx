8ca2a4bef95d0e2e8a3d80eaa4bb363a6e0f7c1f
Cairin Michie <cairinmichie@gmail.com>
2022-04-08 10:41:54 +0200

    feat(protea): implement the new payment-method flow

---
+++ typescript/protea/app/routes/flows/$flowId/payment-method/review.tsx
@@ -0,0 +1,116 @@
+import React from 'react'
+import { Form, LoaderFunction, json, useLoaderData } from 'remix'
+import type { ActionFunction } from 'remix'
+import { Button } from '~/components'
+import { completeFlow, getCurrentFlow } from '~/lib/flows.server'
+import { apolloClient } from '~/lib/apollo.server'
+import {
+  LinkUsdBankAccountMutation,
+  LinkUsdBankAccountMutationVariables,
+  LinkUsdBankAccountDocument,
+  VerifyUsdBankAccountMutation,
+  VerifyUsdBankAccountMutationVariables,
+  VerifyUsdBankAccountDocument
+} from '~/generated/types'
+
+export const loader: LoaderFunction = async ({ request, params }) => {
+  const flow = await getCurrentFlow(request, params)
+  return json({
+    flow
+  })
+}
+
+export default function PaymentMethodReviewPage() {
+  const { flow } = useLoaderData()
+  const { accountNumber, institution, name, routingNumber, type } = flow?.data
+  return (
+    <>
+      <Form
+        id='payment-method-review'
+        action={`/flows/${flow.id}/payment-method/review`}
+        method='post'
+        className='hidden'
+      />
+
+      <div className='col-span-full flex flex-col pb-4 text-medium sm:col-span-6 sm:col-start-2 lg:col-start-4'>
+        <span className='font-sans text-sm font-medium'>Account Type</span>
+        <span className='font-sans text-base font-normal'>{type}</span>
+      </div>
+      <div className='col-span-full flex flex-col pb-4 text-medium sm:col-span-6 sm:col-start-2 lg:col-start-4'>
+        <span className='font-sans text-sm font-medium'>Institution</span>
+        <span className='font-sans text-base font-normal'>{institution}</span>
+      </div>
+      <div className='col-span-full flex flex-col pb-4 text-medium sm:col-span-6 sm:col-start-2 lg:col-start-4'>
+        <span className='font-sans text-sm font-medium'>Account number</span>
+        <span className='font-sans text-base font-normal'>{accountNumber}</span>
+      </div>
+      <div className='col-span-full flex flex-col pb-4 text-medium sm:col-span-6 sm:col-start-2 lg:col-start-4'>
+        <span className='font-sans text-sm font-medium'>Routing number</span>
+        <span className='font-sans text-base font-normal'>{routingNumber}</span>
+      </div>
+      <div className='col-span-full flex flex-col pb-4 text-medium sm:col-span-6 sm:col-start-2 lg:col-start-4'>
+        <span className='font-sans text-sm font-medium'>Nickname</span>
+        <span className='font-sans text-base font-normal'>{name}</span>
+      </div>
+
+      <div className='col-span-full flex justify-end pt-4 sm:col-span-6 sm:col-start-2 lg:col-start-4'>
+        <Button form='payment-method-review' type='submit'>
+          Confirm
+        </Button>
+      </div>
+    </>
+  )
+}
+
+export const action: ActionFunction = async ({ request, params }) => {
+  const flow = await getCurrentFlow(request, params)
+  const { accountNumber, institution, name, routingNumber, type } = flow?.data
+  const cookie = request.headers.get('cookie')
+  const linkUsdBankAccountMutationVariables = {
+    input: {
+      accountNumber: accountNumber,
+      institution: institution,
+      name: name,
+      routingNumber: routingNumber,
+      type: type
+    }
+  }
+
+  const linkRes = await apolloClient.mutate<
+    LinkUsdBankAccountMutation,
+    LinkUsdBankAccountMutationVariables
+  >({
+    mutation: LinkUsdBankAccountDocument,
+    variables: linkUsdBankAccountMutationVariables,
+    context: {
+      headers: {
+        cookie: cookie
+      }
+    }
+  })
+  if (
+    linkRes.data?.linkUsdBankAccount.success &&
+    linkRes.data?.linkUsdBankAccount.fundingSource != null
+  ) {
+    const verifyUsdBankAccountMutationVariables = {
+      input: {
+        FundingSourceId: linkRes.data?.linkUsdBankAccount?.fundingSource.id
+      }
+    }
+
+    const res = await apolloClient.mutate<
+      VerifyUsdBankAccountMutation,
+      VerifyUsdBankAccountMutationVariables
+    >({
+      mutation: VerifyUsdBankAccountDocument,
+      variables: verifyUsdBankAccountMutationVariables,
+      context: {
+        headers: {
+          cookie: cookie
+        }
+      }
+    })
+    if (res.data?.verifyUsdBankAccount.success)
+      return await completeFlow(request)
+  }
+}
