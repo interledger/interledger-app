8a6facb8498a31cbf06edfb30f15b480e5527a6a
Donovan Changfoot <don@fynbos.dev>
2022-04-20 15:57:37 +0200

    feat(cf): pay.fynbos.dev records and access

--- infra/cloudflare/fynbos.dev/devClusterAccess.go
+++ infra/cloudflare/fynbos.dev/devClusterAccess.go
@@ -132,3 +132,41 @@ func CreateDevRetoolAccess(ctx *pulumi.Context, zoneID pulumi.IDOutput) error {
 
 	return nil
 }
+
+func CreateDevPayAccess(ctx *pulumi.Context, zoneID pulumi.IDOutput) error {
+	app, err := cloudflare.NewAccessApplication(ctx, "dev-pay-app", &cloudflare.AccessApplicationArgs{
+		AutoRedirectToIdentity: pulumi.Bool(true),
+		Domain:                 pulumi.String("pay.fynbos.dev"),
+		Name:                   pulumi.String("Dev Cluster"),
+		SessionDuration:        pulumi.String("24h"),
+		Type:                   pulumi.String("self_hosted"),
+		ZoneId:                 zoneID,
+		// Allowed IDP was manually created so we don't need to store secrets when deploying it
+		AllowedIdps: pulumi.StringArray{
+			pulumi.String("4271ed52-0611-4386-b1f8-f8e8c1c8391d"),
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	_, err = cloudflare.NewAccessPolicy(ctx, "dev-pay-access-policy", &cloudflare.AccessPolicyArgs{
+		ApplicationId: app.ID(),
+		ZoneId:        zoneID,
+		Name:          pulumi.String("Dev pay policy"),
+		Precedence:    pulumi.Int(1),
+		Decision:      pulumi.String("allow"),
+		Includes: cloudflare.AccessPolicyIncludeArray{
+			&cloudflare.AccessPolicyIncludeArgs{
+				EmailDomains: pulumi.StringArray{
+					pulumi.String("fynbos.dev"),
+				},
+			},
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
