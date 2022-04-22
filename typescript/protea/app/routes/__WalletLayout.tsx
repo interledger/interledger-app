1daa8c378bf26f78ab2eb8165d379ddd438f04dd
Cairin Michie <cairinmichie@gmail.com>
2022-04-22 16:10:55 +0200

    feat(protea): migrate transact to action instead of page
    Before this /transact was one the the main home pages in the Nav. Now it
    is the main action button on the other home pages. Transact currently
    encompasses send, receive, deposit, withdraw.
    
    Changes:
    - Added Deposit/Withdraw icons
    - Add temporary placeholder for /receive
    - Temporarily bumped headless-ui to insiders to ensure desired FAB functionality.
    - Remove /transact/.. placeholder pages
    - Move /activity into the main Nav, and update the header appropriately.
    - Created FAB components for mobile and desktop in the WalletLayout.

--- typescript/protea/app/routes/__WalletLayout.tsx
+++ typescript/protea/app/routes/__WalletLayout.tsx
@@ -1,7 +1,13 @@
-import { Link, NavLink, Outlet, useLocation } from '@remix-run/react'
+import {
+  Link,
+  NavLink,
+  Outlet,
+  useLocation,
+  useNavigate
+} from '@remix-run/react'
 import { route } from 'routes-gen'
 import type { FC } from 'react'
-import React from 'react'
+import React, { Fragment } from 'react'
 import {
   ConnectIcon,
   SettingsIcon,
@@ -9,13 +15,19 @@ import {
   WalletIcon,
   Logo,
   LogoIcon,
-  Router
+  Router,
+  QRIcon,
+  SendIcon,
+  ActivityIcon,
+  WithdrawIcon,
+  DepositIcon
 } from '~/components'
+import { Menu, Popover, Transition } from '@headlessui/react'
 
 // Allow us to show/hide the NavRail and NavDrawer on certain pages.
 function showNav(pathname: string) {
   return (
-    ['/home', '/transact', '/connect', '/settings'].findIndex(
+    ['/home', '/activity', '/connect', '/settings'].findIndex(
       (val) => val === pathname
     ) >= 0
   )
@@ -24,7 +36,7 @@ function showNav(pathname: string) {
 // Allow us to show/hide the NavBar on certain pages.
 function showNavBar(pathname: string) {
   return (
-    ['/home', '/transact', '/connect'].findIndex((val) => val === pathname) >= 0
+    ['/home', '/activity', '/connect'].findIndex((val) => val === pathname) >= 0
   )
 }
 
@@ -39,11 +51,12 @@ export default function WalletLayout() {
             <Link to={route('/')} aria-label='Fynbos logo'>
               <LogoIcon className='mx-auto mb-4 h-8' />
             </Link>
+            <NavFAB />
             <NavListItem icon={<WalletIcon />} to={route('/home')}>
               Home
             </NavListItem>
-            <NavListItem icon={<TransactIcon />} to={route('/transact')}>
-              Transact
+            <NavListItem icon={<ActivityIcon />} to={route('/activity')}>
+              Activity
             </NavListItem>
             <NavListItem icon={<ConnectIcon />} to={route('/connect')}>
               Connect
@@ -57,16 +70,19 @@ export default function WalletLayout() {
       {showNav(location.pathname) && (
         <NavDrawer>
           <NavList>
-            <div className='mb-6 ml-4 h-8'>
+            <div className='mb-2 ml-4'>
               <Router to={route('/')} aria-label='Fynbos logo'>
                 <Logo className='h-8' />
               </Router>
             </div>
+            <div className='pb-6'>
+              <NavFAB />
+            </div>
             <NavListItem icon={<WalletIcon />} to={route('/home')}>
               Home
             </NavListItem>
-            <NavListItem icon={<TransactIcon />} to={route('/transact')}>
-              Transact
+            <NavListItem icon={<ActivityIcon />} to={route('/activity')}>
+              Activity
             </NavListItem>
             <NavListItem icon={<ConnectIcon />} to={route('/connect')}>
               Connect
@@ -87,14 +103,17 @@ export default function WalletLayout() {
             <NavListItem icon={<WalletIcon />} to={route('/home')}>
               Home
             </NavListItem>
-            <NavListItem icon={<TransactIcon />} to={route('/transact')}>
-              Transact
+            <NavListItem icon={<ActivityIcon />} to={route('/activity')}>
+              Activity
             </NavListItem>
             <NavListItem icon={<ConnectIcon />} to={route('/connect')}>
               Connect
             </NavListItem>
           </NavList>
         </NavBar>
+      )}
+      {(location.pathname == '/home' || location.pathname == '/activity') && (
+        <HomeFAB />
       )}
     </div>
   )
@@ -163,5 +182,205 @@ const NavListItem: FC<NavListItemProps> = ({ children, icon, to }) => {
         </li>
       )}
     </NavLink>
+  )
+}
+
+function NavFAB() {
+  return (
+    <Menu as='div'>
+      <div>
+        <Menu.Button className='inline-flex cursor-pointer items-center space-x-3 rounded-2xl bg-container-primary p-4 text-medium hover:bg-container-primary-hover focus-visible:outline-2 focus-visible:outline-focus lg:w-full'>
+          <TransactIcon /> <span className='hidden lg:flex'>Transact</span>
+        </Menu.Button>
+      </div>
+      <Transition
+        as={Fragment}
+        enter='transition ease-out duration-100'
+        enterFrom='transform opacity-0 scale-95'
+        enterTo='transform opacity-100 scale-100'
+        leave='transition ease-in duration-75'
+        leaveFrom='transform opacity-100 scale-100'
+        leaveTo='transform opacity-0 scale-95'
+      >
+        <Menu.Items className='absolute ml-20 -mt-14 w-56 origin-top-left rounded-xl bg-container shadow-lg focus:outline-none lg:ml-[15.5rem]'>
+          <Menu.Item>
+            {({ active }) => (
+              <Router
+                className={`flex w-full items-center space-x-3 px-4 py-3 text-sm first-of-type:rounded-t-xl last-of-type:rounded-b-xl hover:bg-container-hover ${
+                  active ? 'bg-container-hover' : ''
+                }`}
+                to={route('/flows/:flowId/send/to', {
+                  flowId: 'init'
+                })}
+              >
+                <SendIcon />
+                <span>Send</span>
+              </Router>
+            )}
+          </Menu.Item>
+          <Menu.Item>
+            {({ active }) => (
+              <Router
+                className={`flex w-full items-center space-x-3 px-4 py-3 text-sm first-of-type:rounded-t-xl last-of-type:rounded-b-xl hover:bg-container-hover ${
+                  active ? 'bg-container-hover' : ''
+                }`}
+                to={route('/receive')}
+              >
+                <QRIcon />
+                <span>Recieve</span>
+              </Router>
+            )}
+          </Menu.Item>
+          <Menu.Item>
+            {({ active }) => (
+              <Router
+                className={`flex w-full items-center space-x-3 px-4 py-3 text-sm first-of-type:rounded-t-xl last-of-type:rounded-b-xl hover:bg-container-hover ${
+                  active ? 'bg-container-hover' : ''
+                }`}
+                to={route('/flows/:flowId/deposit/payment-method', {
+                  flowId: 'init'
+                })}
+              >
+                <DepositIcon />
+                <span>Deposit</span>
+              </Router>
+            )}
+          </Menu.Item>
+          <Menu.Item>
+            {({ active }) => (
+              <Router
+                className={`flex w-full items-center space-x-3 px-4 py-3 text-sm first-of-type:rounded-t-xl last-of-type:rounded-b-xl hover:bg-container-hover ${
+                  active ? 'bg-container-hover' : ''
+                }`}
+                to={route('/flows/:flowId/withdraw/payment-method', {
+                  flowId: 'init'
+                })}
+              >
+                <WithdrawIcon />
+                <span>Withdraw</span>
+              </Router>
+            )}
+          </Menu.Item>
+        </Menu.Items>
+      </Transition>
+    </Menu>
+  )
+}
+
+function HomeFAB() {
+  // TODO Scroll state for extended FAB
+  const navigate = useNavigate()
+  return (
+    <div className='fixed top-16 w-full max-w-sm px-4'>
+      <Popover className='relative'>
+        {({ open }) => (
+          <>
+            <Popover.Button
+              onClick={(event: any) => {
+                if (open) {
+                  event.preventDefault()
+                  navigate(
+                    route('/flows/:flowId/send/to', {
+                      flowId: 'init'
+                    })
+                  )
+                }
+              }}
+              className='fixed right-4 bottom-24 font-display text-sm font-medium transition-all sm:hidden'
+            >
+              {!open && (
+                <div className='flex w-min cursor-pointer items-center space-x-3 rounded-2xl bg-container-primary-active p-4 text-medium shadow-lg focus-visible:outline-2 focus-visible:outline-focus'>
+                  <TransactIcon /> <span>Transact</span>
+                </div>
+              )}
+              {open && (
+                <div className='flex items-center space-x-4'>
+                  <span>Send</span>
+                  <div className='flex w-min cursor-pointer items-center space-x-3 rounded-2xl bg-primary p-4 text-white shadow-lg focus-visible:outline-2 focus-visible:outline-focus'>
+                    <SendIcon />
+                  </div>
+                </div>
+              )}
+            </Popover.Button>
+            <Transition as={Fragment}>
+              <Popover.Panel className='fixed right-6 bottom-[168px] z-10 flex flex-col-reverse space-y-4 space-y-reverse font-display text-sm font-medium '>
+                <Transition.Child
+                  as={Fragment}
+                  enter='transition ease-out duration-200'
+                  enterFrom='opacity-0 translate-y-2'
+                  enterTo='opacity-100 translate-y-0'
+                  leave='transition ease-in duration-150'
+                  leaveFrom='opacity-100 translate-y-0'
+                  leaveTo='opacity-0 translate-y-2'
+                >
+                  <div className='flex items-center justify-end space-x-6'>
+                    <span>Receive</span>
+                    <Router
+                      to={route('/receive')}
+                      className='flex items-center justify-center rounded-xl bg-container-primary p-2 shadow-lg'
+                    >
+                      <QRIcon />
+                    </Router>
+                  </div>
+                </Transition.Child>
+                <Transition.Child
+                  as={Fragment}
+                  enter='transition ease-out duration-200 delay-[25ms]'
+                  enterFrom='opacity-0 translate-y-2'
+                  enterTo='opacity-100 translate-y-0'
+                  leave='transition ease-in duration-150'
+                  leaveFrom='opacity-100 translate-y-0'
+                  leaveTo='opacity-0 translate-y-2'
+                >
+                  <div className='flex items-center justify-end space-x-6'>
+                    <span>Deposit</span>
+                    <Router
+                      to={route('/flows/:flowId/deposit/payment-method', {
+                        flowId: 'init'
+                      })}
+                      className='flex items-center justify-center rounded-xl bg-container-primary p-2 shadow-lg'
+                    >
+                      <DepositIcon />
+                    </Router>
+                  </div>
+                </Transition.Child>
+                <Transition.Child
+                  as={Fragment}
+                  enter='transition ease-out duration-200 delay-[50ms]'
+                  enterFrom='opacity-0 translate-y-2'
+                  enterTo='opacity-100 translate-y-0'
+                  leave='transition ease-in duration-150'
+                  leaveFrom='opacity-100 translate-y-0'
+                  leaveTo='opacity-0 translate-y-2'
+                >
+                  <div className='flex items-center justify-end space-x-6'>
+                    <span>Withdraw</span>
+                    <Router
+                      to={route('/flows/:flowId/withdraw/payment-method', {
+                        flowId: 'init'
+                      })}
+                      className='flex items-center justify-center rounded-xl bg-container-primary p-2 shadow-lg'
+                    >
+                      <WithdrawIcon />
+                    </Router>
+                  </div>
+                </Transition.Child>
+              </Popover.Panel>
+            </Transition>
+            <Transition.Child
+              as={Fragment}
+              enter='ease-out duration-300'
+              enterFrom='opacity-0'
+              enterTo='opacity-100'
+              leave='ease-in duration-200'
+              leaveFrom='opacity-100'
+              leaveTo='opacity-0'
+            >
+              <Popover.Overlay className='fixed inset-0 -z-10 bg-white/90' />
+            </Transition.Child>
+          </>
+        )}
+      </Popover>
+    </div>
   )
 }
