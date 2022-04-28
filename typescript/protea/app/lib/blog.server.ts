594b262137a2dc9111266ed320c4fa69c9973a3f
Cairin Michie <cairinmichie@gmail.com>
2022-04-28 12:51:51 +0200

    fix(protea): blog to work with remix

---
+++ typescript/protea/app/lib/blog.server.ts
@@ -0,0 +1,52 @@
+import { DateTime } from 'luxon'
+
+import * as postA from '~/routes/blog/connecting-the-internet-economy.mdx'
+const modules = [postA]
+
+export type Author = {
+  name: string
+  twitterHandle: string
+  avatar: string
+}
+
+export type BlogMeta = {
+  title: string
+  authors: Author[]
+  description: string
+  date: string
+  slug: string
+}
+
+const authors: any = {
+  cairin: {
+    name: 'Cairin Michie',
+    twitterHandle: 'cairinbruce',
+    avatar: '/Frame 9.png'
+  },
+  adrian: {
+    name: 'Adrian Hope-Bailie',
+    twitterHandle: 'ahopebailie',
+    avatar: '/adrian.profile.webp'
+  }
+}
+
+export async function getAllPosts() {
+  const posts = modules.sort(
+    (mod1, mod2) =>
+      DateTime.fromJSDate(mod2.attributes.meta.date).toSeconds() -
+      DateTime.fromJSDate(mod1.attributes.meta.date).toSeconds()
+  )
+  return posts.map((mod) => {
+    return {
+      ...mod.attributes.meta,
+      slug: mod.filename.replace(/\.mdx?$/, ''),
+      authors: mod.attributes.meta.authors.map(
+        // TODO: handle random authors
+        (author: string) => authors[author]
+      ),
+      date: DateTime.fromJSDate(mod.attributes.meta.date).toFormat(
+        'dd LLLL yyyy'
+      )
+    }
+  })
+}
