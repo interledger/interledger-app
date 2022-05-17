7a2014c8808f0e360f1147afc9a80ccbc093cf61
Donovan Changfoot <don@fynbos.dev>
2022-05-17 16:25:12 +0200

    feat(backend): implement onboard unit customer activity methods
    Fill in implementation of to create account and map the unit customer
    to the account.

--- go/backend/onboarding/unitOnboardingActivity.go
+++ go/backend/onboarding/unitOnboardingActivity.go
@@ -1,9 +1,12 @@
 package onboarding
 
 import (
+	context "context"
 	"fmt"
 
 	"github.com/go-playground/validator/v10"
+	"gitlab.com/fynbos/backend/accounts"
+	"gitlab.com/fynbos/backend/identity"
 	"gitlab.com/fynbos/backend/providers/unit"
 )
 
@@ -11,10 +14,14 @@ type (
 	Activity struct {
 		validator *validator.Validate
 		up        unit.Service
+		as        accounts.Service
+		is        identity.Service
 	}
 
 	ActivityArgs struct {
-		Up unit.Service `validate:"required"`
+		Up unit.Service     `validate:"required"`
+		As accounts.Service `validate:"required"`
+		Is identity.Service `validate:"required"`
 	}
 )
 
@@ -24,13 +31,57 @@ func NewActivity(args *ActivityArgs) (*Activity, error) {
 		return nil, fmt.Errorf("%w %s", ErrInvalidArgument, err)
 	}
 
-	return &Activity{v, args.Up}, nil
+	return &Activity{
+		validator: v,
+		up:        args.Up,
+		as:        args.As,
+		is:        args.Is,
+	}, nil
 }
 
-func (a *Activity) CreateAccount() error {
-	return nil
+func (a *Activity) CreateAccount(ctx context.Context, identityID string, country string) (string, error) {
+	// make sure identity exists
+	_, err := a.is.Get(ctx, identityID)
+	if err != nil {
+		return "", fmt.Errorf("%w %s", ErrInternal, err)
+	}
+
+	acc, err := a.as.Create(ctx, &accounts.CreateAccountArgs{
+		IdentityID: identityID,
+		Country:    country,
+	})
+	if err != nil {
+		return "", fmt.Errorf("%w %s", ErrInternal, err)
+	}
+
+	return acc.ID, nil
 }
 
-func (a *Activity) MapCustomerToAccount() error {
+type MapCustomerToAccountArgs struct {
+	CustomerID string `validate:"required"`
+	Type       string `validate:"required"`
+	AccountID  string `validate:"required"`
+}
+
+func (a *Activity) MapCustomerToAccount(ctx context.Context, args *MapCustomerToAccountArgs) error {
+	if err := a.validator.Struct(args); err != nil {
+		return fmt.Errorf("%w %s", ErrInvalidArgument, err)
+	}
+
+	// make sure account exists
+	_, err := a.as.Get(ctx, args.AccountID)
+	if err != nil {
+		return fmt.Errorf("%w %s", ErrInternal, err)
+	}
+
+	_, err = a.up.CreateCustomer(ctx, &unit.CreateCustomerArgs{
+		ID:        args.CustomerID,
+		AccountID: args.AccountID,
+		Type:      args.Type,
+	})
+	if err != nil {
+		return fmt.Errorf("%w %s", ErrInternal, err)
+	}
+
 	return nil
 }
--- go/backend/onboarding/unitOnboardingWorkflow_test.go
+++ go/backend/onboarding/unitOnboardingWorkflow_test.go
@@ -1,8 +1,11 @@
 package onboarding
 
 import (
+	context "context"
 	"testing"
 
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
 	"go.temporal.io/sdk/testsuite"
 )
@@ -29,7 +32,29 @@ func (s *UnitTestSuite) AfterTest(suiteName, testName string) {
 }
 
 func (s *UnitTestSuite) TestOnboardingSuccess() {
-	s.env.ExecuteWorkflow(OnboardUnitCustomerWorkflow)
+	accountID := uuid.NewString()
+	workflowArgs := &OnboardUnitCustomerArgs{
+		CustomerID: uuid.NewString(),
+		Type:       "individual",
+		IdentityID: uuid.NewString(),
+		Country:    "US",
+	}
+	s.env.OnActivity(s.oa.CreateAccount, mock.Anything, mock.Anything, mock.Anything).Return(
+		func(ctx context.Context, identityID string, country string) (string, error) {
+			s.Equal(workflowArgs.IdentityID, identityID)
+			s.Equal(workflowArgs.Country, country)
+			return accountID, nil
+		},
+	)
+	s.env.OnActivity(s.oa.MapCustomerToAccount, mock.Anything, mock.Anything).Return(
+		func(ctx context.Context, args *MapCustomerToAccountArgs) error {
+			s.Equal(workflowArgs.Type, args.Type)
+			s.Equal(workflowArgs.CustomerID, args.CustomerID)
+			s.Equal(accountID, args.AccountID)
+			return nil
+		},
+	)
+	s.env.ExecuteWorkflow(OnboardUnitCustomerWorkflow, workflowArgs)
 	s.True(s.env.IsWorkflowCompleted())
 	s.NoError(s.env.GetWorkflowError())
 }
