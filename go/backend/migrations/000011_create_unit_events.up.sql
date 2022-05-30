751320cbc9f5aa8424c84021b9d69f7ee6b9a3a4
Ömer Talha Özdemir <omertoast@gmail.com>
2022-05-30 16:41:57 +0300

    reafactor(backend): change raw_event type to JSONB

--- go/backend/migrations/000011_create_unit_events.up.sql
+++ go/backend/migrations/000011_create_unit_events.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS unit_events
 (
 	id 							TEXT 			NOT NULL PRIMARY KEY,
 	type 						TEXT 			NOT NULL,
-	raw_event 			TEXT 			NOT NULL,
+	raw_event 			JSONB 		NOT NULL,
 	created_at 			TIMESTAMP NOT NULL DEFAULT now(),
 	updated_at 			TIMESTAMP NOT NULL DEFAULT now()
 );
