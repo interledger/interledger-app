adbff98eb5edda0d87f1b02fd36ee24683fe4e80
Donovan Changfoot <don@fynbos.dev>
2022-06-01 01:49:32 +0200

    feat(backend): scaffold create mx bank account workflow and tests
    We will have the member and user guids from mx.
    We then need to perform the following:
    1) Get the mx account id the user selected during verification
    2) Create an mx account on our system
    3) initiate identity aggregation on mx
    4) wait till aggregation is complete
    5) verify user owns account.
    6) get account info
    7) set funding source mask
    8) create counter party on unit

--- go/backend/fundingsources/createMxBankAccountWorkflow.go
+++ go/backend/fundingsources/createMxBankAccountWorkflow.go
@@ -1,6 +1,8 @@
 package fundingsources
 
 import (
+	"time"
+
 	"go.temporal.io/sdk/workflow"
 )
 
@@ -10,9 +12,78 @@ type CreateMxBankAccountWorkflowArgs struct {
 	MxMemberGuid    string `validate:"required"`
 }
 
-type AccountInfo struct{}
+type AccountInfo struct {
+	AccountNumber string
+}
 
 func CreateMxBankAccountWorkflow(ctx workflow.Context, args *CreateMxBankAccountWorkflowArgs) error {
+	var a *Activity
+	ao := workflow.ActivityOptions{
+		StartToCloseTimeout:    10 * time.Second,
+		ScheduleToCloseTimeout: 15 * time.Second,
+	}
+
+	ctx = workflow.WithActivityOptions(ctx, ao)
+	logger := workflow.GetLogger(ctx)
+
+	logger.Info("Creating mx bank account")
+
+	mxAccountID := ""
+	err := workflow.ExecuteActivity(ctx, a.GetSelectedMxAccountGuid, args.MxUserGuid, args.MxMemberGuid).Get(ctx, &mxAccountID)
+	if err != nil {
+		logger.Error("Failed to find mx account.", err)
+		return err
+	}
+
+	err = workflow.ExecuteActivity(
+		ctx,
+		a.CreateMxAccount,
+		args.FundingSourceID,
+		args.MxUserGuid,
+		args.MxMemberGuid,
+		mxAccountID,
+	).Get(ctx, nil)
+	if err != nil {
+		logger.Error("Failed to create mx account.", err)
+		return err
+	}
+
+	err = workflow.ExecuteActivity(ctx, a.StartIdentityAggregation, args.MxUserGuid, args.MxMemberGuid).Get(ctx, nil)
+	if err != nil {
+		logger.Error("Failed to start mx identity aggregation.", err)
+		return err
+	}
+
+	err = workflow.ExecuteActivity(ctx, a.WaitForIdentityAggregation, args.MxUserGuid, args.MxMemberGuid).Get(ctx, nil)
+	if err != nil {
+		logger.Error("Mx identity aggregation failed.")
+		return err
+	}
+
+	err = workflow.ExecuteActivity(ctx, a.VerifyOwnership, args.FundingSourceID, args.MxUserGuid, args.MxMemberGuid, mxAccountID).Get(ctx, nil)
+	if err != nil {
+		logger.Error("Bank account is not owned by user.")
+		return err
+	}
+
+	accountInfo := &AccountInfo{}
+	err = workflow.ExecuteActivity(ctx, a.GetBankAccountInfo, args.MxUserGuid, mxAccountID).Get(ctx, accountInfo)
+	if err != nil {
+		logger.Error("Failed to get bank account info.")
+		return err
+	}
+
+	err = workflow.ExecuteActivity(ctx, a.SetMask, args.FundingSourceID, accountInfo.AccountNumber).Get(ctx, nil)
+	if err != nil {
+		logger.Error("Failed to set mask for funding source.")
+		return err
+	}
+
+	err = workflow.ExecuteActivity(ctx, a.CreateUnitCounterParty, args.FundingSourceID, accountInfo).Get(ctx, nil)
+	if err != nil {
+		logger.Error("Failed to create unit counter party.")
+		return err
+	}
 
 	return nil
 }
