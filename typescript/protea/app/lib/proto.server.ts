6046b15914d7d4ef511ea870a1d30f2dc2e5e609
Cairin Michie <cairinmichie@gmail.com>
2022-06-06 11:08:10 +0200

    feat(protea): implement decoding of rpc error messages
    Protobuf-ts doesn't handle deserialisation of rpc error messages.
    This will allow the client to receive standardized error messages from
    the backend.

--- typescript/protea/app/lib/proto.server.ts
+++ typescript/protea/app/lib/proto.server.ts
@@ -1,6 +1,22 @@
 import { ChannelCredentials } from '@grpc/grpc-js'
 import { BackendServiceClient } from '~/generated/protobuf-ts/backend/v1/backend_client'
 import { GrpcTransport } from '@protobuf-ts/grpc-transport'
+import { base64decode } from '@protobuf-ts/runtime'
+import {
+  RetryInfo,
+  DebugInfo,
+  QuotaFailure,
+  PreconditionFailure,
+  BadRequest,
+  RequestInfo,
+  ResourceInfo,
+  Help,
+  LocalizedMessage,
+  ErrorInfo
+} from '~/generated/protobuf-ts/google/rpc/error_details'
+import { Status } from '~/generated/protobuf-ts/google/rpc/status'
+import type { RpcError } from '@protobuf-ts/runtime-rpc'
+import { Any } from '~/generated/protobuf-ts/google/protobuf/any'
 
 const transport = new GrpcTransport({
   host: 'dns:backend-admin:443',
@@ -25,4 +41,61 @@ if (process.env.NODE_ENV === 'production') {
   grpcClient = global.__grpcClient
 }
 
-export { grpcClient }
+export { grpcClient, StatusError, isGrpcError }
+
+export interface GrpcError {
+  code: number
+  details: any[]
+}
+
+function isGrpcError(res: any): res is GrpcError {
+  return (res as GrpcError).details !== undefined
+}
+
+/**
+ * This function is intended to deserialise and parse the RpcError
+ * returned by grpcClient functions. It will return the error message
+ * in the same shape of successful rpc calls, with the error message in the response.
+ * @param err The error received from a grpc call.
+ * @returns GrpcError - the error code and details
+ */
+function StatusError(err: RpcError): GrpcError {
+  console.log('An error', err)
+  if (!err.meta) {
+    // return null
+    throw new Error('No meta on error')
+  }
+
+  const buffer = base64decode(err.meta['grpc-status-details-bin'] as string)
+
+  if (!buffer || typeof buffer === 'string') {
+    // return null
+  }
+
+  let status: Status | undefined
+
+  status = Status.fromBinary(buffer)
+  const details: any[] = status.details
+    .map((detail) => {
+      return Any.unpack(detail, typeRegistry[detail.typeUrl]) || null
+    })
+    .filter(Boolean)
+
+  return {
+    code: status.code,
+    details
+  }
+}
+
+const typeRegistry: Record<string, any> = {
+  'type.googleapis.com/google.rpc.RetryInfo': RetryInfo,
+  'type.googleapis.com/google.rpc.DebugInfo': DebugInfo,
+  'type.googleapis.com/google.rpc.QuotaFailure': QuotaFailure,
+  'type.googleapis.com/google.rpc.PreconditionFailure': PreconditionFailure,
+  'type.googleapis.com/google.rpc.BadRequest': BadRequest,
+  'type.googleapis.com/google.rpc.RequestInfo': RequestInfo,
+  'type.googleapis.com/google.rpc.ResourceInfo': ResourceInfo,
+  'type.googleapis.com/google.rpc.Help': Help,
+  'type.googleapis.com/google.rpc.LocalizedMessage': LocalizedMessage,
+  'type.googleapis.com/google.rpc.ErrorInfo': ErrorInfo
+}
