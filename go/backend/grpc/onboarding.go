c07beab2c310d6d38a24714ab47d96c1a5ade081
Ömer Talha Özdemir <omertoast@gmail.com>
2022-06-14 11:39:34 +0300

    feat(backend): add phone verification to grpc

--- go/backend/grpc/onboarding.go
+++ go/backend/grpc/onboarding.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/go-playground/validator/v10"
 	"gitlab.com/fynbos/backend/onboarding"
+	"gitlab.com/fynbos/backend/twilio"
 	backendv1 "gitlab.com/fynbos/proto/backend/v1"
 )
 
@@ -137,9 +138,12 @@ func (s *rpcService) SendPhoneVerification(
 		return nil, ValidationError(err, validateSendPhoneVerificationDescription)
 	}
 
-	// TODO: Send a verification token from twilio
+	_, err := s.twilioService.SendVerificationCode(ctx, req.GetTo())
+	if err != nil {
+		return nil, InternalError(err)
+	}
 
-	_, err := s.onboardingService.UpdateOnboarding(ctx, &onboarding.UpdateOnboardingArgs{
+	_, err = s.onboardingService.UpdateOnboarding(ctx, &onboarding.UpdateOnboardingArgs{
 		Id:    req.GetOnboardingId(),
 		Phone: req.GetTo(),
 	})
@@ -183,10 +187,16 @@ func (s *rpcService) CheckPhoneVerificationCode(
 		return nil, ValidationError(err, validateCheckPhoneVerificationCodeDescription)
 	}
 
-	// TODO Check the verification token with twilio.
+	_, err := s.twilioService.CheckVerificationCode(ctx, &twilio.CheckVerificationCodeArgs{
+		PhoneNumber: req.GetTo(),
+		Code:        req.GetCode(),
+	})
+	if err != nil {
+		return nil, InternalError(err)
+	}
 
 	// If successful set phoneVerified in onboarding table
-	_, err := s.onboardingService.UpdateOnboarding(ctx, &onboarding.UpdateOnboardingArgs{
+	_, err = s.onboardingService.UpdateOnboarding(ctx, &onboarding.UpdateOnboardingArgs{
 		Id:            req.GetOnboardingId(),
 		PhoneVerified: true,
 	})
