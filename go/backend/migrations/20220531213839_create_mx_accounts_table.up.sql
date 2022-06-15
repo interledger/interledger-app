04bb0844e2de1c29b8c32638082ce187a32bca43
Donovan Changfoot <don@fynbos.dev>
2022-06-15 16:02:15 +0200

    feat(backend): map the mx account to the funding source
    The ID of the workflow is used as the id of the funding-source. This is
    mapped to the mx account.

--- go/backend/migrations/20220531213839_create_mx_accounts_table.up.sql
+++ go/backend/migrations/20220531213839_create_mx_accounts_table.up.sql
@@ -1,10 +1,11 @@
 CREATE TABLE IF NOT EXISTS mx_accounts
 (
-	guid			TEXT NOT NULL,
-	user_guid		TEXT NOT NULL,
-	member_guid		TEXT NOT NULL,
-	account_id		UUID NOT NULL,
-	created_at		TIMESTAMP NOT NULL DEFAULT now(),
-	updated_at		TIMESTAMP NOT NULL DEFAULT now(),
+	guid				TEXT NOT NULL,
+	user_guid			TEXT NOT NULL,
+	member_guid			TEXT NOT NULL,
+	account_id			UUID NOT NULL,
+	fundingsource_id	UUID NOT NULL,
+	created_at			TIMESTAMP NOT NULL DEFAULT now(),
+	updated_at			TIMESTAMP NOT NULL DEFAULT now(),
 	PRIMARY KEY (guid, account_id)
 );
