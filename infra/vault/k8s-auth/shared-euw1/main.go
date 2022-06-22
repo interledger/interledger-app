44670b45e593b4beede095a08cf3bd5582729e61
Matt de Haast <matt@fynbos.dev>
2022-06-22 11:02:40 +0200

    feat(infra): vault k8s auth shared-euw1
    add k8s auth to vault for the shared-euw1 cluster.
    This allows service account tokens to be used to
    auth against vault and roles attached to that. Also
    added the export of the ca from the clusters to be
    used for adding the auth

---
+++ infra/vault/k8s-auth/shared-euw1/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"encoding/base64"
+	"github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes"
+	v1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/core/v1"
+	"github.com/pulumi/pulumi-vault/sdk/v5/go/vault"
+	vaultk8s "github.com/pulumi/pulumi-vault/sdk/v5/go/vault/kubernetes"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		k8sStack, err := pulumi.NewStackReference(ctx, "fynbos/aws-shared-eu-west-1-shared-k8s/main", nil)
+		if err != nil {
+			return err
+		}
+		endpoint := k8sStack.GetStringOutput(pulumi.String("clusterEndpoint"))
+		ca := k8sStack.GetStringOutput(pulumi.String("ca"))
+		oidcProvider := k8sStack.GetStringOutput(pulumi.String("oidcProvider"))
+
+		kubeConfig := k8sStack.GetStringOutput(pulumi.String("kubeconfig"))
+		kubeProvider, err := kubernetes.NewProvider(ctx, "kubernetes-provider", &kubernetes.ProviderArgs{
+			Kubeconfig: kubeConfig,
+		})
+		if err != nil {
+			return err
+		}
+
+		// the pulumiID value is {NAMESPACE/SECRET_NAME}. Manully lookup as its easier.
+		secret, err := v1.GetSecret(ctx, "secret", pulumi.ID("vault/vault-token-96gx2"), nil, pulumi.Provider(kubeProvider))
+		if err != nil {
+			return err
+		}
+		token := secret.Data.MapIndex(pulumi.String("token"))
+
+		k8sAuth, err := vault.NewAuthBackend(ctx, "k8s-auth", &vault.AuthBackendArgs{
+			Type: pulumi.String("kubernetes"),
+			Path: pulumi.String("k8s-shared-euw1"),
+		})
+		if err != nil {
+			return err
+		}
+
+		_, err = vaultk8s.NewAuthBackendConfig(ctx, "k8s-auth-config", &vaultk8s.AuthBackendConfigArgs{
+			Backend:              k8sAuth.Path,
+			KubernetesHost:       endpoint,
+			KubernetesCaCert:     base64Decode(ca),
+			TokenReviewerJwt:     base64Decode(token),
+			Issuer:               oidcProvider,
+			DisableIssValidation: pulumi.Bool(true),
+		})
+
+		return nil
+	})
+}
+
+func base64Decode(input pulumi.StringPtrInput) pulumi.StringOutput {
+	return input.ToStringPtrOutput().ApplyT(func(arg interface{}) (string, error) {
+		str := arg.(*string)
+		data, err := base64.StdEncoding.DecodeString(*str)
+		if err != nil {
+			return "", err
+		}
+
+		return string(data), nil
+	}).(pulumi.StringOutput)
+}
