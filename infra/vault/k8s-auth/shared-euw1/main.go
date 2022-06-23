945ec6f8737715775db15f2ebe82df1980f0992b
Matt de Haast <matt@fynbos.dev>
2022-06-23 11:52:08 +0200

    feat(infra) vault shared k8s auth policy
    add the required policies for k8s app to user vault.
    apps have the ability to read from their
    {{cluster}}/{{namespace}}/{{service_account}} path
    freely.

--- infra/vault/k8s-auth/shared-euw1/main.go
+++ infra/vault/k8s-auth/shared-euw1/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/pulumi/pulumi-vault/sdk/v5/go/vault"
 	vaultk8s "github.com/pulumi/pulumi-vault/sdk/v5/go/vault/kubernetes"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	"gitlab.com/fynbos/infra/aws/modules/utils"
 )
 
 func main() {
@@ -27,7 +28,7 @@ func main() {
 			return err
 		}
 
-		// the pulumiID value is {NAMESPACE/SECRET_NAME}. Manully lookup as its easier.
+		// the pulumiID value is {NAMESPACE/SECRET_NAME}. Manually lookup as its easier.
 		secret, err := v1.GetSecret(ctx, "secret", pulumi.ID("vault/vault-token-96gx2"), nil, pulumi.Provider(kubeProvider))
 		if err != nil {
 			return err
@@ -50,7 +51,34 @@ func main() {
 			Issuer:               oidcProvider,
 			DisableIssValidation: pulumi.Bool(true),
 		})
+		if err != nil {
+			return err
+		}
 
+		policy, err := vault.NewPolicy(ctx, "kubernetes-reader", &vault.PolicyArgs{
+			Name:   pulumi.String("shared-euw1-kubernetes-reader"),
+			Policy: newPolicy(k8sAuth.Accessor),
+		})
+		if err != nil {
+			return err
+		}
+
+		_, err = vaultk8s.NewAuthBackendRole(ctx, "kubernetes-role", &vaultk8s.AuthBackendRoleArgs{
+			RoleName: pulumi.String("k8s-app"),
+			Backend:  k8sAuth.Path,
+			BoundServiceAccountNames: pulumi.StringArray{
+				pulumi.String("*"),
+			},
+			BoundServiceAccountNamespaces: pulumi.StringArray{
+				pulumi.String("*"),
+			},
+			TokenPolicies: pulumi.StringArray{
+				policy.Name,
+			},
+		}, pulumi.DependsOn([]pulumi.Resource{policy}))
+		if err != nil {
+			return err
+		}
 		return nil
 	})
 }
@@ -64,5 +92,20 @@ func base64Decode(input pulumi.StringPtrInput) pulumi.StringOutput {
 		}
 
 		return string(data), nil
+	}).(pulumi.StringOutput)
+}
+
+func newPolicy(authAccessor pulumi.StringOutput) pulumi.StringOutput {
+	return pulumi.All(authAccessor).ApplyT(func(args []interface{}) (string, error) {
+		data := struct {
+			AuthAccessor string
+		}{
+			AuthAccessor: args[0].(string),
+		}
+		parsedPolicy, err := utils.ParseTemplateAsBytes(data, "./policy.hcl")
+		if err != nil {
+			return "", err
+		}
+		return parsedPolicy.String(), nil
 	}).(pulumi.StringOutput)
 }
