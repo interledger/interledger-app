a416d994995393bb0acce6c714bdb0857dd96c8f
Matt de Haast <matt@fynbos.dev>
2022-06-27 15:54:39 +0200

    feat(infra): dev cluster cockroach
    create some helper functions in the cockroach service
    to deploy the various components required. Also update
    the PKI role to allow for the service name for certs.
    
    Finally, the cockroach cluster was deployed.

--- infra/clusters/dev-eu-west-1/cockroachdb/main.go
+++ infra/clusters/dev-eu-west-1/cockroachdb/main.go
@@ -6,6 +6,7 @@ import (
 	v1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/core/v1"
 	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/meta/v1"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	crdb "gitlab.com/fynbos/infra/services/cockroach/v1"
 )
 
 func main() {
@@ -45,8 +46,7 @@ func main() {
 		}
 
 		// Create node issuer - needs a role from vault
-		// Create KMSIssuer for internal Vault TLS
-		_, err = apiextensions.NewCustomResource(ctx, "crdb-issuer", &apiextensions.CustomResourceArgs{
+		issuer, err := apiextensions.NewCustomResource(ctx, "crdb-issuer", &apiextensions.CustomResourceArgs{
 			ApiVersion: pulumi.String("cert-manager.io/v1"),
 			Kind:       pulumi.String("Issuer"),
 			Metadata: metav1.ObjectMetaArgs{
@@ -76,20 +76,126 @@ func main() {
 			return err
 		}
 
-		//// Should this issuer be here or in cert-manager?
-		//_, err = helm.NewRelease(ctx, "cert-manager", &helm.ReleaseArgs{
-		//	Namespace: namespace.Metadata.Name().Elem(),
-		//	Chart:     pulumi.String("vault"),
-		//	Version:   pulumi.String("0.20.0"),
-		//	RepositoryOpts: helm.RepositoryOptsArgs{
-		//		Repo: pulumi.String("https://helm.releases.hashicorp.com"),
-		//	},
-		//	Values: pulumi.Map{},
-		//}, pulumi.Provider(kubeProvider))
-		//
-		//if err != nil {
-		//	return err
-		//}
+		// Create Cert
+		nodeCert, err := apiextensions.NewCustomResource(ctx, "crdb-tls-cert", &apiextensions.CustomResourceArgs{
+			ApiVersion: pulumi.String("cert-manager.io/v1"),
+			Kind:       pulumi.String("Certificate"),
+			Metadata: metav1.ObjectMetaArgs{
+				Name:      pulumi.String("crdb-tls-cert"),
+				Namespace: namespace.Metadata.Name(),
+			},
+			OtherFields: kubernetes.UntypedArgs{
+				"spec": pulumi.Map{
+					"secretName":  pulumi.String("cockroachdb-node"),
+					"duration":    pulumi.String("720h"),
+					"renewBefore": pulumi.String("168h"),
+					"usages": pulumi.StringArray{
+						pulumi.String("digital signature"),
+						pulumi.String("key encipherment"),
+						pulumi.String("server auth"),
+						pulumi.String("client auth"),
+					},
+					"privateKey": pulumi.Map{
+						"algorithm": pulumi.String("RSA"),
+						"size":      pulumi.Int(2048),
+					},
+					"commonName": pulumi.String("node"),
+					"subject": pulumi.Map{
+						"organizations": pulumi.StringArray{
+							pulumi.String("Cockroach"),
+						},
+					},
+					"dnsNames": pulumi.StringArray{
+						pulumi.String("localhost"),
+						pulumi.String("127.0.0.1"),
+						pulumi.Sprintf("%s-public", "cockroachdb"),
+						pulumi.Sprintf("%s-public.%s", "cockroachdb", namespace.Metadata.Name().Elem()),
+						pulumi.Sprintf("%s-public.%s.svc.cluster.local", "cockroachdb", namespace.Metadata.Name().Elem()),
+						pulumi.Sprintf("*.%s", "cockroachdb"),
+						pulumi.Sprintf("*.%s.%s", "cockroachdb", namespace.Metadata.Name().Elem()),
+						pulumi.Sprintf("*.%s.%s.svc.cluster.local", "cockroachdb", namespace.Metadata.Name().Elem()),
+					},
+					"issuerRef": pulumi.Map{
+						"name":  issuer.Metadata.Name(),
+						"kind":  issuer.Kind,
+						"group": pulumi.String("cert-manager.io"),
+					},
+				},
+			},
+		}, pulumi.Provider(kubeProvider), pulumi.DependsOn([]pulumi.Resource{namespace, issuer}))
+		if err != nil {
+			return err
+		}
+
+		// Create Cert
+		rootCert, err := apiextensions.NewCustomResource(ctx, "crdb-root-cert", &apiextensions.CustomResourceArgs{
+			ApiVersion: pulumi.String("cert-manager.io/v1"),
+			Kind:       pulumi.String("Certificate"),
+			Metadata: metav1.ObjectMetaArgs{
+				Name:      pulumi.String("crdb-root-cert"),
+				Namespace: namespace.Metadata.Name(),
+			},
+			OtherFields: kubernetes.UntypedArgs{
+				"spec": pulumi.Map{
+					"secretName":  pulumi.String("cockroachdb-root"),
+					"duration":    pulumi.String("168h"),
+					"renewBefore": pulumi.String("24h"),
+					"usages": pulumi.StringArray{
+						pulumi.String("digital signature"),
+						pulumi.String("key encipherment"),
+						pulumi.String("client auth"),
+					},
+					"privateKey": pulumi.Map{
+						"algorithm": pulumi.String("RSA"),
+						"size":      pulumi.Int(2048),
+					},
+					"commonName": pulumi.String("root"),
+					"subject": pulumi.Map{
+						"organizations": pulumi.StringArray{
+							pulumi.String("Cockroach"),
+						},
+					},
+					"issuerRef": pulumi.Map{
+						"name":  issuer.Metadata.Name(),
+						"kind":  issuer.Kind,
+						"group": pulumi.String("cert-manager.io"),
+					},
+				},
+			},
+		}, pulumi.Provider(kubeProvider), pulumi.DependsOn([]pulumi.Resource{namespace, issuer}))
+		if err != nil {
+			return err
+		}
+
+		serviceAccount, err := crdb.DeployRbac(ctx, namespace.Metadata.Name().Elem(), pulumi.Provider(kubeProvider), pulumi.DependsOn([]pulumi.Resource{namespace}))
+		if err != nil {
+			return err
+		}
+
+		_, err = crdb.DeployStatefulSet(ctx, crdb.StatefulSetArgs{
+			Namespace:          namespace.Metadata.Name().Elem(),
+			Replicas:           3,
+			ServiceAccountName: serviceAccount.Metadata.Name(),
+			CertSecretName:     pulumi.String("cockroachdb-node"),
+			ClientSecretName:   pulumi.String("cockroachdb-root"),
+		}, pulumi.Provider(kubeProvider), pulumi.DependsOn([]pulumi.Resource{serviceAccount, namespace, nodeCert, rootCert}))
+		if err != nil {
+			return err
+		}
+
+		err = crdb.DeployPublicService(ctx, namespace.Metadata.Name(), pulumi.Provider(kubeProvider), pulumi.DependsOn([]pulumi.Resource{namespace}))
+		if err != nil {
+			return err
+		}
+		err = crdb.DeployPrivateService(ctx, namespace.Metadata.Name(), pulumi.Provider(kubeProvider), pulumi.DependsOn([]pulumi.Resource{namespace}))
+		if err != nil {
+			return err
+		}
+
+		err = crdb.DeployPodDistributionBudget(ctx, pulumi.Provider(kubeProvider))
+		if err != nil {
+			return err
+		}
 
 		return nil
 	})
