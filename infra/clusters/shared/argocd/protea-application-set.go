d136a7ab87ab21250125418e95e0be992f316600
Matt de Haast <matt@fynbos.dev>
2022-07-08 14:15:47 +0200

    feat(argocd): add protea applciationset
    add the first application for protea that currently
    only references the dev cluster. This will auto
    sync the folder in rooibos to the dev cluster

---
+++ infra/clusters/shared/argocd/protea-application-set.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes"
+	"github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/apiextensions"
+	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/meta/v1"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+type proteaApplicationSetArgs struct {
+	Namespace pulumi.StringInput
+}
+
+func newProteaApplicationSet(ctx *pulumi.Context, args proteaApplicationSetArgs, opts ...pulumi.ResourceOption) error {
+
+	_, err := apiextensions.NewCustomResource(ctx, "protea-application-set", &apiextensions.CustomResourceArgs{
+		ApiVersion: pulumi.String("argoproj.io/v1alpha1"),
+		Kind:       pulumi.String("ApplicationSet"),
+		Metadata: metav1.ObjectMetaArgs{
+			Name:      pulumi.String("protea"),
+			Namespace: args.Namespace,
+		},
+		OtherFields: kubernetes.UntypedArgs{
+			"spec": pulumi.Map{
+				"generators": pulumi.MapArray{
+					pulumi.Map{
+						"list": pulumi.Map{
+							"elements": pulumi.MapArray{
+								pulumi.Map{
+									"cluster": pulumi.String("dev-eu1"),
+									"url":     pulumi.String("https://AF83FFCC8A31D16D2E1C9C1788205863.sk1.eu-west-1.eks.amazonaws.com"),
+								},
+							},
+						},
+					},
+				},
+				"template": pulumi.Map{
+					"metadata": pulumi.Map{
+						"name": pulumi.String("{{cluster}}-protea"),
+					},
+					"spec": pulumi.Map{
+						"project": pulumi.String("default"),
+						"source": pulumi.Map{
+							"repoURL":        pulumi.String("https://gitlab.com/fynbos/rooibos.git"),
+							"targetRevision": pulumi.String("main"),
+							"path":           pulumi.String("protea/envs/{{cluster}}"),
+						},
+						"destination": pulumi.Map{
+							"server":    pulumi.String("{{url}}"),
+							"namespace": pulumi.String("protea"),
+						},
+						"syncPolicy": pulumi.Map{
+							"automated": pulumi.Map{},
+						},
+					},
+				},
+			},
+		},
+	}, opts...)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
