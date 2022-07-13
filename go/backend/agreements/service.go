9cda96020ad479356c27eaaf915e2ecc46f7b9b1
Ömer Talha Özdemir <omertoast@gmail.com>
2022-07-13 17:42:05 +0300

    feat(backend): store raw agreements in database
    - markdowns folder created to store raw
    test markdown agreements
    - service arg added to specify agreements
    directory
    - storeAgreement function parses raw
    markdown agreements in the given directory
    into agreement objects and inserts them in database

--- go/backend/agreements/service.go
+++ go/backend/agreements/service.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"io/ioutil"
+	"regexp"
+	"strings"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/jmoiron/sqlx"
@@ -13,6 +16,7 @@ import (
 var (
 	ErrInternal        = errors.New("agreements: internal error")
 	ErrInvalidArgument = errors.New("agreements: invalid argument")
+	ErrNotFound        = errors.New("agreements: not found")
 )
 
 type (
@@ -21,12 +25,14 @@ type (
 	}
 
 	ServiceArgs struct {
-		Db *sqlx.DB `validate:"required"`
+		Db            *sqlx.DB `validate:"required"`
+		AgreementsDir string   `validate:"required"`
 	}
 
 	service struct {
-		validator *validator.Validate
-		db        *sqlx.DB
+		validator     *validator.Validate
+		db            *sqlx.DB
+		agreementsDir string
 	}
 
 	SignedAgreements struct {
@@ -37,6 +43,13 @@ type (
 		CreatedAt    string         `db:"created_at"`
 		UpdatedAt    string         `db:"updated_at"`
 	}
+
+	Agreement struct {
+		ID      string
+		Name    string
+		Version string
+		Content string
+	}
 )
 
 func NewService(args *ServiceArgs) (Service, error) {
@@ -45,9 +58,18 @@ func NewService(args *ServiceArgs) (Service, error) {
 		return nil, fmt.Errorf("%w %s", ErrInvalidArgument, err)
 	}
 
+	err := StoreAgreements(context.Background(), &StoreAgreementsArgs{
+		db:  args.Db,
+		dir: args.AgreementsDir,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("%w %s", ErrInternal, err.Error())
+	}
+
 	return &service{
-		validator: v,
-		db:        args.Db,
+		validator:     v,
+		db:            args.Db,
+		agreementsDir: args.AgreementsDir,
 	}, nil
 }
 
@@ -70,4 +92,52 @@ func (s *service) SignAgreement(ctx context.Context, args *SignAgreementArgs) (*
 	}
 
 	return &signedAgreements, nil
+}
+
+type StoreAgreementsArgs struct {
+	dir string
+	db  *sqlx.DB
+}
+
+func StoreAgreements(ctx context.Context, args *StoreAgreementsArgs) error {
+	agreementFiles, err := ioutil.ReadDir(args.dir)
+	if err != nil {
+		return fmt.Errorf("%w %s", ErrNotFound, err.Error())
+	}
+
+
+	regex, err := regexp.Compile(`^[a-zA-Z0-9_]+-[0-9]+\.[0-9]+\.[0-9]+\.md$`)
+	if err != nil {
+		return fmt.Errorf("%w %s", ErrInternal, err.Error())
+	}
+
+	for _, agreementFile := range agreementFiles {
+		if !regex.MatchString(agreementFile.Name()) {
+			continue
+		}
+
+		agreementID := agreementFile.Name()[:len(agreementFile.Name())-3]
+
+		agreementName := agreementID[:strings.Index(agreementID, "-")]
+		if agreementName == "" {
+			continue
+		}
+
+		agreementVersion := agreementID[strings.Index(agreementID, "-")+1:]
+		if agreementVersion == "" {
+			continue
+		}
+
+		agreementContent, err := ioutil.ReadFile(fmt.Sprintf("%s/%s", args.dir, agreementFile.Name()))
+		if err != nil {
+			return fmt.Errorf("%w %s", ErrInternal, err.Error())
+		}
+
+		_, err = args.db.ExecContext(ctx, `INSERT INTO agreements (id, name, version, content) VALUES ($1, $2, $3, $4)`, agreementID, agreementName, agreementVersion, string(agreementContent))
+		if err != nil {
+			return fmt.Errorf("%w %s", ErrInternal, err.Error())
+		}
+	}
+
+	return nil
 }
