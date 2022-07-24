def1476f8a1bcad9dbc92f679469ca12b3d2606b
Ömer Talha Özdemir <omertoast@gmail.com>
2022-07-25 02:12:04 +0300

    feat(backend): store a single signature in a single row
    As @barnarddt suggested, there are problems
    storing multiple agreement ids in a single row.
    Such as looping over the signatures and finding
    the latest version user has signed for a specific
    agreement. From now on there is going to be a
    single row for every single agreement signature.

--- go/backend/migrations/20220725064300_create_agreement_signatures_table.up.sql
+++ go/backend/migrations/20220725064300_create_agreement_signatures_table.up.sql
@@ -1,9 +1,12 @@
 CREATE TABLE IF NOT EXISTS agreement_signatures
 (
 	id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-	agreement_ids STRING[] NOT NULL,
+	agreement_id TEXT NOT NULL,
   identity_id         TEXT NOT NULL,
 	ip_address TEXT NOT NULL,
 	created_at TIMESTAMP NOT NULL DEFAULT now(),
-	updated_at TIMESTAMP NOT NULL DEFAULT now()
+	updated_at TIMESTAMP NOT NULL DEFAULT now(),
+	CONSTRAINT fk_agreement
+		FOREIGN KEY(agreement_id)
+			REFERENCES agreements(id)
 );
