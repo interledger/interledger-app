d58e3eae873f90bbc4d8bf9c1903483d70c4d18f
Donovan Changfoot <don@fynbos.dev>
2022-08-05 12:40:23 +0200

    feat(backend): grpc endpoint to get deposit

--- go/backend/grpc/deposit_test.go
+++ go/backend/grpc/deposit_test.go
@@ -86,3 +86,94 @@ func TestInitiateDeposit(t *testing.T) {
 		})
 	}
 }
+
+func TestGetDeposit(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	t.Cleanup(func() {
+		ctrl.Finish()
+	})
+	c := NewTestContainer(t, ctrl)
+	_, _, client := startTestServer(t, c)
+	user := &user.User{
+		ID: uuid.NewString(),
+	}
+	accountID := uuid.NewString()
+	c.AccountService.EXPECT().GetByIdentityID(gomock.Any(), gomock.Any()).Return(
+		&accounts.Account{
+			ID:         accountID,
+			IdentityID: user.ID,
+		},
+		nil,
+	).AnyTimes()
+	depositID := uuid.NewString()
+
+	cases := []struct {
+		Name          string
+		ExpectedError string
+		Deposit       deposits.Deposit
+		User          *_user.User
+	}{
+		{
+			Name:          "Returns deposit",
+			ExpectedError: "",
+			Deposit: deposits.Deposit{
+				ID:              depositID,
+				AccountID:       accountID,
+				FundingSourceId: uuid.NewString(),
+				Amount:          1000,
+				State:           deposits.Created,
+			},
+			User: user,
+		},
+		{
+			Name:          "Requires authenticated user",
+			ExpectedError: "rpc error: code = Unauthenticated desc = Unauthenticated: no user.",
+			Deposit: deposits.Deposit{
+				ID:              depositID,
+				AccountID:       accountID,
+				FundingSourceId: uuid.NewString(),
+				Amount:          1000,
+				State:           deposits.Created,
+			},
+			User: nil,
+		},
+		{
+			Name:          "Deposit must belong to account",
+			ExpectedError: "rpc error: code = Internal desc = Internal server error: Failed to get deposit.",
+			Deposit: deposits.Deposit{
+				ID:              depositID,
+				AccountID:       uuid.NewString(),
+				FundingSourceId: uuid.NewString(),
+				Amount:          1000,
+				State:           deposits.Created,
+			},
+			User: user,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.Name, func(st *testing.T) {
+			if tc.User != nil {
+				c.DepositService.EXPECT().Get(gomock.Any(), gomock.Any()).Return(&tc.Deposit, nil).Times(1)
+			}
+
+			deposit, err := client.GetDeposit(
+				_user.ActingAsContext(st, context.Background(), tc.User),
+				&backendv1.GetDepositRequest{
+					Id: depositID,
+				},
+			)
+			if tc.ExpectedError == "" {
+				assert.Nil(st, err)
+				assert.Equal(st, tc.Deposit.ID, deposit.GetId())
+				assert.Equal(st, tc.Deposit.Amount, deposit.GetAmount())
+				assert.Equal(st, tc.Deposit.FundingSourceId, deposit.GetFundingsourceId())
+				assert.Equal(st, string(tc.Deposit.State), deposit.GetState())
+			} else {
+				assert.Nil(st, deposit)
+				assert.EqualError(st, err, tc.ExpectedError)
+			}
+		})
+	}
+}
