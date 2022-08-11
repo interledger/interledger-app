3ca211357b7770ad913e7935dfdce9270df94c8d
Cairin Michie <cairinmichie@gmail.com>
2022-08-11 16:17:36 +0200

    chore(protea): placeholders for static pages
    This ensures we can use the route guard for these routes in the static
    pages.

---
+++ typescript/protea/app/routes/legal.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className='flex h-screen w-full items-center justify-center font-display text-5xl font-medium text-medium'>
+      Coming soon <span className='text-primary'>.</span>
+    </div>
+  )
+}
