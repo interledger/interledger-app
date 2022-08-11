7d2f77831e770c952f41b9f785042e245b24ef21
Matt de Haast <matt@fynbos.dev>
2022-08-11 15:51:36 +0200

    feat(backend): open telemetry
    begin adding open telemetry

---
+++ go/backend/tracing/tracing.go
@@ -0,0 +1,61 @@
+package tracing
+
+import (
+	"context"
+	"fmt"
+	"gitlab.com/fynbos/env"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.12.0"
+	"log"
+)
+
+func InitTraceProvider() (func(context.Context) error, error) {
+	ctx := context.Background()
+
+	res, err := resource.New(ctx,
+		resource.WithAttributes(
+			// the service name used to display traces in backends
+			semconv.ServiceNameKey.String("backend"),
+		),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create resource: %w", err)
+	}
+
+	var traceExporter sdktrace.SpanExporter
+
+	if env.IsDev() {
+		client := otlptracegrpc.NewClient()
+		traceExporter, err = otlptrace.New(ctx, client)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create trace exporter: %w", err)
+		}
+	} else {
+		traceExporter, err = stdouttrace.New(stdouttrace.WithPrettyPrint()) // for debugging
+		if err != nil {
+			log.Panicf("failed to initialize stdouttrace exporter %v\n", err)
+		}
+	}
+
+	// Register the trace exporter with a TracerProvider, using a batch
+	// span processor to aggregate spans before export.
+	bsp := sdktrace.NewBatchSpanProcessor(traceExporter)
+	tracerProvider := sdktrace.NewTracerProvider(
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
+		sdktrace.WithResource(res),
+		sdktrace.WithSpanProcessor(bsp),
+	)
+	otel.SetTracerProvider(tracerProvider)
+
+	// set global propagator to trace context (the default is no-op).
+	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
+
+	// Shutdown will flush any remaining spans and shut down the exporter.
+	return tracerProvider.Shutdown, nil
+}
