8a23751569f3f493e0c094db02c53e21538516c0
Donovan Changfoot <don@fynbos.dev>
2022-09-08 22:39:08 +0200

    feat(protea): require otp when linking bank account

--- typescript/protea/app/routes/flows/$flowId/linked-account/details.tsx
+++ typescript/protea/app/routes/flows/$flowId/linked-account/details.tsx
@@ -1,130 +1,85 @@
-import { useEffect, useState } from 'react'
+import { ConnectWidget } from '@mxenabled/web-widget-sdk'
 import type { ActionArgs, LoaderArgs } from '@remix-run/node'
-import { redirect } from '@remix-run/node'
-import { json } from '@remix-run/node'
-import { Form, useLoaderData } from '@remix-run/react'
-import { Button, Select, TextField } from '~/components'
-import { getCurrentFlow, updateFlow } from '~/lib/flows.server'
+import { json, redirect } from '@remix-run/node'
+import { useActionData, useLoaderData, useSubmit } from '@remix-run/react'
+import { useEffect } from 'react'
 import { route } from 'routes-gen'
+import { getCurrentFlow, updateFlow } from '~/lib/flows.server'
+import { grpcClient, isGrpcError, StatusError } from '~/lib/proto.server'
 
 export async function loader({ request, params }: LoaderArgs) {
   const flow = await getCurrentFlow(request, params)
+  let rpc = await grpcClient
+    .getBankAccountWidget(
+      {},
+      {
+        meta: {
+          cookies: request.headers.get('cookie') || ''
+        }
+      }
+    )
+    .then((v) => v)
+    .catch(StatusError)
+  if (isGrpcError(rpc)) {
+    throw rpc
+  }
 
-  const accountTypes = [
-    { id: '1', name: 'Checking' },
-    { id: '2', name: 'Savings' }
-  ]
-
-  return json({
-    accountTypes,
-    flow
-  })
+  return json({ widgetUrl: rpc.response.url, flow })
 }
 
 export default function Page() {
-  const { accountTypes, flow } = useLoaderData<typeof loader>()
+  const submit = useSubmit()
+  const { widgetUrl, flow } = useLoaderData<typeof loader>()
+  const actionData = useActionData<typeof action>()
 
-  const [selected, setSelected] = useState(accountTypes[0])
+  useEffect(function () {
+    const widget = new ConnectWidget({
+      container: '#widget',
+      url: widgetUrl,
+      onMemberConnected: (event) => {
+        let formData = new FormData()
+        formData.append('memberGuid', event.member_guid)
+        formData.append('userGuid', event.user_guid)
 
-  useEffect(() => {
-    if (accountTypes && flow?.data.type) {
-      const sel = accountTypes.find(
-        (val: any) => val.name == flow?.data.type
-      ) as { id: string; name: string }
-      setSelected(sel)
-    }
-  }, [flow?.data.type, accountTypes])
+        submit(formData, { method: 'post', action: `/flows/${flow.id}/linked-account/type` })
+        widget.unmount()
+      }
+    })
+  }, [])
 
   return (
     <>
-      <Form
-        id='linked-account-details'
-        action={`/flows/${flow.id}/linked-account/details`}
-        method='post'
-        className='hidden'
-      />
-      <Select
-        id='type'
-        value={selected}
-        onChange={setSelected}
-        options={accountTypes}
-        label='Account type'
-        className='col-span-full flex flex-col sm:col-span-6 sm:col-start-2 lg:col-start-4'
-      />
-      <input
-        form='linked-account-details'
-        value={String(selected.name)}
-        name='type'
-        type='hidden'
-      />
-
-      <TextField
-        id='institution'
-        form='linked-account-details'
-        label='Institution'
-        name='institution'
-        defaultValue={flow?.data.institution}
-        type='text'
-        className='col-span-full flex flex-col sm:col-span-6 sm:col-start-2 lg:col-start-4'
-        required
-      />
-
-      <TextField
-        id='accountNumber'
-        form='linked-account-details'
-        label='Account number'
-        name='accountNumber'
-        defaultValue={flow?.data.accountNumber}
-        type='text'
-        className='col-span-full flex flex-col sm:col-span-6 sm:col-start-2 lg:col-start-4'
-        required
-      />
-
-      <TextField
-        id='routingNumber'
-        form='linked-account-details'
-        label='Routing number'
-        name='routingNumber'
-        defaultValue={flow?.data.routingNumber}
-        type='text'
-        className='col-span-full flex flex-col sm:col-span-6 sm:col-start-2 lg:col-start-4'
-        required
-      />
-
-      <TextField
-        id='name'
-        form='linked-account-details'
-        label='Nickname'
-        name='name'
-        defaultValue={flow?.data.name}
-        type='text'
-        className='col-span-full flex flex-col sm:col-span-6 sm:col-start-2 lg:col-start-4'
-        required
-      />
-
-      <div className='col-span-full flex justify-end pt-4 sm:col-span-6 sm:col-start-2 lg:col-start-4'>
-        <Button form='linked-account-details' type='submit'>
-          Continue
-        </Button>
-      </div>
+      <div id='widget' className='col-span-full sm:col-span-6 sm:col-start-2 lg:col-start-4 h-screen'></div>
     </>
   )
 }
 
 export async function action({ request, params }: ActionArgs) {
   const form = await request.formData()
-  const accountNumber = form.get('accountNumber') as string
-  const institution = form.get('institution') as string
-  const name = form.get('name') as string
-  const routingNumber = form.get('routingNumber') as string
-  const type = form.get('type') as string
+  const memberGuid = form.get('memberGuid')?.toString() || ''
+  const userGuid = form.get('userGuid')?.toString() || ''
+  let rpc = await grpcClient
+    .addBankAccount(
+      {
+        memberGuid,
+        userGuid,
+      },
+      {
+        meta: {
+          cookies: request.headers.get('cookie') || ''
+        }
+      }
+    )
+    .then((v) => v)
+    .catch(StatusError)
+  if (isGrpcError(rpc)) {
+    throw rpc
+  }
 
   const headers = await updateFlow(request, {
-    accountNumber: accountNumber,
-    institution: institution,
-    name: name,
-    routingNumber: routingNumber,
-    type: type
+    memberGuid: memberGuid,
+    userGuid: userGuid,
+    fundingsourceId: rpc.response.fundingsourceId,
   })
 
   const flow = await getCurrentFlow(request, params)
