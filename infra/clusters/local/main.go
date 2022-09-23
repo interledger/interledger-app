761e06ae34381e718cea2b3ad28ce824fefd542a
Matt de Haast <matt@fynbos.dev>
2022-09-23 08:42:01 +0200

    chore(local-dev): simplify with new refactor
    simplified our current infra setup for localdev
    to only use what we are currently going to be
    using for the new refactor. At a later stage
    will attempt to move off pulumi

--- infra/clusters/local/main.go
+++ infra/clusters/local/main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"encoding/base64"
 	"os"
 
-	"github.com/pulumi/pulumi-random/sdk/v4/go/random"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 	"gitlab.com/fynbos/infra/services/backend"
 	cert_manager "gitlab.com/fynbos/infra/services/cert-manager"
@@ -12,15 +10,8 @@ import (
 	"gitlab.com/fynbos/infra/services/ingress"
 	"gitlab.com/fynbos/infra/services/kratos"
 	"gitlab.com/fynbos/infra/services/mailhog"
-	mockstaticresponseservers "gitlab.com/fynbos/infra/services/mock-static-response-servers"
-	"gitlab.com/fynbos/infra/services/pacioli"
-	"gitlab.com/fynbos/infra/services/postgres"
 	"gitlab.com/fynbos/infra/services/protea"
-	"gitlab.com/fynbos/infra/services/rafiki"
-	"gitlab.com/fynbos/infra/services/redis"
-	"gitlab.com/fynbos/infra/services/retool"
 	"gitlab.com/fynbos/infra/services/temporal"
-	"gitlab.com/fynbos/infra/services/tigerbeetle"
 )
 
 func main() {
@@ -148,188 +139,6 @@ func main() {
 			ZendeskToken:         "todo",
 		})
 		if err != nil {
-			return err
-		}
-
-		err = tigerbeetle.DeployTigerBeetle(ctx, tigerbeetle.DeployTigerBeetleArgs{
-			IsLocal:   true,
-			ImageRepo: "localhost:5005",
-			ImageTag:  "latest",
-		})
-		if err != nil {
-			return err
-		}
-
-		pcCert, err := cockroach.CreateClientCert(ctx, &cockroach.ClientCertArgs{
-			Name:      "pacioli",
-			Issuer:    "ca-issuer",
-			Namespace: "default",
-		}, pulumi.DependsOn([]pulumi.Resource{caResource}))
-		if err != nil {
-			return err
-		}
-		err = pacioli.DeployPacioli(ctx, &pacioli.DeployPacioliArgs{
-			Cert:      pcCert,
-			ImageRepo: "localhost:5005",
-			ImageTag:  "latest",
-			Namespace: "default",
-		})
-		if err != nil {
-			return err
-		}
-
-		err = ingress.DeployHost(ctx, &ingress.DeployHostArgs{
-			Name:     "retool-ingress",
-			Hostname: "localhost", // use localhost so Google will redirect.
-		}, pulumi.DependsOnInputs(ingressChart.Ready))
-		_, err = retool.DeployRetool(ctx, retool.Args{
-			Hostname:       "losthost",
-			CreatePostgres: true,
-		})
-		if err != nil {
-			return err
-		}
-
-		err = redis.DeployRedis(ctx, &redis.DeployRedisArgs{
-			EnableSentinal: false,
-			ReplicaCount:   0,
-		}, pulumi.DependsOn([]pulumi.Resource{caResource}))
-		if err != nil {
-			return err
-		}
-
-		postgresUserPassword, err := random.NewRandomPassword(ctx, "rafiki-postgres", &random.RandomPasswordArgs{
-			Length:  pulumi.Int(32),
-			Special: pulumi.Bool(true),
-		})
-		if err != nil {
-			return err
-		}
-		err = postgres.DeployPostgres(ctx, &postgres.DeployPostgresArgs{
-			ReadReplicasCount:    0,
-			PostgresUserPassword: postgresUserPassword,
-		}, pulumi.DependsOn([]pulumi.Resource{caResource}))
-		if err != nil {
-			return err
-		}
-
-		err = ingress.DeployHost(ctx, &ingress.DeployHostArgs{
-			Name:     "rafiki-ingress",
-			Hostname: "pay.fynbos.test",
-		}, pulumi.DependsOnInputs(ingressChart.Ready))
-		if err != nil {
-			return err
-		}
-
-		streamSecret, err := random.NewRandomString(ctx, "rafiki-stream-secret", &random.RandomStringArgs{
-			Length: pulumi.Int(32),
-		})
-		if err != nil {
-			return err
-		}
-		streamSecretBase64 := streamSecret.Result.ApplyT(func(str string) string {
-			return base64.RawStdEncoding.EncodeToString([]byte(str)[0:32])
-		}).(pulumi.StringInput)
-
-		rafikiPostgresCert, err := postgres.CreateClientCert(ctx, &postgres.ClientCertArgs{
-			Name:      "rafiki",
-			Issuer:    "ca-issuer",
-			Namespace: "default",
-		})
-		if err != nil {
-			return err
-		}
-		rafikiRedisCert, err := redis.CreateClientCert(ctx, &redis.ClientCertArgs{
-			Name:      "rafiki",
-			Issuer:    "ca-issuer",
-			Namespace: "default",
-		})
-		if err != nil {
-			return err
-		}
-
-		// Fynbos' rafiki instance.
-		err = rafiki.DeployRafiki(ctx, &rafiki.DeployRafikiArgs{
-			Name:                    "rafiki",
-			DeployPlaygroundIngress: true,
-			ImageRepo:               "localhost:5005/rafiki-backend",
-			ImageTag:                "latest",
-			DbBaseUrl:               "postgresql://rafiki@postgres-postgresql/rafiki",
-			DbCert:                  rafikiPostgresCert,
-			TbClusterID:             "0",
-			// TbReplicaAddresses:         "[\"tigerbeetle-0.tigerbeetle.default.svc.cluster.local\"]",
-			// waiting for update for client to handle dns.
-			// you will need to manually look up the tigerbeetle-0 pod and get its ip address for now.
-			TbReplicaAddresses:         "[\"10.244.0.23:8080\"]",
-			IlpAddress:                 "test.fynbos",
-			NonceRedisKey:              "noncefynbos",
-			RedisUrl:                   "redis://redis-master:6379/0",
-			RedisCert:                  rafikiRedisCert,
-			AuthServerGrantUrl:         "http://mockauth",
-			AuthServerIntrospectionUrl: "http://mockauth",
-			StreamSecret:               streamSecretBase64,
-			AdminKey:                   streamSecret.Result,                     // re-using for local cluster
-			WebhookUrl:                 "https://end3sf6r22xva.x.pipedream.net", // using request bin for now
-			Hostname:                   "pay.fynbos.test",
-			PublicHost:                 "http://rafiki", // using rafiki as coredns will find rafiki
-		})
-		if err != nil {
-			return err
-		}
-
-		// Peer rafiki instance for dev testing.
-		err = ingress.DeployHost(ctx, &ingress.DeployHostArgs{
-			Name:     "peer-ingress",
-			Hostname: "peer.fynbos.test",
-		}, pulumi.DependsOnInputs(ingressChart.Ready))
-		if err != nil {
-			return err
-		}
-		peerPostgresCert, err := postgres.CreateClientCert(ctx, &postgres.ClientCertArgs{
-			Name:      "peer",
-			Issuer:    "ca-issuer",
-			Namespace: "default",
-		})
-		if err != nil {
-			return err
-		}
-		peerRedisCert, err := redis.CreateClientCert(ctx, &redis.ClientCertArgs{
-			Name:      "peer",
-			Issuer:    "ca-issuer",
-			Namespace: "default",
-		})
-		if err != nil {
-			return err
-		}
-		err = rafiki.DeployRafiki(ctx, &rafiki.DeployRafikiArgs{
-			Name:                    "peer",
-			DeployPlaygroundIngress: true,
-			ImageRepo:               "localhost:5005/rafiki-backend",
-			ImageTag:                "latest",
-			DbBaseUrl:               "postgresql://peer@postgres-postgresql/peer",
-			DbCert:                  peerPostgresCert,
-			TbClusterID:             "0",
-			// TbReplicaAddresses:         "[\"tigerbeetle-0.tigerbeetle.default.svc.cluster.local\"]",
-			// waiting for update for client to handle dns.
-			// you will need to manually look up the tigerbeetle-0 pod and get its ip address for now.
-			TbReplicaAddresses:         "[\"10.244.0.23:8080\"]",
-			IlpAddress:                 "test.peer",
-			NonceRedisKey:              "noncepeer",
-			RedisUrl:                   "redis://redis-master:6379/1",
-			RedisCert:                  peerRedisCert,
-			AuthServerGrantUrl:         "http://mockauth",
-			AuthServerIntrospectionUrl: "http://mockauth",
-			StreamSecret:               streamSecretBase64,
-			AdminKey:                   streamSecret.Result,                     // re-using for local cluster
-			WebhookUrl:                 "https://end3sf6r22xva.x.pipedream.net", // using request bin for now
-			Hostname:                   "peer.fynbos.test",
-			PublicHost:                 "http://peer", // using peer as coredns will find peer
-		})
-		if err != nil {
-			return err
-		}
-
-		if err := mockstaticresponseservers.DeployMockGnapServer(ctx, "mockauth"); err != nil {
 			return err
 		}
 
