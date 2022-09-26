7ad428417cbbb8537413c70cd35e8f386e50be5e
Donovan Changfoot <don@fynbos.dev>
2022-09-26 13:40:59 +0200

    chore(dev): deploy resources from rooibos into kind cluster

--- dev/kind/run.sh
+++ dev/kind/run.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env sh
+set -ex
+
+ROOIBOS_DIR=$(dirname "$0")/../../../rooibos
 
 # Create cluster
 ctlptl apply -f "$(dirname "$0")/config.yaml"
@@ -11,21 +14,17 @@ docker push localhost:5005/backend:latest
 docker build "$(dirname "$0")/../../typescript/protea" -f "$(dirname "$0")/../../typescript/protea/Dockerfile" --target dev -t localhost:5005/protea:latest
 docker push localhost:5005/protea:latest
 
-## Build pacioli base docker and upload to cluster
-#docker build "$(dirname "$0")/../../go/" -f "$(dirname "$0")/../../go/pacioli/Dockerfile" -t localhost:5005/pacioli:latest
-#docker push localhost:5005/pacioli:latest
-
 # Pull and push temporal
 docker pull 823058932981.dkr.ecr.eu-west-1.amazonaws.com/temporalite
 docker tag 823058932981.dkr.ecr.eu-west-1.amazonaws.com/temporalite localhost:5005/temporalite
 docker push localhost:5005/temporalite
 
-## Pull and push tigerbeetle
-#docker pull 823058932981.dkr.ecr.eu-west-1.amazonaws.com/tigerbeetle@sha256:b1fe98356a0db183b56b555eac17c5a43f4b61305f5ac711ea741d5085a2f977
-#docker tag 823058932981.dkr.ecr.eu-west-1.amazonaws.com/tigerbeetle@sha256:b1fe98356a0db183b56b555eac17c5a43f4b61305f5ac711ea741d5085a2f977 localhost:5005/tigerbeetle:latest
-#docker push localhost:5005/tigerbeetle:latest
-#
-## Pull and push rafiki
-#docker pull 823058932981.dkr.ecr.eu-west-1.amazonaws.com/rafiki-backend
-#docker tag 823058932981.dkr.ecr.eu-west-1.amazonaws.com/rafiki-backend localhost:5005/rafiki-backend
-#docker push localhost:5005/rafiki-backend
+# deploy
+[ ! -d $ROOIBOS_DIR ] && git clone git@gitlab.com:fynbos/rooibos.git $ROOIBOS_DIR
+[ -d $ROOIBOS_DIR/certmanager/envs/local ] && while ! kubectl apply -k $ROOIBOS_DIR/certmanager/envs/local; do echo "Retrying to apply certmanager resources in 5s."; sleep 5; done
+[ -d $ROOIBOS_DIR/emissary/envs/local ] && while ! kubectl apply -k $ROOIBOS_DIR/emissary/envs/local; do echo "Retrying to apply emissary resources in 5s."; sleep 5; done
+[ -d $ROOIBOS_DIR/cockroach/envs/local ] && while ! kubectl apply -k $ROOIBOS_DIR/cockroach/envs/local; do echo "Retrying to apply cockroach resources in 5s."; sleep 5; done
+[ -d $ROOIBOS_DIR/backend/envs/local ] && while ! kubectl apply -k $ROOIBOS_DIR/backend/envs/local; do echo "Retrying to apply backend resources in 5s."; sleep 5; done
+[ -d $ROOIBOS_DIR/kratos/envs/local ] && while ! kubectl apply -k $ROOIBOS_DIR/kratos/envs/local; do echo "Retrying to apply kratos resources in 5s."; sleep 5; done
+[ -d $ROOIBOS_DIR/protea/envs/local ] && while ! kubectl apply -k $ROOIBOS_DIR/protea/envs/local; do echo "Retrying to apply protea resources in 5s."; sleep 5; done
+[ -d $ROOIBOS_DIR/temporalite/envs/local ] && while ! kubectl apply -k $ROOIBOS_DIR/temporalite/envs/local; do echo "Retrying to apply temporal resources in 5s."; sleep 5; done
