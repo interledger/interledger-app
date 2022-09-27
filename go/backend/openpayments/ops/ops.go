6775f30cfff9f072adc7e501a1dd19eb5e3bf3df
barnard <barnard@fynbos.dev>
2022-09-27 14:56:03 +0200

    fix(backends): add catch all http handler for openpayments

--- go/backend/openpayments/ops/ops.go
+++ go/backend/openpayments/ops/ops.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"fmt"
 	"net/url"
+	"strings"
 
 	"gitlab.com/fynbos/backend/db"
 	"gitlab.com/fynbos/backend/openpayments"
@@ -17,8 +18,13 @@ func CreatePaymentPointer(ctx context.Context, b Backends, pointer openpayments.
 		return fmt.Errorf("%w %s", openpayments.ErrInvalidArgument, err)
 	}
 
+	ppURL, err := sanitizePaymentPointer(pointer.URL)
+	if err != nil {
+		return err
+	}
+
 	_, err = b.DB().ExecContext(ctx, "INSERT INTO payment_pointers (wallet_id, url, alias, asset, scale) VALUES ($1,$2,$3,$4,$5)",
-		pointer.WalletID, pointer.URL, pointer.Alias, pointer.Asset, pointer.AssetScale)
+		pointer.WalletID, ppURL, pointer.Alias, pointer.Asset, pointer.AssetScale)
 
 	if db.IsErrorCode(err, db.UniqueViolationError) {
 		return fmt.Errorf("%w payment pointer url exists already (%s)", openpayments.ErrPaymentPointerExists, pointer.URL)
@@ -32,19 +38,59 @@ func CreatePaymentPointer(ctx context.Context, b Backends, pointer openpayments.
 
 func GetPaymentPointer(ctx context.Context, b Backends, pointerURLRaw string) (*openpayments.PaymentPointer, error) {
 
-	pointerURL, err := url.ParseRequestURI(pointerURLRaw)
+	ppURL, err := sanitizePaymentPointer(pointerURLRaw)
 	if err != nil {
-		return nil, openpayments.ErrInvalidPointerURL
+		return nil, err
 	}
 
 	var pp openpayments.PaymentPointer
-	err = b.DB().GetContext(ctx, &pp, "SELECT wallet_id, url, alias, asset, scale FROM payment_pointers WHERE url=$1", pointerURL.String())
+	err = b.DB().GetContext(ctx, &pp, "SELECT wallet_id, url, alias, asset, scale FROM payment_pointers WHERE url=$1", ppURL)
 	if errors.Is(err, sql.ErrNoRows) {
-		return nil, fmt.Errorf("%w unkown payment pointer url(%s)", openpayments.ErrPaymentPointerNotFound, pointerURL.String())
+		return nil, fmt.Errorf("%w unkown payment pointer url(%s)", openpayments.ErrPaymentPointerNotFound, ppURL)
 	}
 	if err != nil {
 		return nil, fmt.Errorf("%w %s", openpayments.ErrInternal, err)
 	}
 
 	return &pp, nil
+}
+
+var reservedURLParts = []string{"outgoing-payments", "incoming-payments", "quotes"}
+
+// sanitizePaymentPointer takes a full URL and checks for any reserved words and invalid formatting
+func sanitizePaymentPointer(rawURL string) (string, error) {
+	pointerURL, err := url.ParseRequestURI(rawURL)
+	if err != nil {
+		return "", openpayments.ErrInvalidPointerURL
+	}
+
+	pathParts := strings.Split(pointerURL.Path, "/")
+	for _, pp := range pathParts {
+		for _, res := range reservedURLParts {
+			if strings.EqualFold(pp, res) {
+				return "", openpayments.ErrInvalidPointerURL
+			}
+		}
+	}
+
+	return strings.TrimSuffix(pointerURL.String(), "/"), nil
+}
+
+// ExtractPaymentPointer takes a full URL and removes the known suffix and what is left is the original Payment pointer
+// returns the payment pointer as well as the matching suffix
+func ExtractPaymentPointer(rawURL string) (string, string, error) {
+	var res string
+	for _, res = range reservedURLParts {
+		if strings.Contains(rawURL, res) {
+			sanitized, err := sanitizePaymentPointer(rawURL[:strings.LastIndex(rawURL, res)])
+			if err != nil {
+				return "", "", err
+			}
+			return sanitized, res, nil
+		}
+	}
+
+	// No suffix found, return the original sanitized
+	sanitized, err := sanitizePaymentPointer(rawURL)
+	return sanitized, "", err
 }
--- go/backend/openpayments/server/http.go
+++ go/backend/openpayments/server/http.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"path"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/riandyrn/otelchi"
@@ -19,40 +20,102 @@ func StartOpenPaymentsHTTP(b Backends, port string) {
 	router := chi.NewRouter()
 	router.Use(otelchi.Middleware("open_payments", otelchi.WithChiRoutes(router)))
 
-	router.Get("/{payment_pointer}", getPaymentPointer(b))
+	router.NotFound(catchAllHandler(b))
 	log.Info("payment pointers served on http://localhost:{port}/{paymentpointer}", zap.String("port", port))
 	go func() {
 		log.Fatalln(http.ListenAndServe(fmt.Sprintf(":%s", port), router))
 	}()
 }
 
-func getPaymentPointer(b Backends) http.HandlerFunc {
+func catchAllHandler(b Backends) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
-		ctx := req.Context()
-		pp, err := ops.GetPaymentPointer(ctx, b, req.URL.String())
-		if err != nil {
-			if errors.Is(err, openpayments.ErrPaymentPointerNotFound) {
-				http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
-				return
-			}
+		if req.Method == http.MethodPost {
+			postHandler(b, w, req)
+		} else if req.Method == http.MethodGet {
+			getHandler(b, w, req)
+		} else {
+			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		}
+	}
+}
 
-			log.Error("error when getting payment pointer via HTTP", zap.Error(err), zap.String("url", req.URL.String()))
-			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+func getFullURL(req *http.Request) string {
+	url := path.Join(req.Host, req.URL.String())
+	if req.TLS == nil {
+		return fmt.Sprintf("http://%s", url)
+	}
+	return fmt.Sprintf("https://%s")
+}
+
+// postHandler handles all incoming POST requests and routes them according to the URL contents and suffixing.
+func postHandler(b Backends, w http.ResponseWriter, req *http.Request) {
+
+	ctx := req.Context()
+	ppURL, suffix, err := ops.ExtractPaymentPointer(getFullURL(req))
+
+	if err != nil {
+		log.Error("error trying to parse post to payment pointer", zap.Error(err), zap.String("url", req.URL.String()))
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	_, err = ops.GetPaymentPointer(ctx, b, ppURL)
+	if err != nil {
+		if errors.Is(err, openpayments.ErrPaymentPointerNotFound) {
+			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+			return
+		}
+		if errors.Is(err, openpayments.ErrInvalidPointerURL) {
+			http.Error(w, "Invalid Payment Pointer", http.StatusBadRequest)
+			return
+		}
+		log.Error("failed to get payment pointer", zap.Error(err), zap.String("url", req.URL.String()))
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	switch suffix {
+	case "outgoing-payments":
+		// TODO: handle
+		http.Error(w, http.StatusText(http.StatusNotImplemented), http.StatusNotImplemented)
+		return
+	case "incoming-payments":
+		// TODO: handle
+		http.Error(w, http.StatusText(http.StatusNotImplemented), http.StatusNotImplemented)
+		return
+	case "quote":
+		// TODO: handle
+		http.Error(w, http.StatusText(http.StatusNotImplemented), http.StatusNotImplemented)
+		return
+	default:
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+	}
+}
+
+// getHandler handles all incoming GET requests and handles them as required
+func getHandler(b Backends, w http.ResponseWriter, req *http.Request) {
+	ctx := req.Context()
+	pp, err := ops.GetPaymentPointer(ctx, b, getFullURL(req))
+	if err != nil {
+		if errors.Is(err, openpayments.ErrPaymentPointerNotFound) {
+			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+			return
+		}
+		if errors.Is(err, openpayments.ErrInvalidPointerURL) {
+			http.Error(w, "Invalid Payment Pointer", http.StatusBadRequest)
 			return
 		}
 
-		ppBytes, err := json.Marshal(pp)
-		if err != nil {
-			log.Error("error marshalling payment pointer", zap.Error(err), zap.String("url", req.URL.String()))
-			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-			return
-		}
+		log.Error("error when getting payment pointer via HTTP", zap.Error(err), zap.String("url", req.URL.String()))
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
 
-		_, err = w.Write(ppBytes)
-		if err != nil {
-			log.Error("error writing http response", zap.Error(err), zap.String("url", req.URL.String()))
-			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-			return
-		}
+	w.Header().Set("Content-Type", "application/json")
+	err = json.NewEncoder(w).Encode(pp)
+	if err != nil {
+		log.Error("error writing http response", zap.Error(err), zap.String("url", req.URL.String()))
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
 	}
 }
