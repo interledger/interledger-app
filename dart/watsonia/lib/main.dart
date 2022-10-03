5211bfbb94f6556203e4946ad3244de1c57fa821
Cairin Michie <cairinmichie@gmail.com>
2022-10-03 08:56:14 +0200

    feat(watsonia): first build and layouts
    Adds tailwind color theme.

--- dart/watsonia/lib/main.dart
+++ dart/watsonia/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:watsonia/styles/colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -69,47 +72,164 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    return Container(
+        width: 100,
+        color: const Color(0xffF8FAFC),
+        child: SafeArea(
+          child: Column(
+            children: [
+              Container(
+                  height: 64,
+                  width: double.infinity,
+                  alignment: Alignment.centerLeft,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.all(16.0),
+                        child: Icon(Icons.menu),
+                      ),
+                      Text(
+                        "fynbos",
+                        style: GoogleFonts.poppins(
+                          textStyle: const TextStyle(
+                              color: Color(0xFF0F172A),
+                              fontSize: 24,
+                              fontWeight: FontWeight.w500),
+                        ),
+                      ),
+                    ],
+                  )),
+              Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.white),
+                width: double.infinity,
+                margin: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Sign up",
+                      style: GoogleFonts.poppins(
+                        textStyle: const TextStyle(
+                            color: Color(0xFF0F172A),
+                            fontSize: 24,
+                            fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                    Text(
+                      "Here's what we will need to create your account.",
+                      style: GoogleFonts.poppins(
+                        textStyle: const TextStyle(
+                            decoration: TextDecoration.none,
+                            color: Color(0xFF0F172A),
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400),
+                      ),
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: 32,
+                          height: 32,
+                          decoration: BoxDecoration(
+                              color: TWColors.yellow[300],
+                              borderRadius: const BorderRadius.only(
+                                  topLeft: Radius.circular(32))),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.fromLTRB(0, 0, 16, 0),
+                          width: 32,
+                          height: 32,
+                          decoration: BoxDecoration(
+                            color: TWColors.slate[500],
+                            borderRadius: const BorderRadius.only(
+                              bottomLeft: Radius.circular(32),
+                            ),
+                          ),
+                        ),
+                        Flexible(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Personal details",
+                                style: GoogleFonts.poppins(
+                                  textStyle: const TextStyle(
+                                      color: Color(0xFF0F172A),
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                              ),
+                              Text(
+                                "Your legal name, email address and country of residence.",
+                                style: GoogleFonts.poppins(
+                                  textStyle: const TextStyle(
+                                      color: Color(0xFF0F172A),
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w400),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: 32,
+                          height: 32,
+                          decoration: BoxDecoration(
+                              color: TWColors.slate[300],
+                              borderRadius: const BorderRadius.only(
+                                  bottomLeft: Radius.circular(32))),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.fromLTRB(0, 0, 16, 0),
+                          width: 32,
+                          height: 32,
+                          decoration: BoxDecoration(
+                            color: TWColors.lime[500],
+                            borderRadius: BorderRadius.circular(32),
+                          ),
+                        ),
+                        Flexible(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Mobile phone number",
+                                style: GoogleFonts.poppins(
+                                  textStyle: const TextStyle(
+                                      color: Color(0xFF0F172A),
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                              ),
+                              Text(
+                                "A mobile phone number we can verify",
+                                style: GoogleFonts.poppins(
+                                  textStyle: const TextStyle(
+                                      color: Color(0xFF0F172A),
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w400),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
