726f1619234e2a4e72d528205692556707c5682e
barnard <barnard@fynbos.dev>
2022-10-04 10:55:25 +0200

    feat(backends): list users for a wallet

--- go/backend/user/api.go
+++ go/backend/user/api.go
@@ -5,6 +5,7 @@ import "context"
 type Client interface {
 	UserForCookie(ctx context.Context, cookie string) (*User, error)
 	UserForContext(ctx context.Context) (*User, error)
+	ListUsers(ctx context.Context, walletID string) ([]User, error)
 	WalletForContext(ctx context.Context) (*Wallet, error)
 	CreateNewWallet(ctx context.Context, userID, walletName string) (*Wallet, error)
 	ListWallets(ctx context.Context, userID string) ([]Wallet, error)
