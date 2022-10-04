f4afbe8a515235b209c7cfa1e6f94b8c925b2eff
Donovan Changfoot <don@fynbos.dev>
2022-10-04 11:49:38 +0200

    feat(backend): machnet webhook parses events and sends to machnet provider

--- go/backend/providers/machnet/api.go
+++ go/backend/providers/machnet/api.go
@@ -1,9 +1,14 @@
 package machnet
 
-import "context"
+import (
+	"context"
+
+	"gitlab.com/fynbos/backend/providers/machnet/external"
+)
 
 type Client interface {
 	GetUser(ctx context.Context, walletID string) (*User, error)
 	CreateUser(ctx context.Context, args CreateArgs) (*User, error)
 	GetWidgetToken(ctx context.Context, walletID string) (*WidgetToken, error)
+	HandleEvent(ctx context.Context, event external.Event) error
 }
--- go/backend/providers/machnet/webhook/webhook.go
+++ go/backend/providers/machnet/webhook/webhook.go
@@ -1,9 +1,35 @@
 package webhook
 
-import "net/http"
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+
+	"gitlab.com/fynbos/backend/providers/machnet/external"
+)
 
 func New(b Backends) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "failed to read body", http.StatusInternalServerError)
+			return
+		}
+
+		var event external.Event
+		err = json.Unmarshal(body, &event)
+		if err != nil {
+			http.Error(w, "failed to parse payload", http.StatusBadRequest)
+			return
+		}
+
+		// TODO: validate payload
+		err = b.Machnet().HandleEvent(r.Context(), event)
+		if err != nil {
+			http.Error(w, "internal server error", http.StatusInternalServerError)
+			return
+		}
+
 		w.WriteHeader(http.StatusOK)
 	}
 }
