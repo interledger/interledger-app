444df9d83d461864b128c0e8d74e195793f88d0b
Cairin Michie <cairinmichie@gmail.com>
2022-10-05 09:57:28 +0200

    feat(protea): add home shapes

--- typescript/protea/app/components/Decor/Shapes.tsx
+++ typescript/protea/app/components/Decor/Shapes.tsx
@@ -1,4 +1,4 @@
-import type { FC } from 'react'
+import type { FC, Key } from 'react'
 import clsx from 'clsx'
 
 type Radius =
@@ -14,25 +14,155 @@ type Radius =
   | 'rounded-r-full'
 
 type ShapeProps = {
-  key?: string
   width?: string
   radius: Radius
   color: string
 }
 
-export const Shape: FC<ShapeProps> = ({
-  children,
-  key,
-  width,
-  radius,
-  color
-}) => {
+export const Shape: FC<ShapeProps> = ({ children, width, radius, color }) => {
   return (
-    <div
-      key={key}
-      className={clsx('aspect-square', width || 'w-full', radius, color)}
-    >
+    <div className={clsx('aspect-square', width || 'w-full', radius, color)}>
       {children}
+    </div>
+  )
+}
+
+export function HomeShapes() {
+  const shapes: ShapeProps[][] = [
+    [
+      {
+        radius: 'rounded-tl-full',
+        color: 'bg-slate-600'
+      },
+      {
+        radius: 'rounded-full',
+        color: 'bg-transparent'
+      },
+      {
+        radius: 'rounded-tr-full',
+        color: 'bg-yellow-400'
+      },
+      {
+        radius: 'rounded-tl-full',
+        color: 'bg-rose-300'
+      },
+      {
+        radius: 'rounded-full',
+        color: 'bg-lime-400'
+      },
+      {
+        radius: 'rounded-b-full',
+        color: 'bg-transparent'
+      },
+      {
+        radius: 'rounded-full',
+        color: 'bg-rose-500'
+      },
+      {
+        radius: 'rounded-tr-full',
+        color: 'bg-lime-300'
+      },
+      {
+        radius: 'rounded-b-full',
+        color: 'bg-transparent'
+      },
+      {
+        radius: 'rounded-b-full',
+        color: 'bg-transparent'
+      },
+      {
+        width: 'w-0 lg:w-full',
+        radius: 'rounded-tr-full',
+        color: 'bg-lime-500'
+      },
+      {
+        width: 'w-0 lg:w-full',
+        radius: 'rounded-b-full',
+        color: 'bg-transparent'
+      },
+      {
+        width: 'w-0 lg:w-full',
+        radius: 'rounded-b-full',
+        color: 'bg-transparent'
+      },
+      {
+        width: 'w-0 lg:w-full',
+        radius: 'rounded-tl-full',
+        color: 'bg-rose-400'
+      }
+    ],
+    [
+      {
+        radius: 'rounded-tl-full',
+        color: 'bg-transparent'
+      },
+      {
+        radius: 'rounded-full',
+        color: 'bg-rose-400'
+      },
+      {
+        radius: 'rounded-bl-full',
+        color: 'bg-lime-500'
+      },
+      {
+        radius: 'rounded-tl-full',
+        color: 'bg-transparent'
+      },
+      {
+        radius: 'rounded-tl-full',
+        color: 'bg-slate-300'
+      },
+      {
+        radius: 'rounded-tl-full',
+        color: 'bg-yellow-200'
+      },
+      {
+        radius: 'rounded-br-full',
+        color: 'bg-slate-500'
+      },
+      {
+        radius: 'rounded-tr-full',
+        color: 'bg-transparent'
+      },
+      {
+        radius: 'rounded-full',
+        color: 'bg-rose-100'
+      },
+      {
+        radius: 'rounded-bl-full',
+        color: 'bg-rose-300'
+      },
+      {
+        width: 'w-0 lg:w-full',
+        radius: 'rounded-bl-full',
+        color: 'bg-yellow-200'
+      },
+      {
+        width: 'w-0 lg:w-full',
+        radius: 'rounded-full',
+        color: 'bg-lime-400'
+      },
+      {
+        width: 'w-0 lg:w-full',
+        radius: 'rounded-tr-full',
+        color: 'bg-yellow-400'
+      },
+      {
+        width: 'w-0 lg:w-full',
+        radius: 'rounded-tl-full',
+        color: 'bg-transparent'
+      }
+    ]
+  ]
+  return (
+    <div>
+      {shapes.map((shapeRow, outerIndex) => (
+        <div className='flex' key={`shapeRow${outerIndex}`}>
+          {shapeRow.map((shape, index) => (
+            <Shape key={`shape${outerIndex}${index}`} {...shape} />
+          ))}
+        </div>
+      ))}
     </div>
   )
 }
