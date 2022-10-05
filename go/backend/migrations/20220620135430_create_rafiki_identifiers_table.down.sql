91ed193266567105685add6d16d22bf91a3656bc
Matt de Haast <matt@fynbos.dev>
2022-10-05 08:26:05 +0200

    chore(backend): deprecate rafiki migrations
    remove the rafiki migrations from our codebase

--- go/backend/migrations/20220620135430_create_rafiki_identifiers_table.down.sql
+++
@@ -1,1 +0,0 @@
-DROP TABLE IF EXISTS rafiki_identifiers;
--- go/backend/migrations/20220620135430_create_rafiki_identifiers_table.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS rafiki_identifiers
-(
-	id			TEXT PRIMARY KEY,
-	account_id	UUID NOT NULL,
-	asset_code	TEXT NOT NULL,
-	asset_scale	INT8 NOT NULL,
-	created_at	TIMESTAMP NOT NULL DEFAULT now(),
-	updated_at	TIMESTAMP NOT NULL DEFAULT now()
-);
