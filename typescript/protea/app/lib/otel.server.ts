11c7d74b7d998b1048693dbdd378c38812badf6c
Matt de Haast <matt@fynbos.dev>
2022-10-06 11:17:58 +0200

    feat(protea): add open telemetry support
    add basic open telemetry support to protea and only
    instantiate it when the OTLP header is set for
    honeycomb

---
+++ typescript/protea/app/lib/otel.server.ts
@@ -0,0 +1,13 @@
+import { NodeSDK } from '@opentelemetry/sdk-node'
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node'
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto'
+import { RemixInstrumentation } from 'opentelemetry-instrumentation-remix'
+
+// The Trace Exporter exports the data to Honeycomb and uses
+// the environment variables for endpoint, service name, and API Key.
+const traceExporter = new OTLPTraceExporter()
+
+export const sdk = new NodeSDK({
+  traceExporter,
+  instrumentations: [getNodeAutoInstrumentations(), new RemixInstrumentation()]
+})
