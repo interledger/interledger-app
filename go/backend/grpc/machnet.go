8fc126bf85df1ec74551eeef1a5ad9b7114aeb04
Matt de Haast <matt@fynbos.dev>
2022-10-20 15:23:41 +0200

    feat(backend): machnet grpc senduser
    add machnet send user functionlity on the grpc endpoint that allows
    a caller to initiate a machnet send user being created as well
    as a check to see if a send user exists for machnet

--- go/backend/grpc/machnet.go
+++ go/backend/grpc/machnet.go
@@ -2,7 +2,8 @@ package grpc
 
 import (
 	"context"
-
+	"errors"
+	"gitlab.com/fynbos/backend/providers/machnet"
 	backendv1 "gitlab.com/fynbos/proto/backend/v1"
 )
 
@@ -28,5 +29,59 @@ func (r rpcService) GetMachnetWidgetToken(
 		Value:            token.Value,
 		ExpiresInMinutes: int64(token.ExpiresInMinutes),
 		UserId:           token.UserID,
+	}, nil
+}
+
+func (r rpcService) CreateSendUser(
+	ctx context.Context, req *backendv1.Empty,
+) (*backendv1.Empty, error) {
+	_, err := r.b.Users().UserForContext(ctx)
+	if err != nil {
+		return nil, ForbiddenError("Unauthenticated.")
+	}
+
+	wallet, err := r.b.Users().WalletForContext(ctx)
+	if err != nil {
+		return nil, ForbiddenError("Unauthenticated.")
+	}
+
+	await, err := r.b.Machnet().CreateSendUser(ctx, wallet.ID)
+	if err != nil {
+		return nil, toGRPCError(err)
+	}
+
+	err = await(ctx)
+	if err != nil {
+		return nil, toGRPCError(err)
+	}
+
+	return &backendv1.Empty{}, nil
+}
+
+func (r rpcService) HasSendUser(
+	ctx context.Context, req *backendv1.Empty,
+) (*backendv1.HasSendUserResponse, error) {
+	_, err := r.b.Users().UserForContext(ctx)
+	if err != nil {
+		return nil, ForbiddenError("Unauthenticated.")
+	}
+
+	wallet, err := r.b.Users().WalletForContext(ctx)
+	if err != nil {
+		return nil, ForbiddenError("Unauthenticated.")
+	}
+
+	_, err = r.b.Machnet().GetUserByWalletID(ctx, wallet.ID)
+	if err != nil {
+		if errors.Is(err, machnet.ErrNotFound) {
+			return &backendv1.HasSendUserResponse{
+				HasSendUser: false,
+			}, nil
+		}
+		return nil, toGRPCError(err)
+	}
+
+	return &backendv1.HasSendUserResponse{
+		HasSendUser: true,
 	}, nil
 }
--- go/backend/grpc/machnet_test.go
+++ go/backend/grpc/machnet_test.go
@@ -57,3 +57,93 @@ func TestGetMachnetWidgetToken(t *testing.T) {
 		assert.Equal(st, machnetUserID, rpc.GetUserId())
 	})
 }
+
+func TestHasSendUser(t *testing.T) {
+	t.Parallel()
+	ctx := context.Background()
+	ctrl := gomock.NewController(t)
+	t.Cleanup(func() {
+		ctrl.Finish()
+	})
+	c := NewTestContainer(t, ctrl)
+	_, _, client := startTestServer(t, c)
+	user := &_user.User{
+		ID: uuid.NewString(),
+	}
+	wallet, err := c.Users().CreateNewWallet(ctx, user.ID, "default")
+	require.NoError(t, err)
+
+	t.Run("returns true if user exists", func(st *testing.T) {
+		machnetUserID := uuid.NewString()
+		c.machnet.EXPECT().GetUserByWalletID(gomock.Any(), wallet.ID).Return(&machnet.User{
+			ID:        machnetUserID,
+			WalletID:  wallet.ID,
+			CreatedAt: "",
+			UpdatedAt: "",
+			KYCStatus: machnet.KYCStatusInProgress,
+		}, nil).Times(1)
+
+		rpc, err := client.HasSendUser(
+			user_mock.ActingAsContext(t, context.Background(), user),
+			&backendv1.Empty{},
+		)
+		require.NoError(st, err)
+
+		assert.True(t, rpc.HasSendUser)
+	})
+
+	t.Run("returns false if no user exists", func(st *testing.T) {
+		c.machnet.EXPECT().GetUserByWalletID(gomock.Any(), wallet.ID).Return(nil, machnet.ErrNotFound).Times(1)
+
+		rpc, err := client.HasSendUser(
+			user_mock.ActingAsContext(t, context.Background(), user),
+			&backendv1.Empty{},
+		)
+		require.NoError(st, err)
+
+		assert.False(t, rpc.HasSendUser)
+	})
+}
+
+func TestCreateSendUser(t *testing.T) {
+	t.Parallel()
+	ctx := context.Background()
+	ctrl := gomock.NewController(t)
+	t.Cleanup(func() {
+		ctrl.Finish()
+	})
+	c := NewTestContainer(t, ctrl)
+	_, _, client := startTestServer(t, c)
+	user := &_user.User{
+		ID: uuid.NewString(),
+	}
+	wallet, err := c.Users().CreateNewWallet(ctx, user.ID, "default")
+	require.NoError(t, err)
+
+	t.Run("returns if workflow runs correctly", func(st *testing.T) {
+		await := func(ctx context.Context) error {
+			return nil
+		}
+		c.machnet.EXPECT().CreateSendUser(gomock.Any(), wallet.ID).Return(await, nil).Times(1)
+
+		_, err := client.CreateSendUser(
+			user_mock.ActingAsContext(t, context.Background(), user),
+			&backendv1.Empty{},
+		)
+		require.NoError(st, err)
+	})
+
+	t.Run("returns error if workflow fails", func(st *testing.T) {
+		await := func(ctx context.Context) error {
+			return machnet.ErrInternal
+		}
+		c.machnet.EXPECT().CreateSendUser(gomock.Any(), wallet.ID).Return(await, nil).Times(1)
+
+		_, err := client.CreateSendUser(
+			user_mock.ActingAsContext(t, context.Background(), user),
+			&backendv1.Empty{},
+		)
+		require.Error(st, err)
+	})
+
+}
