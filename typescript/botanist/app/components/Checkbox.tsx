950f048d90d354dfe4e5c33fc7614023971468dd
Matt de Haast <matt@fynbos.dev>
2022-10-20 14:17:27 +0200

    feat(botanist): init
    initialize botanist our admin panel repo

---
+++ typescript/botanist/app/components/Checkbox.tsx
@@ -0,0 +1,38 @@
+import type { InputHTMLAttributes } from 'react'
+import { forwardRef } from 'react'
+
+interface CheckboxProps extends InputHTMLAttributes<HTMLInputElement> {
+  // Override the `className` of the root `div` of the Input. Defaults to **min-w-full**.
+  className?: string
+  // The message from errors produced by form validation.
+  errorMessage?: string
+}
+
+export const Checkbox = forwardRef<any, CheckboxProps>(
+  ({ className, errorMessage, children, ...inputProps }, ref) => {
+    return (
+      <div className={className || 'min-w-full'}>
+        <div className='ml-2 flex h-6 w-6 items-center'>
+          <input
+            ref={ref}
+            {...inputProps}
+            type='checkbox'
+            className='h-[1.125rem] w-[1.125rem] cursor-pointer rounded-sm border-2 border-base text-transparent focus:ring-transparent focus-visible:ring-focus'
+          />
+        </div>
+        <div className='ml-3 text-sm'>
+          <label htmlFor={inputProps.id} className='cursor-pointer text-xs'>
+            {children}
+          </label>
+          <div className='-ml-7 h-7 pt-2'>
+            {errorMessage && (
+              <p className='text-xs text-error'>{errorMessage}</p>
+            )}
+          </div>
+        </div>
+      </div>
+    )
+  }
+)
+
+Checkbox.displayName = 'Checkbox'
---
+++ typescript/botanist/tailwind.config.js
@@ -0,0 +1,78 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  mode: 'jit',
+  content: ['./app/**/*.{ts,tsx}'],
+  darkMode: 'media',
+  theme: {
+    fontFamily: {
+      display: ['Poppins'],
+      sans: ['Inter'],
+      mono: ['"Overpass Mono"', '"Source Code Pro"']
+    },
+    extend: {
+      colors: {
+        brand: colors.rose[500]
+      },
+      // Token colours
+      textColor: {
+        strong: 'rgba(var(--text-strong), <alpha-value>)',
+        medium: 'rgba(var(--text-medium), <alpha-value>)',
+        weak: 'rgba(var(--text-weak), <alpha-value>)',
+        disabled: 'rgba(var(--text-disabled), <alpha-value>)',
+        primary: 'rgba(var(--text-primary), <alpha-value>)',
+        error: 'rgba(var(--text-error), <alpha-value>)',
+        success: 'rgba(var(--text-success), <alpha-value>)'
+      },
+      backgroundColor: {
+        app: 'rgba(var(--bg-app), <alpha-value>)',
+        page: 'rgba(var(--bg-page), <alpha-value>)',
+        container: 'rgba(var(--bg-container), <alpha-value>)',
+        'container-hover': 'rgba(var(--bg-container-hover), <alpha-value>)',
+        strong: 'rgba(var(--bg-strong), <alpha-value>)',
+        disabled: 'rgba(var(--bg-disabled), <alpha-value>)',
+        primary: 'rgba(var(--bg-primary), <alpha-value>)',
+        'container-primary': 'rgba(var(--bg-container-primary), <alpha-value>)',
+        'container-primary-hover':
+          'rgba(var(--bg-container-primary-hover), <alpha-value>)',
+        'container-primary-active':
+          'rgba(var(--bg-container-primary-active), <alpha-value>)',
+        'container-secondary':
+          'rgba(var(--bg-container-secondary), <alpha-value>)',
+        scrim: 'rgba(var(--bg-scrim), <alpha-value>)',
+        snackbar: 'rgba(var(--bg-snackbar), <alpha-value>)',
+        footer: 'rgba(var(--bg-footer), <alpha-value>)'
+      },
+      borderColor: {
+        base: 'rgba(var(--border-base), <alpha-value>)',
+        focus: 'rgba(var(--border-focus), <alpha-value>)',
+        hover: 'rgba(var(--border-hover), <alpha-value>)',
+        active: 'rgba(var(--border-active), <alpha-value>)',
+        error: 'rgba(var(--border-error), <alpha-value>)'
+      },
+      ringColor: {
+        base: 'rgba(var(--border-base), <alpha-value>)',
+        focus: 'rgba(var(--border-focus), <alpha-value>)',
+        hover: 'rgba(var(--border-hover), <alpha-value>)',
+        active: 'rgba(var(--border-active), <alpha-value>)',
+        error: 'rgba(var(--border-error), <alpha-value>)'
+      },
+      outlineColor: {
+        base: 'rgba(var(--border-base), <alpha-value>)',
+        focus: 'rgba(var(--border-focus), <alpha-value>)',
+        hover: 'rgba(var(--border-hover), <alpha-value>)',
+        active: 'rgba(var(--border-active), <alpha-value>)',
+        error: 'rgba(var(--border-error), <alpha-value>)'
+      },
+      divideColor: {
+        base: 'rgba(var(--border-base), <alpha-value>)',
+        focus: 'rgba(var(--border-focus), <alpha-value>)',
+        hover: 'rgba(var(--border-hover), <alpha-value>)',
+        active: 'rgba(var(--border-active), <alpha-value>)',
+        error: 'rgba(var(--border-error), <alpha-value>)'
+      }
+      // End token colours
+    }
+  },
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')]
+}
