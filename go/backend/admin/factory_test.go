c6b8c947e492056ff26c8c4a525675920fa26de3
Matt de Haast <matt@fynbos.dev>
2022-10-21 17:05:26 +0200

    feat(backend): init admin server
    init the admin server to run on its own port. This allows for
    easier management of it. Also removed it from old grpc server
    and made it start and listen on startup in main on port 8448

--- go/backend/admin/factory_test.go
+++ go/backend/admin/factory_test.go
@@ -3,122 +3,50 @@ package admin_test
 import (
 	"context"
 	"fmt"
+	"gitlab.com/fynbos/backend/admin"
 	"net"
 	"testing"
-
-	"gitlab.com/fynbos/backend/kyc"
-
-	signup_client "gitlab.com/fynbos/backend/signup/client"
-
-	"gitlab.com/fynbos/backend/signup"
-
-	"gitlab.com/fynbos/backend/linkedaccounts"
-
-	user_mock "gitlab.com/fynbos/backend/user/client/mock"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/golang/mock/gomock"
 	"github.com/jmoiron/sqlx"
-	"github.com/osohq/go-oso"
 	"gitlab.com/fynbos/backend/admin/auth"
-	"gitlab.com/fynbos/backend/agreements"
-	agreements_client "gitlab.com/fynbos/backend/agreements/client"
 	"gitlab.com/fynbos/backend/country"
-	country_client "gitlab.com/fynbos/backend/country/client"
-	_grpc "gitlab.com/fynbos/backend/grpc"
 	"gitlab.com/fynbos/backend/healthcheck"
-	linked_account_mock "gitlab.com/fynbos/backend/linkedaccounts/client/mock"
-	"gitlab.com/fynbos/backend/providers/fakecash"
-	fakecash_mock "gitlab.com/fynbos/backend/providers/fakecash/client/mock"
-	"gitlab.com/fynbos/backend/providers/machnet"
-	machnet_mock "gitlab.com/fynbos/backend/providers/machnet/client/mock"
-	"gitlab.com/fynbos/backend/supporttickets"
-	support_mock "gitlab.com/fynbos/backend/supporttickets/client/mock"
-	"gitlab.com/fynbos/backend/twilio"
-	"gitlab.com/fynbos/backend/user"
 	test_utils "gitlab.com/fynbos/backend/utils"
 	"gitlab.com/fynbos/backend/waitlist"
 	waitlist_mock "gitlab.com/fynbos/backend/waitlist/client/mock"
-	"gitlab.com/fynbos/pacioli"
-	"gitlab.com/fynbos/proto/backend/v1"
-	"go.temporal.io/sdk/client"
+	adminv1 "gitlab.com/fynbos/proto/backend/admin/v1"
 	"go.temporal.io/sdk/mocks"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
 type TestContainer struct {
-	Ctx             context.Context
-	Ctrl            *gomock.Controller
-	Db              *sqlx.DB
-	linkedAccounts  linkedaccounts.Client
-	fakeCash        *fakecash_mock.MockClient
-	machnet         *machnet_mock.MockClient
-	Hs              healthcheck.Service
-	Si              signup.Client
-	Oso             *oso.Oso
-	Cs              country.Client
-	Tp              *mocks.Client
-	AdminConn       *grpc.ClientConn
-	AdminClient     backend.BackendAdminServiceClient
-	AdminServer     *grpc.Server
-	PacioliClient   pacioli.Client
-	PacioliLedgerID uint32
-	ValidatorImpl   *validator.Validate
-	Auth            auth.Service
-	AgreementsImpl  agreements.Client
-	Tickets         *support_mock.MockClient
-	UsersImpl       user.Client
-	WaitlistImpl    *waitlist_mock.MockClient
-	TwilioImpl      *twilio.MockService
-}
-
-func (c *TestContainer) KYC() kyc.Client {
-	return nil
+	Ctx           context.Context
+	Ctrl          *gomock.Controller
+	Db            *sqlx.DB
+	Hs            healthcheck.Service
+	Cs            country.Client
+	Tp            *mocks.Client
+	AdminConn     *grpc.ClientConn
+	AdminClient   adminv1.BackendClient
+	AdminServer   *grpc.Server
+	ValidatorImpl *validator.Validate
+	Auth          auth.Service
+	WaitlistImpl  *waitlist_mock.MockClient
 }
 
 func (c *TestContainer) AdminAuth() auth.Service {
 	return c.Auth
 }
 
-func (c *TestContainer) Agreements() agreements.Client {
-	return c.AgreementsImpl
-}
-
-func (c *TestContainer) LinkedAccounts() linkedaccounts.Client {
-	return c.linkedAccounts
-}
-
-func (c *TestContainer) Machnet() machnet.Client {
-	return c.machnet
-}
-
-func (c *TestContainer) FakeCash() fakecash.Client {
-	return c.fakeCash
-}
-
 func (c *TestContainer) HealthCheck() healthcheck.Service {
 	return c.Hs
 }
 
-func (c *TestContainer) Signup() signup.Client {
-	return c.Si
-}
-
-func (c *TestContainer) SupportTickets() supporttickets.Client {
-	return c.Tickets
-}
-
-func (c *TestContainer) Twilio() twilio.Service {
-	return c.TwilioImpl
-}
-
 func (c *TestContainer) Waitlist() waitlist.Client {
 	return c.WaitlistImpl
-}
-
-func (c *TestContainer) Temporal() client.Client {
-	return c.Tp
 }
 
 func (c *TestContainer) Validator() *validator.Validate {
@@ -127,17 +55,6 @@ func (c *TestContainer) Validator() *validator.Validate {
 
 func (c *TestContainer) DB() *sqlx.DB {
 	return c.Db
-}
-
-func (c *TestContainer) Countries() country.Client {
-	return c.Cs
-}
-
-func (c *TestContainer) Pacioli() pacioli.Client {
-	return c.PacioliClient
-}
-func (c *TestContainer) Users() user.Client {
-	return c.UsersImpl
 }
 
 func (c *TestContainer) Cleanup(ctx context.Context) error {
@@ -158,17 +75,6 @@ func NewTestContainer(ctx context.Context, t *testing.T) (*TestContainer, error)
 	db := test_utils.MigrateCockroachDB(t, ctx)
 	c.Db = db
 
-	pClient := test_utils.SetupPacioli(t, ctx)
-	c.PacioliClient = pClient
-	c.PacioliLedgerID = 1
-
-	cs := country_client.New(c)
-	c.Cs = cs
-
-	c.Tp = &mocks.Client{}
-
-	si := signup_client.New(c)
-	c.Si = si
 	port, err := test_utils.GetFreePort()
 	if err != nil {
 		t.Fatal(err)
@@ -187,15 +93,10 @@ func NewTestContainer(ctx context.Context, t *testing.T) (*TestContainer, error)
 	att := auth.NewMockService()
 	c.Auth = att
 
-	c.AgreementsImpl = agreements_client.New(c)
 	c.Ctrl = gomock.NewController(t)
-	c.linkedAccounts = linked_account_mock.NewMockClient(c.Ctrl)
-	c.TwilioImpl = twilio.NewMockService(c.Ctrl)
-	c.UsersImpl = user_mock.NewMock()
 	c.WaitlistImpl = waitlist_mock.NewMockClient(c.Ctrl)
-	c.Tickets = support_mock.NewMockClient(c.Ctrl)
 
-	server, err := _grpc.NewServer(c)
+	server, err := admin.NewServer(c)
 	if err != nil {
 		return nil, err
 	}
@@ -211,43 +112,7 @@ func NewTestContainer(ctx context.Context, t *testing.T) (*TestContainer, error)
 		return nil, err
 	}
 	c.AdminConn = adminConn
-	c.AdminClient = backend.NewBackendAdminServiceClient(adminConn)
+	c.AdminClient = adminv1.NewBackendClient(adminConn)
 
 	return c, nil
-}
-
-// note https://github.com/golang/mock/issues/139
-// we run the grpc server in a goroutine so any errors from the mock will be swallowed.
-// TODO: see if there is a test server for grpc
-func NewAdminClient(t *testing.T, svr *grpc.Server) (client backend.BackendAdminServiceClient, cleanup func()) {
-	port, err := test_utils.GetFreePort()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	listener, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", port))
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// run the server
-	go func() {
-		if err := svr.Serve(listener); err != nil {
-			panic(err)
-		}
-	}()
-
-	conn, err := grpc.Dial(fmt.Sprintf("0.0.0.0:%d", port), grpc.WithBlock(), grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	client = backend.NewBackendAdminServiceClient(conn)
-	cleanup = func() {
-		if err := conn.Close(); err != nil {
-			t.Fatal(err)
-		}
-	}
-
-	return client, cleanup
 }
