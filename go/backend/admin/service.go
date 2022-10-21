c1f42162c2383919fd3a6af1c47b6746ab418461
Matt de Haast <matt@fynbos.dev>
2022-10-21 17:05:26 +0200

    feat(backend): init admin server
    init the admin server to run on its own port. This allows for
    easier management of it. Also removed it from old grpc server
    and made it start and listen on startup in main on port 8448

--- go/backend/admin/service.go
+++ go/backend/admin/service.go
@@ -1,17 +1,7 @@
 package admin
 
-import (
-	"github.com/go-playground/validator/v10"
-	"gitlab.com/fynbos/backend/admin/auth"
-	backendv1 "gitlab.com/fynbos/proto/backend/v1"
-	"go.temporal.io/sdk/client"
-)
-
 type AdminRpcService struct {
-	backendv1.UnimplementedBackendAdminServiceServer
-	Validator   *validator.Validate
-	AuthService auth.Service
-	Temporal    client.Client
+	b Backends
 }
 
 //func authorizeAdmin(email string) bool {
