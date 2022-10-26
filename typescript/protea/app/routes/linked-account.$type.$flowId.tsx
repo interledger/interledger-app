8c868ce168c159936cb5e4b413b84126a8e5c406
Cairin Michie <cairinmichie@gmail.com>
2022-10-26 12:55:55 +0200

    feat(protea): add linked account flow
    Controls for adding linked send account.
    Allows diverting to add personal details if no send user exists.

---
+++ typescript/protea/app/routes/linked-account.$type.$flowId.tsx
@@ -0,0 +1,105 @@
+import type { LoaderArgs } from '@remix-run/node'
+import { json } from '@remix-run/node'
+import { useLoaderData, useNavigate, useParams } from '@remix-run/react'
+import { useCallback, useEffect } from 'react'
+import { useScript } from '~/lib/useScript'
+import { requireUserSession } from '~/lib/kratos.server'
+import {
+  grpcClient,
+  httpMapping,
+  isGrpcError,
+  StatusError
+} from '~/lib/proto.server'
+import { Shape } from '~/components'
+import { route } from 'routes-gen'
+
+export async function loader({ request }: LoaderArgs) {
+  await requireUserSession(request)
+  let rpc = await grpcClient
+    .getMachnetWidgetToken(
+      {},
+      {
+        meta: {
+          cookies: request.headers.get('cookie') || ''
+        }
+      }
+    )
+    .then((v) => v)
+    .catch(StatusError)
+  if (isGrpcError(rpc)) {
+    throw json({}, httpMapping(rpc.code))
+  }
+
+  console.log(rpc.response.expiresInMinutes)
+
+  let widgetScriptUrl = 'https://widget.v4sandbox.machpay.com/widget/widget.js'
+  return json({
+    widgetScriptUrl,
+    widgetUserId: rpc.response.userId,
+    widgetToken: rpc.response.value
+  })
+}
+
+export default function Page() {
+  const data = useLoaderData<typeof loader>()
+  const scriptStatus = useScript(data.widgetScriptUrl)
+  const navigate = useNavigate()
+  const params = useParams()
+
+  const listener = useCallback(
+    (event: any) => {
+      console.log('listener', event)
+      if (event.data.type == 'CARD' && event.data.status == 'CARD_ADDED') {
+        navigate(
+          route('/linked-account/:type/:flowId/success', {
+            type: params.type as string,
+            flowId: params.flowId as string
+          })
+        )
+      }
+    },
+    [navigate, params.flowId, params.type]
+  )
+
+  useEffect(() => {
+    if (scriptStatus === 'ready') {
+      const widget = new (window as any).MachnetWidget({
+        elementId: 'widget',
+        userId: data.widgetUserId,
+        width: '100%',
+        height: '200px',
+        type: 'card',
+        locale: 'en',
+        stylesheet: '',
+        token: data.widgetToken
+      })
+      widget.init()
+
+      window.addEventListener('message', listener)
+    }
+  }, [data.widgetToken, data.widgetUserId, listener, scriptStatus])
+
+  return (
+    <div className='flex w-full flex-col rounded-2xl bg-page p-4 pb-8'>
+      <div className='flex justify-between'>
+        <h1 className='font-display text-2xl font-medium'>Debit card</h1>
+        <div className='hidden sm:flex'>
+          <Shape
+            width={'w-8'}
+            radius={'rounded-br-full'}
+            color={'bg-lime-300'}
+          />
+          <Shape
+            width={'w-8'}
+            radius={'rounded-br-full'}
+            color={'bg-slate-500'}
+          />
+        </div>
+      </div>
+      <p className='mt-6 text-medium'>
+        Please provide your debit card details.
+      </p>
+      <div id='widget' className='mt-6 w-full' />
+    </div>
+  )
+}
---
+++ typescript/protea/app/routes/linked-account.$type.tsx
@@ -0,0 +1,180 @@
+import type { LoaderArgs, ActionArgs } from '@remix-run/node'
+import { json, redirect } from '@remix-run/node'
+import { Form, useLoaderData } from '@remix-run/react'
+import { requireUserSession } from '~/lib/kratos.server'
+import { Button, Shape } from '~/components'
+import { route } from 'routes-gen'
+import {
+  flowType,
+  getCurrentFlow,
+  requireFlow,
+  updateFlow
+} from '~/lib/flows.server'
+import {
+  grpcClient,
+  httpMapping,
+  isGrpcError,
+  StatusError
+} from '~/lib/proto.server'
+
+export async function loader({ request, params }: LoaderArgs) {
+  await requireUserSession(request)
+  let headers
+
+  const response = await grpcClient
+    .hasSendUser(
+      {},
+      {
+        meta: {
+          cookies: String(request.headers.get('cookie')) || ''
+        }
+      }
+    )
+    .then((v) => v)
+    .catch(StatusError)
+  console.log(response)
+  if (isGrpcError(response)) {
+    throw json({}, httpMapping(response.code))
+  }
+
+  const hasSendUser = response.response.hasSendUser
+  if (!hasSendUser) {
+    headers = await requireFlow(request, flowType.PersonalDetails)
+  } else if (params.type == 'card') {
+    headers = await requireFlow(request, flowType.LinkCardAccount)
+  } else
+    throw json(
+      { title: `Linking type ${params.type} not allowed.` },
+      { status: 400 }
+    )
+
+  return json({ hasSendUser, type: params.type }, { headers })
+}
+
+export default function Page() {
+  const { type } = useLoaderData<typeof loader>()
+  switch (type) {
+    case 'card':
+      return <CardPage />
+    default:
+      return null
+  }
+}
+
+function CardPage() {
+  const { hasSendUser } = useLoaderData<typeof loader>()
+  return (
+    <div className='flex w-full flex-col rounded-2xl bg-page p-4 pb-8'>
+      <h1 className='mb-6 font-display text-2xl font-medium'>
+        Add a send account
+      </h1>
+      <span>Here’s what we will need to add a send account.</span>
+      {!hasSendUser && (
+        <>
+          <div className='mt-6 flex items-start'>
+            <Shape
+              width={'w-8'}
+              radius={'rounded-br-full'}
+              color={'bg-rose-300'}
+            />
+            <Shape
+              width={'w-8'}
+              radius={'rounded-full'}
+              color={'bg-lime-500'}
+            />
+            <div className='ml-5'>
+              <h3 className='mb-1 font-medium text-strong'>Personal details</h3>
+              <p className='text-sm text-medium'>Date of birth and gender.</p>
+            </div>
+          </div>
+          <div className='mt-10 flex items-start'>
+            <Shape
+              width={'w-8'}
+              radius={'rounded-full'}
+              color={'bg-yellow-300'}
+            />
+            <Shape
+              width={'w-8'}
+              radius={'rounded-bl-full'}
+              color={'bg-slate-300'}
+            />
+            <div className='ml-5'>
+              <h3 className='mb-1 font-medium text-strong'>Address details</h3>
+              <p className='text-sm text-medium'>Your physical address.</p>
+            </div>
+          </div>
+        </>
+      )}
+      <div className='mt-10 flex items-start'>
+        <Shape width={'w-8'} radius={'rounded-br-full'} color={'bg-lime-300'} />
+        <Shape
+          width={'w-8'}
+          radius={'rounded-br-full'}
+          color={'bg-slate-500'}
+        />
+        <div className='ml-5'>
+          <h3 className='mb-1 font-medium text-strong'>Debit card</h3>
+          <p className='text-sm text-medium'>Your debit card details.</p>
+        </div>
+      </div>
+
+      <Form
+        id='link-card'
+        action={'/linked-account/card'}
+        method='post'
+        className='hidden'
+      />
+      <input
+        form='link-card'
+        value={hasSendUser ? 'hasSendUser' : undefined}
+        name='hasSendUser'
+        type='hidden'
+      />
+      <div className='mt-12'>
+        <Button form='link-card' type='submit'>
+          Let's go
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export async function action({ request, params }: ActionArgs) {
+  const form = await request.formData()
+  const hasSendUser = form.get('hasSendUser') as string
+
+  console.log(hasSendUser)
+
+  let flow
+  if (!hasSendUser) {
+    flow = await getCurrentFlow(request, flowType.PersonalDetails)
+    const headers = await updateFlow(request, flowType.PersonalDetails, {
+      returnTo: route('/linked-account/:type/:flowId', {
+        type: params.type as string,
+        flowId: flow.id
+      })
+    })
+
+    return redirect(
+      route('/personal-details/:flowId/about', {
+        flowId: flow.id
+      }),
+      {
+        headers
+      }
+    )
+  } else if (params.type == 'card') {
+    flow = await getCurrentFlow(request, flowType.LinkCardAccount)
+  } else
+    throw json(
+      { title: `Linking type ${params.type} not allowed.` },
+      { status: 400 }
+    )
+
+  return redirect(
+    route('/linked-account/:type/:flowId', {
+      type: params.type,
+      flowId: flow.id
+    })
+  )
+}
