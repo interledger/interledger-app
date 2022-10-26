2015e68ff05c64782968fc7b06ddd9d92dbc1d0f
Cairin Michie <cairinmichie@gmail.com>
2022-10-26 12:48:45 +0200

    refactor(protea): address fetching code
    Matches fields needed by grpc endpoint.

--- typescript/protea/app/routes/api/maps/geocode.ts
+++ typescript/protea/app/routes/api/maps/geocode.ts
@@ -1,6 +1,7 @@
 import type { LoaderArgs } from '@remix-run/node'
 import { json } from '@remix-run/node'
 import { mapsClient } from '~/lib/maps.server'
+import type { AddressComponent } from '@googlemaps/google-maps-services-js'
 
 export async function loader({ request }: LoaderArgs) {
   const url = new URL(request.url)
@@ -18,24 +19,45 @@ export async function loader({ request }: LoaderArgs) {
       return r.data.results[0]
     })
     .catch((e) => {
-      console.log(e)
+      console.error(e)
       return e
     })
 
-  const addressComponents = place.address_components
+  const addressComponents: AddressComponent[] = place.address_components
 
   return json({
-    formattedAddress: place.formatted_address,
-    street: { id: placeId, name: place.formatted_address.split(',')[0] },
+    street: { id: placeId, name: place.formatted_address },
+    line1: `${
+      addressComponents.find((item: any) =>
+        item.types.includes('street_number')
+      )?.long_name
+    } ${
+      addressComponents.find((item: any) => item.types.includes('route'))
+        ?.long_name
+    }`,
+    line2: addressComponents.find((item: any) =>
+      item.types.includes('locality')
+    )?.long_name,
+    building: '',
     apartment: '',
     city: addressComponents.find((item: any) =>
       item.types.includes('administrative_area_level_2')
-    ).long_name,
-    state: addressComponents.find((item: any) =>
-      item.types.includes('administrative_area_level_1')
-    ).short_name,
-    zip: addressComponents.find((item: any) =>
+    )?.long_name,
+    state: `${
+      addressComponents.find((item: any) => item.types.includes('country'))
+        ?.short_name
+    }-${
+      addressComponents.find((item: any) =>
+        item.types.includes('administrative_area_level_1')
+      )?.short_name
+    }`,
+    zipCode: addressComponents.find((item: any) =>
       item.types.includes('postal_code')
-    ).short_name
+    )?.short_name,
+    countryCode: addressComponents.find((item: any) =>
+      item.types.includes('country')
+    )?.short_name,
+    placeID: placeId,
+    formattedAddress: place.formatted_address
   })
 }
