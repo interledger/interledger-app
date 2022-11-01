52ec60da402206b920aaf820a557dd9f609c698b
Donovan Changfoot <don@fynbos.dev>
2022-11-01 12:01:23 +0200

    fix(backend): return already exists if user tries to create more than one machnet wallet

--- go/backend/providers/machnet/errors.go
+++ go/backend/providers/machnet/errors.go
@@ -3,8 +3,9 @@ package machnet
 import "errors"
 
 var (
-	ErrInternal         = errors.New("machnet: internal error")
-	ErrNotFound         = errors.New("machnet: not found")
-	ErrIncompleteKYC    = errors.New("machnet: incomplete kyc information")
-	ErrInvalidSignature = errors.New("machnet: invalid signature")
+	ErrInternal              = errors.New("machnet: internal error")
+	ErrNotFound              = errors.New("machnet: not found")
+	ErrIncompleteKYC         = errors.New("machnet: incomplete kyc information")
+	ErrInvalidSignature      = errors.New("machnet: invalid signature")
+	ErrUserHasExistingWallet = errors.New("machnet: user has an existing wallet")
 )
