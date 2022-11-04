db76781ad99b815e17c1eeda4f4511d0451d443f
Matt de Haast <matt@fynbos.dev>
2022-11-04 14:16:57 +0200

    feat(protea): support serving files from cdn
    add support for production builds to be served from the CDN

--- typescript/protea/remix.config.js
+++ typescript/protea/remix.config.js
@@ -1,11 +1,14 @@
 /**
  * @type {import('@remix-run/dev/config').AppConfig}
  */
+
+const publicPath = process.env.NODE_ENV === 'production' ? 'https://cdn.fynbos.app/protea/public/' : '/build/'
+
 module.exports = {
   appDirectory: 'app',
   serverBuildTarget: 'node-cjs',
   assetsBuildDirectory: 'public/build',
-  publicPath: '/build/',
+  publicPath: publicPath,
   devServerPort: 8002,
   ignoredRouteFiles: ['.*', '**/*.draft.mdx'],
   sourcemap: true
