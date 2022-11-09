814270d1fc265682782592d7a1ff9aa64a80c1f4
Cairin Michie <cairinmichie@gmail.com>
2022-11-09 10:24:58 +0200

    feat(protea): add redis client

---
+++ typescript/protea/app/lib/redis.server.ts
@@ -0,0 +1,26 @@
+import type { RedisClientType } from '@redis/client'
+import { createClient } from '@redis/client'
+
+let redisClient: RedisClientType
+
+const url = 'redis://redis/'
+
+declare global {
+  var __redisClient: RedisClientType | undefined
+}
+
+// this is needed because in development we don't want to restart
+// the server with every change, but we want to make sure we don't
+// create a new connection to the Client with every change either.
+if (process.env.NODE_ENV === 'production') {
+  redisClient = createClient({ url })
+  redisClient.connect()
+} else {
+  if (!global.__redisClient) {
+    global.__redisClient = createClient({ url })
+    global.__redisClient.connect()
+  }
+  redisClient = global.__redisClient
+}
+
+export { redisClient }
