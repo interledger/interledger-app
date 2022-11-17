80d0d59338809341a9b7ab04aa34dda65cded7be
Cairin Michie <cairinmichie@gmail.com>
2022-11-17 15:02:07 +0200

    feat(protea): add qr code to pay page

--- typescript/protea/app/routes/pay.tsx
+++ typescript/protea/app/routes/pay.tsx
@@ -1,7 +1,7 @@
 import type { ActionArgs, LoaderArgs } from '@remix-run/node'
 import { json, redirect } from '@remix-run/node'
 import { Form, useActionData, useLoaderData } from '@remix-run/react'
-import { Button, Layouts, TextField } from '~/components'
+import { Button, Icon, Layouts, Snackbar, TextField } from '~/components'
 import { requireUserSession } from '~/lib/kratos.server'
 import { flowType, requireFlow, updateFlow } from '~/lib/flows.server'
 import { route } from 'routes-gen'
@@ -12,12 +12,18 @@ import {
   openPaymentsClient,
   StatusError
 } from '~/lib/proto.server'
+import { getWalletPaymentPointer } from '~/lib/wallet.server'
+import { generateQR, qrSvg } from '~/lib/qr.server'
+import { useState } from 'react'
 
 export async function loader({ request }: LoaderArgs) {
   await requireUserSession(request)
   const flow = await requireFlow(request, flowType.Pay)
+  const paymentPointer = await getWalletPaymentPointer(request)
 
-  return json({ flow })
+  const paymentPointerQR = qrSvg(await generateQR(paymentPointer.url))
+
+  return json({ flow, paymentPointer, paymentPointerQR })
 }
 
 export const handle = {
@@ -25,40 +31,101 @@ export const handle = {
 }
 
 export default function Page() {
-  const { flow } = useLoaderData<typeof loader>()
+  const { flow, paymentPointer, paymentPointerQR } =
+    useLoaderData<typeof loader>()
   const actionData = useActionData<typeof action>()
-  return (
-    <div className='flex w-full flex-col rounded-2xl bg-page p-4 pb-8'>
-      <h1 className='mb-6 font-display text-2xl font-medium'>Pay</h1>
-      <span>Enter the recipient’s payment pointer.</span>
-      <Form
-        id='pay-payment-pointer'
-        action='/pay'
-        method='post'
-        className='hidden'
-      />
-      <TextField
-        id='paymentPointer'
-        label='Payment pointer'
-        name='paymentPointer'
-        form='pay-payment-pointer'
-        type='text'
-        className='mt-6'
-        defaultValue={flow.data?.paymentPointer?.url}
-        aria-invalid={Boolean(actionData?.errors.paymentPointer) || undefined}
-        aria-describedby={
-          actionData?.errors.paymentPointer ? 'paymentPointer-error' : undefined
-        }
-        required
-        errorMessage={actionData?.errors.paymentPointer}
-      />
 
-      <div className='mt-6'>
-        <Button form='pay-payment-pointer' type='submit'>
-          Pay
-        </Button>
+  const [snackbar, setSnackbar] = useState<any>({
+    message: '',
+    action: '',
+    icon: 'close',
+    show: false
+  })
+  const [showSnackbar, setShowSnackbar] = useState<boolean>(
+    snackbar.show ?? false
+  )
+
+  return (
+    <>
+      <div className='flex w-full flex-col rounded-2xl bg-page p-4 pb-8'>
+        <h1 className='mb-6 font-display text-2xl font-medium'>Pay</h1>
+        <span>Enter the recipient’s payment pointer.</span>
+        <Form
+          id='pay-payment-pointer'
+          action='/pay'
+          method='post'
+          className='hidden'
+        />
+        <TextField
+          id='paymentPointer'
+          label='Payment pointer'
+          name='paymentPointer'
+          form='pay-payment-pointer'
+          type='text'
+          className='mt-6'
+          defaultValue={flow.data?.paymentPointer?.url}
+          aria-invalid={Boolean(actionData?.errors.paymentPointer) || undefined}
+          aria-describedby={
+            actionData?.errors.paymentPointer
+              ? 'paymentPointer-error'
+              : undefined
+          }
+          required
+          errorMessage={actionData?.errors.paymentPointer}
+        />
+
+        <div className='mt-6'>
+          <Button form='pay-payment-pointer' type='submit'>
+            Pay
+          </Button>
+        </div>
       </div>
-    </div>
+      <div className='mt-6 flex w-full flex-col rounded-2xl bg-page p-4 pb-8'>
+        <h1 className='mb-6 font-display text-2xl font-medium'>Receive</h1>
+        <span>Present or share your payment pointer.</span>
+
+        <div
+          className='mt-8 sm:px-16'
+          dangerouslySetInnerHTML={{ __html: paymentPointerQR }}
+        />
+        <button
+          type='button'
+          onClick={async () => {
+            navigator.clipboard.writeText(paymentPointer.formatted).then(
+              () => {
+                setSnackbar({
+                  message: 'Payment pointer copied to clipboard.',
+                  icon: 'close'
+                })
+                setShowSnackbar(true)
+              },
+              () => {
+                setSnackbar({
+                  message: "Couldn't copy to clipboard.",
+                  icon: 'close'
+                })
+                setShowSnackbar(true)
+              }
+            )
+          }}
+          className='mt-8 flex flex items-center justify-between rounded-xl bg-container p-4 hover:bg-container-hover'
+        >
+          <span className='font-medium text-medium'>
+            {paymentPointer.formatted}
+          </span>
+          <Icon className='text-medium'>content_copy</Icon>
+        </button>
+      </div>
+      <Snackbar
+        message={snackbar.message}
+        action={snackbar.action}
+        icon={snackbar.icon}
+        show={showSnackbar}
+        id='cookie-snackbar'
+        dismissAfter={3000}
+        onClose={() => setShowSnackbar(false)}
+      />
+    </>
   )
 }
 
