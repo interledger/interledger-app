f7f046eefa487fbab132e6ec9347046484f3523e
Cairin Michie <cairinmichie@gmail.com>
2022-11-30 11:43:34 +0200

    fix(protea): linked accounts settings page

--- typescript/protea/app/routes/settings.linked-accounts.tsx
+++ typescript/protea/app/routes/settings.linked-accounts.tsx
@@ -10,52 +10,12 @@ import {
   Router,
   WalletGrid
 } from '~/components'
-import {
-  grpcClient,
-  httpMapping,
-  isGrpcError,
-  StatusError
-} from '~/lib/proto.server'
 import { requireUserSession } from '~/lib/kratos.server'
+import { getLinkedAccounts } from '~/lib/wallet.server'
 
 export async function loader({ request }: LoaderArgs) {
   await requireUserSession(request)
-  const cookie = String(request.headers.get('cookie'))
-
-  const response = await grpcClient
-    .getLinkedAccounts(
-      {},
-      {
-        meta: {
-          cookies: cookie || ''
-        }
-      }
-    )
-    .then((v) => v)
-    .catch(StatusError)
-  if (isGrpcError(response)) {
-    throw json({}, httpMapping(response.code))
-  }
-
-  console.log(response.response.linkedAccounts)
-  const linkedAccounts = response.response.linkedAccounts
-    .filter((account) => account.type != 'wallet')
-    .map((linkedAccount) => ({
-      id: linkedAccount.id,
-      name:
-        linkedAccount.type == 'sendCard'
-          ? `Card ending ${linkedAccount.mask}`
-          : 'Bank account',
-      type: linkedAccount.type == 'sendCard' ? 'send' : 'receive',
-      icon: linkedAccount.type == 'sendCard' ? 'credit_card' : 'account_balance'
-    }))
-
-  return json({
-    linkedAccounts: linkedAccounts,
-    hasSend: linkedAccounts.filter(({ type }) => type == 'send').length > 0,
-    hasReceive:
-      linkedAccounts.filter(({ type }) => type == 'receive').length > 0
-  })
+  return json(await getLinkedAccounts(request))
 }
 
 export const handle = {
@@ -63,25 +23,15 @@ export const handle = {
 }
 
 export default function Page() {
-  const { linkedAccounts, hasSend, hasReceive } = useLoaderData<typeof loader>()
+  const { linkedAccounts, canTopUp, canWithdraw } =
+    useLoaderData<typeof loader>()
 
   return (
     <WalletGrid>
       <div className='col-span-full flex flex-col rounded-2xl bg-page p-4 pb-6 sm:col-span-6 sm:col-start-2 lg:col-start-4'>
         <h1 className='font-display text-2xl font-medium'>Linked accounts</h1>
         {linkedAccounts.length == 0 && (
-          <>
-            <p className='mt-6'>
-              You currently do not have any linked accounts.
-            </p>
-
-            <div className='mt-6 flex items-center space-x-3 rounded-xl bg-container-secondary p-4 text-medium'>
-              <Icon>tips_and_updates</Icon>
-              <p className='text-sm'>
-                Enable sending or receiving in order to transact.
-              </p>
-            </div>
-          </>
+          <p className='mt-6'>You do not have any linked accounts.</p>
         )}
 
         {linkedAccounts &&
@@ -102,11 +52,11 @@ export default function Page() {
                 </div>
               </div>
               <div className='flex items-center space-x-3 '>
-                {method.type == 'send' && (
+                {method.type == 'card' && (
                   <Chip color={ChipColor.orange}>Send</Chip>
                 )}
-                {method.type == 'receive' && (
-                  <Chip color={ChipColor.purple}>Receive</Chip>
+                {method.type == 'bank' && (
+                  <Chip color={ChipColor.purple}>Withdraw</Chip>
                 )}
                 <Icon>navigate_next</Icon>
               </div>
@@ -116,49 +66,51 @@ export default function Page() {
           // TODO: Create route for choosing what type of linked account to add
           <Router
             className='mt-6 text-sm font-medium text-primary'
-            to={route('/linked-account/:type', { type: 'new' })}
+            to={route('/linked-account/:type/widget', { type: 'new' })}
           >
             Link another account
           </Router>
         )}
       </div>
-      {!hasSend && (
+      {!canTopUp && (
         <div className='col-span-full flex flex-col space-y-6 rounded-2xl bg-page p-4 pb-6 sm:col-span-6 sm:col-start-2 lg:col-start-4'>
           <div className='flex items-start space-x-4'>
             <div className='flex items-center justify-between rounded-full bg-container p-5 text-medium'>
               <Icon>credit_card</Icon>
             </div>
             <div className='flex flex-col space-y-2'>
-              <h1 className='font-medium text-medium'>Send money</h1>
+              <h1 className='font-medium text-medium'>Add a debit card</h1>
               <p className='text-sm text-medium'>
-                Easily send money from a debit card.
+                Add a debit card to easily send payments or top up your cash
+                balance.
               </p>
               <Router
                 className='text-sm font-medium text-primary'
                 to={route('/linked-account/:type', { type: 'card' })}
               >
-                Enable sending
+                Add a debit card
               </Router>
             </div>
           </div>
         </div>
       )}
-      {!hasReceive && (
+      {!canWithdraw && (
         <div className='col-span-full flex flex-col space-y-6 rounded-2xl bg-page p-4 pb-6 sm:col-span-6 sm:col-start-2 lg:col-start-4'>
           <div className='flex items-start space-x-4'>
             <div className='flex items-center justify-between rounded-full bg-container p-5 text-medium'>
               <Icon>account_balance</Icon>
             </div>
             <div className='flex flex-col space-y-2'>
-              <h1 className='font-medium text-medium'>Receive money</h1>
+              <h1 className='font-medium text-medium'>Add a bank account</h1>
               <p className='text-sm text-medium'>
-                Receive money into your bank account securely.
+                Add a bank account to securely withdraw from your cash balance
+                at any time.
               </p>
               <Router
                 className='text-sm font-medium text-primary'
                 to={route('/linked-account/:type', { type: 'bank' })}
               >
-                Enable receiving
+                Add a bank account
               </Router>
             </div>
           </div>
