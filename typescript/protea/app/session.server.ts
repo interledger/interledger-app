c9e0e416924146d40703cd7637a78e5211a45716
Cairin Michie <cairinmichie@gmail.com>
2022-12-05 11:56:08 +0200

    sync test maxage

--- typescript/protea/app/session.server.ts
+++ typescript/protea/app/session.server.ts
@@ -2,20 +2,22 @@ import { createCookie, createSessionStorage } from '@remix-run/node'
 import { redisClient } from '~/lib/redis.server'
 import { v4 } from 'uuid'
 
-const EXPIRATION_DURATION_IN_SECONDS = 60 * 60 * 24 // a day
+const EXPIRATION_DURATION_IN_SECONDS = 10 //60 * 60 * 24 // a day
 
 const cookie = createCookie('user_settings', {
   httpOnly: true,
   path: '/',
   secrets: ['TODO:secrets'],
   sameSite: 'lax', //true,
-  expires: new Date(Date.now() + EXPIRATION_DURATION_IN_SECONDS * 1000)
+  maxAge: EXPIRATION_DURATION_IN_SECONDS
+  // expires: new Date(Date.now() + EXPIRATION_DURATION_IN_SECONDS * 1000)
 })
 
 export const { getSession, commitSession, destroySession } =
   createSessionStorage({
     cookie,
     async createData(data, expires) {
+      console.log('CREATE', data, expires)
       const id = v4()
       await redisClient.set(id, JSON.stringify(data), {
         PXAT: expires?.valueOf(),
@@ -25,16 +27,19 @@ export const { getSession, commitSession, destroySession } =
     },
     async readData(id) {
       const data = await redisClient.get(id)
+      console.log('READ', data)
       if (data == null) return null
       return JSON.parse(data)
     },
     async updateData(id, data, expires) {
+      console.log('UPDATE', data, expires)
       await redisClient.set(id, JSON.stringify(data), {
         PXAT: expires?.valueOf(),
         XX: true
       })
     },
     async deleteData(id) {
+      console.log('DELETE')
       await redisClient.del(id)
     }
   })
