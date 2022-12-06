dac79d29b85a081898e2c27ec333bbf814f90431
Cairin Michie <cairinmichie@gmail.com>
2022-12-06 14:38:57 +0200

    feat(protea): select-none on icons

--- typescript/protea/app/components/icons.tsx
+++ typescript/protea/app/components/icons.tsx
@@ -9,7 +9,7 @@ export const Icon: FC<IconProps> = ({ className, children }) => {
   return (
     <span
       className={clsx(
-        'inline-block h-6 w-6 whitespace-nowrap fill-current text-center font-icon text-2xl font-normal normal-case not-italic leading-6 tracking-normal antialiased',
+        'inline-block h-6 w-6 select-none whitespace-nowrap fill-current text-center font-icon text-2xl font-normal normal-case not-italic leading-6 tracking-normal antialiased',
         className
       )}
     >
