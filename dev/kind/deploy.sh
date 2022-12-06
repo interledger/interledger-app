76307cf7d931d6678ffe8e8a6ea5efc90dddf60a
Matt de Haast <matt@fynbos.dev>
2022-12-06 10:23:31 +0200

    feat(dev-env): update to latest temporal
    moved away from temporalite and to temporal

--- dev/kind/deploy.sh
+++ dev/kind/deploy.sh
@@ -10,4 +10,4 @@ ROOIBOS_DIR=$(dirname "$0")/../../../rooibos
 [ -d $ROOIBOS_DIR/backend/envs/local ] && while ! kubectl apply -k $ROOIBOS_DIR/backend/envs/local; do echo "Retrying to apply backend resources in 5s."; sleep 5; done
 [ -d $ROOIBOS_DIR/kratos/envs/local ] && while ! kubectl apply -k $ROOIBOS_DIR/kratos/envs/local; do echo "Retrying to apply kratos resources in 5s."; sleep 5; done
 [ -d $ROOIBOS_DIR/protea/envs/local ] && while ! kubectl apply -k $ROOIBOS_DIR/protea/envs/local; do echo "Retrying to apply protea resources in 5s."; sleep 5; done
-[ -d $ROOIBOS_DIR/temporalite/envs/local ] && while ! kubectl apply -k $ROOIBOS_DIR/temporalite/envs/local; do echo "Retrying to apply temporal resources in 5s."; sleep 5; done
+[ -d $ROOIBOS_DIR/temporal/envs/local ] && while ! kubectl apply -k $ROOIBOS_DIR/temporal/envs/local; do echo "Retrying to apply temporal resources in 5s."; sleep 5; done
--- dev/kind/run.sh
+++ dev/kind/run.sh
@@ -16,3 +16,17 @@ docker push localhost:5005/protea:latest
 docker pull 823058932981.dkr.ecr.eu-west-1.amazonaws.com/temporalite
 docker tag 823058932981.dkr.ecr.eu-west-1.amazonaws.com/temporalite localhost:5005/temporalite
 docker push localhost:5005/temporalite
+
+# Pull and push temporal
+docker pull 823058932981.dkr.ecr.eu-west-1.amazonaws.com/temporalio/auto-setup:1.18.5
+docker tag 823058932981.dkr.ecr.eu-west-1.amazonaws.com/temporalio/auto-setup:1.18.5 localhost:5005/temporalio/auto-setup
+docker push localhost:5005/temporalio/auto-setup
+docker pull 823058932981.dkr.ecr.eu-west-1.amazonaws.com/temporalio/ui:2.9.0
+docker tag 823058932981.dkr.ecr.eu-west-1.amazonaws.com/temporalio/ui:2.9.0 localhost:5005/temporalio/ui
+docker push localhost:5005/temporalio/ui
+docker pull 823058932981.dkr.ecr.eu-west-1.amazonaws.com/temporalio/admin-tools:1.18.5
+docker tag 823058932981.dkr.ecr.eu-west-1.amazonaws.com/temporalio/admin-tools:1.18.5 localhost:5005/temporalio/admin-tools
+docker push localhost:5005/temporalio/admin-tools
+docker pull 823058932981.dkr.ecr.eu-west-1.amazonaws.com/temporalio/server:1.18.5
+docker tag 823058932981.dkr.ecr.eu-west-1.amazonaws.com/temporalio/server:1.18.5 localhost:5005/temporalio/server
+docker push localhost:5005/temporalio/server
