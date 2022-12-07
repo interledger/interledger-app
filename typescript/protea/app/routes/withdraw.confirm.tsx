f8eca233ac971575f38951617b7d4969ca1eab09
Cairin Michie <cairinmichie@gmail.com>
2022-12-07 17:06:31 +0200

    feat(protea): withdraw page

---
+++ typescript/protea/app/routes/withdraw.confirm.tsx
@@ -0,0 +1,123 @@
+import type { ActionArgs, LoaderArgs } from '@remix-run/node'
+import { json, redirect } from '@remix-run/node'
+import { Form, useLoaderData } from '@remix-run/react'
+import { Button, Layouts } from '~/components'
+import { exitFlow, flowType, requireFlow } from '~/lib/flows.server'
+import { route } from 'routes-gen'
+import {
+  grpcClient,
+  httpMapping,
+  isGrpcError,
+  StatusError
+} from '~/lib/proto.server'
+import { getClientIP } from '~/lib/ip.server'
+import { getLinkedAccounts } from '~/lib/wallet.server'
+
+export async function loader({ request }: LoaderArgs) {
+  const flow = await requireFlow(request, flowType.Withdraw)
+  const { linkedAccounts } = await getLinkedAccounts(request)
+  console.log(
+    linkedAccounts.find((account) => account.id == flow.data.toLinkedAccountId)
+  )
+  return json({
+    flow,
+    linkedAccount: linkedAccounts.find(
+      (account) => account.id == flow.data.toLinkedAccountId
+    )
+  })
+}
+
+export const handle = {
+  layout: Layouts.FocusLayout
+}
+
+export default function Page() {
+  const { flow, linkedAccount } = useLoaderData<typeof loader>()
+
+  return (
+    <>
+      <div className='flex w-full flex-col rounded-2xl bg-page p-4 pb-8'>
+        <h1 className='mb-6 font-display text-2xl font-medium'>
+          Confirm withdrawal
+        </h1>
+        <span>Please check the details and confirm your withdrawal.</span>
+
+        <div className='mt-6 flex w-full flex-col space-y-1'>
+          <span className='text-sm'>Withdraw to:</span>
+          <span className='text-sm'>{linkedAccount?.name}</span>
+        </div>
+        <div className='mt-6 flex w-full justify-between'>
+          <span className='text-sm'>You pay</span>
+          <span className='text-sm font-medium text-strong'>
+            {flow?.data.displaySendAmount || '$ 0.00'}
+          </span>
+        </div>
+        <div className='mt-2 flex w-full justify-between'>
+          <span className='text-sm'>Total fees</span>
+          <span className='text-sm font-medium text-strong'>
+            free <sup>*</sup>
+          </span>
+        </div>
+        <div className='mt-2 flex w-full justify-between'>
+          <span className='text-sm'>They receive</span>
+          <span className='text-sm text-2xl font-medium text-strong'>
+            {flow?.data.displayReceiveAmount || '$ 0.00'}
+          </span>
+        </div>
+
+        <Form
+          id='withdraw-confirm'
+          action='/withdraw/confirm'
+          method='post'
+          className='hidden'
+        />
+        <div className='mt-6'>
+          <Button form='withdraw-confirm' type='submit'>
+            Confirm
+          </Button>
+        </div>
+      </div>
+      <div className='mt-6 flex w-full space-x-2'>
+        <span className='text-xs text-medium'>*</span>
+        <span className='text-xs text-medium'>
+          For a limited time, Fynbos will absorb the fees associated with making
+          a withdrawal.
+        </span>
+      </div>
+    </>
+  )
+}
+
+export async function action({ request }: ActionArgs) {
+  const flow = await requireFlow(request, flowType.Withdraw)
+
+  const clientIpAddress = getClientIP(request)
+
+  console.log({
+    toLinkedAccountId: flow.data.toLinkedAccountId,
+    amount: flow.data.amount,
+    ipAddress: clientIpAddress
+  })
+
+  const response = await grpcClient
+    .withdrawFromMachnetWallet(
+      {
+        toLinkedAccountId: flow.data.toLinkedAccountId,
+        amount: flow.data.receiveAmount,
+        ipAddress: clientIpAddress
+      },
+      {
+        meta: {
+          cookies: String(request.headers.get('cookie')) || ''
+        }
+      }
+    )
+    .then((v) => v)
+    .catch(StatusError)
+  if (isGrpcError(response)) {
+    console.log(response)
+    throw json({}, httpMapping(response.code))
+  }
+  await exitFlow(request, flowType.Withdraw)
+  return redirect(route('/'))
+}
