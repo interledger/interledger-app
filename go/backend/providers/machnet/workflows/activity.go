f16a0214652170df5409259b4c91e3214c99da43
barnard <barnard@fynbos.dev>
2022-12-12 14:30:57 +0200

    feat(backends): Post to transactions from workflows

--- go/backend/providers/machnet/workflows/activity.go
+++ go/backend/providers/machnet/workflows/activity.go
@@ -7,8 +7,6 @@ import (
 	"regexp"
 	"strings"
 
-	"gitlab.com/fynbos/backend/transactions"
-
 	"gitlab.com/fynbos/backend/user"
 
 	"gitlab.com/fynbos/backend/linkedaccounts"
@@ -343,10 +341,6 @@ func (a *Activity) WithdrawFromWallet(ctx context.Context, args machnet.Withdraw
 		ToLinkedAccountID: args.ToLinkedAccountID,
 		Status:            withdrawal.Status,
 	}, nil
-}
-
-func (a *Activity) AddTransactionTransfer(ctx context.Context, args transactions.CreateTransferArgs) error {
-	return a.b.Transactions().AddTransfer(ctx, args)
 }
 
 type StartWalletTransferArgs struct {
