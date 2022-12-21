74fd099055b7b05ff875467054f5b1016d3c199c
barnard <barnard@fynbos.dev>
2022-12-21 11:34:31 +0200

    feat(backends): Add currency package

---
+++ go/backend/currency/amount.go
@@ -0,0 +1,135 @@
+package currency
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+
+	pb "gitlab.com/fynbos/proto/backend/v1"
+
+	"gitlab.com/fynbos/log"
+	"go.uber.org/zap"
+)
+
+type Currency string
+
+func ParseCurrency(cc string) Currency {
+	return Currency(cc)
+}
+
+func (c Currency) String() string {
+	return string(c)
+}
+
+func (c Currency) Valid() bool {
+	_, ok := currencyScale[c]
+	return ok
+}
+
+func (c Currency) Scale() int {
+	scale, ok := currencyScale[c]
+	if !ok {
+		log.Warn("unknown scale for currency", zap.String("currency", string(c)))
+		return 2 // Default to cent scale
+	}
+
+	return scale
+}
+
+const (
+	USD Currency = "USD"
+)
+
+var currencyScale = map[Currency]int{
+	USD: 2,
+}
+
+var currencyFormat = map[Currency]string{
+	USD: "$%s",
+}
+
+type Amount struct {
+	Value    uint64   `validate:"gt=0" json:"value,string"`
+	Currency Currency `validate:"iso4217"  json:"assetCode"`
+	Scale    int      `validate:"gt=0" json:"assetScale"`
+}
+
+// FormatAmount returns the amount in the scale as representative of a human readable format.
+// i.e. Amount { value: 1000, scale: 2} returns "10.00"
+func (a *Amount) FormatAmount() string {
+	if a == nil {
+		return ""
+	}
+	scale := a.Scale
+	if a.Scale <= 0 {
+		// Fallback to double-check that the scale actually is 0
+		scale = a.Currency.Scale()
+	}
+
+	return strconv.FormatFloat(a.Float64(), 'f', scale, 64)
+}
+
+// Format returns the amount with its specified formatting
+// i.e. Amount { value: 1000, scale: 2, currency: USD} returns "$10.00"
+func (a *Amount) Format() string {
+	if a == nil {
+		return ""
+	}
+
+	format, ok := currencyFormat[a.Currency]
+	if !ok {
+		format = "%s " + a.Currency.String()
+	}
+
+	return fmt.Sprintf(format, a.FormatAmount())
+}
+
+// Float64 returns the floating point representation of the amount in the scale defined
+// i.e. Amount { value: 1000, scale: 2, currency: USD} returns 10.0
+func (a *Amount) Float64() float64 {
+	if a == nil {
+		return 0.00
+	}
+
+	scale := float64(a.Scale)
+	if a.Scale <= 0 {
+		// Fallback to double-check that the scale actually is 0
+		scale = float64(a.Currency.Scale())
+	}
+
+	amnt := float64(a.Value)
+	if scale > 0 {
+		amnt /= math.Pow(10, scale)
+	}
+
+	return amnt
+}
+
+func (a *Amount) ToPB() *pb.Amount {
+	if a == nil {
+		return nil
+	}
+	return &pb.Amount{
+		Amount:     a.Value,
+		Asset:      a.Currency.String(),
+		AssetScale: int32(a.Scale),
+	}
+}
+
+func FromPB(a *pb.Amount) Amount {
+	return Amount{
+		Value:    a.GetAmount(),
+		Currency: Currency(a.GetAsset()),
+		Scale:    int(a.GetAssetScale()),
+	}
+}
+
+func FromFloat(f float64, cc Currency) Amount {
+	amt := uint64(f * math.Pow(10, float64(cc.Scale())))
+
+	return Amount{
+		Value:    amt,
+		Currency: cc,
+		Scale:    cc.Scale(),
+	}
+}
---
+++ go/backend/currency/amount_test.go
@@ -0,0 +1,209 @@
+package currency_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"gitlab.com/fynbos/backend/currency"
+)
+
+func TestCurrency(t *testing.T) {
+	t.Parallel()
+	cases := []struct {
+		cc    string
+		valid bool
+		scale int
+	}{
+		{
+			cc:    "USD",
+			valid: true,
+			scale: 2,
+		},
+		{
+			cc:    "ZAR",
+			valid: false,
+			scale: 2,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(fmt.Sprintf("currenct=%s", tc.cc), func(t *testing.T) {
+			c := currency.ParseCurrency(tc.cc)
+			assert.Equal(t, tc.valid, c.Valid())
+			assert.Equal(t, tc.scale, c.Scale())
+		})
+	}
+}
+
+func TestAmount_Float64(t *testing.T) {
+	t.Parallel()
+	cases := []struct {
+		name string
+		in   currency.Amount
+		out  float64
+	}{
+		{
+			name: "currency and scale",
+			in: currency.Amount{
+				Value:    1000,
+				Currency: currency.USD,
+				Scale:    2,
+			},
+			out: 10.00,
+		},
+		{
+			name: "scale omitted",
+			in: currency.Amount{
+				Value:    1000,
+				Currency: currency.USD,
+			},
+			out: 10.00,
+		},
+		{
+			name: "currency and scale omitted",
+			in: currency.Amount{
+				Value: 1000,
+			},
+			out: 10.00, // Default to 2 decimal points
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			assert.Equal(t, tc.out, tc.in.Float64())
+		})
+	}
+}
+
+func TestAmount_FormatAmount(t *testing.T) {
+	t.Parallel()
+	cases := []struct {
+		name string
+		in   currency.Amount
+		out  string
+	}{
+		{
+			name: "currency and scale",
+			in: currency.Amount{
+				Value:    1000,
+				Currency: currency.USD,
+				Scale:    2,
+			},
+			out: "10.00",
+		},
+		{
+			name: "scale omitted",
+			in: currency.Amount{
+				Value:    1000,
+				Currency: currency.USD,
+			},
+			out: "10.00",
+		},
+		{
+			name: "currency and scale omitted",
+			in: currency.Amount{
+				Value: 1000,
+			},
+			out: "10.00", // Default to 2 decimal points
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			assert.Equal(t, tc.out, tc.in.FormatAmount())
+		})
+	}
+}
+
+func TestAmount_Format(t *testing.T) {
+	t.Parallel()
+	cases := []struct {
+		name string
+		in   currency.Amount
+		out  string
+	}{
+		{
+			name: "currency and scale",
+			in: currency.Amount{
+				Value:    1000,
+				Currency: currency.USD,
+				Scale:    2,
+			},
+			out: "$10.00",
+		},
+		{
+			name: "scale omitted",
+			in: currency.Amount{
+				Value:    1000,
+				Currency: currency.USD,
+			},
+			out: "$10.00",
+		},
+		{
+			name: "currency without a format",
+			in: currency.Amount{
+				Value:    1000,
+				Currency: currency.ParseCurrency("ZAR"),
+			},
+			out: "10.00 ZAR", // Default to 2 decimal points
+		},
+		{
+			name: "currency without a format scale provided",
+			in: currency.Amount{
+				Value:    1000,
+				Scale:    8,
+				Currency: currency.ParseCurrency("BTC"),
+			},
+			out: "0.00001000 BTC",
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			assert.Equal(t, tc.out, tc.in.Format())
+		})
+	}
+}
+
+func TestFromFloat(t *testing.T) {
+	t.Parallel()
+	cases := []struct {
+		name    string
+		in      float64
+		inCC    string
+		format  string
+		float64 float64
+	}{
+		{
+			name:    "10 USD",
+			in:      10.00,
+			inCC:    "USD",
+			format:  "$10.00",
+			float64: 10.00,
+		},
+		{
+			name:    "too many decimals",
+			in:      10.0012,
+			inCC:    "USD",
+			format:  "$10.00",
+			float64: 10.00,
+		},
+		{
+			name:    "unknown currency",
+			in:      10.00,
+			inCC:    "ZAR",
+			format:  "10.00 ZAR",
+			float64: 10.00,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			a := currency.FromFloat(tc.in, currency.ParseCurrency(tc.inCC))
+
+			assert.Equal(t, tc.float64, a.Float64())
+			assert.Equal(t, tc.format, a.Format())
+		})
+	}
+}
