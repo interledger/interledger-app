1366232d07b376ef78a0e90495d4eca5cdd64dc2
Donovan Changfoot <don@fynbos.dev>
2023-01-04 15:04:48 +0200

    fix(backend): `start` uses `DB_URL` env variable
    Use `DB_URL` variable which should be the url to pgbouncer

--- go/backend/cli/cli.go
+++ go/backend/cli/cli.go
@@ -103,7 +103,7 @@ func ParseStartArgs() (*StartArgs, error) {
 	if openPaymentsPort == "" {
 		openPaymentsPort = "8081"
 	}
-	dbUrl := os.Getenv("DB_URL_WITH_CERTS")
+	dbUrl := os.Getenv("DB_URL")
 	if dbUrl == "" {
 		dbUrl = "cockroach://backend@cockroachdb-public:26257/backend?sslmode=verify-full&sslrootcert=/cockroach-certs/ca.crt&sslcert=/cockroach-certs/client.backend.crt&sslkey=/cockroach-certs/client.backend.key&max_conns=20&max_idle_conns=4"
 	}
