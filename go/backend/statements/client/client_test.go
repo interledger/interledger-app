66903595629eb2ca118715fa1e5a8faa38970edb
Donovan Changfoot <don@fynbos.dev>
2023-01-10 12:02:58 +0200

    refactor(backend): use gopdf to generate pdf imperatively
    `wkhtmltopdf` required the use of WtWebkit. This was not feasible to
    add to our docker image since we use the `distroless` image in production.

--- go/backend/statements/client/client_test.go
+++ go/backend/statements/client/client_test.go
@@ -2,27 +2,45 @@ package client_test
 
 import (
 	"context"
+	"log"
 	"os"
 	"testing"
+	"time"
 
+	"gitlab.com/fynbos/backend/currency"
 	"gitlab.com/fynbos/backend/providers/machnet"
 	"gitlab.com/fynbos/backend/statements/client"
 	"gitlab.com/fynbos/backend/transactions"
 )
 
 func TestGenerateWalletStatement(t *testing.T) {
-	t.Skip("Needs wkhtmltopdf to be installed on your machine.")
 	statements := client.New()
 
 	pdf, err := statements.GenerateWalletStatementPDF(context.Background(), &machnet.Wallet{
 		AvailableBalance: 1000,
-	}, []transactions.Transaction{})
+	}, []transactions.Transaction{
+		{
+			Amount: currency.Amount{
+				Value:    10,
+				Currency: currency.Currency("USD"),
+			},
+			Timestamp: time.Now(),
+		},
+	})
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	err = os.WriteFile("./statement.pdf", pdf, 0644)
+	tempPdf, err := os.CreateTemp("", "tmpfile-")
 	if err != nil {
 		t.Fatal(err)
+	}
+	t.Cleanup(func() {
+		// comment out if you want to view the pdf.
+		_ = os.Remove(tempPdf.Name())
+	})
+
+	if _, err := tempPdf.Write(pdf); err != nil {
+		log.Fatal(err)
 	}
 }
