aa708c70369ec7d381c42024d6dd19e09ee4c75d
Cairin Michie <cairinmichie@gmail.com>
2023-01-12 17:24:54 +0200

    feat(protea): init storybook
    - Installs storybook with vite-builder
    - Installs unit tests with vitest
    - Provides two use case POCs

---
+++ typescript/protea/app/components/Buttons.stories.tsx
@@ -0,0 +1,39 @@
+import type { StoryFn, Meta } from '@storybook/react'
+import { unstable_createRemixStub as createRemixStub } from '@remix-run/testing'
+import { Button } from '~/components'
+
+const meta: Meta<typeof Button> = {
+  title: 'components/Button',
+  component: Button,
+  argTypes: { onClick: { action: 'clicked' } },
+  decorators: [
+    (Story) => {
+      const RemixStub = createRemixStub([
+        {
+          path: '/',
+          element: <Story />
+        }
+      ])
+
+      return <RemixStub />
+    }
+  ]
+}
+
+export default meta
+
+const Template: StoryFn<typeof Button> = (_args) => (
+  <Button {..._args}>{_args.children}</Button>
+)
+
+export const ButtonStory = Template.bind({})
+ButtonStory.storyName = 'Default Button'
+ButtonStory.args = {
+  children: 'Basic button'
+}
+
+export const ShrunkButton = Template.bind({})
+ShrunkButton.args = {
+  children: 'Another',
+  shrink: true
+}
