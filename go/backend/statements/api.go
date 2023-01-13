b9c9918fcc1bbcf5c71b3c35190cc21d0acb7c15
Donovan Changfoot <don@fynbos.dev>
2023-01-13 11:56:09 +0200

    feat(backend): use pdf template and add table to it

--- go/backend/statements/api.go
+++ go/backend/statements/api.go
@@ -3,14 +3,10 @@ package statements
 import (
 	"context"
 
-	"gitlab.com/fynbos/backend/db"
 	"gitlab.com/fynbos/backend/providers/machnet"
 	"gitlab.com/fynbos/backend/transactions"
 )
 
 type Client interface {
 	GenerateWalletStatementPDF(ctx context.Context, wallet *machnet.Wallet, transactions []transactions.Transaction) ([]byte, error)
-	Store(ctx context.Context, statement Statement) error
-	List(ctx context.Context, page db.Pagination) ([]Statement, error)
-	GetSignedURL(ctx context.Context, id string) (*Statement, error)
 }
