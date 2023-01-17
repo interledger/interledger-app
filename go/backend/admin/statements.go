7e86a50e28256460d12815be7c6ae5c5145c9455
Donovan Changfoot <don@fynbos.dev>
2023-01-17 12:08:54 +0200

    fix(backend): search for machnet wallet when emailing statement

--- go/backend/admin/statements.go
+++ go/backend/admin/statements.go
@@ -6,22 +6,52 @@ import (
 	"time"
 
 	"gitlab.com/fynbos/backend/email"
+	"gitlab.com/fynbos/backend/linkedaccounts"
+	"gitlab.com/fynbos/backend/providers/machnet"
 	adminv1 "gitlab.com/fynbos/proto/backend/admin/v1"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
+
+type validateStatementsArgs struct {
+	Period   string `validate:"required"`
+	WalletID string `validate:"required,uuid"`
+}
 
 func (s *AdminRpcService) EmailWalletStatement(
 	ctx context.Context, req *adminv1.EmailWalletStatementRequest,
 ) (*adminv1.Empty, error) {
-	pdf, err := s.b.Machnet().GetStatement(ctx, req.GetWalletID(), req.GetPeriod())
+	if err := s.b.Validator().StructCtx(ctx, &validateStatementsArgs{
+		Period:   req.GetPeriod(),
+		WalletID: req.GetWalletID(),
+	}); err != nil {
+		return nil, toGRPCError(err)
+	}
+
+	// look up wallet linked account to get the machnet provider id
+	las, err := s.b.LinkedAccounts().ListByWalletId(ctx, req.GetWalletID())
 	if err != nil {
-		return nil, status.Error(codes.Internal, err.Error())
+		return nil, toGRPCError(err)
+	}
+
+	var walletLinkedAccount *linkedaccounts.LinkedAccount
+	for _, la := range las {
+		if la.Provider == machnet.ProviderName && la.Type == machnet.TypeWallet {
+			walletLinkedAccount = &la
+			break
+		}
+	}
+
+	if walletLinkedAccount == nil {
+		return nil, NotFoundError("Machnet wallet found.")
+	}
+
+	pdf, err := s.b.Machnet().GetStatement(ctx, walletLinkedAccount.ProviderID, req.GetPeriod())
+	if err != nil {
+		return nil, toGRPCError(err)
 	}
 
 	periodStart, err := time.Parse("2006-01-02", req.GetPeriod())
 	if err != nil {
-		return nil, status.Error(codes.Internal, err.Error())
+		return nil, toGRPCError(err)
 	}
 
 	err = s.b.Email().SendMailTemplate(ctx, req.GetWalletID(), email.StatementTemplateID,
@@ -38,7 +68,7 @@ func (s *AdminRpcService) EmailWalletStatement(
 		},
 	)
 	if err != nil {
-		return nil, status.Error(codes.Internal, err.Error())
+		return nil, toGRPCError(err)
 	}
 
 	return &adminv1.Empty{}, nil
