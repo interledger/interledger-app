a365b14de7883364bda0163501d0efc5b7e87a78
Donovan Changfoot <don@fynbos.dev>
2023-01-17 12:09:25 +0200

    feat(botanist): page to email statements

--- typescript/botanist/app/routes.d.ts
+++ typescript/botanist/app/routes.d.ts
@@ -1,12 +1,14 @@
 declare module "routes-gen" {
   export type RouteParams = {
     "/api/allowSignup": Record<string, never>;
+    "/statements": Record<string, never>;
     "/": Record<string, never>;
   };
 
   export function route<
     T extends
       | ["/api/allowSignup"]
+      | ["/statements"]
       | ["/"]
   >(...args: T): typeof args[0];
 }
