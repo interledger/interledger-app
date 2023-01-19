f0e1fc2fad09446584a70498291b53543eabf7b8
Cairin Michie <cairinmichie@gmail.com>
2023-01-19 12:15:49 +0200

    fix(protea): select options fixes
    Sometimes another input component under the options of a select would
    take focus away from a select option. This should fix that.
    
    Also, ensures selected option text is left aligned.

--- typescript/protea/app/components/Select.tsx
+++ typescript/protea/app/components/Select.tsx
@@ -54,7 +54,9 @@ export const Select: FC<SelectProps> = ({
           >
             {({ disabled }: { disabled: boolean }) => (
               <div className='flex h-full items-center justify-between overflow-hidden rounded-xl'>
-                <span className='ml-4'>{disabled ? 'Hello' : value?.name}</span>
+                <span className='ml-4 text-left'>
+                  {disabled ? 'Hello' : value?.name}
+                </span>
                 <div className='flex h-full items-center bg-container px-4 text-medium'>
                   <Icon>unfold_more</Icon>
                 </div>
@@ -67,7 +69,7 @@ export const Select: FC<SelectProps> = ({
             leaveFrom='opacity-100'
             leaveTo='opacity-0'
           >
-            <Listbox.Options className='absolute mt-2 max-h-60 w-full overflow-auto rounded-xl bg-container py-1 shadow-lg focus:outline-none sm:text-sm'>
+            <Listbox.Options className='absolute z-10 mt-2 max-h-60 w-full overflow-auto rounded-xl bg-container py-1 shadow-lg focus:outline-none sm:text-sm'>
               {options.map((option, index) => (
                 <Listbox.Option
                   key={index}
