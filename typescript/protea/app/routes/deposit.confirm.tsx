996099cafbf17b5c8d3eb457623540abbf3d0835
Cairin Michie <cairinmichie@gmail.com>
2023-01-19 11:36:59 +0200

    feat(protea): snackbar feedback on top up or withdraw

--- typescript/protea/app/routes/deposit.confirm.tsx
+++ typescript/protea/app/routes/deposit.confirm.tsx
@@ -13,6 +13,7 @@ import {
   StatusError
 } from '~/lib/proto.server'
 import { randomUUID } from 'crypto'
+import { flashSnackbar } from '~/lib/snackbar.server'
 
 export async function loader({ request }: LoaderArgs) {
   const flow = await requireFlow(request, flowType.TopUp)
@@ -153,5 +154,13 @@ export async function action({ request }: ActionArgs) {
     throw json({}, httpMapping(response.code))
   }
   await exitFlow(request, flowType.TopUp)
-  return redirect(route('/'))
+
+  return redirect(route('/'), {
+    headers: {
+      'Set-Cookie': await flashSnackbar(request, {
+        message: 'Top up created successfully.',
+        icon: 'close'
+      })
+    }
+  })
 }
