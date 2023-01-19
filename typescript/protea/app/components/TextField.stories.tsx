37c99ad6461764f62187e4d48e2260f0f8d24c8c
Cairin Michie <cairinmichie@gmail.com>
2023-01-19 18:00:05 +0200

    A bunch of stories and tests to get the ball rolling

---
+++ typescript/protea/app/components/TextField.stories.tsx
@@ -0,0 +1,34 @@
+import type { StoryFn, Meta } from '@storybook/react'
+import { unstable_createRemixStub as createRemixStub } from '@remix-run/testing'
+import { TextField } from '~/components'
+
+const meta: Meta<typeof TextField> = {
+  title: 'components/TextField',
+  component: TextField,
+  decorators: [
+    (Story) => {
+      const RemixStub = createRemixStub([
+        {
+          path: '/',
+          element: <Story />
+        }
+      ])
+
+      return <RemixStub />
+    }
+  ]
+}
+
+export default meta
+
+const Template: StoryFn<typeof TextField> = (_args) => (
+  <TextField {..._args}>{_args.children}</TextField>
+)
+
+export const TextFieldStory = Template.bind({})
+TextFieldStory.storyName = 'Default TextField'
+TextFieldStory.args = {
+  name: 'something',
+  type: 'text',
+  label: 'This is an input'
+}
