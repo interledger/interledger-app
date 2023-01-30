2e44ac90052185f7feef77ff7e74b624f91ddb5c
barnard <barnard@fynbos.dev>
2023-01-30 14:31:54 +0200

    feat(backend): Add grant GNAP endpoint stub method

--- go/backend/authorisation/http/http.go
+++ go/backend/authorisation/http/http.go
@@ -1,12 +1,18 @@
 package http
 
 import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
 	"net/http"
 
-	"gitlab.com/fynbos/backend/authorisation/ops"
-
 	"github.com/go-chi/chi/v5"
+	"github.com/google/uuid"
 	"github.com/riandyrn/otelchi"
+	"gitlab.com/fynbos/backend/authorisation"
+	"gitlab.com/fynbos/backend/authorisation/ops"
+	"gitlab.com/fynbos/log"
+	"go.uber.org/zap"
 )
 
 func AuthorisationHTTPHandler(b ops.Backends) http.Handler {
@@ -22,7 +28,37 @@ func AuthorisationHTTPHandler(b ops.Backends) http.Handler {
 
 func grantHandler(b ops.Backends) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
-		http.Error(w, http.StatusText(http.StatusNotImplemented), http.StatusNotImplemented)
+
+		var gr authorisation.GrantRequest
+		err := json.NewDecoder(req.Body).Decode(&gr)
+		if err != nil {
+			log.Error("failed to unmarshal grant request body", zap.Error(err))
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			return
+		}
+
+		// TODO:
+		// - Lookup signatures by client URI (payment pointer)
+		// - Validate signature provided in req
+
+		label := gr.AccessToken.Label
+		if label == "" {
+			label = fmt.Sprintf("auto_label_%d", rand.Int31n(1000))
+		}
+
+		resp := authorisation.GrantAccessTokenResp{AccessToken: authorisation.AccessToken{
+			Value:     uuid.NewString(),
+			Access:    gr.AccessToken.Access,
+			Label:     gr.AccessToken.Label,
+			ExpiresIn: 60 * 60, // Valid for 1 hour
+		}}
+
+		err = json.NewEncoder(w).Encode(resp)
+		if err != nil {
+			log.Error("failed to marshal grant request response body", zap.Error(err))
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
 	}
 }
 
---
+++ go/backend/authorisation/types.go
@@ -0,0 +1,109 @@
+package authorisation
+
+type Grant struct {
+	ID    string
+	State GrantState
+}
+
+type GrantState string
+
+const (
+	GrantStateProcessing GrantState = "processing"
+	GrantStatePending    GrantState = "pending"
+	GrantStateApproved   GrantState = "approved"
+	GrantStateFinalized  GrantState = "finalized"
+)
+
+func (gs GrantState) IsValid() bool {
+	return gs == GrantStateProcessing || gs == GrantStatePending || gs == GrantStateApproved || gs == GrantStateFinalized
+}
+
+func (gs GrantState) ValidTransition(next GrantState) bool {
+	if !next.IsValid() {
+		return false
+	}
+
+	switch gs {
+	case GrantStateProcessing:
+		return next == GrantStatePending || next == GrantStateFinalized || next == GrantStateApproved
+	case GrantStatePending:
+		return next == GrantStatePending || next == GrantStateFinalized || next == GrantStateProcessing
+	case GrantStateApproved:
+		return next == GrantStateProcessing || next == GrantStateFinalized || next == GrantStateApproved
+	}
+
+	return false
+}
+
+type (
+	GrantRequest struct {
+		AccessToken AccessTokenReq `json:"access_token"`
+		Client      Client         `json:"client"`
+		Interact    Interact       `json:"interact"`
+		Subject     Subject        `json:"subject"`
+	}
+
+	AccessTokenReq struct {
+		Access []Access `json:"access"`
+		Label  string   `json:"label"`
+	}
+
+	Display struct {
+		Name string `json:"name"`
+		URI  string `json:"uri"`
+	}
+
+	Jwk struct {
+		Kty string `json:"kty"`
+		E   string `json:"e"`
+		Kid string `json:"kid"`
+		Alg string `json:"alg"`
+		N   string `json:"n"`
+	}
+
+	Key struct {
+		Proof string `json:"proof"`
+		Jwk   Jwk    `json:"jwk"`
+	}
+
+	Client struct {
+		Display Display `json:"display"`
+		Key     Key     `json:"key"`
+	}
+
+	Finish struct {
+		Method string `json:"method"`
+		URI    string `json:"uri"`
+		Nonce  string `json:"nonce"`
+	}
+
+	Interact struct {
+		Start  []string `json:"start"`
+		Finish Finish   `json:"finish"`
+	}
+
+	Subject struct {
+		SubIDFormats     []string `json:"sub_id_formats"`
+		AssertionFormats []string `json:"assertion_formats"`
+	}
+
+	GrantAccessTokenResp struct {
+		AccessToken AccessToken `json:"access_token"`
+	}
+
+	Access struct {
+		Type      string   `json:"type"`
+		Actions   []string `json:"actions"`
+		Locations []string `json:"locations"`
+		Datatypes []string `json:"datatypes"`
+	}
+
+	AccessToken struct {
+		Value     string   `json:"value"`
+		Access    []Access `json:"access"`
+		Label     string   `json:"label"`
+		Manage    string   `json:"manage,omitempty"`
+		ExpiresIn int      `json:"expires_in,omitempty"`
+		Flags     []string `json:"flags,omitempty"`
+	}
+)
