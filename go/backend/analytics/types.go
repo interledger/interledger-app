377b5221d51836d1becf294b2f73e4d998c1aea4
Matt de Haast <matt@fynbos.dev>
2023-01-31 14:58:05 +0200

    feat(backend): add analytics for trx
    add analytics for transactions

--- go/backend/analytics/types.go
+++ go/backend/analytics/types.go
@@ -1,8 +1,20 @@
 package analytics
 
+import (
+	"gitlab.com/fynbos/backend/currency"
+	"gitlab.com/fynbos/backend/transactions"
+)
+
 type IdentifyArgs struct {
 	UserId    string
 	Email     string
 	FirstName string
 	LastName  string
+}
+
+type WalletTransactionArgs struct {
+	ID       string
+	TrxType  transactions.TransactionType
+	Provider transactions.Provider
+	Amount   currency.Amount
 }
