5e94f7e5cbb6eca9001a8c993a1e4cbe0171058c
Cairin Michie <cairinmichie@gmail.com>
2023-02-02 18:39:30 +0200

    feat(protea): paginated infinite scroll on transactions page
    - Updates the transactions endpoint to use the new pagination

--- typescript/protea/app/routes/transactions.tsx
+++ typescript/protea/app/routes/transactions.tsx
@@ -1,17 +1,61 @@
-import { Fragment } from 'react'
+import { Fragment, useCallback, useEffect, useState } from 'react'
 import type { LoaderArgs } from '@remix-run/node'
 import { json } from '@remix-run/node'
-import { useLoaderData } from '@remix-run/react'
+import type { ShouldRevalidateFunction } from '@remix-run/react'
+import { useFetcher, useLoaderData, useSearchParams } from '@remix-run/react'
 import { route } from 'routes-gen'
-import { HomeShapes, Icon, Layouts, Router, WalletGrid } from '~/components'
+import {
+  Card,
+  HomeShapes,
+  Icon,
+  Layouts,
+  Router,
+  WalletGrid
+} from '~/components'
 import type { Transaction } from '~/lib/wallet.server'
 import { getTransactionsWithPending } from '~/lib/wallet.server'
 
+/**
+ * Allows us to change the searchParams without revalidating the pages data
+ * This is useful for pagination.
+ */
+export const shouldRevalidate: ShouldRevalidateFunction = ({
+  currentUrl,
+  defaultShouldRevalidate,
+  nextUrl
+}) => {
+  if (currentUrl.search !== nextUrl.search) return false
+  return defaultShouldRevalidate
+}
+
 export async function loader({ request }: LoaderArgs) {
-  const transactions = await getTransactionsWithPending(request)
+  const url = new URL(request.url)
+  const pages = parseInt(url.searchParams.get('pages') || '1')
+
+  let pageInfo = {
+    // pageToken is only set by fetcher so initial page loads this should be blank
+    pageToken: url.searchParams.get('pageToken') || '',
+    pageSize: 30
+  }
+
+  let allTransactions: Transaction[] = []
+
+  /**
+   * We can loop over pages as fetcher should omit this.
+   * This allows us to pull all necessary data when navigating back to this page.
+   */
+  for (let i = 0; i < pages; i++) {
+    const { transactions, nextPageToken } = await getTransactionsWithPending(
+      request,
+      pageInfo
+    )
+    pageInfo.pageToken = nextPageToken
+    allTransactions = [...allTransactions, ...transactions]
+    if (nextPageToken == '') break
+  }
 
   const dateGroupedTransactions = Object.values(
-    [...transactions].reduce<{
+    [...allTransactions].reduce<{
       [date: string]: Transaction[]
     }>((prev, current) => {
       prev[current.date] = prev[current.date] || []
@@ -21,7 +65,8 @@ export async function loader({ request }: LoaderArgs) {
   )
 
   return json({
-    transactions: dateGroupedTransactions
+    transactions: dateGroupedTransactions,
+    nextPageToken: pageInfo.pageToken
   })
 }
 
@@ -30,10 +75,99 @@ export const handle = {
 }
 
 export default function Page() {
-  const { transactions } = useLoaderData<typeof loader>()
+  const initialPage = useLoaderData<typeof loader>()
+  let [, setSearchParams] = useSearchParams()
+  const fetcher = useFetcher()
+  const [transactions, setTransactions] = useState(initialPage.transactions)
+  const [nextPageToken, setNextPageToken] = useState<string>(
+    initialPage.nextPageToken
+  )
+  const [scrollPosition, setScrollPosition] = useState(0)
+  const [clientHeight, setClientHeight] = useState(0)
+  const [height, setHeight] = useState(null)
+  const [shouldFetch, setShouldFetch] = useState(true)
+
+  const divHeight = useCallback(
+    (node: any) => {
+      if (node !== null) {
+        setHeight(node.getBoundingClientRect().height)
+      }
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [transactions.length]
+  )
+
+  // Add Listeners to scroll and client resize
+  useEffect(() => {
+    const scrollListener = () => {
+      setClientHeight(window.innerHeight)
+      setScrollPosition(window.scrollY)
+    }
+
+    // Avoid running during SSR
+    if (typeof window !== 'undefined') {
+      window.addEventListener('scroll', scrollListener)
+    }
+
+    // Clean up
+    return () => {
+      if (typeof window !== 'undefined') {
+        window.removeEventListener('scroll', scrollListener)
+      }
+    }
+  }, [clientHeight, scrollPosition])
+
+  // Trigger fetching new data when the user scrolls near the bottom
+  useEffect(() => {
+    if (!shouldFetch || !height) return
+    if (clientHeight + scrollPosition + 100 < height) return
+
+    if (nextPageToken != '') {
+      fetcher.load(`/transactions?pageToken=${nextPageToken}`)
+      setSearchParams(
+        (old) => {
+          old.set('pages', `${parseInt(old.get('pages') || '1') + 1}`)
+          return old
+        },
+        { replace: true, preventScrollReset: true }
+      )
+    }
+
+    setShouldFetch(false)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [clientHeight, scrollPosition])
+
+  // Handle new transactions being fetched and add to transaction list
+  useEffect(() => {
+    if (fetcher.data && fetcher.data.transactions.length > 0) {
+      setTransactions((currentTransactions) => {
+        const lastOfCurrent =
+          currentTransactions[currentTransactions.length - 1]
+        const newTransactions = fetcher.data.transactions
+        // Verifies if current and new transaction sets have date group overlap
+        if (
+          lastOfCurrent[lastOfCurrent.length - 1].date ==
+          newTransactions[0][0].date
+        ) {
+          const last = currentTransactions.pop() ?? []
+          const first = newTransactions.shift()
+          return [
+            ...currentTransactions,
+            [...last, ...first],
+            ...newTransactions
+          ]
+        }
+        return [...currentTransactions, ...newTransactions]
+      })
+
+      setNextPageToken(fetcher.data.nextPageToken || '')
+      setShouldFetch(true)
+    }
+  }, [fetcher.data])
+
   return (
-    <WalletGrid>
-      <div className='col-span-full flex flex-col rounded-2xl bg-page p-4 pb-6 sm:col-span-6 sm:col-start-2 lg:col-start-4'>
+    <WalletGrid ref={divHeight}>
+      <Card className='col-span-full sm:col-span-6 sm:col-start-2 lg:col-start-4'>
         <div className='mt-2'>
           <HomeShapes />
         </div>
@@ -52,11 +186,11 @@ export default function Page() {
             </Router>
           </div>
         )}
-      </div>
+      </Card>
       {transactions.map((transactionGroup, index) => (
-        <div
+        <Card
           key={`group-${index}`}
-          className='col-span-full flex flex-col rounded-2xl bg-page p-4 pb-6 sm:col-span-6 sm:col-start-2 lg:col-start-4'
+          className='col-span-full sm:col-span-6 sm:col-start-2 lg:col-start-4'
         >
           <span className='text-xs text-medium'>
             {transactionGroup[0].date}
@@ -80,7 +214,7 @@ export default function Page() {
               </Router>
             </Fragment>
           ))}
-        </div>
+        </Card>
       ))}
     </WalletGrid>
   )
