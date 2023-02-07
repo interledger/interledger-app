acc0db430cd1a99e57fb9f3b339d139cfd68609b
Cairin Michie <cairinmichie@gmail.com>
2023-02-07 20:52:20 +0200

    sync

---
+++ dart/watsonia/lib/components/Card.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Card extends StatelessWidget {
+  const Card({
+    super.key,
+    required this.children,
+  });
+
+  final List<Widget> children;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20), color: Colors.white),
+        width: double.infinity,
+        margin: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+        padding: const EdgeInsets.fromLTRB(16, 16, 16, 24),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start, children: children));
+  }
+}
