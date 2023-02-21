bf7a5900b83c9b2946eda74c242721909a7b0be6
Donovan Changfoot <don@fynbos.dev>
2023-02-21 12:43:49 +0200

    chore(dev): update coredns config for local deployment

--- dev/kind/deploy.sh
+++ dev/kind/deploy.sh
@@ -12,3 +12,14 @@ ROOIBOS_DIR=$(dirname "$0")/../../../rooibos
 [ -d $ROOIBOS_DIR/protea/envs/local ] && while ! kubectl apply -k $ROOIBOS_DIR/protea/envs/local; do echo "Retrying to apply protea resources in 5s."; sleep 5; done
 [ -d $ROOIBOS_DIR/botanist/envs/local ] && while ! kubectl apply -k $ROOIBOS_DIR/botanist/envs/local; do echo "Retrying to apply botanist resources in 5s."; sleep 5; done
 [ -d $ROOIBOS_DIR/temporal/envs/local ] && while ! kubectl apply -k $ROOIBOS_DIR/temporal/envs/local; do echo "Retrying to apply temporal resources in 5s."; sleep 5; done
+
+
+# update coredns config to resolve local.fynbos.me to emissary
+if [ -d $ROOIBOS_DIR/coredns/envs/local ]
+then
+	echo "Updating coredns config"
+	EMISSARY_SERVICE_IP=$(kubectl get --namespace=emissary service/emissary-emissary-ingress -o=json | jq -r '.spec.clusterIP')
+	sed "s/{EMISSARY_SERVICE_IP}/$EMISSARY_SERVICE_IP/g" $ROOIBOS_DIR/coredns/envs/local/config-example.yaml > $ROOIBOS_DIR/coredns/envs/local/config.yaml
+	kubectl apply -k $ROOIBOS_DIR/coredns/envs/local
+	kubectl --namespace=kube-system rollout restart deployment/coredns
+fi
