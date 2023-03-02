d56185bfac7f28d6e90e9033007ff933f68ff205
Matt de Haast <matt@fynbos.dev>
2023-03-02 13:57:22 +0200

    feat(backend): payment pointers type
    add an initiate payment pointers type to make parsing and using
    them easier. Initially used in the contacts package.

--- go/backend/contacts/api.go
+++ go/backend/contacts/api.go
@@ -3,10 +3,11 @@ package contacts
 import (
 	"context"
 	"gitlab.com/fynbos/backend/db"
+	"gitlab.com/fynbos/backend/paymentpointers"
 )
 
 type Client interface {
 	Create(ctx context.Context, args CreateContactArgs) (*Contact, error)
 	List(ctx context.Context, walletID string, page db.Pagination) ([]Contact, error)
-	Get(ctx context.Context, walletID string, pp string) (*Contact, error)
+	Get(ctx context.Context, walletID string, pp *paymentpointers.PaymentPointer) (*Contact, error)
 }
--- go/backend/contacts/ops/service.go
+++ go/backend/contacts/ops/service.go
@@ -7,9 +7,17 @@ import (
 	"fmt"
 	"gitlab.com/fynbos/backend/contacts"
 	"gitlab.com/fynbos/backend/db"
+	"gitlab.com/fynbos/backend/paymentpointers"
 )
 
 const contactCols = ` id, name, payment_pointer, wallet_id `
+
+type dbContact struct {
+	ID             string
+	Name           string
+	PaymentPointer string `db:"payment_pointer"`
+	WalletID       string `db:"wallet_id"`
+}
 
 func Create(ctx context.Context, b Backends, args contacts.CreateContactArgs) (*contacts.Contact, error) {
 	err := b.Validator().Struct(args)
@@ -17,15 +25,25 @@ func Create(ctx context.Context, b Backends, args contacts.CreateContactArgs) (*
 		return nil, fmt.Errorf("%w %s", contacts.ErrInvalidArgument, err)
 	}
 
-	var contact contacts.Contact
-	err = b.DB().GetContext(ctx, &contact,
+	var c dbContact
+	err = b.DB().GetContext(ctx, &c,
 		fmt.Sprintf("INSERT INTO contacts (name, payment_pointer, wallet_id) values ($1, $2, $3) returning %s", contactCols),
-		args.Name, args.PaymentPointer, args.WalletID)
+		args.Name, args.PaymentPointer.String(), args.WalletID)
 	if err != nil {
 		return nil, fmt.Errorf("%w %s", contacts.ErrInternal, err)
 	}
 
-	return &contact, nil
+	pp, err := paymentpointers.Parse(c.PaymentPointer)
+	if err != nil {
+		return nil, fmt.Errorf("%w %s error parsing payment pointer", contacts.ErrInternal, err)
+	}
+
+	return &contacts.Contact{
+		ID:             c.ID,
+		Name:           c.Name,
+		PaymentPointer: pp,
+		WalletID:       c.WalletID,
+	}, nil
 }
 
 func List(ctx context.Context, b Backends, walletID string, page db.Pagination) ([]contacts.Contact, error) {
@@ -37,20 +55,36 @@ func List(ctx context.Context, b Backends, walletID string, page db.Pagination) 
 		sqlArgs = []interface{}{walletID, page.PageToken}
 	}
 
-	var contactList []contacts.Contact
-	err := b.DB().SelectContext(ctx, &contactList, sqlStmt, sqlArgs...)
+	var cl []dbContact
+	err := b.DB().SelectContext(ctx, &cl, sqlStmt, sqlArgs...)
 	if err != nil {
 		return nil, fmt.Errorf("%w %s", contacts.ErrInternal, err)
+	}
+
+	var contactList []contacts.Contact
+	for _, c := range cl {
+
+		pp, err := paymentpointers.Parse(c.PaymentPointer)
+		if err != nil {
+			return nil, fmt.Errorf("%w %s error parsing payment pointer", contacts.ErrInternal, err)
+		}
+
+		contactList = append(contactList, contacts.Contact{
+			ID:             c.ID,
+			Name:           c.Name,
+			PaymentPointer: pp,
+			WalletID:       c.WalletID,
+		})
 	}
 
 	return contactList, nil
 }
 
-func Get(ctx context.Context, b Backends, walletID, pp string) (*contacts.Contact, error) {
-	var contact contacts.Contact
-	err := b.DB().GetContext(ctx, &contact,
+func Get(ctx context.Context, b Backends, walletID string, pp *paymentpointers.PaymentPointer) (*contacts.Contact, error) {
+	var c dbContact
+	err := b.DB().GetContext(ctx, &c,
 		fmt.Sprintf("SELECT %s from contacts where wallet_id = $1 and payment_pointer = $2", contactCols),
-		walletID, pp)
+		walletID, pp.String())
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			return nil, fmt.Errorf("%w %s", contacts.ErrNotFound, err)
@@ -58,5 +92,15 @@ func Get(ctx context.Context, b Backends, walletID, pp string) (*contacts.Contac
 		return nil, fmt.Errorf("%w %s", contacts.ErrInternal, err)
 	}
 
-	return &contact, nil
+	ppDb, err := paymentpointers.Parse(c.PaymentPointer)
+	if err != nil {
+		return nil, fmt.Errorf("%w %s error parsing payment pointer", contacts.ErrInternal, err)
+	}
+
+	return &contacts.Contact{
+		ID:             c.ID,
+		Name:           c.Name,
+		PaymentPointer: ppDb,
+		WalletID:       c.WalletID,
+	}, nil
 }
