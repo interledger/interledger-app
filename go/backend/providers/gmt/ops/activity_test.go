e569a73c831d7dc69c53b31c30e1aabbc7f79b06
barnard <barnard@fynbos.dev>
2023-03-17 15:54:48 +0200

    feat(backend): Save GMT transaction receipt

--- go/backend/providers/gmt/ops/activity_test.go
+++ go/backend/providers/gmt/ops/activity_test.go
@@ -2,18 +2,68 @@ package ops
 
 import (
 	"context"
-	"github.com/stretchr/testify/assert"
 	"testing"
 
 	"github.com/google/uuid"
-	"github.com/jmoiron/sqlx"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"go.temporal.io/sdk/testsuite"
-
 	"gitlab.com/fynbos/backend/db"
+	users_client "gitlab.com/fynbos/backend/user/client"
+	"go.temporal.io/sdk/testsuite"
 )
 
 func TestActivity_UpdateSendRecvUser(t *testing.T) {
+	t.Parallel()
+	ctx := context.Background()
+	b := NewTestBackends(db.MigrateTestDB(t, ctx))
+	uc := users_client.New(b, "URL", "Admin")
+
+	testSuite := &testsuite.WorkflowTestSuite{}
+	env := testSuite.NewTestActivityEnvironment()
+	a := NewActivity(b)
+	env.RegisterActivity(a.UpdateSendRecvUser)
+
+	sWallet, err := uc.CreateNewWallet(ctx, uuid.NewString(), "test_sender")
+	require.NoError(t, err)
+
+	rWallet, err := uc.CreateNewWallet(ctx, uuid.NewString(), "test_receiver")
+	require.NoError(t, err)
+
+	sid, err := getSenderID(ctx, b, sWallet.ID)
+	require.NoError(t, err)
+	assert.Equal(t, int64(0), sid)
+
+	rid, err := getReceiverID(ctx, b, rWallet.ID)
+	require.NoError(t, err)
+	assert.Equal(t, int64(0), rid)
+
+	cr := ComplianceResp{
+		SenderID:         23,
+		SenderWalletID:   sWallet.ID,
+		ReceiverID:       43,
+		ReceiverWalletID: rWallet.ID,
+	}
+	_, err = env.ExecuteActivity(a.UpdateSendRecvUser, cr)
+	require.NoError(t, err)
+
+	sid, err = getSenderID(ctx, b, sWallet.ID)
+	require.NoError(t, err)
+	assert.Equal(t, int64(23), sid)
+
+	sid, err = getSenderID(ctx, b, rWallet.ID)
+	require.NoError(t, err)
+	assert.Equal(t, int64(0), sid)
+
+	rid, err = getReceiverID(ctx, b, rWallet.ID)
+	require.NoError(t, err)
+	assert.Equal(t, int64(43), rid)
+
+	rid, err = getReceiverID(ctx, b, sWallet.ID)
+	require.NoError(t, err)
+	assert.Equal(t, int64(0), rid)
+}
+
+func TestActivity_SaveReceipt(t *testing.T) {
 	t.Parallel()
 	ctx := context.Background()
 	b := NewTestBackends(db.MigrateTestDB(t, ctx))
@@ -21,44 +71,35 @@ func TestActivity_UpdateSendRecvUser(t *testing.T) {
 	testSuite := &testsuite.WorkflowTestSuite{}
 	env := testSuite.NewTestActivityEnvironment()
 	a := NewActivity(b)
-	env.RegisterActivity(a.UpdateSendRecvUser)
+	env.RegisterActivity(a.SaveReceipt)
 
-	sWallet := newWallet(t, b.DB())
-	rWallet := newWallet(t, b.DB())
-	cr := ComplianceResp{
-		SenderID:         23,
-		SenderWalletID:   sWallet,
-		ReceiverID:       43,
-		ReceiverWalletID: rWallet,
+	tr := TransactionResp{
+		ID:         "some_id_for_us",
+		ReceiptRef: "some_id_for_gmt",
+		Status:     "created",
+		Licence:    "USA_will_let_me",
+		RTR:        "You have not right to a refund, NO!!",
+		ErrorMsg:   "",
+		Contact:    "Call us on XXXX-XXX-XXX for any queries",
 	}
-	_, err := env.ExecuteActivity(a.UpdateSendRecvUser, cr)
+	_, err := env.ExecuteActivity(a.SaveReceipt, tr)
 	require.NoError(t, err)
 
-	sid, err := getSenderID(ctx, b, sWallet)
-	require.NoError(t, err)
-	assert.Equal(t, int64(23), sid)
-
-	sid, err = getSenderID(ctx, b, rWallet)
-	require.NoError(t, err)
-	assert.Equal(t, int64(0), sid)
-
-	rid, err := getReceiverID(ctx, b, rWallet)
-	require.NoError(t, err)
-	assert.Equal(t, int64(43), rid)
-
-	rid, err = getReceiverID(ctx, b, sWallet)
-	require.NoError(t, err)
-	assert.Equal(t, int64(0), rid)
-}
-
-func newWallet(t *testing.T, db *sqlx.DB) string {
-	walletID := uuid.NewString()
-	_, err := db.Exec(
-		"INSERT INTO wallets (id, name) VALUES ($1, $2);",
-		walletID,
-		"test",
-	)
+	var res struct {
+		ID         string `db:"external_id"`
+		ReceiptRef string `db:"receipt"`
+		Licence    string `db:"licence"`
+		RTR        string `db:"right_to_return"`
+		ErrMsg     string `db:"error_msg"`
+		Contact    string `db:"contact"`
+	}
+	err = b.DB().GetContext(ctx, &res, "select external_id, receipt, licence, right_to_return, error_msg, contact from gmt_receipts")
 	require.NoError(t, err)
 
-	return walletID
+	assert.Equal(t, tr.ID, res.ID)
+	assert.Equal(t, tr.ReceiptRef, res.ReceiptRef)
+	assert.Equal(t, tr.Licence, res.Licence)
+	assert.Equal(t, tr.RTR, res.RTR)
+	assert.Equal(t, tr.ErrorMsg, res.ErrMsg)
+	assert.Equal(t, tr.Contact, res.Contact)
 }
