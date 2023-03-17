75143f625f1b105d5f5a546a549194667a003bd1
Donovan Changfoot <don@fynbos.dev>
2023-03-17 14:04:42 +0200

    feat(backend): mx client and grpc interfaces

--- typescript/protea/app/generated/protobuf-ts/backend/v1/backend.ts
+++ typescript/protea/app/generated/protobuf-ts/backend/v1/backend.ts
@@ -466,6 +466,15 @@ export interface PaymentPointer {
 export interface Empty {
 }
 /**
+ * @generated from protobuf message backend.v1.MXWidgetResponse
+ */
+export interface MXWidgetResponse {
+    /**
+     * @generated from protobuf field: string url = 1;
+     */
+    url: string;
+}
+/**
  * @generated from protobuf message backend.v1.ConnectionLimits
  */
 export interface ConnectionLimits {
@@ -3029,6 +3038,53 @@ class Empty$Type extends MessageType<Empty> {
  * @generated MessageType for protobuf message backend.v1.Empty
  */
 export const Empty = new Empty$Type();
+// @generated message type with reflection information, may provide speed optimized methods
+class MXWidgetResponse$Type extends MessageType<MXWidgetResponse> {
+    constructor() {
+        super("backend.v1.MXWidgetResponse", [
+            { no: 1, name: "url", kind: "scalar", T: 9 /*ScalarType.STRING*/ }
+        ]);
+    }
+    create(value?: PartialMessage<MXWidgetResponse>): MXWidgetResponse {
+        const message = { url: "" };
+        globalThis.Object.defineProperty(message, MESSAGE_TYPE, { enumerable: false, value: this });
+        if (value !== undefined)
+            reflectionMergePartial<MXWidgetResponse>(this, message, value);
+        return message;
+    }
+    internalBinaryRead(reader: IBinaryReader, length: number, options: BinaryReadOptions, target?: MXWidgetResponse): MXWidgetResponse {
+        let message = target ?? this.create(), end = reader.pos + length;
+        while (reader.pos < end) {
+            let [fieldNo, wireType] = reader.tag();
+            switch (fieldNo) {
+                case /* string url */ 1:
+                    message.url = reader.string();
+                    break;
+                default:
+                    let u = options.readUnknownField;
+                    if (u === "throw")
+                        throw new globalThis.Error(`Unknown field ${fieldNo} (wire type ${wireType}) for ${this.typeName}`);
+                    let d = reader.skip(wireType);
+                    if (u !== false)
+                        (u === true ? UnknownFieldHandler.onRead : u)(this.typeName, message, fieldNo, wireType, d);
+            }
+        }
+        return message;
+    }
+    internalBinaryWrite(message: MXWidgetResponse, writer: IBinaryWriter, options: BinaryWriteOptions): IBinaryWriter {
+        /* string url = 1; */
+        if (message.url !== "")
+            writer.tag(1, WireType.LengthDelimited).string(message.url);
+        let u = options.writeUnknownFields;
+        if (u !== false)
+            (u == true ? UnknownFieldHandler.onWrite : u)(this.typeName, message, writer);
+        return writer;
+    }
+}
+/**
+ * @generated MessageType for protobuf message backend.v1.MXWidgetResponse
+ */
+export const MXWidgetResponse = new MXWidgetResponse$Type();
 // @generated message type with reflection information, may provide speed optimized methods
 class ConnectionLimits$Type extends MessageType<ConnectionLimits> {
     constructor() {
@@ -7039,5 +7095,6 @@ export const BackendService = new ServiceType("backend.v1.BackendService", [
     { name: "SetIdentityPublic", options: {}, I: SetIdentityPublicRequest, O: Identity },
     { name: "StartIdentityVerification", options: {}, I: StartIdentityVerificationRequest, O: Identity },
     { name: "KYCStatus", options: {}, I: Empty, O: KYCStatusResponse },
-    { name: "StartKYC", options: {}, I: Empty, O: Empty }
+    { name: "StartKYC", options: {}, I: Empty, O: Empty },
+    { name: "GetMXWidget", options: {}, I: Empty, O: MXWidgetResponse }
 ]);
