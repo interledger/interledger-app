4ea2743060e4fe5dc6cfbe5b9cca2b4bbfb792c1
Donovan Changfoot <don@fynbos.dev>
2023-03-17 13:54:02 +0200

    feat(backend): mx ops get widget for wallet

---
+++ go/backend/providers/mx/ops/ops.go
@@ -0,0 +1,40 @@
+package ops
+
+import (
+	"context"
+	"fmt"
+
+	"gitlab.com/fynbos/backend/providers/mx"
+	"gitlab.com/fynbos/backend/providers/mx/external"
+)
+
+func GetWidget(ctx context.Context, b Backends, walletID string) (string, error) {
+	externalUsers, err := b.External().ListUsersByID(ctx, walletID)
+	if err != nil {
+		return "", fmt.Errorf("%w %s", mx.ErrInternal, err)
+	}
+
+	var user *external.User
+	for _, usr := range externalUsers.Users {
+		if usr.ID == walletID {
+			user = &usr
+			break
+		}
+	}
+	if user == nil {
+		user, err = b.External().CreateUser(ctx, walletID)
+		if err != nil {
+			return "", fmt.Errorf("%w %s", mx.ErrInternal, err)
+		}
+	}
+
+	widget, err := b.External().GetWidgetURL(ctx, external.GetWidgetURLArgs{
+		UserGuid:            user.Guid,
+		IncludeTransactions: false,
+	})
+	if err != nil {
+		return "", fmt.Errorf("%w %s", mx.ErrInternal, err)
+	}
+
+	return widget.URL, nil
+}
