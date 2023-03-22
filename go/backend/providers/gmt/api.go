33f417d2d0780574e477403da94ba2c70482d17c
barnard <barnard@fynbos.dev>
2023-03-22 12:59:50 +0200

    feat(backend): GMT update transactions and transfers

--- go/backend/providers/gmt/api.go
+++ go/backend/providers/gmt/api.go
@@ -2,18 +2,8 @@ package gmt
 
 import (
 	"context"
-
-	"gitlab.com/fynbos/backend/currency"
 )
 
 type Client interface {
 	StartUserOnboarding(ctx context.Context, walletID string) (Await, error)
 }
-
-type TransfersArgs struct {
-	FromLinkedAccountID string `validate:"uuid"`
-	ToLinkedAccountID   string `validate:"uuid"`
-	Amount              currency.Amount
-}
-
-type Await func(context.Context, interface{}) error
