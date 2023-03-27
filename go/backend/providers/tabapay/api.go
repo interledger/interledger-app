7fda3d46ac7380f69862fcb591c3828f04d7f2fb
Donovan Changfoot <don@fynbos.dev>
2023-03-27 13:27:07 +0200

    feat(backend): tabapay client

--- go/backend/providers/tabapay/api.go
+++ go/backend/providers/tabapay/api.go
@@ -2,10 +2,8 @@ package tabapay
 
 import (
 	"context"
-
-	"gitlab.com/fynbos/backend/linkedaccounts"
 )
 
 type Client interface {
-	CreateCard(ctx context.Context, args CreateCardArgs) (*linkedaccounts.LinkedAccount, error)
+	CreateCard(ctx context.Context, args CreateCardArgs) (Await, error)
 }
--- go/backend/providers/tabapay/ops/ops.go
+++ go/backend/providers/tabapay/ops/ops.go
@@ -3,57 +3,27 @@ package ops
 import (
 	"context"
 	"fmt"
+	"time"
 
-	"gitlab.com/fynbos/backend/linkedaccounts"
 	"gitlab.com/fynbos/backend/providers/tabapay"
-	"gitlab.com/fynbos/backend/providers/tabapay/external"
+	"gitlab.com/fynbos/backend/providers/tabapay/workflows"
+	"go.temporal.io/api/enums/v1"
+	"go.temporal.io/sdk/client"
 )
 
-func CreateCard(ctx context.Context, b Backends, args tabapay.CreateCardArgs) (*linkedaccounts.LinkedAccount, error) {
-	owner, err := b.KYC().GetIndividualDetails(ctx, args.WalletID)
-	if err != nil {
-		return nil, fmt.Errorf("%w %s", tabapay.ErrInternal, err)
-	}
-	if owner.Address == nil {
-		return nil, fmt.Errorf("%w require address for wallet.", tabapay.ErrInternal)
-	}
+func CreateCard(ctx context.Context, b Backends, args tabapay.CreateCardArgs) (tabapay.Await, error) {
+	// TODO: check that this is a debit card
 
-	resp, err := b.External().CreateAccount(ctx, external.CreateAccountArgs{
-		ReferenceID: args.ReferenceID,
-		Card: external.Card{
-			AccountNumber:  args.CardNumber,
-			ExpirationDate: args.ExpirationDate,
-			SecurityCode:   args.CVV,
-		},
-		Owner: external.Owner{
-			Name: external.Name{
-				First: owner.FirstName,
-				Last:  owner.LastName,
-			},
-			Address: &external.Address{
-				Line1:   owner.Address.Line1,
-				Line2:   owner.Address.Line2,
-				City:    owner.Address.City,
-				State:   owner.Address.State,
-				ZipCode: owner.Address.ZipCode,
-				Country: owner.Address.CountryCode,
-			},
-		},
-	})
+	wo := client.StartWorkflowOptions{
+		ID:                       "tabapay_create_card" + args.IdempotencyKey,
+		TaskQueue:                "backend",
+		WorkflowExecutionTimeout: 2 * time.Minute,
+		WorkflowIDReusePolicy:    enums.WORKFLOW_ID_REUSE_POLICY_REJECT_DUPLICATE,
+	}
+	await, err := b.Temporal().ExecuteWorkflow(ctx, wo, workflows.CreateTabapayCardWorkflow, args)
 	if err != nil {
 		return nil, fmt.Errorf("%w %s", tabapay.ErrInternal, err)
 	}
 
-	la, err := b.LinkedAccounts().Create(ctx, &linkedaccounts.CreateArgs{
-		ID:         args.ReferenceID,
-		WalletID:   args.WalletID,
-		Name:       args.Name,
-		Provider:   tabapay.ProviderName,
-		ProviderID: resp.AccountID,
-	})
-	if err != nil {
-		return nil, fmt.Errorf("%w %s", tabapay.ErrInternal, err)
-	}
-
-	return la, nil
+	return await.Get, nil
 }
