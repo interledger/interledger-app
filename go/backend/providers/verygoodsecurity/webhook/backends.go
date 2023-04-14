d8283bf4ea1c4b5ec5c3a17f12c861f314ddc59a
Donovan Changfoot <don@fynbos.dev>
2023-04-14 14:27:35 +0200

    feat(backend): start tabapay new card workflow from vgs webhook

--- go/backend/providers/verygoodsecurity/webhook/backends.go
+++ go/backend/providers/verygoodsecurity/webhook/backends.go
@@ -1,9 +1,11 @@
 package webhook
 
 import (
+	"gitlab.com/fynbos/backend/providers/tabapay"
 	"gitlab.com/fynbos/backend/providers/verygoodsecurity"
 )
 
 type Backends interface {
 	VGS() verygoodsecurity.Client
+	Tabapay() tabapay.Client
 }
