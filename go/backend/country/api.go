a032f137544d6c07e5f85fb50a791b06189877fe
Donovan Changfoot <don@fynbos.dev>
2023-04-18 11:08:53 +0200

    refactor(backends): define countries in code

--- go/backend/country/api.go
+++
@@ -1,9 +0,0 @@
-package country
-
-import "context"
-
-type Client interface {
-	Get(ctx context.Context, id string) (*Country, error)
-	GetByAlpha2(ctx context.Context, code string) (*Country, error)
-	GetAll(ctx context.Context) ([]Country, error)
-}
