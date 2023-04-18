1394d343a5fa401a8585f1ca316d09adabcea523
Matt de Haast <matt@fynbos.dev>
2023-04-18 15:11:37 +0200

    feat(backend): keys vault support
    add support for vault for our keys service

--- go/backend/vault/vault.go
+++ go/backend/vault/vault.go
@@ -2,39 +2,105 @@ package vault
 
 import (
 	"context"
+	"encoding/base64"
 	"fmt"
 	vault "github.com/hashicorp/vault/api"
 	auth "github.com/hashicorp/vault/api/auth/kubernetes"
+	"gitlab.com/fynbos/env"
 	"gitlab.com/fynbos/log"
-	"go.uber.org/zap"
+	"os"
 )
 
-func NewVaultClient() (*vault.Client, error) {
+type client struct {
+	vc                *vault.Client
+	transitEnginePath string
+}
 
-	config := vault.DefaultConfig()
-	config.Address = "https://vault1.fynbos.cloud:8200"
-	client, err := vault.NewClient(config)
+func NewClient() (Client, error) {
+
+	// Only login to vault on non-local environments.
+	if !env.IsLocal() {
+		addr := os.Getenv("VAULT_ADDR")
+		mountPath := os.Getenv("VAULT_MOUNT_PATH")
+		transitEnginePath := os.Getenv("VAULT_TRANSIT_ENGINE_PATH")
+
+		config := vault.DefaultConfig()
+		config.Address = addr
+		vc, err := vault.NewClient(config)
+		if err != nil {
+			return nil, fmt.Errorf("unable to initialize Vault client: %w", err)
+		}
+
+		k8sAuth, err := auth.NewKubernetesAuth(
+			"k8s-app",
+			auth.WithMountPath(mountPath),
+		)
+		if err != nil {
+			return nil, fmt.Errorf("unable to initialize Kubernetes auth method: %w", err)
+		}
+
+		log.Info("Attempting to auth vault")
+		authInfo, err := vc.Auth().Login(context.Background(), k8sAuth)
+		if err != nil {
+			return nil, fmt.Errorf("unable to log in with Kubernetes auth: %w", err)
+		}
+		if authInfo == nil {
+			return nil, fmt.Errorf("no auth info was returned after login")
+		}
+
+		log.Info("Successfully authed vault")
+		return client{vc: vc, transitEnginePath: transitEnginePath}, nil
+	}
+
+	return client{vc: nil}, nil
+}
+
+// CreateKey Currently only supports ED25119 keys
+func (c client) CreateKey(keyName string) error {
+	keyPath := fmt.Sprintf("%s/keys/%s", c.transitEnginePath, keyName)
+
+	data := map[string]interface{}{
+		"type":                   "ed25519",
+		"convergent_encryption":  false,
+		"derived":                false,
+		"exportable":             true,
+		"allow_plaintext_backup": false,
+		"deletion_allowed":       true,
+	}
+
+	_, err := c.vc.Logical().Write(keyPath, data)
+	return err
+}
+
+func (c client) Sign(keyName string, input *SignInput) (string, error) {
+	encodedInput := base64.StdEncoding.EncodeToString([]byte(input.Input))
+	keyPath := fmt.Sprintf("%s/sign/%s", c.transitEnginePath, keyName)
+
+	data := map[string]interface{}{
+		"input": encodedInput,
+	}
+
+	resp, err := c.vc.Logical().Write(keyPath, data)
 	if err != nil {
-		return nil, fmt.Errorf("unable to initialize Vault client: %w", err)
+		return "", err
 	}
 
-	k8sAuth, err := auth.NewKubernetesAuth(
-		"k8s-app",
-		auth.WithMountPath("k8s-dev-euw1"),
-	)
+	return resp.Data["signature"].(string), nil
+}
+
+func (c client) Verify(keyName string, input *VerifyInput) (bool, error) {
+	keyPath := fmt.Sprintf("%s/verify/%s", c.transitEnginePath, keyName)
+	encodedInput := base64.StdEncoding.EncodeToString([]byte(input.Input))
+
+	data := map[string]interface{}{
+		"input":     encodedInput,
+		"signature": input.Signature,
+	}
+
+	resp, err := c.vc.Logical().Write(keyPath, data)
 	if err != nil {
-		return nil, fmt.Errorf("unable to initialize Kubernetes auth method: %w", err)
+		return false, err
 	}
 
-	authInfo, err := client.Auth().Login(context.TODO(), k8sAuth)
-	if err != nil {
-		return nil, fmt.Errorf("unable to log in with Kubernetes auth: %w", err)
-	}
-	if authInfo == nil {
-		return nil, fmt.Errorf("no auth info was returned after login")
-	}
-
-	log.Info("Successfully authed", zap.Any("authInfo", authInfo))
-
-	return client, nil
+	return resp.Data["valid"].(bool), nil
 }
