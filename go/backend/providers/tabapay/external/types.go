48c7be221c6847081f4d2a329e024b9767a37d3e
Donovan Changfoot <don@fynbos.dev>
2023-04-25 14:39:18 +0200

    wip

--- go/backend/providers/tabapay/external/types.go
+++ go/backend/providers/tabapay/external/types.go
@@ -292,13 +292,13 @@ type (
 	}
 
 	Lookup3DSArgs struct {
-		ID3DS                   string `json:"3dsID"`
-		AuthenticationIndicator string `json:"authenticationIndicator"`
-		TransactionMode         string `json:"transactionMode"`
-		TransactionType         string `json:"transactionType"`
-		ProductCode             string `json:"productCode"`
-		Account                 Account
-		Order                   Order
+		ID3DS                   string  `json:"3dsID"`
+		AuthenticationIndicator string  `json:"authenticationIndicator"`
+		TransactionMode         string  `json:"transactionMode"`
+		TransactionType         string  `json:"transactionType"`
+		ProductCode             string  `json:"productCode"`
+		Account                 Account `json:"account"`
+		Order                   Order   `json:"order"`
 	}
 
 	Lookup3DSResponse struct {
