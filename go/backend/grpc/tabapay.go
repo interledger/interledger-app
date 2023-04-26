704c1c9a0fa6c0fd508c500679223a7b5e7cc678
Donovan Changfoot <don@fynbos.dev>
2023-04-26 14:09:34 +0200

    refactor(backend): decouple 3ds from making payment

--- go/backend/grpc/tabapay.go
+++ go/backend/grpc/tabapay.go
@@ -3,10 +3,57 @@ package grpc
 import (
 	"context"
 
-	"gitlab.com/fynbos/backend/providers/gmt"
+	"gitlab.com/fynbos/backend/currency"
+	"gitlab.com/fynbos/backend/linkedaccounts"
 	"gitlab.com/fynbos/backend/providers/tabapay"
 	backendv1 "gitlab.com/fynbos/proto/backend/v1"
 )
+
+func (s *rpcService) Init3DS(
+	ctx context.Context, req *backendv1.Init3DSRequest,
+) (*backendv1.Init3DSResponse, error) {
+	_, err := s.b.Users().UserForContext(ctx)
+	if err != nil {
+		return nil, UnauthenticatedError("Unauthenticated.")
+	}
+
+	w, err := s.b.Users().WalletForContext(ctx)
+	if err != nil {
+		return nil, UnauthenticatedError("Unauthenticated.")
+	}
+
+	quote, err := s.b.OpenPayments().GetQuote(ctx, req.GetQuoteID())
+	if err != nil {
+		return nil, InternalError("Quote not found.")
+	}
+
+	fromLinkedAcc, err := s.b.LinkedAccounts().Get(ctx, quote.FromLinkedAccount)
+	if err != nil {
+		return nil, toGRPCError(err)
+	}
+	if fromLinkedAcc.WalletID != w.ID {
+		return nil, NotFoundError("Quote not found.")
+	}
+	if !linkedaccounts.Requires3DS(fromLinkedAcc) {
+		return nil, InternalError("3DS not supported.")
+	}
+
+	init3DS, err := s.b.Tabapay().Init3DS(ctx, tabapay.Init3DSArgs{
+		Amount:         quote.SendAmount,
+		IdempotencyKey: req.GetIdempotencyKey(),
+		CardID:         fromLinkedAcc.ProviderID,
+	})
+	if err != nil {
+		return nil, toGRPCError(err)
+	}
+
+	return &backendv1.Init3DSResponse{
+		Id:                  init3DS.ID,
+		Jwt:                 init3DS.JWT,
+		DeviceCollectionURL: init3DS.DeviceCollectionURL,
+		SongbirdURL:         tabapay.GetSongbirdURL(),
+	}, nil
+}
 
 func (s *rpcService) Lookup3DS(
 	ctx context.Context, req *backendv1.Lookup3DSRequest,
@@ -21,52 +68,38 @@ func (s *rpcService) Lookup3DS(
 		return nil, UnauthenticatedError("Unauthenticated.")
 	}
 
-	op, err := s.b.OpenPayments().GetOutgoingPayment(ctx, req.GetOutgoingPaymentID())
+	session, err := s.b.Tabapay().Get3DSSession(ctx, req.GetThreeDSID())
 	if err != nil {
 		return nil, toGRPCError(err)
 	}
 
-	// make sure payment belongs to wallet
-	_, err = s.b.Transactions().GetTransactionByForeignID(ctx, w.ID, op.ID)
-	if err != nil {
-		return nil, toGRPCError(err)
-	}
-
-	linkedCard, err := s.b.LinkedAccounts().Get(ctx, op.FromLinkedAccount)
-	if err != nil {
-		return nil, toGRPCError(err)
-	}
-
-	lookupResp, err := s.b.Tabapay().Lookup3DS(ctx, tabapay.Lookup3DSArgs{
-		ThreeDSID:               req.GetThreeDSID(),
-		OutgoingPaymentID:       req.GetOutgoingPaymentID(),
-		CardID:                  linkedCard.ProviderID,
-		AuthenticationIndicator: tabapay.AuthenticatorIndicatorPayment,
-		TransactionMode:         tabapay.TransactionModeComputer,
-		ProductCode:             tabapay.ProductCodeQuasiCashTransaction,
-		DeviceChannel:           tabapay.DeviceChannelBrowser,
-		Amount:                  op.SendAmount,
-		BrowserInfo:             tabapay.BrowserInfo("true|Mozilla/5.0 iPhone|text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8|true|en-US|32|414|896|300|127.0.0.1"),
+	la, err := s.b.LinkedAccounts().GetByProviderID(ctx, linkedaccounts.GetByProviderIDArgs{
+		Provider:   tabapay.ProviderName,
+		ProviderID: session.CardID,
 	})
 	if err != nil {
 		return nil, toGRPCError(err)
 	}
+	if la.WalletID != w.ID {
+		return nil, NotFoundError("")
+	}
 
-	if tabapay.IsFrictionlessAuthentication(*lookupResp) {
-		err = s.b.GMT().Authenticate3DS(ctx, gmt.Authenticate3DSArgs{
-			OutgoingPaymentID:      req.GetOutgoingPaymentID(),
-			JWT:                    "",
-			ThreeDSID:              req.GetThreeDSID(),
-			ThreeDSVersion:         lookupResp.Version,
-			ProcessorTransactionID: lookupResp.ProcessorTransactionID,
-			DsTransactionID:        lookupResp.DsTransactionID,
-			Status:                 lookupResp.Status,
-			UCAF:                   lookupResp.UCAF,
-			XID:                    lookupResp.XID,
-		})
-		if err != nil {
-			return nil, toGRPCError(err)
-		}
+	lookupResp, err := s.b.Tabapay().Lookup3DS(ctx, tabapay.Lookup3DSArgs{
+		ThreeDSID:               req.GetThreeDSID(),
+		IdempotencyKey:          req.GetIdempotencyKey(),
+		CardID:                  session.CardID,
+		AuthenticationIndicator: tabapay.AuthenticatorIndicatorPayment,
+		TransactionMode:         tabapay.TransactionModeComputer,
+		ProductCode:             tabapay.ProductCodeQuasiCashTransaction,
+		DeviceChannel:           tabapay.DeviceChannelBrowser,
+		Amount: currency.Amount{
+			Value:    session.Amount,
+			Currency: currency.ParseCurrency(session.Currency),
+		},
+		BrowserInfo: tabapay.BrowserInfo("true|Mozilla/5.0 iPhone|text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8|true|en-US|32|414|896|300|127.0.0.1"),
+	})
+	if err != nil {
+		return nil, toGRPCError(err)
 	}
 
 	return &backendv1.Lookup3DSResponse{
@@ -89,36 +122,26 @@ func (s *rpcService) Authenticate3DS(
 		return nil, UnauthenticatedError("Unauthenticated.")
 	}
 
-	op, err := s.b.OpenPayments().GetOutgoingPayment(ctx, req.GetOutgoingPaymentID())
+	session, err := s.b.Tabapay().Get3DSSession(ctx, req.GetThreeDSID())
 	if err != nil {
 		return nil, toGRPCError(err)
 	}
 
-	// make sure payment belongs to wallet
-	_, err = s.b.Transactions().GetTransactionByForeignID(ctx, w.ID, op.ID)
-	if err != nil {
-		return nil, toGRPCError(err)
-	}
-
-	authResp, err := s.b.Tabapay().Authenticate3DS(ctx, tabapay.Authenticate3DSArgs{
-		OutgoingPaymentID: req.GetOutgoingPaymentID(),
-		ThreeDSID:         req.GetThreeDSID(),
-		JWT:               req.GetJwt(),
+	la, err := s.b.LinkedAccounts().GetByProviderID(ctx, linkedaccounts.GetByProviderIDArgs{
+		Provider:   tabapay.ProviderName,
+		ProviderID: session.CardID,
 	})
 	if err != nil {
 		return nil, toGRPCError(err)
 	}
+	if la.WalletID != w.ID {
+		return nil, NotFoundError("")
+	}
 
-	err = s.b.GMT().Authenticate3DS(ctx, gmt.Authenticate3DSArgs{
-		OutgoingPaymentID:      req.GetOutgoingPaymentID(),
-		JWT:                    req.GetJwt(),
-		ThreeDSID:              req.GetThreeDSID(),
-		ThreeDSVersion:         authResp.Version3DS,
-		ProcessorTransactionID: authResp.ProcessorTransactionID,
-		DsTransactionID:        authResp.DsTransactionID,
-		Status:                 authResp.Status,
-		UCAF:                   authResp.UCAF,
-		XID:                    authResp.XID,
+	authResp, err := s.b.Tabapay().Authenticate3DS(ctx, tabapay.Authenticate3DSArgs{
+		IdempotencyKey: req.GetIdempotencyKey(),
+		ThreeDSID:      req.GetThreeDSID(),
+		JWT:            req.GetJwt(),
 	})
 	if err != nil {
 		return nil, toGRPCError(err)
--- go/backend/linkedaccounts/ops/service_test.go
+++ go/backend/linkedaccounts/ops/service_test.go
@@ -73,8 +73,6 @@ func TestLinkedAccounts(s *testing.T) {
 		laByProviderID, err := c.LinkedAccounts.GetByProviderID(ctx, linkedaccounts.GetByProviderIDArgs{
 			Provider:   "mx",
 			ProviderID: "123",
-			Type:       "bank",
-			WalletID:   wallet.ID,
 		})
 		require.NoError(t, err)
 		assert.Equal(t, linkedAccount.ID, laByProviderID.ID)
