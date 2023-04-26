8e523f2173b8a11872098a56a6e598d3edf3b3ed
Donovan Changfoot <don@fynbos.dev>
2023-04-26 16:10:13 +0200

    refactor(backend): record 3ds session and its revisions

--- go/backend/providers/tabapay/ops/types.go
+++ go/backend/providers/tabapay/ops/types.go
@@ -1,6 +1,11 @@
 package ops
 
-import "gitlab.com/fynbos/backend/currency"
+import (
+	"database/sql"
+	"time"
+
+	"gitlab.com/fynbos/backend/currency"
+)
 
 type PullFromCardArgs struct {
 	WalletID            string
@@ -9,3 +14,27 @@ type PullFromCardArgs struct {
 	SettlementAccountID string
 	Amount              currency.Amount
 }
+
+type dbThreeDSSession struct {
+	ID                     string       `db:"id"`
+	CardID                 string       `db:"card_id"`
+	OrderID                string       `db:"order_id"`
+	Revision               int          `db:"revision"`
+	Amount                 string       `db:"amount"`
+	Currency               string       `db:"currency"`
+	Version                string       `db:"version"`
+	Enrolled               string       `db:"enrolled"`
+	ProcessorTransactionID string       `db:"processor_transaction_id"`
+	DsTransactionID        string       `db:"ds_transaction_id"`
+	Status                 string       `db:"status"`
+	ECI                    string       `db:"eci"`
+	UCAF                   string       `db:"ucaf"`
+	XID                    string       `db:"xid"`
+	ChallengeURL           string       `db:"challenge_url"`
+	Payload                string       `db:"payload"`
+	InitAt                 time.Time    `db:"init_at"`
+	LookupAt               sql.NullTime `db:"lookup_at"`
+	AuthenticatedAt        sql.NullTime `db:"authenticated_at"`
+}
+
+var dbThreeDSSessionFields = "id, card_id, order_id, revision, amount, currency, version, enrolled, processor_transaction_id, ds_transaction_id, status, eci, ucaf, xid, challenge_url, payload, init_at, lookup_at, authenticated_at"
