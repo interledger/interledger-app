afb7cb98bc589a490d5da4914b2a68e052944a0a
Ömer Talha Özdemir <omer@fynbos.dev>
2023-04-26 14:14:45 +0300

    feat(backend): twitter oauth callback handler

--- go/backend/main.go
+++ go/backend/main.go
@@ -75,6 +75,7 @@ import (
 	transactions_client "gitlab.com/fynbos/backend/transactions/client"
 	_twilio "gitlab.com/fynbos/backend/twilio"
 	"gitlab.com/fynbos/backend/twitter"
+	twitter_handler "gitlab.com/fynbos/backend/twitter/callback"
 	twitter_client "gitlab.com/fynbos/backend/twitter/client"
 	"gitlab.com/fynbos/backend/user"
 	user_client "gitlab.com/fynbos/backend/user/client"
@@ -215,6 +216,8 @@ func start(args *cli.StartArgs) {
 	router.Handle("/kratos/login", analytics_webhook.NewHandleLogin(b))
 	router.Handle("/kratos/logout", analytics_webhook.NewHandleLogout(b))
 	router.Handle("/webhooks/persona", kyc_ops.NewHandlePersonaWebhook(b))
+
+	router.Handle("/auth/twitter/callback", twitter_handler.NewTwitterCallbackHandler(b))
 
 	serveHTTP(&http.Server{Addr: ":" + args.OpenPaymentsPort, Handler: open_server.OpenPaymentsHTTPHandler(b)}, &wg)
 
