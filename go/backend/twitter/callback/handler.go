ba778d1fc0a84e4a29ec342267ed7f9d17c81aa7
Ömer Talha Özdemir <omer@fynbos.dev>
2023-04-26 14:14:45 +0300

    feat(backend): twitter oauth callback handler

---
+++ go/backend/twitter/callback/handler.go
@@ -0,0 +1,43 @@
+package handler
+
+import (
+	"net/http"
+
+	"gitlab.com/fynbos/backend/twitter"
+	"gitlab.com/fynbos/backend/twitter/ops"
+	"gitlab.com/fynbos/log"
+	"go.uber.org/zap"
+)
+
+type Backends interface {
+	Twitter() twitter.Client
+}
+
+func NewTwitterCallbackHandler(b Backends) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		state := r.URL.Query().Get("state")
+		if state == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			log.Error("Couldn't fetch state from query params")
+			return
+		}
+		authCode := r.URL.Query().Get("code")
+		if authCode == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			log.Error("Couldn't fetch auth code from query params")
+			return
+		}
+
+		_, err := b.Twitter().CreateAccessToken(r.Context(), &ops.CreateAccessTokenArgs{
+			State:    state,
+			AuthCode: authCode,
+		})
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Error("Error creating access token", zap.Error(err))
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+	}
+}
