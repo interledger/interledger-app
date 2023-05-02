ddf848e842d311c50a674c2f2615d017a4960f4b
Donovan Changfoot <don@fynbos.dev>
2023-05-02 11:38:25 +0200

    feat(backend): store tokenized basis theory card

--- go/backend/providers/basistheory/ops/ops.go
+++ go/backend/providers/basistheory/ops/ops.go
@@ -2,10 +2,58 @@ package ops
 
 import (
 	"context"
+	"database/sql"
+	"errors"
+	"fmt"
 
 	"gitlab.com/fynbos/backend/providers/basistheory"
+	"gitlab.com/fynbos/backend/providers/basistheory/external"
+)
+
+const (
+	cardFields       = "id, wallet_id, token_id, expiration_month, expiration_year, tokenized_number, created_at, updated_at"
+	insertCardFields = "wallet_id, token_id, expiration_month, expiration_year, tokenized_number"
 )
 
 func CreateCard(ctx context.Context, b Backends, tokenID, walletID string) (*basistheory.Card, error) {
-	return nil, nil
+	var card basistheory.Card
+	err := b.DB().GetContext(
+		ctx,
+		&card,
+		fmt.Sprintf("SELECT %s FROM basistheory_cards WHERE wallet_id=$1 AND token_id=$2;", cardFields),
+		walletID,
+		tokenID,
+	)
+	if err != nil && !errors.Is(err, sql.ErrNoRows) {
+		return nil, fmt.Errorf("%w %s", basistheory.ErrInternal, err)
+	}
+	if card.ID != "" {
+		return &card, nil
+	}
+
+	token, err := b.External().GetToken(ctx, tokenID)
+	if err != nil {
+		return nil, fmt.Errorf("%w %s", basistheory.ErrInternal, err)
+	}
+
+	cardData, err := external.ExtractCardDataFrom(token)
+	if err != nil {
+		return nil, fmt.Errorf("%w %s.", basistheory.ErrInternal, err)
+	}
+
+	err = b.DB().GetContext(
+		ctx,
+		&card,
+		fmt.Sprintf("INSERT INTO basistheory_cards (%s) VALUES ($1, $2, $3, $4, $5) RETURNING %s", insertCardFields, cardFields),
+		walletID,
+		token.Id,
+		cardData.ExpirationMonth,
+		cardData.ExpirationYear,
+		cardData.Number,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("%w %s", basistheory.ErrInternal, err)
+	}
+
+	return &card, nil
 }
