565194c586ec5409b07ceebb7769a02e8656c145
Donovan Changfoot <don@fynbos.dev>
2023-05-05 16:26:22 +0200

    feat(backend): fail workflow if card network response is unsuccessful

--- go/backend/providers/tabapay/external/types.go
+++ go/backend/providers/tabapay/external/types.go
@@ -351,3 +351,35 @@ type (
 		XID                    string `json:"XID"`
 	}
 )
+
+func (t *RetrieveTransactionResponse) GetFees() *Fees {
+	if t.Fees != nil {
+		return t.Fees
+	}
+
+	return &Fees{}
+}
+
+func (t *RetrieveTransactionResponse) GetReversal() *Reversal {
+	if t.Reversal != nil {
+		return t.Reversal
+	}
+
+	return &Reversal{}
+}
+
+func (t *CreateTransactionResponse) GetFees() *Fees {
+	if t.Fees != nil {
+		return t.Fees
+	}
+
+	return &Fees{}
+}
+
+func (t *CreateTransactionResponse) GetCard() *CardResponse {
+	if t.Card != nil {
+		return t.Card
+	}
+
+	return &CardResponse{}
+}
