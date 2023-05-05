e834411360d4b3d1577f78bc2d88e479f76b8c19
barnard <barnard@fynbos.dev>
2023-05-05 11:27:39 +0200

    feat(backends): Add features override in the DB per wallet

--- go/backend/features/ops/ops.go
+++ go/backend/features/ops/ops.go
@@ -2,6 +2,9 @@ package ops
 
 import (
 	"context"
+	"database/sql"
+	"errors"
+	"fmt"
 	"strings"
 
 	"gitlab.com/fynbos/backend/kyc"
@@ -9,7 +12,40 @@ import (
 	"gitlab.com/fynbos/backend/features"
 )
 
+func SetFeatures(ctx context.Context, b Backends, walletID string, feat features.WalletFeatures) (*features.WalletFeatures, error) {
+
+	_, err := b.DB().ExecContext(ctx, "INSERT INTO wallet_features "+
+		"(wallet_id, send_enabled, receive_enabled, linked_accounts_enabled, cards_enabled, banks_enabled, identities_enabled, twitter_enabled) "+
+		"VALUES ($1, $2, $3, $4, $5, $6, $7, $8)  ON CONFLICT (wallet_id) DO UPDATE SET "+
+		"send_enabled = excluded.send_enabled, "+
+		"receive_enabled = excluded.receive_enabled, "+
+		"linked_accounts_enabled = excluded.linked_accounts_enabled, "+
+		"cards_enabled = excluded.cards_enabled, "+
+		"banks_enabled = excluded.banks_enabled, "+
+		"identities_enabled = excluded.identities_enabled, "+
+		"twitter_enabled = excluded.twitter_enabled, "+
+		"updated_at=now()",
+		walletID, feat.SendEnabled, feat.ReceiveEnabled, feat.LinkedAccEnabled, feat.CardsEnabled, feat.BanksEnabled, feat.IdentitiesEnabled, feat.TwitterEnabled)
+	if err != nil {
+		return nil, fmt.Errorf("%w %s", features.ErrInternal, err)
+	}
+
+	return Features(ctx, b, walletID)
+}
+
 func Features(ctx context.Context, b Backends, walletID string) (*features.WalletFeatures, error) {
+	// Check DB for feature overrides
+	var res features.WalletFeatures
+	err := b.DB().GetContext(ctx, &res,
+		"SELECT send_enabled, receive_enabled, linked_accounts_enabled, cards_enabled, banks_enabled, identities_enabled, twitter_enabled FROM wallet_features WHERE wallet_id=$1",
+		walletID)
+	if err == nil {
+		return &res, nil
+	}
+	if err != nil && !errors.Is(err, sql.ErrNoRows) {
+		return nil, fmt.Errorf("%w %s", features.ErrInternal, err)
+	}
+
 	kycStatus, err := b.KYC().GetKYCStatus(ctx, walletID)
 	if err != nil {
 		return nil, err
@@ -20,26 +56,30 @@ func Features(ctx context.Context, b Backends, walletID string) (*features.Walle
 	}
 
 	// Identities are enabled default everywhere
-	res := &features.WalletFeatures{
+	res = features.WalletFeatures{
 		IdentitiesEnabled: true,
 		TwitterEnabled:    true,
 	}
 
 	id, err := b.KYC().GetIndividualDetails(ctx, walletID)
+	if err != nil {
+		return nil, err
+	}
+
 	if id.CountryCode == "US" {
-		res.RecvEnabled = true
+		res.ReceiveEnabled = true
 		res.LinkedAccEnabled = true
 		res.BanksEnabled = true
 		res.CardsEnabled = true
 	}
-	if id.Address != nil && !isGMTSendState(id.Address.State) {
+	if id.Address != nil && isGMTSendState(id.Address.State) {
 		res.SendEnabled = true
 		res.LinkedAccEnabled = true
 		res.BanksEnabled = true
 		res.CardsEnabled = true
 	}
 
-	return res, nil
+	return &res, nil
 }
 
 var gmtUSStates = []string{
