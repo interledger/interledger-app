ad8c7b9d371b04038253a080e8df972e74c7c4ab
Cairin Michie <cairinmichie@gmail.com>
2023-05-05 17:03:34 +0200

    feat(botanist): update wallet pages
    Also upgrade all dependencies and migrations that needed to be done.

---
+++ typescript/botanist/app/components/Chip.tsx
@@ -0,0 +1,29 @@
+import type { FC, ReactNode } from 'react'
+import clsx from 'clsx'
+
+// TODO: Refactor to use static string types for colours rather
+export enum ChipColor {
+  green = 'bg-green-200 text-green-800',
+  purple = 'bg-purple-200 text-purple-800',
+  orange = 'bg-orange-200 text-orange-800',
+  yellow = 'bg-yellow-200 text-yellow-800',
+  blue = 'bg-blue-200 text-blue-800'
+}
+
+export type ChipProps = {
+  children?: ReactNode
+  color: ChipColor
+}
+
+export const Chip: FC<ChipProps> = ({ children, color = ChipColor.blue }) => {
+  return (
+    <div
+      className={clsx(
+        'flex items-center justify-center rounded-lg px-3 py-1.5 text-xs',
+        color
+      )}
+    >
+      {children}
+    </div>
+  )
+}
