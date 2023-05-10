cb5946725d79a9848109c131e57fceeea57f35d6
Donovan Changfoot <don@fynbos.dev>
2023-05-10 16:04:04 +0200

    feat(backend): basistheory create card token

--- go/backend/providers/basistheory/external/api.go
+++ go/backend/providers/basistheory/external/api.go
@@ -4,8 +4,10 @@ import (
 	"context"
 
 	"github.com/Basis-Theory/basistheory-go/v3"
+	bt "gitlab.com/fynbos/backend/providers/basistheory"
 )
 
 type Client interface {
 	GetToken(ctx context.Context, id string) (*basistheory.Token, error)
+	CreateCardToken(ctx context.Context, args bt.CreateCardArgs) (*basistheory.CreateTokenResponse, error)
 }
