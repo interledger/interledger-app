1405571edfe87e66db3cc4397ab9fd3563d0018c
Matt de Haast <matt@fynbos.dev>
2023-05-10 14:06:20 +0200

    temp: logging debug

--- go/backend/vault/vault.go
+++ go/backend/vault/vault.go
@@ -8,6 +8,7 @@ import (
 	auth "github.com/hashicorp/vault/api/auth/kubernetes"
 	"gitlab.com/fynbos/env"
 	"gitlab.com/fynbos/log"
+	"go.uber.org/zap"
 	"os"
 )
 
@@ -86,6 +87,8 @@ func (c client) Sign(keyName string, input string) ([]byte, error) {
 	}
 
 	signatureBase64Url := resp.Data["signature"].(string)[9:] // remove "vault:v1:" prefix
+
+	log.Info("Vault signing", zap.Any("resp", resp), zap.String("signature", resp.Data["signature"].(string)))
 
 	signature, err := base64.RawURLEncoding.DecodeString(signatureBase64Url)
 	if err != nil {
