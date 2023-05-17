03e7cb7389d82e0433d4944c5258703f506dd99e
Ömer Talha Özdemir <omer@fynbos.dev>
2023-05-17 14:01:24 +0300

    feat(backend): small tweaks from review
    small tweaks from the review to ensure a nicer
    structure

--- go/backend/vault/vault.go
+++ go/backend/vault/vault.go
@@ -9,6 +9,7 @@ import (
 	"gitlab.com/fynbos/env"
 	"gitlab.com/fynbos/log"
 	"os"
+	"strings"
 )
 
 type client struct {
@@ -85,7 +86,7 @@ func (c client) Sign(keyName string, input string) ([]byte, error) {
 		return nil, err
 	}
 
-	signatureBase64Url := resp.Data["signature"].(string)[9:] // remove "vault:v1:" prefix
+	signatureBase64Url := strings.TrimPrefix(resp.Data["signature"].(string), "vault:1:") // remove "vault:v1:" prefix
 
 	signature, err := base64.StdEncoding.DecodeString(signatureBase64Url)
 	if err != nil {
