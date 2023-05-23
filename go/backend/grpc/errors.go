e0de938a20fb24527ef3e7ae8395d0ad5cc4798e
Matt de Haast <matt@fynbos.dev>
2023-05-23 10:42:56 +0200

    feat(identities): rpc error
    add duplicate rpc error

--- go/backend/grpc/errors.go
+++ go/backend/grpc/errors.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"github.com/go-playground/validator/v10"
+	"gitlab.com/fynbos/backend/identities"
 
 	"gitlab.com/fynbos/backend/linkedaccounts"
 	"gitlab.com/fynbos/backend/signup"
@@ -20,10 +21,11 @@ import (
 var errorStatus = map[error]error{
 	user.ErrNoUserFound: status.Error(codes.Unauthenticated, "Unauthenticated"),
 	//mx.ErrNotFound:       status.Error(codes.NotFound, "Bank account not found"),
-	twilio.ErrInvalidOTP:       NewValidationError("OTP", "Could not validate OTP"),
-	user.ErrDuplicateWallet:    status.Error(codes.AlreadyExists, "Wallet already exists"),
-	linkedaccounts.ErrNotFound: NotFoundError("linked account not found"),
-	signup.ErrDuplicatePhone:   status.Error(codes.AlreadyExists, "Phone number already exists with a user."),
+	twilio.ErrInvalidOTP:        NewValidationError("OTP", "Could not validate OTP"),
+	user.ErrDuplicateWallet:     status.Error(codes.AlreadyExists, "Wallet already exists"),
+	linkedaccounts.ErrNotFound:  NotFoundError("linked account not found"),
+	signup.ErrDuplicatePhone:    status.Error(codes.AlreadyExists, "Phone number already exists with a user."),
+	identities.ErrAlreadyExists: status.Error(codes.AlreadyExists, "Identity already exists"),
 }
 
 func validationDesc(fe validator.FieldError) string {
