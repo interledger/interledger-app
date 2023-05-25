9b1540d2cf1f62606a8da39174da7a0ad9ada5b1
Cairin Michie <cairinmichie@gmail.com>
2023-05-25 10:52:44 +0200

    feat(protea): twitter identities

--- typescript/protea/app/routes/settings_.linked-identities.tsx
+++ typescript/protea/app/routes/settings_.linked-identities.tsx
@@ -2,7 +2,16 @@ import type { LoaderArgs, MetaFunction } from '@remix-run/node'
 import { json } from '@remix-run/node'
 import { useLoaderData } from '@remix-run/react'
 import { route } from 'routes-gen'
-import { Card, Layouts, Router, Snackbar } from '~/components'
+import {
+  Card,
+  Chip,
+  ChipColor,
+  Icon,
+  Layouts,
+  Router,
+  Snackbar,
+  TwitterIcon
+} from '~/components'
 import { getKycStatus, getLinkedIdentities } from '~/lib/wallet.server'
 import { useState } from 'react'
 import { getSnackbar } from '~/lib/snackbar.server'
@@ -36,35 +45,66 @@ export default function Page() {
 
   const [showSnackbar, setSnackbar] = useState<boolean>(snackbar.show ?? false)
 
-  // TODO we need to add a fallback if we can't open a popup window
-  const openPopup = () => {
-    window.open('/connect/twitter', 'twitter_connect', 'width=700,height=560')
-  }
-
   return (
     <>
-      <Card>
-        <div>
-          <p>Twitter</p>
-        </div>
-        {linkedIdentities.map((identity) => (
+      {/*TODO Handle other identity types when we get there*/}
+      {linkedIdentities.length > 0 && (
+        <Card>
+          {linkedIdentities.map((identity) => (
+            <Router
+              key={identity.id}
+              className='mt-2 flex items-center justify-between rounded-xl bg-nav p-3 text-medium hover:bg-nav-hover'
+              to={route('/settings/linked-identities/:identityId', {
+                identityId: identity.id
+              })}
+            >
+              <div className='flex space-x-3'>
+                <TwitterIcon className='text-medium' />
+                <span>@{identity.identifier}</span>
+              </div>
+              <div className='flex space-x-3'>
+                {identity.state == 'verified' && (
+                  <Chip color={ChipColor.green}>Verified</Chip>
+                )}
+                {identity.state == 'unverified' && (
+                  <Chip color={ChipColor.yellow}>Unverified</Chip>
+                )}
+                {identity.state == 'failed' && (
+                  <Chip color={ChipColor.red}>Failed</Chip>
+                )}
+                {identity.state == 'pending' && (
+                  <Chip color={ChipColor.orange}>Pending</Chip>
+                )}
+                <Icon>navigate_next</Icon>
+              </div>
+            </Router>
+          ))}
           <Router
-            key={identity.id}
-            className='mt-2 flex items-center justify-between rounded-xl bg-nav p-3 text-medium hover:bg-nav-hover'
-            to={route('/settings/linked-identities/:identityId', {
-              identityId: identity.id
-            })}
+            className='mt-4 text-sm font-medium text-primary rounded focus-visible:outline focus-visible:outline-2 focus-visible:outline-focus'
+            to={route('/connect/twitter')}
           >
-            {identity.identifier} - ({identity.state})
+            Link another identity
           </Router>
-        ))}
-        <a
-          className='text-sm font-medium text-primary rounded focus-visible:outline focus-visible:outline-2 focus-visible:outline-focus'
-          onClick={openPopup}
-        >
-          Link twitter identity
-        </a>
-      </Card>
+        </Card>
+      )}
+      {linkedIdentities.length == 0 && (
+        <Card className='space-y-4'>
+          <div className='flex items-center space-x-4'>
+            <div className='flex items-center justify-between rounded-full bg-nav p-5 text-medium'>
+              <TwitterIcon />
+            </div>
+            <div className='flex flex-col space-y-1'>
+              <h1 className='font-medium text-medium'>Twitter</h1>
+              <Router
+                className='text-sm font-medium text-primary'
+                to={route('/connect/twitter')}
+              >
+                Link a Twitter identity
+              </Router>
+            </div>
+          </div>
+        </Card>
+      )}
       <Snackbar
         message={snackbar.message}
         action={snackbar.action}
@@ -72,7 +112,6 @@ export default function Page() {
         show={showSnackbar}
         id='cookie-snackbar'
         dismissAfter={3000}
-        offset
         onClose={() => setSnackbar(false)}
       />
     </>
