c7ec73d56c844997c2126a66e851eed923128f3d
barnard <barnard@fynbos.dev>
2023-05-29 18:37:34 +0200

    fix(backends): Change GMT update transaction Payload

--- go/backend/providers/gmt/external/types.go
+++ go/backend/providers/gmt/external/types.go
@@ -867,7 +867,7 @@ type WsVoidTransactions struct {
 }
 
 type WsChangeRequestData struct {
-	XMLName                   xml.Name `xml:"wsChangeRequestData"`
+	XMLName                   xml.Name `xml:"data"`
 	XmlNS                     string   `xml:"xmlns:a,attr"`
 	ReceiverAccount           string   `xml:"a:ReceiverAccount,omitempty"`
 	ReceiverAccountType       string   `xml:"a:ReceiverAccountType,omitempty"`
