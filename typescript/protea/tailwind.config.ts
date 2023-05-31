aa55963ecb93bedd67204c49eeb3754c42f1b920
Cairin Michie <cairinmichie@gmail.com>
2023-05-31 16:15:22 +0200

    feat(protea): scaffold render functions for marketing pages

--- typescript/protea/tailwind.config.ts
+++ typescript/protea/tailwind.config.ts
@@ -1,5 +1,7 @@
-const colors = require('tailwindcss/colors')
-/** @type {import('tailwindcss').Config} */
+import type { Config } from 'tailwindcss'
+import colors from 'tailwindcss/colors'
+// const colors = require('tailwindcss/colors')
+
 module.exports = {
   mode: 'jit',
   content: ['./app/**/*.{ts,tsx}'],
@@ -10,6 +12,21 @@ module.exports = {
       sans: ['Inter'],
       mono: ['"JetBrains Mono"'],
       icon: ['"Material Symbols Outlined"', { fontFeatureSettings: '"liga"' }]
+    },
+    fontSize: {
+      xs: ['0.75rem', { lineHeight: '1rem' }],
+      sm: ['0.875rem', { lineHeight: '1.25rem' }],
+      base: ['1rem', { lineHeight: '1.5rem' }],
+      lg: ['1.125rem', { lineHeight: '1.75rem' }],
+      xl: ['1.25rem', { lineHeight: '1.75rem' }],
+      '2xl': ['1.5rem', { lineHeight: '2rem' }],
+      '3xl': ['1.875rem', { lineHeight: '2.25rem' }],
+      '4xl': ['2.25rem', { lineHeight: '2.6875rem' }],
+      '5xl': ['3rem', { lineHeight: '3.5625rem' }],
+      '6xl': ['3.75rem', { lineHeight: '4.5rem' }],
+      '7xl': ['4.5rem', { lineHeight: '15.375rem' }],
+      '8xl': ['6rem', { lineHeight: '7.1875rem' }],
+      '9xl': ['8rem', { lineHeight: '9.5625rem' }]
     },
     extend: {
       colors: {
@@ -91,4 +108,4 @@ module.exports = {
     }
   },
   plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')]
-}
+} satisfies Config
