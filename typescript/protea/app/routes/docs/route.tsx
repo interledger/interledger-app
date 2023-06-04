c8a7109b604c0b2f83681892d1eed9b631cdb6d8
Cairin Michie <cairinmichie@gmail.com>
2023-06-04 15:32:01 +0200

    feat(protea): amend marketing routes to match new system

---
+++ typescript/protea/app/routes/docs/route.tsx
@@ -0,0 +1,45 @@
+import type { LoaderArgs } from '@remix-run/node'
+import { json } from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
+import { toRemixMeta } from 'react-datocms'
+import type { ApplicationProps } from '~/components'
+import { Layouts, MarketingPageWithSections } from '~/components'
+import type { SectionRecord } from '~/generated/dato-cms-graphql'
+import { getAboutRoute } from '~/lib/marketing.server'
+
+export async function loader({ request }: LoaderArgs) {
+  if (process.env.FYNBOS_ENV == 'prod')
+    throw json(null, { status: 404, statusText: 'Not found' })
+  const { aboutRoute, footer } = await getAboutRoute()
+  return json({ aboutRoute, footer })
+}
+
+export const handle: ApplicationProps = {
+  layout: Layouts.Marketing,
+  scaffold: {
+    header: {},
+    footer: (match) => match.data.footer
+  }
+}
+
+export function meta({ data, params }: any) {
+  return {
+    ...toRemixMeta(data.aboutRoute.seoMeta),
+    'twitter:url': 'https://fynbos.app/docs',
+    'og:url': 'https://fynbos.app/docs'
+  }
+}
+
+export default function Page() {
+  const { aboutRoute } = useLoaderData<typeof loader>()
+  return (
+    <>
+      {aboutRoute?.body.map((section) => (
+        <MarketingPageWithSections
+          key={section.id}
+          section={section as SectionRecord}
+        />
+      ))}
+    </>
+  )
+}
