6621c03c13e3fdc2110417075bc65ff39bc8c9a9
Cairin Michie <cairinmichie@gmail.com>
2023-06-15 10:20:23 +0200

    feat(protea): remove display font from app pages

--- typescript/protea/app/routes/waitlist.tsx
+++ typescript/protea/app/routes/waitlist.tsx
@@ -134,9 +134,7 @@ export default function Page() {
         )}
         {mug.available && (
           <>
-            <span className='font-display text-2xl font-medium'>
-              Congratulations!
-            </span>
+            <span className='text-2xl font-medium'>Congratulations!</span>
             <div className='mt-4 flex flex-col space-y-4 sm:flex-row-reverse sm:items-center sm:space-x-6 sm:space-y-0 sm:space-x-reverse'>
               <span className='text-medium'>
                 You got your hands on a limited edition Fynbos mug.
