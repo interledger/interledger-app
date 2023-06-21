26df11846d22d531bdae4106dbc247e81fc8875f
Cairin Michie <cairinmichie@gmail.com>
2023-06-21 15:39:22 +0200

    feat(protea): remove default button disable on submit
    - Instead make the cursor spin, and we can disable the button manually
    on important pages

--- typescript/protea/app/components/Buttons.tsx
+++ typescript/protea/app/components/Buttons.tsx
@@ -1,4 +1,3 @@
-import { useNavigation } from '@remix-run/react'
 import clsx from 'clsx'
 import type { ButtonHTMLAttributes } from 'react'
 import { forwardRef } from 'react'
@@ -18,12 +17,11 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 
 export const Button = forwardRef<any, ButtonProps>(
   ({ children, shrink, ...buttonProps }, ref) => {
-    const navigation = useNavigation()
     return (
       <button
         ref={ref}
         {...buttonProps}
-        disabled={buttonProps.disabled ?? navigation.state == 'submitting'}
+        disabled={buttonProps.disabled}
         className={clsx(
           'flex h-12 w-full items-center justify-center rounded-full border border-transparent bg-primary px-6 font-display font-medium text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-focus active:ring-blue-400 hover:enabled:bg-blue-400 disabled:cursor-not-allowed disabled:bg-disabled disabled:text-disabled',
           shrink && 'sm:max-w-fit',
@@ -40,12 +38,11 @@ Button.displayName = 'Button'
 
 export const OutlineButton = forwardRef<any, ButtonProps>(
   ({ children, shrink, ...buttonProps }, ref) => {
-    const navigation = useNavigation()
     return (
       <button
         ref={ref}
         {...buttonProps}
-        disabled={buttonProps.disabled ?? navigation.state == 'submitting'}
+        disabled={buttonProps.disabled}
         className={clsx(
           'flex h-12 w-full items-center justify-center rounded-full border border-transparent px-6 font-display font-medium text-primary outline outline-2 -outline-offset-2 outline-blue-500 hover:text-primary-hover hover:outline-hover focus-visible:bg-container-primary disabled:cursor-not-allowed disabled:bg-disabled disabled:text-disabled',
           shrink && 'sm:max-w-fit',
@@ -64,12 +61,11 @@ interface TextButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
 
 export const TextButton = forwardRef<any, TextButtonProps>(
   ({ children, ...buttonProps }, ref) => {
-    const navigation = useNavigation()
     return (
       <button
         ref={ref}
         {...buttonProps}
-        disabled={buttonProps.disabled ?? navigation.state == 'submitting'}
+        disabled={buttonProps.disabled}
         className={clsx(
           'rounded text-sm font-medium text-primary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-4 focus-visible:outline-blue-500 active:ring-blue-400 disabled:cursor-not-allowed disabled:text-disabled',
           buttonProps.className
@@ -89,12 +85,11 @@ interface IconButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 
 export const IconButton = forwardRef<any, IconButtonProps>(
   ({ children, ...buttonProps }, ref) => {
-    const navigation = useNavigation()
     return (
       <button
         ref={ref}
         {...buttonProps}
-        disabled={buttonProps.disabled ?? navigation.state == 'submitting'}
+        disabled={buttonProps.disabled}
         className='-m-3 flex rounded-lg p-3 text-medium focus-visible:outline focus-visible:outline-2 focus-visible:-outline-offset-4 focus-visible:outline-blue-500 active:ring-blue-400 disabled:cursor-not-allowed disabled:text-disabled'
       >
         <Icon className={buttonProps.className}>{children}</Icon>
