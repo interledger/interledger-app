9f3ae58ee68006d0c63bedb704b566ba3f70eee6
Cairin Michie <cairinmichie@gmail.com>
2023-06-21 15:44:33 +0200

    components

--- typescript/protea/app/components/Card.tsx
+++ typescript/protea/app/components/Card.tsx
@@ -1,5 +1,7 @@
+import { NavLink } from '@remix-run/react'
+import type { RemixNavLinkProps } from '@remix-run/react/dist/components'
 import clsx from 'clsx'
-import type { HTMLAttributes } from 'react'
+import type { HTMLAttributes, ReactNode, RefAttributes } from 'react'
 import { forwardRef } from 'react'
 
 const Card = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(
@@ -8,7 +10,7 @@ const Card = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(
       <div
         ref={ref}
         className={clsx(
-          'flex w-full flex-col rounded-2xl bg-container-strong p-4',
+          'flex w-full flex-col rounded-[1.25rem] bg-container-strong p-4',
           className
         )}
         {...props}
@@ -37,7 +39,10 @@ const CardRow = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(
     return (
       <div
         ref={ref}
-        className={clsx('flex w-full justify-between', className)}
+        className={clsx(
+          'mt-4 flex w-full justify-between first-of-type:mt-0',
+          className
+        )}
         {...props}
       />
     )
@@ -45,4 +50,51 @@ const CardRow = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(
 )
 CardRow.displayName = 'CardRow'
 
-export { Card, CardRow, CardTitle }
+const CardColumn = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(
+  ({ className, ...props }, ref) => {
+    return (
+      <div
+        ref={ref}
+        className={clsx(
+          'mt-4 flex w-full flex-col gap-y-2 first-of-type:mt-0',
+          className
+        )}
+        {...props}
+      />
+    )
+  }
+)
+CardColumn.displayName = 'CardColumn'
+
+const CardLink = forwardRef<
+  any,
+  RemixNavLinkProps & RefAttributes<HTMLAnchorElement>
+>(({ children, className, ...props }, ref) => {
+  return (
+    <NavLink
+      ref={ref}
+      className={clsx(
+        'group relative mt-4 flex w-full rounded-xl py-1 first-of-type:mt-6 focus-visible:outline-2 focus-visible:outline-focus',
+        className
+      )}
+      {...props}
+    >
+      {({ isActive }) => (
+        <>
+          <div
+            className={clsx(
+              'absolute -inset-2 flex rounded-xl',
+              isActive ? 'bg-nav-hover' : 'group-hover:bg-nav'
+            )}
+          />
+          <div className={clsx('z-10 flex w-full', className)}>
+            {children as ReactNode}
+          </div>
+        </>
+      )}
+    </NavLink>
+  )
+})
+CardLink.displayName = 'CardLink'
+
+export { Card, CardRow, CardColumn, CardTitle, CardLink }
