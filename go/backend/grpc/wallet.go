baffbdc337973a6411c8b8f88907d9fa1bacc156
barnard <barnard@fynbos.dev>
2023-06-29 11:30:54 +0200

    feat(backend): Add wallet Info rpc call for frontpage render

--- go/backend/grpc/wallet.go
+++ go/backend/grpc/wallet.go
@@ -3,6 +3,14 @@ package grpc
 import (
 	"context"
 	"errors"
+	"sync"
+
+	"gitlab.com/fynbos/backend/openpayments"
+
+	"gitlab.com/fynbos/backend/db"
+	"gitlab.com/fynbos/backend/paymentpointers"
+	"gitlab.com/fynbos/backend/providers/mx"
+	"gitlab.com/fynbos/backend/providers/tabapay"
 	"gitlab.com/fynbos/backend/user"
 	pb "gitlab.com/fynbos/proto/backend/v1"
 )
@@ -47,4 +55,96 @@ func (s *rpcService) GetPublicWalletDetails(ctx context.Context, req *pb.GetPubl
 		Id:         w.ID,
 		PublicName: w.Name,
 	}, toGRPCError(err)
+}
+
+func (s *rpcService) GetWalletInfo(ctx context.Context, _ *pb.Empty) (*pb.WalletInfo, error) {
+	_, err := s.b.Users().UserForContext(ctx)
+	if err != nil && !errors.Is(err, user.ErrNoUserFound) {
+		return nil, ForbiddenError("Unauthenticated.")
+	}
+
+	w, err := s.b.Users().WalletForContext(ctx)
+	if err != nil {
+		return nil, ForbiddenError("Unauthenticated.")
+	}
+
+	var hasWalletAddress, hasCard, hasBank, hasIdentities, hasTxs bool
+	var anyErr error
+	var wg sync.WaitGroup
+	var pp paymentpointers.PaymentPointer
+
+	wg.Add(4)
+
+	go func() {
+		defer wg.Done()
+		opp, err := s.b.OpenPayments().GetWalletPaymentPointer(ctx, w.ID)
+		if errors.Is(err, openpayments.ErrPaymentPointerNotFound) {
+			return
+		}
+		if err != nil {
+			anyErr = err
+			return
+		}
+
+		pp, err = paymentpointers.Parse(opp.URL)
+		if err != nil {
+			anyErr = err
+			return
+		}
+		hasWalletAddress = true
+	}()
+
+	go func() {
+		defer wg.Done()
+		lal, err := s.b.LinkedAccounts().ListByWalletId(ctx, w.ID)
+		if err != nil {
+			anyErr = err
+			return
+		}
+
+		for _, la := range lal {
+			if la.Provider == mx.ProviderName {
+				hasBank = true
+			}
+			if la.Provider == tabapay.ProviderName {
+				hasCard = true
+			}
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		ids, err := s.b.Identities().List(ctx, w.ID)
+		if err != nil {
+			anyErr = err
+			return
+		}
+		hasIdentities = len(ids) > 0
+	}()
+
+	go func() {
+		defer wg.Done()
+		txs, err := s.b.Transactions().List(ctx, db.Pagination{PageSize: 1}, w.ID)
+		if err != nil {
+			anyErr = err
+			return
+		}
+		hasTxs = len(txs) > 0
+	}()
+
+	wg.Wait()
+	if anyErr != nil {
+		return nil, toGRPCError(anyErr)
+	}
+
+	return &pb.WalletInfo{
+		WalletID:         w.ID,
+		Url:              pp.String(),
+		FormattedURL:     pp.ShortString(),
+		HasCard:          hasCard,
+		HasBank:          hasBank,
+		HasIdentities:    hasIdentities,
+		HasTransacted:    hasTxs,
+		HasWalletAddress: hasWalletAddress,
+	}, nil
 }
