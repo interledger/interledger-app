3f1769794c81a991091e765e07b56ae99103af3b
Donovan Changfoot <don@fynbos.dev>
2023-07-04 12:25:30 +0200

    refactor(backend): track persona inquiry state

--- go/backend/kyc/ops/persona.go
+++ go/backend/kyc/ops/persona.go
@@ -25,7 +25,7 @@ func GetPersonaInquiry(ctx context.Context, b Backends, cl persona.Client, walle
 	// Check if there is an ongoing inquiry for this wallet that we can resume
 	var ongoingID string
 	err = b.DB().GetContext(ctx, &ongoingID, "SELECT external_id FROM kyc_persona_inquiries WHERE wallet_id=$1 AND state IN ($2, $3, $4)",
-		walletID, "pending", "created", "started")
+		walletID, persona.InquiryPending, persona.InquiryCreated, persona.InquiryStarted)
 	if err != nil && !errors.Is(err, sql.ErrNoRows) {
 		return nil, fmt.Errorf("%w %s", kyc.ErrInternal, err)
 	}
@@ -44,6 +44,7 @@ func GetPersonaInquiry(ctx context.Context, b Backends, cl persona.Client, walle
 		return &kyc.PersonaInquiry{
 			ID:           inquiry.ID,
 			SessionToken: inquiry.Meta.SessionToken,
+			Status:       persona.InquiryStatus(inquiry.Attributes.Status),
 		}, nil
 	}
 
@@ -88,7 +89,8 @@ func GetPersonaInquiry(ctx context.Context, b Backends, cl persona.Client, walle
 	}
 
 	return &kyc.PersonaInquiry{
-		ID: inquiry.ID,
+		ID:     inquiry.ID,
+		Status: persona.InquiryStatus(inquiry.Attributes.Status),
 	}, nil
 }
 
