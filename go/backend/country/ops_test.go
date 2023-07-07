50864b71cc51f5068558632762ac4548c8a748ad
Donovan Changfoot <don@fynbos.dev>
2023-07-07 11:02:31 +0200

    fix(backend): parse state from persona account sync

---
+++ go/backend/country/ops_test.go
@@ -0,0 +1,23 @@
+package country_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"gitlab.com/fynbos/backend/country"
+)
+
+func TestGetStateCode(t *testing.T) {
+	lookup := []string{"california", "California", " caliFornia", " CaliforniA ", "california "}
+
+	for _, l := range lookup {
+		code, err := country.GetStateCode(country.US, l)
+		require.NoError(t, err)
+		assert.Equal(t, "US-CA", code)
+	}
+
+	code, err := country.GetStateCode(country.US, "w")
+	require.Error(t, err)
+	assert.Empty(t, code)
+}
