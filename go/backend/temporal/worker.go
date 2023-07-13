6a84c9b2d361e11a5a9f16be006ffa1e842f4ab1
Ömer Talha Özdemir <omer@fynbos.dev>
2023-07-13 11:33:41 +0300

    feat(backend): website identities

--- go/backend/temporal/worker.go
+++ go/backend/temporal/worker.go
@@ -23,6 +23,8 @@ func NewTemporalWorker(b Backends) (worker.Worker, error) {
 	//Identities
 	w.RegisterActivity(platforms.NewTwitterActivity(b))
 	w.RegisterWorkflow(platforms.TwitterVerifyWorkflow)
+	w.RegisterActivity(platforms.NewDNSActivity(b))
+	w.RegisterWorkflow(platforms.DNSVerifyWorkflow)
 
 	//Twitter
 	w.RegisterActivity(twitter_workflows.NewActivity(b))
--- go/backend/twitter/client/mock/mock.go
+++ go/backend/twitter/client/mock/mock.go
@@ -111,11 +111,12 @@ func (mr *MockClientMockRecorder) PostTweet(ctx, id, text interface{}) *gomock.C
 }
 
 // PublishTweetProof mocks base method.
-func (m *MockClient) PublishTweetProof(ctx context.Context, identityID string) error {
+func (m *MockClient) PublishTweetProof(ctx context.Context, identityID string) (string, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "PublishTweetProof", ctx, identityID)
-	ret0, _ := ret[0].(error)
-	return ret0
+	ret0, _ := ret[0].(string)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
 }
 
 // PublishTweetProof indicates an expected call of PublishTweetProof.
