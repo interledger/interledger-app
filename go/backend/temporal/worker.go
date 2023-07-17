d4c39f14db970e3456b9f049ccee829077cf2195
barnard <barnard@fynbos.dev>
2023-07-17 14:27:53 +0200

    fix(backend): Notify GMT of card 2 card transactions out of main workflow context

--- go/backend/temporal/worker.go
+++ go/backend/temporal/worker.go
@@ -35,6 +35,7 @@ func NewTemporalWorker(b Backends) (worker.Worker, error) {
 	w.RegisterWorkflow(gmt_workflows.Card2ACHTransferWorkflow)
 	w.RegisterWorkflow(gmt_workflows.ACH2CardTransferWorkflow)
 	w.RegisterWorkflow(gmt_workflows.Card2CardTransferWorkflow)
+	w.RegisterWorkflow(gmt_workflows.NotifyGMTCard2CardWorkflow)
 
 	w.RegisterActivity(tabapay_workflows.NewActivity(b))
 	w.RegisterWorkflow(tabapay_workflows.CreateTabapayCardWorkflow)
