4668d81c74ec01c81ea045f0b2f3f7fce41151d8
Donovan Changfoot <don@fynbos.dev>
2023-07-20 10:11:50 +0200

    chore(backend): return country list alphabetically with US at the top

--- go/backend/grpc/countries.go
+++ go/backend/grpc/countries.go
@@ -2,6 +2,7 @@ package grpc
 
 import (
 	"context"
+	"sort"
 
 	"gitlab.com/fynbos/backend/country"
 	backendv1 "gitlab.com/fynbos/proto/backend/v1"
@@ -10,10 +11,21 @@ import (
 func (s *rpcService) GetCountries(
 	ctx context.Context, request *backendv1.Empty,
 ) (*backendv1.GetCountriesResponse, error) {
+
 	var ret []*backendv1.Country
-	for country, details := range country.Details {
-		ret = append(ret, &backendv1.Country{Id: country.String(), Name: details.Name})
+	for c, details := range country.Details {
+		ret = append(ret, &backendv1.Country{Id: c.String(), Name: details.Name})
 	}
+
+	sort.Slice(ret, func(i, j int) bool {
+		if country.Country(ret[i].Id) == country.US {
+			return true
+		}
+		if country.Country(ret[j].Id) == country.US {
+			return false
+		}
+		return ret[i].Name < ret[j].Name
+	})
 
 	return &backendv1.GetCountriesResponse{
 		Countries: ret,
