8cd3179785570a4ee4e209f6a4f7c9ec3fb36c58
Cairin Michie <cairinmichie@gmail.com>
2023-07-24 14:21:40 +0200

    sync

--- typescript/protea/app/routes.d.ts
+++ typescript/protea/app/routes.d.ts
@@ -5,10 +5,10 @@ declare module "routes-gen" {
     "/legal/:jurisdiction?/:slug": { "jurisdiction"?: string, "slug": string };
     "/me/identities/:identityId": { "identityId": string };
     "/settings/keys/add-public": Record<string, never>;
+    "/accounts/:accountId/name": { "accountId": string };
     "/what-is-a-payment-pointer": Record<string, never>;
     "/settings/keys/:keyId": { "keyId": string };
     "/temp-cloudflare-error": Record<string, never>;
-    "/accounts/:accountId": { "accountId": string };
     "/api/maps/geocode": Record<string, never>;
     "/recovery/password": Record<string, never>;
     "/settings/password": Record<string, never>;
@@ -27,9 +27,11 @@ declare module "routes-gen" {
     "/transactions": Record<string, never>;
     "/transactions/:transactionId": { "transactionId": string };
     "/blog/:slug": { "slug": string };
+    "/pay/amount": Record<string, never>;
     "/identities": Record<string, never>;
     "/identities/:identityId": { "identityId": string };
     "/accounts": Record<string, never>;
+    "/accounts/:accountId": { "accountId": string };
     "/contacts": Record<string, never>;
     "/pay/3ds": Record<string, never>;
     "/recovery": Record<string, never>;
@@ -61,10 +63,10 @@ declare module "routes-gen" {
       | ["/legal/:jurisdiction?/:slug", RouteParams["/legal/:jurisdiction?/:slug"]]
       | ["/me/identities/:identityId", RouteParams["/me/identities/:identityId"]]
       | ["/settings/keys/add-public"]
+      | ["/accounts/:accountId/name", RouteParams["/accounts/:accountId/name"]]
       | ["/what-is-a-payment-pointer"]
       | ["/settings/keys/:keyId", RouteParams["/settings/keys/:keyId"]]
       | ["/temp-cloudflare-error"]
-      | ["/accounts/:accountId", RouteParams["/accounts/:accountId"]]
       | ["/api/maps/geocode"]
       | ["/recovery/password"]
       | ["/settings/password"]
@@ -83,9 +85,11 @@ declare module "routes-gen" {
       | ["/transactions"]
       | ["/transactions/:transactionId", RouteParams["/transactions/:transactionId"]]
       | ["/blog/:slug", RouteParams["/blog/:slug"]]
+      | ["/pay/amount"]
       | ["/identities"]
       | ["/identities/:identityId", RouteParams["/identities/:identityId"]]
       | ["/accounts"]
+      | ["/accounts/:accountId", RouteParams["/accounts/:accountId"]]
       | ["/contacts"]
       | ["/pay/3ds"]
       | ["/recovery"]
