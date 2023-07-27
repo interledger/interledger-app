2bcf39463ecb5227ca0bbf16ac12ee41e54d99fb
Cairin Michie <cairinmichie@gmail.com>
2023-07-27 13:41:33 +0200

    fix(protea): remove canSend filter on results

--- typescript/protea/app/routes/pay/route.tsx
+++ typescript/protea/app/routes/pay/route.tsx
@@ -81,7 +81,7 @@ export async function loader({ request }: LoaderArgs) {
       .catch(StatusError)
 
     if (!isGrpcError(response)) {
-      results = response.response.results.filter((v) => v.canSend)
+      results = response.response.results
     }
   }
 
