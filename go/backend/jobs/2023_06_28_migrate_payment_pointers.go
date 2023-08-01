b9b7b22f4febe0248a0db1795ce5c6c6209a97b7
barnard <barnard@fynbos.dev>
2023-08-01 13:42:00 +0200

    feat(backend): Write wallet address on all openpayments tables

--- go/backend/jobs/2023_06_28_migrate_payment_pointers.go
+++ go/backend/jobs/2023_06_28_migrate_payment_pointers.go
@@ -54,3 +54,213 @@ func (a *Activity) MigratePaymentPointers(ctx context.Context) error {
 
 	return nil
 }
+
+func MigrateOpenPaymentsObjects(ctx workflow.Context) error {
+	var a *Activity
+	ao := workflow.ActivityOptions{
+		StartToCloseTimeout: 10 * time.Minute,
+	}
+	ctx = workflow.WithActivityOptions(ctx, ao)
+	logger := workflow.GetLogger(ctx)
+	logger.Info("MigratePaymentPointers workflow started")
+
+	err := workflow.ExecuteActivity(ctx, a.MigrateOpenPaymentsQuotes).Get(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	err = workflow.ExecuteActivity(ctx, a.MigrateOpenPaymentsIncomingPayments).Get(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	err = workflow.ExecuteActivity(ctx, a.MigrateOpenPaymentsOutgoingPayments).Get(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (a *Activity) MigrateOpenPaymentsOutgoingPayments(ctx context.Context) error {
+	type PP struct {
+		ID  string `db:"id"`
+		URL string `db:"url"`
+	}
+
+	type OutgoingPayment struct {
+		ID       string `db:"id"`
+		SenderPP string `db:"send_payment_pointer_id"`
+		RecvPP   string `db:"to_payment_pointer_id"`
+	}
+
+	var AllPP []PP
+	err := a.b.DB().SelectContext(ctx, &AllPP, "SELECT id, url FROM payment_pointers")
+	if err != nil {
+		return err
+	}
+
+	ppMap := make(map[string]string)
+	for _, pp := range AllPP {
+		ppMap[pp.ID] = pp.URL
+	}
+
+	var AllOutgoing []OutgoingPayment
+	err = a.b.DB().SelectContext(ctx, &AllOutgoing, "SELECT op.id, op.to_payment_pointer_id, q.send_payment_pointer_id FROM openpayments_outgoing_payment op INNER JOIN openpayments_quotes q ON q.id = op.quote_id")
+	if err != nil {
+		return err
+	}
+
+	uq, err := a.b.DB().PreparexContext(ctx, "UPDATE openpayments_outgoing_payment SET sender_wallet_address=$1, receiver_wallet_address=$2 WHERE id=$3")
+	if err != nil {
+		return err
+	}
+	defer uq.Close()
+
+	var updateCnt int
+	for _, op := range AllOutgoing {
+		sendPP, ok := ppMap[op.SenderPP]
+		if !ok {
+			return errors.New("failed to lookup sender Payment Pointer")
+		}
+
+		recvPP, ok := ppMap[op.RecvPP]
+		if !ok {
+			return errors.New("failed to lookup receiver Payment Pointer")
+		}
+
+		_, err = uq.ExecContext(ctx, sendPP, recvPP, op.ID)
+		if err != nil {
+			return err
+		}
+
+		updateCnt++
+	}
+
+	logger := activity.GetLogger(ctx)
+	logger.Info("Open payments outgoing payments migration", "updateCnt", updateCnt)
+
+	return nil
+}
+
+func (a *Activity) MigrateOpenPaymentsIncomingPayments(ctx context.Context) error {
+	type PP struct {
+		ID  string `db:"id"`
+		URL string `db:"url"`
+	}
+
+	type IncomingPayment struct {
+		ID       string `db:"id"`
+		SenderPP string `db:"from_payment_pointer_id"`
+		RecvPP   string `db:"payment_pointer_id"`
+	}
+
+	var AllPP []PP
+	err := a.b.DB().SelectContext(ctx, &AllPP, "SELECT id, url FROM payment_pointers")
+	if err != nil {
+		return err
+	}
+
+	ppMap := make(map[string]string)
+	for _, pp := range AllPP {
+		ppMap[pp.ID] = pp.URL
+	}
+
+	var AllIncoming []IncomingPayment
+	err = a.b.DB().SelectContext(ctx, &AllIncoming, "SELECT id, from_payment_pointer_id, payment_pointer_id  FROM openpayments_incoming_payment")
+	if err != nil {
+		return err
+	}
+
+	uq, err := a.b.DB().PreparexContext(ctx, "UPDATE openpayments_incoming_payment SET sender_wallet_address=$1, receiver_wallet_address=$2 WHERE id=$3")
+	if err != nil {
+		return err
+	}
+	defer uq.Close()
+
+	var updateCnt int
+	for _, ip := range AllIncoming {
+		sendPP, ok := ppMap[ip.SenderPP]
+		if !ok {
+			return errors.New("failed to lookup sender Payment Pointer")
+		}
+
+		recvPP, ok := ppMap[ip.RecvPP]
+		if !ok {
+			return errors.New("failed to lookup receiver Payment Pointer")
+		}
+
+		_, err = uq.ExecContext(ctx, sendPP, recvPP, ip.ID)
+		if err != nil {
+			return err
+		}
+
+		updateCnt++
+	}
+
+	logger := activity.GetLogger(ctx)
+	logger.Info("Open payments incoming payments migration", "updateCnt", updateCnt)
+
+	return nil
+}
+
+func (a *Activity) MigrateOpenPaymentsQuotes(ctx context.Context) error {
+	type PP struct {
+		ID  string `db:"id"`
+		URL string `db:"url"`
+	}
+
+	type Quote struct {
+		ID       string `db:"id"`
+		SenderPP string `db:"send_payment_pointer_id"`
+		RecvPP   string `db:"recv_payment_pointer_id"`
+	}
+
+	var AllPP []PP
+	err := a.b.DB().SelectContext(ctx, &AllPP, "SELECT id, url FROM payment_pointers")
+	if err != nil {
+		return err
+	}
+
+	ppMap := make(map[string]string)
+	for _, pp := range AllPP {
+		ppMap[pp.ID] = pp.URL
+	}
+
+	var AllQuotes []Quote
+	err = a.b.DB().SelectContext(ctx, &AllQuotes, "SELECT id, send_payment_pointer_id, recv_payment_pointer_id  FROM openpayments_quotes")
+	if err != nil {
+		return err
+	}
+
+	uq, err := a.b.DB().PreparexContext(ctx, "UPDATE openpayments_quotes SET sender_wallet_address=$1, receiver_wallet_address=$2 WHERE id=$3")
+	if err != nil {
+		return err
+	}
+	defer uq.Close()
+
+	var updateCnt int
+	for _, q := range AllQuotes {
+		sendPP, ok := ppMap[q.SenderPP]
+		if !ok {
+			return errors.New("failed to lookup sender Payment Pointer")
+		}
+
+		recvPP, ok := ppMap[q.RecvPP]
+		if !ok {
+			return errors.New("failed to lookup receiver Payment Pointer")
+		}
+
+		_, err = uq.ExecContext(ctx, sendPP, recvPP, q.ID)
+		if err != nil {
+			return err
+		}
+
+		updateCnt++
+	}
+
+	logger := activity.GetLogger(ctx)
+	logger.Info("Open payments quote migration", "updateCnt", updateCnt)
+
+	return nil
+}
