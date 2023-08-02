7fc9f1de0d45819ad8963c58e8fdb576b662cdee
Matt de Haast <matt@fynbos.dev>
2023-08-02 15:23:18 +0200

    feat(protea): persona details loading state
    ensure the loading of the button on completion
    of the persona inquiry before routing away.

--- typescript/protea/app/routes/personal-details.tsx
+++ typescript/protea/app/routes/personal-details.tsx
@@ -71,6 +71,13 @@ export default function Page() {
     setLoading(!ready)
   }, [ready, setLoading])
 
+  // Unmount make sure the loading state is set to false
+  useEffect(() => {
+    return () => {
+      setLoading(false)
+    }
+  }, [setLoading])
+
   useEffect(() => {
     if (typeof window !== 'undefined' && status == 'ready') {
       personaRef.current = (window as any).Persona
@@ -79,6 +86,7 @@ export default function Page() {
         sessionToken,
         onReady: () => setReady(true),
         onComplete: ({ inquiryId, status, fields }: any) => {
+          setReady(false)
           submit(null, {
             action: '/personal-details',
             method: 'post'
