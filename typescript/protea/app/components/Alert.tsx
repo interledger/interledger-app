ba9fa32532e8b9b09f08a91e57d5b0cc702cb30a
Cairin Michie <cairinmichie@gmail.com>
2023-08-07 15:46:04 +0200

    feat(protea): implement Alert component

---
+++ typescript/protea/app/components/Alert.tsx
@@ -0,0 +1,54 @@
+import clsx from 'clsx'
+import type { HTMLAttributes } from 'react'
+import { forwardRef } from 'react'
+
+const Alert = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(
+  ({ className, ...props }, ref) => {
+    return (
+      <div
+        ref={ref}
+        className={clsx(
+          'flex w-full gap-x-2 rounded-lg bg-nav-active p-2',
+          className
+        )}
+        {...props}
+      />
+    )
+  }
+)
+Alert.displayName = 'Alert'
+
+const AlertTitle = forwardRef<
+  HTMLParagraphElement,
+  HTMLAttributes<HTMLHeadingElement>
+>(({ className, ...props }, ref) => {
+  return (
+    <h2
+      ref={ref}
+      className={clsx('font-medium text-medium', className)}
+      {...props}
+    />
+  )
+})
+AlertTitle.displayName = 'AlertTitle'
+
+const AlertBody = forwardRef<
+  HTMLParagraphElement,
+  HTMLAttributes<HTMLHeadingElement>
+>(({ className, ...props }, ref) => {
+  return <h2 ref={ref} className={clsx('text-medium', className)} {...props} />
+})
+AlertBody.displayName = 'AlertBody'
+
+const AlertContent = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(
+  ({ className, ...props }, ref) => (
+    <div
+      ref={ref}
+      className={clsx('flex w-full flex-col gap-y-2', className)}
+      {...props}
+    />
+  )
+)
+AlertContent.displayName = 'AlertContent'
+
+export { Alert, AlertBody, AlertContent, AlertTitle }
--- typescript/protea/app/components/index.ts
+++ typescript/protea/app/components/index.ts
@@ -1,3 +1,4 @@
+export * from './Alert'
 export * from './Autocomplete'
 export * from './Avatar'
 export * from './Buttons'
