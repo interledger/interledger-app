0025aca2e2ff894ab9d2b66f98b5f4fa8aee4a17
Donovan Changfoot <don@fynbos.dev>
2023-08-15 15:59:13 +0200

    feat(backend): update payment

--- go/backend/payments/api.go
+++ go/backend/payments/api.go
@@ -5,6 +5,6 @@ import "context"
 type Client interface {
 	Lookup(ctx context.Context, id string) (*Payment, error)
 	Create(ctx context.Context, args CreateArgs) (*Payment, error)
-	Update(ctx context.Context, payment Payment) (*Payment, error)
+	Update(ctx context.Context, args UpdateArgs) (*Payment, error)
 	Confirm(ctx context.Context, id string) (*Payment, []RequiredActionType, error)
 }
