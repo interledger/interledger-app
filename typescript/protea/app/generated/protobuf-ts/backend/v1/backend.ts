3c7c1cf01b77bc27164e8ca4bb30487a80846e1b
barnard <barnard@fynbos.dev>
2023-08-17 14:48:35 +0200

    chore(proto): Remove orphaned grpc message

--- typescript/protea/app/generated/protobuf-ts/backend/v1/backend.ts
+++ typescript/protea/app/generated/protobuf-ts/backend/v1/backend.ts
@@ -1798,31 +1798,6 @@ export interface SetSignupCompleteRequest {
     userId: string;
 }
 /**
- * @generated from protobuf message backend.v1.StartMachnetWalletTopupRequest
- */
-export interface StartMachnetWalletTopupRequest {
-    /**
-     * @generated from protobuf field: string fromLinkedAccountId = 1;
-     */
-    fromLinkedAccountId: string;
-    /**
-     * @generated from protobuf field: uint64 amount = 2;
-     */
-    amount: string;
-    /**
-     * @generated from protobuf field: string ipAddress = 3;
-     */
-    ipAddress: string;
-    /**
-     * @generated from protobuf field: string currency = 4;
-     */
-    currency: string;
-    /**
-     * @generated from protobuf field: string idempotencyKey = 5;
-     */
-    idempotencyKey: string;
-}
-/**
  * @generated from protobuf message backend.v1.LookupTransactionRequest
  */
 export interface LookupTransactionRequest {
@@ -8279,81 +8254,6 @@ class SetSignupCompleteRequest$Type extends MessageType<SetSignupCompleteRequest
  * @generated MessageType for protobuf message backend.v1.SetSignupCompleteRequest
  */
 export const SetSignupCompleteRequest = new SetSignupCompleteRequest$Type();
-// @generated message type with reflection information, may provide speed optimized methods
-class StartMachnetWalletTopupRequest$Type extends MessageType<StartMachnetWalletTopupRequest> {
-    constructor() {
-        super("backend.v1.StartMachnetWalletTopupRequest", [
-            { no: 1, name: "fromLinkedAccountId", kind: "scalar", T: 9 /*ScalarType.STRING*/ },
-            { no: 2, name: "amount", kind: "scalar", T: 4 /*ScalarType.UINT64*/ },
-            { no: 3, name: "ipAddress", kind: "scalar", T: 9 /*ScalarType.STRING*/ },
-            { no: 4, name: "currency", kind: "scalar", T: 9 /*ScalarType.STRING*/ },
-            { no: 5, name: "idempotencyKey", kind: "scalar", T: 9 /*ScalarType.STRING*/ }
-        ]);
-    }
-    create(value?: PartialMessage<StartMachnetWalletTopupRequest>): StartMachnetWalletTopupRequest {
-        const message = { fromLinkedAccountId: "", amount: "0", ipAddress: "", currency: "", idempotencyKey: "" };
-        globalThis.Object.defineProperty(message, MESSAGE_TYPE, { enumerable: false, value: this });
-        if (value !== undefined)
-            reflectionMergePartial<StartMachnetWalletTopupRequest>(this, message, value);
-        return message;
-    }
-    internalBinaryRead(reader: IBinaryReader, length: number, options: BinaryReadOptions, target?: StartMachnetWalletTopupRequest): StartMachnetWalletTopupRequest {
-        let message = target ?? this.create(), end = reader.pos + length;
-        while (reader.pos < end) {
-            let [fieldNo, wireType] = reader.tag();
-            switch (fieldNo) {
-                case /* string fromLinkedAccountId */ 1:
-                    message.fromLinkedAccountId = reader.string();
-                    break;
-                case /* uint64 amount */ 2:
-                    message.amount = reader.uint64().toString();
-                    break;
-                case /* string ipAddress */ 3:
-                    message.ipAddress = reader.string();
-                    break;
-                case /* string currency */ 4:
-                    message.currency = reader.string();
-                    break;
-                case /* string idempotencyKey */ 5:
-                    message.idempotencyKey = reader.string();
-                    break;
-                default:
-                    let u = options.readUnknownField;
-                    if (u === "throw")
-                        throw new globalThis.Error(`Unknown field ${fieldNo} (wire type ${wireType}) for ${this.typeName}`);
-                    let d = reader.skip(wireType);
-                    if (u !== false)
-                        (u === true ? UnknownFieldHandler.onRead : u)(this.typeName, message, fieldNo, wireType, d);
-            }
-        }
-        return message;
-    }
-    internalBinaryWrite(message: StartMachnetWalletTopupRequest, writer: IBinaryWriter, options: BinaryWriteOptions): IBinaryWriter {
-        /* string fromLinkedAccountId = 1; */
-        if (message.fromLinkedAccountId !== "")
-            writer.tag(1, WireType.LengthDelimited).string(message.fromLinkedAccountId);
-        /* uint64 amount = 2; */
-        if (message.amount !== "0")
-            writer.tag(2, WireType.Varint).uint64(message.amount);
-        /* string ipAddress = 3; */
-        if (message.ipAddress !== "")
-            writer.tag(3, WireType.LengthDelimited).string(message.ipAddress);
-        /* string currency = 4; */
-        if (message.currency !== "")
-            writer.tag(4, WireType.LengthDelimited).string(message.currency);
-        /* string idempotencyKey = 5; */
-        if (message.idempotencyKey !== "")
-            writer.tag(5, WireType.LengthDelimited).string(message.idempotencyKey);
-        let u = options.writeUnknownFields;
-        if (u !== false)
-            (u == true ? UnknownFieldHandler.onWrite : u)(this.typeName, message, writer);
-        return writer;
-    }
-}
-/**
- * @generated MessageType for protobuf message backend.v1.StartMachnetWalletTopupRequest
- */
-export const StartMachnetWalletTopupRequest = new StartMachnetWalletTopupRequest$Type();
 // @generated message type with reflection information, may provide speed optimized methods
 class LookupTransactionRequest$Type extends MessageType<LookupTransactionRequest> {
     constructor() {
