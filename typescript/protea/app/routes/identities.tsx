f868ec9bb9a4670d6f578b1e9d6ff210a638bf97
barnard <barnard@fynbos.dev>
2023-09-05 15:03:39 +0200

    feat(protea): Add slack identities

--- typescript/protea/app/routes/identities.tsx
+++ typescript/protea/app/routes/identities.tsx
@@ -220,6 +220,68 @@ export default function Page() {
             </div>
           </Card>
         )}
+        {linkedIdentities.slack && (
+          <Card>
+            <CardHeader>
+              <CardTitle>Slack</CardTitle>
+            </CardHeader>
+            {linkedIdentities.slack.map((identity) => (
+              <CardLink
+                key={identity.id}
+                className='mt-2 flex items-center justify-between first-of-type:mt-4'
+                to={route('/identities/:identityId', {
+                  identityId: identity.id
+                })}
+              >
+                <div className='flex space-x-3'>
+                  <DiscordIcon />
+                  <span>{identity.identifier}</span>
+                </div>
+                <div className='flex items-center space-x-3'>
+                  {identity.state == 'verified' && (
+                    <Chip color={ChipColor.green}>Verified</Chip>
+                  )}
+                  {identity.state == 'unverified' && (
+                    <Chip color={ChipColor.yellow}>Unverified</Chip>
+                  )}
+                  {identity.state == 'failed' && (
+                    <Chip color={ChipColor.red}>Failed</Chip>
+                  )}
+                  {identity.state == 'pending' && (
+                    <Chip color={ChipColor.orange}>Pending</Chip>
+                  )}
+                  <Icon>navigate_next</Icon>
+                </div>
+              </CardLink>
+            ))}
+            <CardContent>
+              <Router
+                className='rounded text-sm font-medium text-primary'
+                to={route('/connect/slack')}
+              >
+                Connect another Slack identity
+              </Router>
+            </CardContent>
+          </Card>
+        )}
+        {!linkedIdentities.slack && kycStatus == KycStatus.Approved && (
+          <Card>
+            <div className='flex items-center space-x-4'>
+              <CardIcon>
+                <DiscordIcon />
+              </CardIcon>
+              <div className='flex flex-col space-y-1'>
+                <h3 className='font-medium text-medium'>Discord</h3>
+                <Router
+                  className='text-sm font-medium text-primary'
+                  to={route('/connect/slack')}
+                >
+                  Connect a Slack identity
+                </Router>
+              </div>
+            </div>
+          </Card>
+        )}
         {linkedIdentities.domain && (
           <Card>
             <CardHeader>
