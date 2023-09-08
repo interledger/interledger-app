7e8f845b2f0d9731772dddb742923d87f5569cc7
Matt de Haast <matt@fynbos.dev>
2023-09-08 08:24:08 +0200

    feat(protea): segment page tracking
    add basic page tracking using segment
    
    Signed-off-by: Matt de Haast <matt@fynbos.dev>

--- typescript/protea/app/routes.d.ts
+++ typescript/protea/app/routes.d.ts
@@ -14,12 +14,12 @@ declare module "routes-gen" {
     "/settings/password": Record<string, never>;
     "/waitlist/success": Record<string, never>;
     "/connect/discord": Record<string, never>;
-    "/connect/slack": Record<string, never>;
     "/connect/twitter": Record<string, never>;
     "/contact/success": Record<string, never>;
     "/login/challenge": Record<string, never>;
     "/personal-details": Record<string, never>;
     "/connect/domain": Record<string, never>;
+    "/connect/slack": Record<string, never>;
     "/wallet-address": Record<string, never>;
     "/connect/bank": Record<string, never>;
     "/connect/card": Record<string, never>;
@@ -76,12 +76,12 @@ declare module "routes-gen" {
       | ["/settings/password"]
       | ["/waitlist/success"]
       | ["/connect/discord"]
-      | ["/connect/slack"]
       | ["/connect/twitter"]
       | ["/contact/success"]
       | ["/login/challenge"]
       | ["/personal-details"]
       | ["/connect/domain"]
+      | ["/connect/slack"]
       | ["/wallet-address"]
       | ["/connect/bank"]
       | ["/connect/card"]
