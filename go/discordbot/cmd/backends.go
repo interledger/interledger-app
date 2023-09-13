bba964bf896f4e4df062212053c106710f42afdd
Donovan Changfoot <don@fynbos.dev>
2023-09-13 16:57:56 +0200

    feat(discordbot): store interactions so that user can be notified on payment changes

--- go/discordbot/cmd/backends.go
+++ go/discordbot/cmd/backends.go
@@ -1,16 +1,20 @@
 package cmd
 
 import (
+	"github.com/jmoiron/sqlx"
 	"gitlab.com/fynbos/backend/linkedaccounts"
 	"gitlab.com/fynbos/backend/payments"
+	"gitlab.com/fynbos/discordbot/ops"
 
 	"gitlab.com/fynbos/backend/identities"
 	"gitlab.com/fynbos/backend/wallets"
 )
 
 type Backends interface {
+	DB() *sqlx.DB
 	LinkedAccounts() linkedaccounts.Client
 	Wallets() wallets.Client
 	Identities() identities.Client
 	Payments() payments.Client
+	Discord() ops.Discord
 }
