0f37da0c01029fc30f70240e2f13c604eb9c9fc6
Ömer Talha Özdemir <omer@fynbos.dev>
2023-09-14 11:39:14 +0300

    fix(backend): fynbos.me identities route
    Signed-off-by: Matt de Haast <matt@fynbos.dev>

--- go/backend/main.go
+++ go/backend/main.go
@@ -262,6 +262,7 @@ func start(args *cli.StartArgs) {
 	router.Handle("/kratos/logout", analytics_webhook.NewHandleLogout(b))
 	router.Handle("/webhooks/persona", kyc_ops.NewHandlePersonaWebhook(b))
 	router.Handle("/webhooks/slack/pay", bot.NewSlackCommandHandler(b))
+	router.Handle("/{wallet_id}/identities/{identity_sig_hash}", wallet_handler.NewGetIdentityHandler(b))
 	router.NotFound(wallet_handler.NewWalletRedirectHandler(b))
 
 	serveHTTP(&http.Server{Addr: ":" + args.AuthorisationPort, Handler: auth_http.AuthorisationHTTPHandler(b)}, &wg)
