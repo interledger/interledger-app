b7f7e9762892ee3816d9a40e56bfea4aad7d3a32
Matt de Haast <matt@fynbos.dev>
2023-09-15 09:53:55 +0200

    feat(backend): parse address error
    change parse address to not found error

--- go/backend/wallets/ops/ops.go
+++ go/backend/wallets/ops/ops.go
@@ -116,7 +116,7 @@ func AddAddress(ctx context.Context, b Backends, id, url string) (*wallets.Walle
 func GetFromAddress(ctx context.Context, b Backends, url string) (*wallets.Wallet, error) {
 	address, err := wallets.ParseAddress(url)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("%w address(%s) invalid", wallets.ErrNoWalletFound, address.String())
 	}
 
 	var wid string
