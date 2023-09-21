cf04ed5e7a2cbd535ae2850785d989b5223185a5
barnard <barnard@fynbos.dev>
2023-09-21 10:23:10 +0200

    feat(backend): Workflow to payout rafiki incoming payments to users

--- go/backend/rafiki/ops/backends.go
+++ go/backend/rafiki/ops/backends.go
@@ -2,10 +2,17 @@ package ops
 
 import (
 	"github.com/jmoiron/sqlx"
+	"gitlab.com/fynbos/backend/payments"
 	"gitlab.com/fynbos/backend/rafiki/external"
 )
 
 type Backends interface {
 	DB() *sqlx.DB
 	External() external.Client
+	Payments() payments.Client
+}
+
+type ActivityBackends interface {
+	DB() *sqlx.DB
+	Payments() payments.Client
 }
