71b97b02d7f12ed3c077179bf2c3a1fea6311cef
Matt de Haast <matt@fynbos.dev>
2023-09-28 11:15:42 +0200

    feat(backend): enable GMT compliance

--- go/backend/providers/gmt/ops/activity.go
+++ go/backend/providers/gmt/ops/activity.go
@@ -565,11 +565,10 @@ func receiverFromWallet(ctx context.Context, b Backends, walletID string) (*exte
 		return nil, err
 	}
 
-	// rid, err := getReceiverID(ctx, b, walletID)
-	// if err != nil {
-	// 	return nil, err
-	// }
-	rid := 0 // hard-coded until GMT api is fixed
+	rid, err := getReceiverID(ctx, b, walletID)
+	if err != nil {
+		return nil, err
+	}
 
 	gender := "Male"
 	if recvID.Gender == kyc.GenderFemale {
