84a73cc0520b5f0bbf67d91c84235ff2ce80e9f3
Donovan Changfoot <don@fynbos.dev>
2023-10-10 11:56:56 +0200

    feat(backend): use receive amount for payment if user set it

--- go/backend/grpc/payments.go
+++ go/backend/grpc/payments.go
@@ -206,7 +206,7 @@ func (s *rpcService) CreatePayment(ctx context.Context, req *pb.CreatePaymentReq
 		},
 		SenderAmount:    currency.FromPB(req.SenderAmount),
 		SenderAccount:   req.GetSenderAccount(),
-		ReceiverAmount:  currency.FromPB(req.SenderAmount), // TODO: calculate receive amount
+		ReceiverAmount:  currency.FromPB(req.GetReceiverAmount()),
 		ReceiverAccount: req.GetReceiverAccount(),
 		Note:            req.GetNote(),
 		IPAddress:       req.GetIpAddress(),
@@ -283,7 +283,7 @@ func (s *rpcService) UpdatePayment(ctx context.Context, req *pb.UpdatePaymentReq
 		ID:             req.Id,
 		SenderAmount:   currency.FromPB(req.GetSenderAmount()),
 		SenderAccount:  req.GetSenderAccount(),
-		ReceiverAmount: currency.FromPB(req.GetSenderAmount()), // TODO: calculate receive amount
+		ReceiverAmount: currency.FromPB(req.GetReceiverAmount()),
 		Receiver: payments.Identity{
 			Type:       payments.IdentityType(req.GetReceiverIdentityType()),
 			Identifier: req.GetReceiverIdentity(),
