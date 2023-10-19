24cc73444755e4c473af038156b98f62d5a9affc
barnard <barnard@fynbos.dev>
2023-10-19 14:42:40 +0200

    fix(backend): Lookup event ID for paymentID

--- go/backend/rafiki/ops/webhooks.go
+++ go/backend/rafiki/ops/webhooks.go
@@ -106,6 +106,15 @@ func extractWalletURL(receiver string) string {
 	return receiver
 }
 
+func extractIncomingPaymentID(receiver string) string {
+	const urlPart = "incoming-payments"
+	if strings.Contains(receiver, urlPart) {
+		return receiver[strings.Index(receiver, urlPart)+len(urlPart)+1:]
+	}
+
+	return receiver
+}
+
 func outgoingPaymentCreatedHandle(ctx context.Context, b Backends, hook webhook) error {
 	var op outgoingPaymentData
 	err := json.Unmarshal(hook.Data, &op)
@@ -178,12 +187,12 @@ func outgoingPaymentCreatedHandle(ctx context.Context, b Backends, hook webhook)
 
 	_, err = b.DB().ExecContext(ctx, "INSERT INTO rafiki_outgoing_payments(id, payment_id, event_id) VALUES ($1, $2, $3)", op.Payment.ID, p.ID, hook.ID)
 	if err != nil {
-		log.Error("failed to add outgoing payment from rafiki outoing payment hook")
+		log.Error("failed to add outgoing payment from rafiki outoing payment hook", zap.Error(err))
 	}
 
-	_, err = b.DB().ExecContext(ctx, "UPDATE rafiki_incoming_payments SET payment_id=$1 WHERE id = $2", p.ID, op.Payment.Quote.IncomingPaymentID)
+	_, err = b.DB().ExecContext(ctx, "UPDATE rafiki_incoming_payments SET payment_id=$1 WHERE id=$2", p.ID, extractIncomingPaymentID(op.Payment.Receiver))
 	if err != nil {
-		log.Error("failed to confirm payment from rafiki outoing payment hook")
+		log.Error("failed to set incoming payment ID for rafiki outgoing payment", zap.Error(err))
 	}
 
 	return nil
