74675386283ff4c73a9a3632e1bf39b68ffeccb8
Donovan Changfoot <don@fynbos.dev>
2023-10-27 16:12:48 +0200

    feat(backend): get persona inquiry template according to wallet country

--- go/backend/kyc/ops/persona_test.go
+++ go/backend/kyc/ops/persona_test.go
@@ -8,11 +8,14 @@ import (
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"gitlab.com/fynbos/backend/country"
 	"gitlab.com/fynbos/backend/db"
 	"gitlab.com/fynbos/backend/kyc/ops"
 	"gitlab.com/fynbos/backend/kyc/persona"
 	persona_mock "gitlab.com/fynbos/backend/kyc/persona/mock"
 	signup_mock "gitlab.com/fynbos/backend/signup/client/mock"
+	"gitlab.com/fynbos/backend/wallets"
+	wallet_mock "gitlab.com/fynbos/backend/wallets/client/mock"
 
 	user_mock "gitlab.com/fynbos/backend/user/client/mock"
 )
@@ -26,20 +29,32 @@ func TestGetPersonaInquiry(t *testing.T) {
 	walletID := uuid.NewString()
 	userID := uuid.NewString()
 	uc.WalletUser[walletID] = userID
+	wc := wallet_mock.NewMockClient(ctrl)
+	walletCountry := country.US
+	expectedPersonaInquiryTemplate := string(persona.GetTemplateIDForCountry(ctx, walletCountry))
+	wc.EXPECT().Get(ctx, gomock.Any()).Return(&wallets.Wallet{
+		ID:      walletID,
+		Country: walletCountry,
+	}, nil)
 
 	sc := signup_mock.NewMockClient(ctrl)
 
-	b := ops.NewTestBackends(t, db.MigrateTestDB(t, ctx), nil, uc, sc, nil, nil, nil)
+	b := ops.NewTestBackends(t, db.MigrateTestDB(t, ctx), nil, uc, sc, nil, nil, wc)
 	pc := persona_mock.NewMockClient(ctrl)
 
 	// There is no existing inquiry or KYC data
 	t.Run("creates a new persona inquiry", func(st *testing.T) {
 		inqID := uuid.NewString()
-		pc.EXPECT().CreateInquiry(ctx, gomock.Any(), gomock.Any()).Return(&persona.InquiryData{
-			Type:       "inquiry",
-			ID:         inqID,
-			Attributes: persona.InquiryAttributes{Status: "pending"},
-		}, nil)
+		pc.EXPECT().CreateInquiry(ctx, gomock.Any(), gomock.Any()).DoAndReturn(func(ctx context.Context, args persona.IndividualAttributes, idempotencyKey string) (*persona.InquiryData, error) {
+			assert.Equal(t, walletCountry.String(), args.CountryCode)
+			assert.Equal(t, expectedPersonaInquiryTemplate, args.InquiryTemplateID)
+
+			return &persona.InquiryData{
+				Type:       "inquiry",
+				ID:         inqID,
+				Attributes: persona.InquiryAttributes{Status: "pending"},
+			}, nil
+		})
 
 		inq, err := ops.GetPersonaInquiry(ctx, b, pc, walletID, "")
 		require.NoError(t, err)
