09b0be2d9ad7c4c5d1553872aecd3830bcd0750d
Donovan Changfoot <don@fynbos.dev>
2023-10-27 16:12:12 +0200

    feat(backend): set wallet country from Kratos user

--- go/backend/user/types.go
+++ go/backend/user/types.go
@@ -1,9 +1,12 @@
 package user
 
+import "gitlab.com/fynbos/backend/country"
+
 type User struct {
 	ID          string
 	Email       string
 	PhoneNumber string
+	Country     country.Country
 }
 
 type UserCtxKey string
