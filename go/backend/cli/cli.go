2eb3eb25bf8f2540376aad45f9e836ce0ff908ea
Matt de Haast <matt@fynbos.dev>
2023-11-10 16:31:28 +0200

    feat(pacioli): try connect without pgbouncer

--- go/backend/cli/cli.go
+++ go/backend/cli/cli.go
@@ -114,7 +114,7 @@ func ParseStartArgs() (*StartArgs, error) {
 		dbUrl = "cockroach://backend@cockroachdb-public:26257/backend?sslmode=verify-full&sslrootcert=/cockroach-certs/ca.crt&sslcert=/cockroach-certs/client.backend.crt&sslkey=/cockroach-certs/client.backend.key&max_conns=20&max_idle_conns=4"
 	}
 
-	pacDB := os.Getenv("PACIOLI_DB_URL")
+	pacDB := os.Getenv("PACIOLI_DB_URL_WITH_CERTS")
 	if pacDB == "" {
 		dbUrl = "cockroach://backend@cockroachdb-public:26257/pacioli?sslmode=verify-full&sslrootcert=/cockroach-certs/ca.crt&sslcert=/cockroach-certs/client.backend.crt&sslkey=/cockroach-certs/client.backend.key&max_conns=20&max_idle_conns=4"
 	}
