75366899978e109a8a141805f4d41ed3953e3c14
Matt de Haast <matt@fynbos.dev>
2023-11-16 13:48:59 +0200

    feat(admin): enable zar balance
    add ability to show and enable zar balance
    via the admin panel

---
+++ typescript/botanist/app/routes/wallet.$id.balances.tsx
@@ -0,0 +1,63 @@
+import type { LoaderArgs } from '@remix-run/node'
+import {ActionArgs, json} from '@remix-run/node'
+import {Form, useFetcher, useLoaderData, useParams} from '@remix-run/react'
+import {
+  EnableWalletBalance,
+  GetWalletBalance,
+} from '~/lib/wallet.server'
+import {GridCard, Switch} from '~/components'
+import {route} from "routes-gen";
+import {useCallback} from "react";
+
+export async function loader({ request, params }: LoaderArgs) {
+  const balance = await GetWalletBalance(request, params.id as string)
+
+  return json({
+    balance,
+  })
+}
+
+export default function Page() {
+  const { balance } = useLoaderData<typeof loader>()
+  const {id} = useParams()
+  const fetcher = useFetcher()
+
+  const _onChangeSwitch = useCallback<{ (): void }>(
+    () => {
+      fetcher.submit({ }, { method: 'post' })
+    },
+    [fetcher]
+  )
+
+  return (
+    <>
+      <GridCard
+        className='col-span-full lg:col-span-4'
+        title="Balance"
+        options={balance}
+      />
+      <Form
+        id='features-form'
+        action={route('/wallet/:id/balances', { id: id as string})}
+        method='post'
+        className='hidden'
+      />
+      <div className='flex w-full items-center justify-between'>
+        <dt className='text-xs font-medium capitalize text-weak'>
+          Enable ZAR Balance
+        </dt>
+        <Switch
+          checked={!!balance}
+          disabled={!!balance}
+          onChange={() => _onChangeSwitch()}
+        />
+      </div>
+    </>
+  )
+}
+
+export async function action({ request, params }: ActionArgs) {
+  await EnableWalletBalance(request, params.id as string)
+
+  return null
+}
