9593e197ab81084cc8379ebc9ca6871c8789d255
Ömer Talha Özdemir <omer@fynbos.dev>
2023-11-16 09:54:29 +0300

    feat(protea): add cloudflare turnstile captcha
    this feature needs CF_TURNSTILE_SITE_KEY and
    CF_TURNSTILE_SECRET_KEY env variables
    
    - only visible if requires interaction
    - automatically runs when the page loads

---
+++ typescript/protea/app/components/Turnstile.tsx
@@ -0,0 +1,91 @@
+import { forwardRef, useEffect, useImperativeHandle, useRef } from 'react'
+import { useScript } from '~/lib/useScript'
+
+const Turnstile = forwardRef<TurnstileInstance | undefined, TurnstileProps>(
+  ({ ...props }, ref) => {
+    const { siteKey, appearance } = props
+
+    const scriptStatus = useScript(
+      'https://challenges.cloudflare.com/turnstile/v0/api.js'
+    )
+    const turnstileRef = useRef<HTMLDivElement | null>(null)
+    const firstRendered = useRef(false)
+
+    useImperativeHandle(
+      ref,
+      () => {
+        if (typeof window === 'undefined' || !scriptStatus) {
+          return
+        }
+
+        const { turnstile } = window as any
+        return {
+          reset() {
+            if (!turnstile?.reset) {
+              console.warn('Turnstile has not been loaded')
+              return
+            }
+
+            try {
+              turnstile.reset()
+            } catch (error) {
+              console.warn(`Failed to reset Turnstile widget`, error)
+            }
+          }
+        }
+      },
+      [scriptStatus]
+    )
+
+    useEffect(() => {
+      if (!siteKey) {
+        console.warn('sitekey was not provided')
+        return
+      }
+
+      if (
+        scriptStatus !== 'ready' ||
+        !turnstileRef.current ||
+        firstRendered.current
+      ) {
+        return
+      }
+
+      ;(window as any).turnstile.render(turnstileRef.current, {
+        sitekey: siteKey,
+        callback: (token: string) => {
+          if (props.onSuccess) {
+            props.onSuccess(token)
+          }
+        }
+      })
+
+      firstRendered.current = true
+    }, [siteKey, props, scriptStatus, turnstileRef, firstRendered])
+
+    return (
+      <div
+        ref={turnstileRef}
+        className='cf-turnstile'
+        data-sitekey={siteKey}
+        data-appearance={appearance}
+      />
+    )
+  }
+)
+
+interface TurnstileProps extends React.HTMLAttributes<HTMLDivElement> {
+  siteKey: string
+  appearance?: 'interaction-only' | 'execute' | 'always'
+  onSuccess?: (token: string) => void
+}
+
+export interface TurnstileInstance {
+  reset: () => void
+}
+
+export type TurnstileAppearance = 'interaction-only' | 'execute' | 'always'
+
+Turnstile.displayName = 'Turnstile'
+
+export default Turnstile
