c768a7dc81c9cc4df850b2caa2965e517bd0f839
Cairin Michie <cairinmichie@gmail.com>
2023-11-16 13:34:11 +0200

    feat(protea): unknown recipient icon on home

--- typescript/protea/app/routes/_index/app.tsx
+++ typescript/protea/app/routes/_index/app.tsx
@@ -245,6 +245,11 @@ export function AppPage() {
                       transaction.destinationIdentityType == 'Slack' && (
                         <SlackIcon />
                       )}
+                    {transaction.state != 'Pending' &&
+                      transaction.state != 'Failed' &&
+                      transaction.destinationIdentityType == 'Unknown' && (
+                        <Icon>account_circle</Icon>
+                      )}
                     <div className='flex flex-col space-y-1'>
                       <span className='text-medium'>{transaction.title}</span>
                       <span className='text-xs text-weak'>
