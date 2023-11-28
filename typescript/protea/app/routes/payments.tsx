10f2968d394633f77bff801bc5f21585e1b9a0cb
Cairin Michie <cairinmichie@gmail.com>
2023-11-28 12:24:55 +0200

    feat(protea): withdraw functionality

--- typescript/protea/app/routes/payments.tsx
+++ typescript/protea/app/routes/payments.tsx
@@ -291,24 +291,28 @@ export default function Page() {
                       <Icon className='text-error'>exclamation</Icon>
                     )}
                     {transaction.state != 'Pending' &&
-                      transaction.state != 'Failed' &&
-                      transaction.destinationIdentityType == 'wallet' && (
-                        <FynbosIcon />
-                      )}
-                    {transaction.state != 'Pending' &&
-                      transaction.state != 'Failed' &&
-                      transaction.destinationIdentityType == 'Twitter' && (
-                        <TwitterIcon />
-                      )}
-                    {transaction.state != 'Pending' &&
-                      transaction.state != 'Failed' &&
-                      transaction.destinationIdentityType == 'Discord' && (
-                        <DiscordIcon />
-                      )}
-                    {transaction.state != 'Pending' &&
-                      transaction.state != 'Failed' &&
-                      transaction.destinationIdentityType == 'Slack' && (
-                        <SlackIcon />
+                      transaction.state != 'Failed' && (
+                        <>
+                          {transaction.destinationIdentityType == 'wallet' && (
+                            <>
+                              {transaction.type != 'withdrawal' && (
+                                <FynbosIcon />
+                              )}
+                              {transaction.type == 'withdrawal' && (
+                                <Icon>south_west</Icon>
+                              )}
+                            </>
+                          )}
+                          {transaction.destinationIdentityType == 'Twitter' && (
+                            <TwitterIcon />
+                          )}
+                          {transaction.destinationIdentityType == 'Discord' && (
+                            <DiscordIcon />
+                          )}
+                          {transaction.destinationIdentityType == 'Slack' && (
+                            <SlackIcon />
+                          )}
+                        </>
                       )}
                     <div className='flex w-full flex-col space-y-1'>
                       <span className='truncate text-medium'>
