301412b71f123c3b241f4e0a0af26ece209392fa
Matt de Haast <matt@fynbos.dev>
2023-11-30 14:58:22 +0200

    fix(grpc): country deterministic ordering
    make the country list be deterministic

--- go/backend/grpc/countries_test.go
+++ go/backend/grpc/countries_test.go
@@ -21,4 +21,5 @@ func TestGetCountries(t *testing.T) {
 	require.NoError(t, err)
 	require.Len(t, rpc.Countries, 250)
 	assert.Equal(t, country.US.String(), rpc.Countries[0].Id)
+	assert.Equal(t, country.ZA.String(), rpc.Countries[1].Id)
 }
