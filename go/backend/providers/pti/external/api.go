b38396a73f76c41e457e89e49d4bd759032fe192
Donovan Changfoot <don@fynbos.dev>
2024-01-09 16:18:36 +0200

    fix(backend): add pti scenarioID when starting assessment

--- go/backend/providers/pti/external/api.go
+++ go/backend/providers/pti/external/api.go
@@ -6,5 +6,5 @@ type Client interface {
 	CreateUser(ctx context.Context, args CreateUserArgs) (string, error)
 	CreateWallet(ctx context.Context, args CreateWalletArgs) (*Wallet, error)
 	GetWallet(ctx context.Context, userID, id string) (*Wallet, error)
-	StartUserAssessment(ctx context.Context, args CreateUserArgs) (string, error)
+	StartUserAssessment(ctx context.Context, args StartUserAssessmentArgs) (string, error)
 }
