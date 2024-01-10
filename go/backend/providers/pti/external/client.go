46a654d07825407b318b029dc72335c04f79eb2e
Donovan Changfoot <don@fynbos.dev>
2024-01-10 14:48:37 +0200

    fix(backend): make creating external pti wallet idempotent

--- go/backend/providers/pti/external/client.go
+++ go/backend/providers/pti/external/client.go
@@ -420,6 +420,58 @@ func (c client) GetWallet(ctx context.Context, userID, id string) (*Wallet, erro
 	return &wallet, nil
 }
 
+func (c client) ListWallets(ctx context.Context, userID string) ([]Wallet, error) {
+	meta, ok := httplog.MetaForContext(ctx)
+	if ok {
+		meta.Method = "GET"
+		meta.Provider = "pti"
+	} else {
+		ctx = context.WithValue(ctx, httplog.ContextKey, &httplog.Metadata{
+			Method:   "GET",
+			Provider: "pti",
+		})
+	}
+
+	url, err := url.JoinPath(c.baseURL, "users", userID, "wallets")
+	if err != nil {
+		return nil, fmt.Errorf("%w %s", ErrInternal, err)
+	}
+
+	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("%w %s", ErrInternal, err)
+	}
+	req.Header.Add(ptiClientIDHeader, c.clientID)
+	date := time.Now()
+	req.Header.Add("Date", date.Format(http.TimeFormat))
+	if err := Sign(ctx, req, date, nil, c.privateKey, c.publicKeyThumbprint); err != nil {
+		return nil, fmt.Errorf("%w %s", ErrInternal, err)
+	}
+
+	resp, err := c.api.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("%w %s", ErrInternal, err)
+	}
+
+	err = checkResponseStatusCode(resp)
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("%w %s", ErrInternal, err)
+	}
+
+	var wallets []Wallet
+	err = json.Unmarshal(body, &wallets)
+	if err != nil {
+		return nil, fmt.Errorf("%w %s", ErrInternal, err)
+	}
+
+	return wallets, nil
+}
+
 func (c client) StartUserAssessment(ctx context.Context, args StartUserAssessmentArgs) (string, error) {
 	requestID := uuid.NewString()
 	meta, ok := httplog.MetaForContext(ctx)
