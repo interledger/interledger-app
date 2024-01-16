fb53cb0073438fc1da0198f3cee6f490945a10a2
barnard <barnard@fynbos.dev>
2024-01-16 10:57:47 +0200

    fix(backend): Temporal name clash for activity

--- go/backend/providers/astra/ops/tokens.go
+++ go/backend/providers/astra/ops/tokens.go
@@ -16,6 +16,7 @@ import (
 type dbToken struct {
 	ID               string    `db:"id"`
 	Token            string    `db:"token"`
+	WalletID         string    `db:"wallet_id"`
 	ExpiresAt        time.Time `db:"expires_at"`
 	RefreshToken     string    `db:"refresh_token"`
 	RefreshExpiresAt time.Time `db:"refresh_expires_at"`
@@ -87,7 +88,7 @@ func GetToken(ctx context.Context, b Backends, walletID string) (string, error) 
 		return "", fmt.Errorf("%w %s", astra.ErrInternal, err)
 	}
 
-	if token.RefreshExpiresAt.Before(time.Now()) {
+	if token.ExpiresAt.Before(time.Now()) {
 		return CreateOrRefreshToken(ctx, b, walletID)
 	}
 
