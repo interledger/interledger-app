3796b869c2971e29880e4b2283689e086b5707c8
Matt de Haast <matt@fynbos.dev>
2024-02-06 10:29:44 +0200

    feat(rafiki): usd default and migration job
    change the default rafiki payment pointer to be
    generated to be USD and then add a job to create
    payment pointers for wallets that dont have it
    already.

---
+++ go/backend/jobs/2024_02_06_generate_wallet_payment_pointer.go
@@ -0,0 +1,55 @@
+package jobs
+
+import (
+	"context"
+	"gitlab.com/fynbos/log"
+	"go.uber.org/zap"
+	"time"
+
+	"go.temporal.io/sdk/temporal"
+	"go.temporal.io/sdk/workflow"
+)
+
+func GenerateWalletPaymentPointersJob(ctx workflow.Context) error {
+	var a *Activity
+	ao := workflow.ActivityOptions{
+		StartToCloseTimeout: 10 * time.Minute,
+		RetryPolicy: &temporal.RetryPolicy{
+			MaximumAttempts: 3,
+		},
+	}
+	ctx = workflow.WithActivityOptions(ctx, ao)
+
+	err := workflow.ExecuteActivity(ctx, a.GenerateWalletPaymentPointers).Get(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (a *Activity) GenerateWalletPaymentPointers(ctx context.Context) error {
+	var ids []string
+	err := a.b.DB().SelectContext(
+		ctx,
+		&ids,
+		"select id from wallets where id not in (select wallet_id from rafiki_payment_pointers);",
+	)
+	if err != nil {
+		return err
+	}
+
+	for _, id := range ids {
+		w, err := a.b.Wallets().Get(ctx, id)
+		if err != nil {
+			log.Error("could not get wallet", zap.String("walletID", id), zap.Error(err))
+			continue
+		}
+		err = a.b.Rafiki().CreatePaymentPointer(ctx, *w, "USD")
+		if err != nil {
+			log.Error("couldn't create payment point for wallet", zap.String("walletAddress", w.AddressString()), zap.String("walletID", w.ID), zap.Error(err))
+		}
+	}
+
+	return nil
+}
