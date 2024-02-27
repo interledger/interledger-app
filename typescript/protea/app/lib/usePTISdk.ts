8dd2df8661ad2ce4a63003f2a1fc796f5daabe31
Donovan Changfoot <don@fynbos.dev>
2024-02-27 12:12:54 +0200

    feat(protea): init PTI sdk on deposit/withdrawal/payments for US wallets

---
+++ typescript/protea/app/lib/usePTISdk.ts
@@ -0,0 +1,23 @@
+import { useEffect } from 'react'
+import { useScript } from './useScript'
+
+export function usePTISdk(
+  country: string,
+  sessionId: string,
+  clientId: string
+) {
+  const scriptStatus = useScript('https://sdk.pearsurge.io/0.0.18/index.js')
+
+  useEffect(() => {
+    if (
+      country === 'US' &&
+      scriptStatus == 'ready' &&
+      typeof (window as any).PTI !== 'undefined'
+    ) {
+      ;(window as any).PTI.init({
+        clientId: clientId,
+        sessionId: sessionId
+      })
+    }
+  }, [scriptStatus, clientId, country, sessionId])
+}
