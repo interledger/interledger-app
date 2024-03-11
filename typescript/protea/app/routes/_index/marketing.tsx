5a8b9ee589df83961359a331663a718bbfbb7a4a
Cairin Michie <cairinmichie@gmail.com>
2024-03-11 11:29:02 +0200

    feat: delete specific pages in favour of table

--- typescript/protea/app/routes/_index/marketing.tsx
+++ typescript/protea/app/routes/_index/marketing.tsx
@@ -1,19 +1,3 @@
-import { useLoaderData } from '@remix-run/react'
-import { MarketingPageWithSections } from '~/components/Content'
-
-import type { SectionRecord } from '~/generated/dato-cms-graphql'
-import type { marketingLoader } from './route'
-
 export function MarketingPage() {
-  const { homeRoute } = useLoaderData<typeof marketingLoader>()
-  return (
-    <>
-      {homeRoute?.body.map((section) => (
-        <MarketingPageWithSections
-          key={section.id}
-          section={section as SectionRecord}
-        />
-      ))}
-    </>
-  )
+  return <>Coming soon</>
 }
