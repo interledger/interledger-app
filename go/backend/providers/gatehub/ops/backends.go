c66606a2bd7f403b54ca3d01c99eb43d0d04f3bb
Donovan Changfoot <don@fynbos.dev>
2024-03-19 14:53:09 +0200

    feat(backend): save gatehub user

--- go/backend/providers/gatehub/ops/backends.go
+++ go/backend/providers/gatehub/ops/backends.go
@@ -1,9 +1,13 @@
 package ops
 
 import (
+	"github.com/jmoiron/sqlx"
 	"gitlab.com/fynbos/backend/user"
+	temporal "go.temporal.io/sdk/client"
 )
 
 type Backends interface {
+	DB() *sqlx.DB
 	Users() user.Client
+	Temporal() temporal.Client
 }
