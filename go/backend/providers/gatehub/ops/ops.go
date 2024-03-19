07e48f83892eb53d74384c3bc57f1f5fe9b8562a
Donovan Changfoot <don@fynbos.dev>
2024-03-19 15:30:27 +0200

    feat(backend): get gatehub onboarding widget url

--- go/backend/providers/gatehub/ops/ops.go
+++ go/backend/providers/gatehub/ops/ops.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"gitlab.com/fynbos/backend/providers/gatehub"
+	"gitlab.com/fynbos/backend/providers/gatehub/external"
 	"go.temporal.io/api/enums/v1"
 	"go.temporal.io/api/serviceerror"
 	"go.temporal.io/sdk/client"
@@ -45,4 +46,28 @@ func CreateUser(ctx context.Context, b Backends, walletID string) (gatehub.Await
 	}
 
 	return await.Get, nil
+}
+
+func GetOnboardingWidget(ctx context.Context, b Backends, ec external.Client, walletID string) (string, error) {
+	externalUserID, err := getExternalUserID(ctx, b, walletID)
+	if errors.Is(err, gatehub.ErrNotFound) {
+		await, innerErr := CreateUser(ctx, b, walletID)
+		if innerErr != nil {
+			return "", innerErr
+		}
+
+		innerErr = await(ctx, &externalUserID)
+		if innerErr != nil {
+			return "", fmt.Errorf("%w %s", gatehub.ErrInternal, innerErr)
+		}
+	} else if err != nil {
+		return "", err
+	}
+
+	widget, err := ec.GetOnboardingWidget(ctx, externalUserID)
+	if err != nil {
+		return "", fmt.Errorf("%w %s", gatehub.ErrInternal, err)
+	}
+
+	return widget, nil
 }
