d3ffbeeb1850bc6cdfb52bf663efb93a8cd95cc8
Donovan Changfoot <don@fynbos.dev>
2024-03-19 13:34:00 +0200

    feat(backend): create gatehub user through admin grpc

---
+++ go/backend/providers/gatehub/client/client.go
@@ -0,0 +1,35 @@
+package client
+
+import (
+	"context"
+	"os"
+
+	"gitlab.com/fynbos/backend/providers/gatehub"
+	"gitlab.com/fynbos/backend/providers/gatehub/external"
+	ops "gitlab.com/fynbos/backend/providers/gatehub/ops"
+	"gitlab.com/fynbos/backend/user"
+)
+
+var _ gatehub.Client = Client{}
+
+type Client struct {
+	b  ops.Backends
+	ec external.Client
+}
+
+type Backends interface {
+	Users() user.Client
+}
+
+func New(b Backends) *Client {
+	ec := external.NewClient(os.Getenv("GATEHUB_APP_ID"), os.Getenv("GATEHUB_SECRET"))
+
+	return &Client{
+		b:  b,
+		ec: ec,
+	}
+}
+
+func (c Client) CreateUser(ctx context.Context, walletID string) (string, error) {
+	return ops.CreateUser(ctx, c.b, c.ec, walletID)
+}
