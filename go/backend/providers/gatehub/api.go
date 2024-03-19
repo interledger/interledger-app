189379d89b1b06904b3d279fdf2d331269fb2934
Donovan Changfoot <don@fynbos.dev>
2024-03-19 14:53:09 +0200

    feat(backend): save gatehub user

--- go/backend/providers/gatehub/api.go
+++ go/backend/providers/gatehub/api.go
@@ -3,5 +3,7 @@ package gatehub
 import "context"
 
 type Client interface {
-	CreateUser(ctx context.Context, walletID string) (string, error)
+	CreateUser(ctx context.Context, walletID string) (Await, error)
 }
+
+type Await func(ctx context.Context, result interface{}) error
