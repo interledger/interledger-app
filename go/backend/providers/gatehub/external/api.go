102ee5399575fac859c18301252aedbc0b71bd26
Donovan Changfoot <don@fynbos.dev>
2024-03-19 15:30:27 +0200

    feat(backend): get gatehub onboarding widget url

--- go/backend/providers/gatehub/external/api.go
+++ go/backend/providers/gatehub/external/api.go
@@ -3,6 +3,7 @@ package external
 import "context"
 
 type Client interface {
-	IssueToken(ctx context.Context, product Product) (*IssueTokenResponse, error)
+	IssueToken(ctx context.Context, userID string, product Product) (*IssueTokenResponse, error)
 	CreateUser(ctx context.Context, email string) (*CreateUserResponse, error)
+	GetOnboardingWidget(ctx context.Context, userID string) (string, error)
 }
