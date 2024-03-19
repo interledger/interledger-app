8d5bb40367b92f97fbcebbc5831766973ed720db
Donovan Changfoot <don@fynbos.dev>
2024-03-19 13:36:29 +0200

    feat(backend): gatehub external client create user

--- go/backend/providers/gatehub/external/types.go
+++ go/backend/providers/gatehub/external/types.go
@@ -1,5 +1,7 @@
 package external
 
+import "time"
+
 type Product string
 
 var (
@@ -16,5 +18,26 @@ type (
 	IssueTokenResponse struct {
 		Token     string `json:"token,omitempty"`
 		ExpiresAt string `json:"expires,omitempty"`
+	}
+
+	CreateUserRequest struct {
+		Email string `json:"email,omitempty"`
+	}
+
+	CreateUserResponse struct {
+		ID                 string                 `json:"id"`
+		CreatedAt          time.Time              `json:"createdAt"`
+		UpdatedAt          time.Time              `json:"updatedAt"`
+		ActivatedAt        time.Time              `json:"activatedAt"`
+		Email              string                 `json:"email"`
+		Secret2FA          bool                   `json:"secret2fa"`
+		Type2FA            string                 `json:"type2fa"`
+		Activated          bool                   `json:"activated"`
+		Role               string                 `json:"role"`
+		Meta               map[string]interface{} `json:"meta"`
+		LastPasswordChange time.Time              `json:"lastPasswordChange"`
+		Features           []string               `json:"features"`
+		Managed            bool                   `json:"managed"`
+		ManagedBy          string                 `json:"managedBy"`
 	}
 )
