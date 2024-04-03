1f1e077ccc532f34924d11d20e28bc97a065564f
Donovan Changfoot <don@fynbos.dev>
2024-04-03 16:57:39 +0200

    fix(dev): pin consul to 1.17.3

--- dev/vagrant/consul.sh
+++ dev/vagrant/consul.sh
@@ -11,7 +11,8 @@ elif  [[ $arch == aarch64 ]]; then
 fi
 echo -e '\e[38;5;198m'"CPU is $ARCH"
 
-sudo DEBIAN_FRONTEND=noninteractive apt-get --assume-yes install curl unzip jq < /dev/null > /dev/null
+sudo DEBIAN_FRONTEND=noninteractive apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get --assume-yes install curl unzip jq wget < /dev/null > /dev/null
 mkdir -p /etc/consul
 mkdir -p /etc/consul.d
 cat <<EOF | sudo tee /etc/consul/server.hcl
@@ -151,7 +152,7 @@ EOF
   else
   # if consul is not installed, download and install
     echo -e '\e[38;5;198m'"++++ Consul not installed, installing.."
-    LATEST_URL=$(curl -sL https://releases.hashicorp.com/consul/index.json | jq -r '.versions[].builds[].url' | sort -t. -k 1,1n -k 2,2n -k 3,3n -k 4,4n | egrep -v 'rc|ent|beta' | egrep "linux.*$ARCH" | sort -V | tail -1)
+    LATEST_URL=$(curl -sL https://releases.hashicorp.com/consul/index.json | jq -r '.versions[].builds[].url' | sort -t. -k 1,1n -k 2,2n -k 3,3n -k 4,4n | egrep -v 'rc|ent|beta' | egrep "1.17.3_linux.*$ARCH" | sort -V | tail -1)
     wget -q $LATEST_URL -O /tmp/consul.zip
     mkdir -p /usr/local/bin
     (cd /usr/local/bin && unzip /tmp/consul.zip)
@@ -162,14 +163,6 @@ EOF
     consul members
     consul info
   fi
-  echo -e '\e[38;5;198m'"++++ Adding Consul KV data for Fabio Load Balancer Routes"
-  consul kv put fabio/config/vault1 "route add vault vault.service.consul:9999/ http://10.9.99.10:8200"
-  consul kv put fabio/config/vault2 "route add vault fabio.service.consul:9999/vault http://10.9.99.10:8200 opts \"strip=/vault\""
-  consul kv put fabio/config/nomad "route add nomad nomad.service.consul:9999/ http://10.9.99.10:4646"
-  consul kv put fabio/config/consul "route add consul consul.service.consul:9999/ http://10.9.99.10:8500"
-  consul kv put fabio/config/apache2 "route add apache2 fabio.service.consul:9999/apache2 http://10.9.99.10:8889 opts \"strip=/apache2\""
-  consul kv put fabio/config/countdashtest1 "route add countdashtest fabio.service.consul:9999/countdashtest http://10.9.99.10:9022/ opts \"strip=/countdashtest\""
-  consul kv put fabio/config/docsify "route add docsify docsify.service.consul:9999/ http://10.9.99.10:3333"
 
   echo -e '\e[38;5;198m'"++++ Install DNSMasq"
   sudo systemctl disable systemd-resolved
