db8fafd0360a99bc129f62ba6802e3e38668e105
Donovan Changfoot <don@fynbos.dev>
2024-04-05 07:57:03 +0200

    feat(backend): gatehub issue token

---
+++ go/backend/providers/gatehub/external/external_test.go
@@ -0,0 +1,25 @@
+package external_test
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"gitlab.com/fynbos/backend/providers/gatehub/external"
+	"gitlab.com/fynbos/env"
+)
+
+func TestClient(t *testing.T) {
+	env.SetEnv(t, "local")
+	if os.Getenv("GATEHUB_APP_ID") == "" || os.Getenv("GATEHUB_SECRET") == "" {
+		t.SkipNow()
+	}
+	c := external.NewClient(os.Getenv("GATEHUB_APP_ID"), os.Getenv("GATEHUB_SECRET"))
+
+	token, err := c.IssueToken(context.Background(), external.Onboarding)
+	require.NoError(t, err)
+
+	fmt.Printf("token: %+v", token)
+}
