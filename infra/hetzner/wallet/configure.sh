9fba1cf809bec75b7bf90f92611eecbc6c758936
Donovan Changfoot <don@fynbos.dev>
2024-05-07 09:36:20 +0200

    feat(infra): prod and dev db credentials managed by vault

--- infra/hetzner/wallet/configure.sh
+++ infra/hetzner/wallet/configure.sh
@@ -5,25 +5,30 @@
 # - init vault kv v2
 
 
-mkdir configuration && cd configuration
-export VAULT_ADDR=http://127.0.0.1:8200
+function init_vault () {
+  # Store the vault unseal keys securely
+  vault operator init | tee vault-tokens.txt
 
-# Store the vault unseal keys securely
-vault operator init > vault-tokens.txt
+  vault operator unseal $(cat vault-tokens.txt | grep 'Key 1' | cut -d':' -f2 | xargs)
+  vault operator unseal $(cat vault-tokens.txt | grep 'Key 2' | cut -d':' -f2 | xargs)
+  vault operator unseal $(cat vault-tokens.txt | grep 'Key 3' | cut -d':' -f2 | xargs)
 
-vault operator unseal $(cat vault-tokens.txt | grep 'Key 1' | cut -d':' -f2 | xargs)
-vault operator unseal $(cat vault-tokens.txt | grep 'Key 2' | cut -d':' -f2 | xargs)
-vault operator unseal $(cat vault-tokens.txt | grep 'Key 3' | cut -d':' -f2 | xargs)
+  export VAULT_TOKEN=$(cat vault-tokens.txt | grep 'Root' | cut -d':' -f2 | xargs)
+}
 
-export VAULT_TOKEN=$(cat vault-tokens.txt | grep 'Root' | cut -d':' -f2 | xargs)
+function bootstrap_nomad_acl () {
+  nomad acl bootstrap | tee nomad-acl-token.txt
 
-nomad acl bootstrap > nomad-acl-token.txt
+  export NOMAD_TOKEN=$(cat nomad-acl-token.txt | grep 'Secret ID' | cut -d'=' -f2 | xargs)
 
-export NOMAD_TOKEN=$(cat nomad-acl-token.txt | grep 'Secret ID' | cut -d'=' -f2 | xargs)
+  nomad namespace apply -description "Production environment" prod
+  nomad namespace apply -description "Development environment" dev
+}
 
-vault auth enable -path 'jwt-nomad' 'jwt'
+function configure_vault_workload_identities () {
+  vault auth enable -path 'jwt-nomad' 'jwt'
 
-cat << EOF | tee vault-auth-method-jwt-nomad.json
+  cat << EOF | tee vault-auth-method-jwt-nomad.json
 {
   "jwks_url": "http://127.0.0.1:4646/.well-known/jwks.json",
   "jwt_supported_algs": ["RS256", "EdDSA"],
@@ -31,7 +36,7 @@ cat << EOF | tee vault-auth-method-jwt-nomad.json
 }
 EOF
 
-vault write auth/jwt-nomad/config '@vault-auth-method-jwt-nomad.json'
+  vault write auth/jwt-nomad/config '@vault-auth-method-jwt-nomad.json'
 
 cat << EOF | tee vault-role-nomad-workloads.json
 {
@@ -51,11 +56,11 @@ cat << EOF | tee vault-role-nomad-workloads.json
 }
 EOF
 
-vault write auth/jwt-nomad/role/nomad-workloads '@vault-role-nomad-workloads.json'
+  vault write auth/jwt-nomad/role/nomad-workloads '@vault-role-nomad-workloads.json'
 
-export AUTH_METHOD_ACCESSOR=$(vault auth list | grep 'jwt-nomad' | xargs | cut -d' ' -f3)
+  export AUTH_METHOD_ACCESSOR=$(vault auth list | grep 'jwt-nomad' | xargs | cut -d' ' -f3)
 
-cat << EOF| tee vault-policy-nomad-workloads.hcl
+  cat << EOF| tee vault-policy-nomad-workloads.hcl
 path "kv/data/{{identity.entity.aliases.$AUTH_METHOD_ACCESSOR.metadata.nomad_namespace}}/{{identity.entity.aliases.$AUTH_METHOD_ACCESSOR.metadata.nomad_job_id}}/*" {
   capabilities = ["read"]
 } 
@@ -71,11 +76,38 @@ path "kv/metadata/{{identity.entity.aliases.$AUTH_METHOD_ACCESSOR.metadata.nomad
 path "kv/metadata/*" {
   capabilities = ["list"]
 }
+
+path "database-{{identity.entity.aliases.$AUTH_METHOD_ACCESSOR.metadata.nomad_namespace}}/creds/*" {
+  capabilities = ["read"]
+}
 EOF
 
-vault policy write 'nomad-workloads' 'vault-policy-nomad-workloads.hcl'
+  vault policy write 'nomad-workloads' 'vault-policy-nomad-workloads.hcl'
+}
 
-vault secrets enable -version '2' 'kv'
+function generate_database_root_passwords () {
+  sudo apt-get install --assume-yes uuid-runtime
+  vault secrets enable -version '2' 'kv'
+
+  PROD_DB_PASSWORD=$(uuidgen)
+  DEV_DB_PASSWORD=$(uuidgen)
+
+  vault kv put -mount=kv prod/postgres/config password=$PROD_DB_PASSWORD
+  vault kv put -mount=kv dev/postgres/config password=$DEV_DB_PASSWORD
+
+  echo $PROD_DB_PASSWORD > prod-database-root-password.txt
+  echo $DEV_DB_PASSWORD > dev-database-root-password.txt
+}
+
+
+mkdir configuration && cd configuration
+export VAULT_ADDR=http://127.0.0.1:8200
+
+init_vault
+bootstrap_nomad_acl
+configure_vault_workload_identities
+generate_database_root_passwords
 
 unset VAULT_TOKEN
 unset NOMAD_TOKEN
+unset AUTH_METHOD_ACCESSOR
---
+++ infra/hetzner/wallet/envs/prod/postgres.hcl
@@ -0,0 +1,100 @@
+job "postgres" {
+  datacenters = ["dc1"]
+  type = "service"
+  namespace = "prod"
+
+  group "postgres" {
+    count = 1
+
+    volume "postgres" {
+      type = "host"
+      read_only = false
+      source = "db-prod"
+    }
+
+    restart {
+      attempts = 10
+      interval = "5m"
+      delay = "25s"
+      mode = "delay"
+    }
+
+    network {
+      mode = "bridge"
+      port "postgres" {
+        to = 5432
+        static = 5432
+      }
+    }
+
+    service {
+      name = "postgres"
+      port = 5432
+      connect {
+        sidecar_service {}
+      }
+    }
+
+    vault {}
+
+    task "postgres" {
+      driver = "docker"
+
+      volume_mount {
+        volume = "postgres"
+        destination = "/var/lib/postgresql"
+        read_only = false
+      }
+
+      vault {}
+
+      template {
+        data = <<EOH
+          POSTGRES_USER=postgres
+          POSTGRES_PASSWORD={{with secret "kv/data/prod/postgres/config"}}{{.Data.data.password}}{{end}}
+        EOH
+
+        destination = "secrets/file.env"
+        env         = true
+      }
+
+      template {
+        data = <<EOH
+          -- Create an ADMIN user
+          CREATE ROLE roach WITH LOGIN SUPERUSER PASSWORD 'roach';
+          CREATE ROLE vault WITH LOGIN SUPERUSER PASSWORD 'vault';
+
+          -- Create Kratos user and DB
+          CREATE DATABASE kratos;
+
+          -- Create backend user and DB
+          CREATE DATABASE backend;
+
+          -- Create pacioli user and DB
+          CREATE DATABASE  pacioli;
+          
+          -- Create temporal user and DB
+          CREATE DATABASE temporal;
+          CREATE DATABASE temporal_visibility;
+
+          -- Create rafiki user and DB
+          CREATE DATABASE rafiki_backend;
+          CREATE DATABASE rafiki_auth;
+        EOH
+
+        destination = "$NOMAD_TASK_DIR/init-user-db.sql"
+        env = false
+      }
+
+      config {
+        image = "postgres:16.1"
+
+        volumes = ["$NOMAD_TASK_DIR:/docker-entrypoint-initdb.d"]
+      }
+
+      resources {
+        cpu = 500
+      }
+    }
+  }
+}
