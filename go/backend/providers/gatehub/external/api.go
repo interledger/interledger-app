4667aecb9c201c4209dc5cf19928a2d5c55be4d7
Donovan Changfoot <don@fynbos.dev>
2024-06-06 12:57:57 +0200

    feat(backend): get gatehub user external api call

--- go/backend/providers/gatehub/external/api.go
+++ go/backend/providers/gatehub/external/api.go
@@ -5,6 +5,7 @@ import "context"
 type Client interface {
 	IssueToken(ctx context.Context, userID string, product Product) (*IssueTokenResponse, error)
 	CreateUser(ctx context.Context, email string) (*CreateUserResponse, error)
+	GetUser(ctx context.Context, userID string) (*User, error)
 	GetOnboardingWidget(ctx context.Context, userID string) (string, error)
 	GetOnOffRampWidget(ctx context.Context, userID string, isDeposit bool) (string, error)
 	GetUserWallets(ctx context.Context, userID string) (*GetUserWalletsResponse, error)
