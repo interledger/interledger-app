195a2e0320e84ece60df0b7ee3fc086bb679e4d3
Donovan Changfoot <don@fynbos.dev>
2024-06-10 10:56:50 +0200

    fix(backend): deprecate bankBeneficiaryType field for xago api call

--- go/mockbos/xago/server.go
+++ go/mockbos/xago/server.go
@@ -126,18 +126,17 @@ func (s *Server) AddBeneficiary() http.HandlerFunc {
 		}
 
 		_, err := s.db.CreateXagoBeneficiary(r.Context(), db.CreateXagoBeneficiaryParams{
-			Name:                pgtype.Text{String: req.Name, Valid: true},
-			Scope:               pgtype.Text{String: req.Scope, Valid: true},
-			CurrencyCode:        pgtype.Text{String: req.CurrencyCode, Valid: true},
-			AccountNumber:       pgtype.Text{String: req.AccountNumber, Valid: true},
-			BranchCode:          pgtype.Text{String: req.BranchCode, Valid: true},
-			BankName:            pgtype.Text{String: req.BankName, Valid: true},
-			BankCountry:         pgtype.Text{String: req.BankCountry, Valid: true},
-			AccountName:         pgtype.Text{String: req.AccountName, Valid: true},
-			BankBeneficiaryType: pgtype.Text{String: req.BankBeneficiaryType, Valid: true},
-			Reference:           pgtype.Text{String: req.Reference, Valid: true},
-			Iban:                pgtype.Text{String: req.Iban, Valid: true},
-			Bic:                 pgtype.Text{String: req.Bic, Valid: true},
+			Name:          pgtype.Text{String: req.Name, Valid: true},
+			Scope:         pgtype.Text{String: req.Scope, Valid: true},
+			CurrencyCode:  pgtype.Text{String: req.CurrencyCode, Valid: true},
+			AccountNumber: pgtype.Text{String: req.AccountNumber, Valid: true},
+			BranchCode:    pgtype.Text{String: req.BranchCode, Valid: true},
+			BankName:      pgtype.Text{String: req.BankName, Valid: true},
+			BankCountry:   pgtype.Text{String: req.BankCountry, Valid: true},
+			AccountName:   pgtype.Text{String: req.AccountName, Valid: true},
+			Reference:     pgtype.Text{String: req.Reference, Valid: true},
+			Iban:          pgtype.Text{String: req.Iban, Valid: true},
+			Bic:           pgtype.Text{String: req.Bic, Valid: true},
 		})
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
