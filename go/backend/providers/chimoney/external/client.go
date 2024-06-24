cc495696ca6aef4c081dc3cb4ec0fe88e9db8dbb
barnard <barnard@fynbos.dev>
2024-06-24 14:25:26 +0200

    feat(backend): Add chi money external client

---
+++ go/backend/providers/chimoney/external/client.go
@@ -0,0 +1,234 @@
+package external
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"gitlab.com/fynbos/env"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+)
+
+type Client interface {
+	CreateSubAccount(ctx context.Context, req CreateSubAccountReq) (string, error)
+	Transfer(ctx context.Context, req TransferReq) error
+	Deposit(ctx context.Context, req DepositReq) (string, error)
+	Withdraw(ctx context.Context, req WithdrawalReq) error
+}
+
+type client struct {
+	baseURL string
+	apiKey  string
+	api     *http.Client
+}
+
+func New(transport *http.Client) Client {
+	baseURL := "https://api.chimoney.io/v0.2"
+	if !env.IsProd() {
+		baseURL = "https://api-v2-sandbox.chimoney.io/v0.2"
+	}
+
+	api := otelhttp.DefaultClient
+	if transport != nil {
+		api = transport
+	}
+
+	return &client{
+		api:     api, // TODO redact and intercept
+		baseURL: baseURL,
+		apiKey:  "TODO",
+	}
+}
+
+func (c client) CreateSubAccount(ctx context.Context, req CreateSubAccountReq) (string, error) {
+	endpoint, err := url.JoinPath(c.baseURL, "multicurrency-wallets", "create")
+	if err != nil {
+		return "", err
+	}
+
+	body, err := json.Marshal(req)
+	if err != nil {
+		return "", err
+	}
+
+	httpReq, err := http.NewRequestWithContext(ctx, http.MethodPost, endpoint, bytes.NewBuffer(body))
+	if err != nil {
+		return "", err
+	}
+	httpReq.Header.Add("Content-Type", "application/json")
+	httpReq.Header.Add("Accept", "application/json")
+
+	httpResp, err := c.api.Do(httpReq)
+	if err != nil {
+		return "", err
+	}
+
+	body, err = io.ReadAll(httpResp.Body)
+	if err != nil {
+		return "", err
+	}
+	defer httpResp.Body.Close()
+
+	var respWrapper APIResponse
+	err = json.Unmarshal(body, &respWrapper)
+	if err != nil {
+		return "", err
+	}
+
+	if !strings.EqualFold(respWrapper.Status, "success") || respWrapper.Error != "" {
+		return "", fmt.Errorf("request failed on creating sub account with status (%s) error (%s)", respWrapper.Status, respWrapper.Error)
+	}
+
+	var resp CreateSubAccountResp
+	err = json.Unmarshal(respWrapper.Data, &resp)
+	if err != nil {
+		return "", err
+	}
+
+	return resp.ID, nil
+}
+
+func (c client) Transfer(ctx context.Context, req TransferReq) error {
+	endpoint, err := url.JoinPath(c.baseURL, "multicurrency-wallets", "transfer")
+	if err != nil {
+		return err
+	}
+
+	type transferReq struct {
+		SenderSubAccount    string `json:"subAccount"`
+		ReceiverSubAccount  string `json:"receiver"`
+		Amount              string `json:"amountToSend"`
+		SourceCurrency      string `json:"originCurrency"`
+		DestinationCurrency string `json:"destinationCurrency"`
+	}
+
+	body, err := json.Marshal(transferReq{
+		SenderSubAccount:    req.SenderSubAccount,
+		ReceiverSubAccount:  req.ReceiverSubAccount,
+		Amount:              req.Amount.FormatAmount(),
+		SourceCurrency:      req.Amount.Currency.String(),
+		DestinationCurrency: req.Amount.Currency.String(),
+	})
+	if err != nil {
+		return err
+	}
+
+	httpReq, err := http.NewRequestWithContext(ctx, http.MethodPost, endpoint, bytes.NewBuffer(body))
+	if err != nil {
+		return err
+	}
+	httpReq.Header.Add("Content-Type", "application/json")
+	httpReq.Header.Add("Accept", "application/json")
+
+	httpResp, err := c.api.Do(httpReq)
+	if err != nil {
+		return err
+	}
+
+	body, err = io.ReadAll(httpResp.Body)
+	if err != nil {
+		return err
+	}
+	defer httpResp.Body.Close()
+
+	var respWrapper APIResponse
+	err = json.Unmarshal(body, &respWrapper)
+	if err != nil {
+		return err
+	}
+
+	if !strings.EqualFold(respWrapper.Status, "success") || respWrapper.Error != "" {
+		return fmt.Errorf("request failed on transfering between sub accounts with status (%s) error (%s)", respWrapper.Status, respWrapper.Error)
+	}
+
+	return nil
+}
+
+func (c client) Withdraw(ctx context.Context, req WithdrawalReq) error {
+	endpoint, err := url.JoinPath(c.baseURL, "payouts", "interac")
+	if err != nil {
+		return err
+	}
+
+	body, err := json.Marshal(req)
+	if err != nil {
+		return err
+	}
+
+	httpReq, err := http.NewRequestWithContext(ctx, http.MethodPost, endpoint, bytes.NewBuffer(body))
+	if err != nil {
+		return err
+	}
+	httpReq.Header.Add("Content-Type", "application/json")
+	httpReq.Header.Add("Accept", "application/json")
+
+	httpResp, err := c.api.Do(httpReq)
+	if err != nil {
+		return err
+	}
+
+	body, err = io.ReadAll(httpResp.Body)
+	if err != nil {
+		return err
+	}
+	defer httpResp.Body.Close()
+
+	var respWrapper APIResponse
+	err = json.Unmarshal(body, &respWrapper)
+	if err != nil {
+		return err
+	}
+
+	if !strings.EqualFold(respWrapper.Status, "success") || respWrapper.Error != "" {
+		return fmt.Errorf("request failed on withdrawal with status (%s) error (%s)", respWrapper.Status, respWrapper.Error)
+	}
+
+	return nil
+}
+
+func (c client) Deposit(ctx context.Context, req DepositReq) (string, error) {
+	endpoint, err := url.JoinPath(c.baseURL, "payouts", "interac")
+	if err != nil {
+		return "", err
+	}
+
+	body, err := json.Marshal(req)
+	if err != nil {
+		return "", err
+	}
+
+	httpReq, err := http.NewRequestWithContext(ctx, http.MethodPost, endpoint, bytes.NewBuffer(body))
+	if err != nil {
+		return "", err
+	}
+	httpReq.Header.Add("Content-Type", "application/json")
+	httpReq.Header.Add("Accept", "application/json")
+
+	httpResp, err := c.api.Do(httpReq)
+	if err != nil {
+		return "", err
+	}
+
+	body, err = io.ReadAll(httpResp.Body)
+	if err != nil {
+		return "", err
+	}
+	defer httpResp.Body.Close()
+
+	var respWrapper APIResponse
+	err = json.Unmarshal(body, &respWrapper)
+	if err != nil {
+		return "", err
+	}
+
+	if !strings.EqualFold(respWrapper.Status, "success") || respWrapper.Error != "" {
+		return "", fmt.Errorf("request failed on withdrawal with status (%s) error (%s)", respWrapper.Status, respWrapper.Error)
+	}
+
+	return "", nil
+}
