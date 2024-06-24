1fb12fbfb0e2ec1e5201227b5caaeae8ce800e41
Donovan Changfoot <don@fynbos.dev>
2024-06-24 13:20:05 +0200

    chore(dev): clean up unused files

--- go/backend/create_certs.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-COCKROACH_PATH=../../services/cockroach
-
-rm -rf ./certs
-
-mkdir -p certs
-cp $COCKROACH_PATH/certs/ca.crt ./certs/
-cockroach cert create-client backend --certs-dir=certs --ca-key=$COCKROACH_PATH/safe/ca.key
