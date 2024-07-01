23a8a32bf0ac6a6e71036d28cd70ddc18347697b
barnard <barnard@fynbos.dev>
2024-07-01 09:41:52 +0200

    feat(backend): Chimoney peer to peer payment workflow

--- go/backend/providers/chimoney/errors.go
+++ go/backend/providers/chimoney/errors.go
@@ -3,6 +3,7 @@ package chimoney
 import "errors"
 
 var (
-	ErrInternal = errors.New("chimoney: internal error")
-	ErrNotFound = errors.New("chimoney: not found")
+	ErrInternal            = errors.New("chimoney: internal error")
+	ErrNotFound            = errors.New("chimoney: not found")
+	ErrInsufficientBalance = errors.New("chimoney: insufficient balance")
 )
--- go/backend/providers/chimoney/types.go
+++ go/backend/providers/chimoney/types.go
@@ -1,5 +1,7 @@
 package chimoney
 
+import "gitlab.com/fynbos/backend/currency"
+
 const (
 	ProviderName   = "chimoney"
 	AccTypeBalance = "balance"
@@ -7,3 +9,14 @@ const (
 	LedgerIDCAD   uint32 = 24466639 // Spells chimoney on a Nokia 3320 keyboard
 	CADOpsAccount        = "a99a7194-e446-46e0-baad-49eda340b7c9"
 )
+
+type Balance struct {
+	Total     currency.Amount
+	Available currency.Amount
+}
+
+type TransferArgs struct {
+	SendingWalletID   string
+	ReceivingWalletID string
+	Amount            currency.Amount
+}
