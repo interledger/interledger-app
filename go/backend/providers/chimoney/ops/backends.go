24d8d0ff0378da6755d6da61bf620642e9eec7bf
barnard <barnard@fynbos.dev>
2024-07-02 09:44:58 +0200

    feat(backend): Add unit test for chimoney interac email

--- go/backend/providers/chimoney/ops/backends.go
+++ go/backend/providers/chimoney/ops/backends.go
@@ -1,10 +1,11 @@
 package ops
 
 import (
+	"testing"
+
 	"github.com/jmoiron/sqlx"
 	"gitlab.com/fynbos/backend/kyc"
 	"gitlab.com/fynbos/backend/linkedaccounts"
-	"gitlab.com/fynbos/backend/transactions"
 	"gitlab.com/fynbos/backend/user"
 	"gitlab.com/fynbos/backend/wallets"
 	"gitlab.com/fynbos/pacioli"
@@ -19,5 +20,44 @@ type Backends interface {
 	Wallets() wallets.Client
 	Pacioli() pacioli.Client
 	KYC() kyc.Client
-	Transactions() transactions.Client
+}
+
+type TestBackends struct {
+	DBC *sqlx.DB
+}
+
+func (t TestBackends) LinkedAccounts() linkedaccounts.Client {
+	return nil
+}
+
+func (t TestBackends) Users() user.Client {
+	return nil
+}
+
+func (t TestBackends) Temporal() temporal.Client {
+	return nil
+}
+
+func (t TestBackends) Wallets() wallets.Client {
+	return nil
+}
+
+func (t TestBackends) Pacioli() pacioli.Client {
+	return nil
+}
+
+func (t TestBackends) KYC() kyc.Client {
+	return nil
+}
+
+func (t TestBackends) DB() *sqlx.DB {
+	return t.DBC
+}
+
+func NewTestBackends(_ *testing.T, opts ...func(*TestBackends)) Backends {
+	b := &TestBackends{}
+	for _, opt := range opts {
+		opt(b)
+	}
+	return b
 }
