823d7a8bfd4f4a11b57b0d4a4dd51ab086ebbecd
Donovan Changfoot <don@fynbos.dev>
2024-07-02 12:46:53 +0200

    chore: deprecate slack bot

--- go/backend/slack/client/client.go
+++ go/backend/slack/client/client.go
@@ -2,7 +2,6 @@ package client
 
 import (
 	"context"
-	"net/http"
 
 	"gitlab.com/fynbos/backend/payments"
 
@@ -50,8 +49,4 @@ func (c *client) CreateAuthURL(ctx context.Context, walletID string) (string, er
 
 func (c *client) CreateConnection(ctx context.Context, args slack.CreateConnectionArgs) (*slack.Connection, error) {
 	return ops.CreateConnection(ctx, c.b, args)
-}
-
-func (c *client) BotInstallWebhook() http.HandlerFunc {
-	return ops.BotInstallWebhook(c.b)
 }
