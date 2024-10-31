af0cf98db90dfd1dbbf9a50a1c99f8d8f81bfce3
Donovan Changfoot <don@fynbos.dev>
2024-10-31 14:54:05 +0200

    fix: pin protoc-gen-es used to v1.10.0

--- typescript/protea/app/routes.d.ts
+++ typescript/protea/app/routes.d.ts
@@ -8,6 +8,7 @@ declare module "routes-gen" {
     "/api/fern": Record<string, never>;
     "/api/maps/geocode": Record<string, never>;
     "/api/maps/placesAutocomplete": Record<string, never>;
+    "/api/pti/token": Record<string, never>;
     "/api/sendOtp": Record<string, never>;
     "/callbacks/chimoney": Record<string, never>;
     "/connect/bank/za": Record<string, never>;
@@ -66,6 +67,7 @@ declare module "routes-gen" {
       | ["/api/fern"]
       | ["/api/maps/geocode"]
       | ["/api/maps/placesAutocomplete"]
+      | ["/api/pti/token"]
       | ["/api/sendOtp"]
       | ["/callbacks/chimoney"]
       | ["/connect/bank/za"]
