8e964102904f76d096ae5824f5517819d9028c9a
Donovan Changfoot <don@fynbos.dev>
2024-11-01 17:31:24 +0200

    w

--- typescript/protea/app/routes/api_.pti_.token.ts
+++ typescript/protea/app/routes/api_.pti_.token.ts
@@ -3,17 +3,22 @@ import { isConnectError } from '~/lib/error.server'
 import { grpc } from '~/lib/grpc.server'
 
 export async function action({ request }: ActionFunctionArgs) {
-  const payload = await request.json()
-  const response = await grpc.createPtiToken(request, {
-    url: payload['x-pti-token-payload'].url,
-    method: payload['x-pti-token-payload'].method
-  })
+  try {
+    const payload = await request.json()
+    console.log(payload)
+    const response = await grpc.createPtiToken(request, {
+      url: payload['x-pti-token-payload'].url,
+      method: payload['x-pti-token-payload'].method
+    })
 
-  if (isConnectError(response)) throw response.errorResponse
+    if (isConnectError(response)) throw response.errorResponse
 
-  return json({
-    accessToken: response.accessToken,
-    expiresAt: response.expiresAt,
-    tokenType: response.tokenType
-  })
+    return json({
+      accessToken: response.accessToken,
+      expiresAt: response.expiresAt,
+      tokenType: response.tokenType
+    })
+  } catch (err) {
+    console.log(err)
+  }
 }
