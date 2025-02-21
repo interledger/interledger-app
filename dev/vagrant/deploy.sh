6644da316cb95e37f63a3526b79d035edfd2c19c
Radu-Cristian Popa <radu.popa@breakpointit.eu>
2025-02-21 17:23:55 +0200

    Upgrade Rafiki to alpha 15

--- dev/vagrant/deploy.sh
+++ dev/vagrant/deploy.sh
@@ -34,18 +34,18 @@ function build_docker_images () {
 	docker push ${REGISTRY}/mockbos
 
 	echo "Pulling rafiki backend"
-	docker pull ghcr.io/interledger/rafiki-backend:v1.0.0-alpha.11
-	docker tag ghcr.io/interledger/rafiki-backend:v1.0.0-alpha.11 localhost:5002/rafiki-backend
+	docker pull ghcr.io/interledger/rafiki-backend:v1.0.0-alpha.15
+	docker tag ghcr.io/interledger/rafiki-backend:v1.0.0-alpha.15 localhost:5002/rafiki-backend
 	docker push localhost:5002/rafiki-backend
 
 	echo "Pulling rafiki auth"
-	docker pull ghcr.io/interledger/rafiki-auth:v1.0.0-alpha.11
-	docker tag ghcr.io/interledger/rafiki-auth:v1.0.0-alpha.11 localhost:5002/rafiki-auth
+	docker pull ghcr.io/interledger/rafiki-auth:v1.0.0-alpha.15
+	docker tag ghcr.io/interledger/rafiki-auth:v1.0.0-alpha.15 localhost:5002/rafiki-auth
 	docker push localhost:5002/rafiki-auth
 
 	echo "Pulling rafiki frontend"
-	docker pull ghcr.io/interledger/rafiki-frontend:v1.0.0-alpha.9
-	docker tag ghcr.io/interledger/rafiki-frontend:v1.0.0-alpha.9 localhost:5002/rafiki-frontend
+	docker pull ghcr.io/interledger/rafiki-frontend:v1.0.0-alpha.16
+	docker tag ghcr.io/interledger/rafiki-frontend:v1.0.0-alpha.16 localhost:5002/rafiki-frontend
 	docker push localhost:5002/rafiki-frontend
 }
 
