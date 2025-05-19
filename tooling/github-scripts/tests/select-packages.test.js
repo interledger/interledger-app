0d2a096e84b617955afd4712a0a9421f386772ec
Radu-Cristian Popa <radu.popa@breakpointit.eu>
2025-05-19 07:23:09 +0300

    Update select-packages tests - new assertion

--- tooling/github-scripts/tests/select-packages.test.js
+++ tooling/github-scripts/tests/select-packages.test.js
@@ -1,11 +1,12 @@
-// @ts-check
 import { describe, it } from "node:test";
 import assert from "node:assert";
 
 import { generateBuilds } from "../select-packages.js";
+import { GO_PACKAGES, TS_PACKAGES } from "../constants.js";
 
 describe("select-packages/generateBuilds", () => {
   it("should return the correct builds struncture", () => {
+    const expectedPackages = [...GO_PACKAGES, ...TS_PACKAGES];
     const builds = generateBuilds();
 
     assert.equal(
@@ -19,7 +20,18 @@ describe("select-packages/generateBuilds", () => {
     );
     assert.ok(
       builds.platform.length > 0,
-      'The builds objects "package" property is empty',
+      'The builds objects "platform" property is empty',
     );
+
+    for (const expectedPackage of expectedPackages) {
+      const match = builds.package.find(
+        (pkg) => pkg.name === expectedPackage.name,
+      );
+
+      assert.ok(
+        match,
+        `Expected package with name "${expectedPackage.name}" to be included in builds.package`,
+      );
+    }
   });
 });
