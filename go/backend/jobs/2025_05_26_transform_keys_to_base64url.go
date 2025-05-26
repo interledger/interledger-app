d5b18335655f06cc52c01c7e0a2956f5add238ca
Radu-Cristian Popa <radu.popa@breakpointit.eu>
2025-05-26 13:30:42 +0300

    Update transform job

--- go/backend/jobs/2025_05_26_transform_keys_to_base64url.go
+++ go/backend/jobs/2025_05_26_transform_keys_to_base64url.go
@@ -1,15 +1,12 @@
 package jobs
 
 import (
-	"context"
 	"encoding/base64"
 	"os"
 	"time"
 
-	"gitlab.com/fynbos/backend/db"
 	"gitlab.com/fynbos/backend/keys"
 	"gitlab.com/fynbos/log"
-	"go.temporal.io/sdk/activity"
 	"go.temporal.io/sdk/temporal"
 	"go.temporal.io/sdk/workflow"
 	"go.uber.org/zap"
@@ -18,7 +15,7 @@ import (
 const limit = 100
 const batchSize = 100
 
-func RemoveCustodialKeysAndTranformKeysJob(ctx workflow.Context) error {
+func TransformKeysToBase64URLJob(ctx workflow.Context) error {
 	var a *Activity
 	ao := workflow.ActivityOptions{
 		StartToCloseTimeout: 10 * time.Second,
@@ -30,12 +27,7 @@ func RemoveCustodialKeysAndTranformKeysJob(ctx workflow.Context) error {
 	ctx = workflow.WithActivityOptions(ctx, ao)
 	log.Info("Starting job TransformKeysToBase64URL: transforming public keys values from Base64 to Base64URL")
 
-	err := workflow.ExecuteActivity(ctx, a.RemoveCustodialKeys).Get(ctx, nil)
-	if err != nil {
-		return err
-	}
-
-	err = workflow.ExecuteActivity(ctx, a.TransformWalletKeys).Get(ctx, nil)
+	err := workflow.ExecuteActivity(ctx, a.TransformWalletKeys).Get(ctx, nil)
 	if err != nil {
 		return err
 	}
@@ -45,66 +37,31 @@ func RemoveCustodialKeysAndTranformKeysJob(ctx workflow.Context) error {
 		return err
 	}
 
-	log.Info("Completed job TransformKeysToBase64URL.")
+	log.Info("Completed job TransformKeysToBase64URL")
 	return nil
-}
-
-func (a *Activity) RemoveCustodialKeys(ctx context.Context) error {
-	logger := activity.GetLogger(ctx)
-
-	wallets, err := a.b.Wallets().ListAll(ctx, db.Pagination{
-		PageToken: "",
-		PageSize:  50,
-	})
-
-	if err != nil {
-		return err
-	}
-
-	for _, wallet := range wallets {
-		logger.Info("removing custodial keys for wallet", "wallet", wallet.ID)
-		err = a.b.Keys().RemoveCustodialKeysForWallet(ctx, wallet.ID)
-		if err != nil {
-			return err
-		}
-	}
-
-	rows, err := a.b.DB().ExecContext(ctx, "DELETE FROM wallet_keys WHERE key_type = $1", keys.Custodial)
-	if err != nil {
-		return err
-	}
-
-	affected, err := rows.RowsAffected()
-	if err != nil {
-		return err
-	}
-
-	log.Info("deleted custodial wallet keys", zap.Int64("rows-affected", affected))
-	return nil
-}
-
-type walletKey struct {
-	ID        string `db:"id"`
-	PublicKey string `db:"public_key"`
 }
 
 func (a *Activity) TransformWalletKeys() error {
+	type key struct {
+		ID        string `db:"id"`
+		PublicKey string `db:"public_key"`
+	}
 	offset := 0
 
 	for {
-		var walletsKeys []walletKey
+		var wKeys []key
 
-		err := a.b.DB().Select(&walletsKeys, "SELECT id, public_key FROM wallet_keys WHERE public_key IS NOT NULL AND key_type = $1 AND location = 'database' AND deleted_at IS NULL ORDER BY created_at ASC LIMIT $2 OFFSET $3", keys.NonCustodial, limit, offset)
+		err := a.b.DB().Select(&wKeys, "SELECT id, public_key FROM wallet_keys WHERE public_key IS NOT NULL AND key_type = $1 AND location = 'database' AND deleted_at IS NULL ORDER BY created_at ASC LIMIT $2 OFFSET $3", keys.NonCustodial, limit, offset)
 		if err != nil {
 			return err
 		}
 
-		if len(walletsKeys) == 0 {
+		if len(wKeys) == 0 {
 			break
 		}
 
 		tx := a.b.DB().MustBegin()
-		for _, key := range walletsKeys {
+		for _, key := range wKeys {
 			decoded, err := base64.StdEncoding.DecodeString(key.PublicKey)
 			if err != nil {
 				return err
@@ -112,7 +69,6 @@ func (a *Activity) TransformWalletKeys() error {
 
 			encoded := base64.RawURLEncoding.EncodeToString(decoded)
 
-			// Update the row
 			_, err = tx.Exec(`UPDATE wallet_keys SET public_key = $1 WHERE id = $2`, encoded, key.ID)
 			if err != nil {
 				return err
@@ -129,12 +85,11 @@ func (a *Activity) TransformWalletKeys() error {
 	return nil
 }
 
-type rafikiKey struct {
-	ID string `db:"id"`
-	X  string `db:"x"`
-}
-
 func (a *Activity) TransformRafikiKeys() error {
+	type key struct {
+		ID string `db:"id"`
+		X  string `db:"x"`
+	}
 	offset := 100
 	connString := os.Getenv("RAFIKI_DB_URL")
 
@@ -146,19 +101,19 @@ func (a *Activity) TransformRafikiKeys() error {
 	defer db.Close()
 
 	for {
-		var rafikiKeys []rafikiKey
+		var rKeys []key
 
-		err := a.b.DB().Select(&rafikiKeys, `SELECT id, x FROM \"walletAddressKeys\" WHERE revoked = false ORDER BY "createdAt" DESC LIMIT $1 OFFSET $2`, limit, offset)
+		err := db.Select(&rKeys, `SELECT id, x FROM "walletAddressKeys" WHERE revoked = false ORDER BY "createdAt" DESC LIMIT $1 OFFSET $2`, limit, offset)
 		if err != nil {
 			return err
 		}
 
-		if len(rafikiKeys) == 0 {
+		if len(rKeys) == 0 {
 			break
 		}
 
 		tx := a.b.DB().MustBegin()
-		for _, key := range rafikiKeys {
+		for _, key := range rKeys {
 			decoded, err := base64.StdEncoding.DecodeString(key.X)
 			if err != nil {
 				return err
--- go/backend/rafiki/ops/ops.go
+++ go/backend/rafiki/ops/ops.go
@@ -50,17 +50,17 @@ func CreatePaymentPointer(ctx context.Context, b Backends, w wallets.Wallet) err
 	// This shouldn't really happen from now on since we are not provisioning a
 	// custodial key anymore when the wallet is created.
 
-	keys, err := b.Keys().List(ctx, w.ID)
-	if err != nil {
-		return fmt.Errorf("%w %s", rafiki.ErrInternal, err)
-	}
-
-	for _, key := range keys {
-		err := CreatePaymentPointerKey(ctx, b, key.ID, w.ID)
-		if err != nil {
-			return fmt.Errorf("%w %s", rafiki.ErrInternal, err)
-		}
-	}
+	// keys, err := b.Keys().List(ctx, w.ID)
+	// if err != nil {
+	// 	return fmt.Errorf("%w %s", rafiki.ErrInternal, err)
+	// }
+	//
+	// for _, key := range keys {
+	// 	err := CreatePaymentPointerKey(ctx, b, key.ID, w.ID)
+	// 	if err != nil {
+	// 		return fmt.Errorf("%w %s", rafiki.ErrInternal, err)
+	// 	}
+	// }
 
 	return nil
 }
