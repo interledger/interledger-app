c19a818cf49f6e7d4059106296e54ddc6fe292e8
Arpi Lengyel <arpad.lengyel@breakpointit.eu>
2025-07-18 13:04:56 +0300

    style corrections

--- typescript/protea/app/routes/unavailable.tsx
+++ typescript/protea/app/routes/unavailable.tsx
@@ -34,11 +34,11 @@ export default function Page() {
       <GridColumn
         className='col-span-full'
       >
-        <Card>
-          <CardIcon>
+        <Card className='flex !flex-row'>
+          <CardIcon className='h-16 my-auto'>
             <Icon className='text-red-600'>warning</Icon>
           </CardIcon>
-          <CardContent className='text-lg'>
+          <CardContent className='text-lg ml-2'>
             The application is not yet available in your location, but do not worry we are working tirelessly to solve it as fast as possible! <br />
             We will notify you by email once the application becomes fully functional in your region.
           </CardContent>
