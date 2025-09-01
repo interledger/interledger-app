ac5bacfe892cbf82decb1e44df17e5be12479ea9
Raul <Ranete.raul@yahoo.com>
2025-09-01 16:08:22 +0300

    fix(frontend): disable segment for now (#138)
    Co-authored-by: Raul Ranete <raul.ranete@breakpoinit.eu>

--- typescript/protea/app/root.tsx
+++ typescript/protea/app/root.tsx
@@ -24,7 +24,6 @@ import { AnchorRouter, Error, LiveReload } from '~/components'
 import { Scaffold } from '~/components/Scaffold'
 import { hasUserSession } from '~/lib/kratos.server'
 import { getSnackbar } from '~/lib/snackbar.server'
-import { useSegment } from '~/lib/useSegment'
 import styles from '~/styles/app.css'
 import { getFeatures } from './data/wallet.server'
 
@@ -160,7 +159,7 @@ export async function loader({ request }: LoaderFunctionArgs) {
 function Page() {
   const location = useLocation()
   const { env } = useLoaderData<typeof loader>()
-  useSegment(env.segmentApiKey)
+  // useSegment(env.segmentApiKey)
 
   if (location.pathname == '/temp-cloudflare-error') return <CloudFlareError />
 
