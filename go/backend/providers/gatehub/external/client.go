54298f85123179de3ecd57cc8c4e50cbbbaf5466
Raul Ranete <raul.ranete@breakpoinit.eu>
2025-10-06 18:41:05 +0300

    fix(backend): return error when gateway id is not set

--- go/backend/providers/gatehub/external/client.go
+++ go/backend/providers/gatehub/external/client.go
@@ -62,10 +62,10 @@ func NewClient(appID, secret, gatewayID string, transport *http.Client) Client {
 		api = transport
 	}
 
-	if gatewayID != "" {
+	if gatewayID == "" {
 		log.Error("GATEHUB_GATEWAY_ID is not set")
-
 	}
+
 	return &client{
 		onOffRampClientID:  onOffRampClientID,
 		onboardingClientID: onboardingClientID,
