d9b5c7708ffb207291da4d1780d5bb93912cac5b
Raul Ranete <raul.ranete@breakpoinit.eu>
2025-10-08 12:15:50 +0300

    fix(backcend): simplify xago KYC update

--- go/backend/jobs/2025_09_disable_KYC_status_xago.go
+++ go/backend/jobs/2025_09_disable_KYC_status_xago.go
@@ -2,18 +2,17 @@ package jobs
 
 import (
 	"context"
-	"strings"
 	"time"
 
-	"gitlab.com/fynbos/backend/country"
 	"gitlab.com/fynbos/backend/kyc"
+	"gitlab.com/fynbos/backend/kyc/persona"
 	"gitlab.com/fynbos/log"
 	"go.temporal.io/sdk/temporal"
 	"go.temporal.io/sdk/workflow"
 	"go.uber.org/zap"
 )
 
-func RestartKYCstatusForXagoJob(ctx workflow.Context, params []string) error {
+func RestartKYCstatusForXagoJob(ctx workflow.Context) error {
 	var a *Activity
 	ao := workflow.ActivityOptions{
 		StartToCloseTimeout: 10 * time.Second,
@@ -22,64 +21,36 @@ func RestartKYCstatusForXagoJob(ctx workflow.Context, params []string) error {
 		},
 	}
 	ctx = workflow.WithActivityOptions(ctx, ao)
-	log.Info("starting RestartKYCstatusForXagoJob", zap.Strings("wallet_ids", params))
-	var walletIDs []string = params
-	if len(walletIDs) == 0 {
-		log.Info("No wallet ids provided, getting all ZA id")
-		err := workflow.ExecuteActivity(ctx, a.FetchXagoWalletIds, params).Get(ctx, &walletIDs)
-		if err != nil {
-			log.Error("FetchXagoWalletIds failed to fetch wallets", zap.Any("err", err))
-			return err
-		}
-	}
-
-	if len(walletIDs) == 0 {
-		log.Info("No wallet ids found, exiting job")
-		return nil
-	}
-	err := workflow.ExecuteActivity(ctx, a.UpdatePersonaInquiryXagoStatus, walletIDs).Get(ctx, nil)
+	err := workflow.ExecuteActivity(ctx, a.UpdatePersonaInquiryXagoStatus).Get(ctx, nil)
 	if err != nil {
 		log.Error("UpdatePersonaInquiryStatus failed", zap.Any("err", err))
 		return err
 	}
 
-	err = workflow.ExecuteActivity(ctx, a.RestartXagoKYCForWallets, walletIDs).Get(ctx, nil)
+	err = workflow.ExecuteActivity(ctx, a.RestartXagoKYCForWallets).Get(ctx, nil)
 	if err != nil {
 		log.Error("RestartKYCForWallets failed", zap.Any("err", err))
 		return err
 	}
-	log.Info("completed RestartKYCstatusForXagoJob", zap.Strings("wallet_ids", params))
+	log.Info("completed RestartKYCstatusForXagoJob")
 	return nil
 }
 
-func (a *Activity) UpdatePersonaInquiryXagoStatus(ctx context.Context, walletIds []string) error {
-	query := "UPDATE kyc_persona_inquiries SET state = 'expired' where wallet_id in ($1)"
-	_, err := a.b.DB().ExecContext(ctx, query, strings.Join(walletIds, ","))
+func (a *Activity) UpdatePersonaInquiryXagoStatus(ctx context.Context) error {
+	query := "UPDATE kyc_persona_inquiries SET state = $1 where wallet_id in (SELECT id FROM wallets where country = 'ZA')"
+	_, err := a.b.DB().ExecContext(ctx, query, persona.InquiryExpired)
 	if err != nil {
 		return err
 	}
 
 	return nil
 }
-func (a *Activity) RestartXagoKYCForWallets(ctx context.Context, walletIds []string) error {
-
-	query := "UPDATE wallet_kyc_status SET status = $1 where wallet_id in ($2)"
-	_, err := a.b.DB().ExecContext(ctx, query, kyc.StatusUnknown, strings.Join(walletIds, ","))
+func (a *Activity) RestartXagoKYCForWallets(ctx context.Context) error {
+	query := "UPDATE wallet_kyc_status SET status = $1 where wallet_id in (SELECT id FROM wallets where country = 'ZA')"
+	_, err := a.b.DB().ExecContext(ctx, query, kyc.StatusUnknown)
 	if err != nil {
 		return err
 	}
 
 	return nil
-}
-
-func (a *Activity) FetchXagoWalletIds(ctx context.Context) ([]string, error) {
-	query := "SELECT id FROM wallets where country=$1"
-	var walletList []string
-
-	err := a.b.DB().SelectContext(ctx, &walletList, query, country.ZA)
-	if err != nil {
-		return nil, err
-	}
-
-	return walletList, nil
 }
