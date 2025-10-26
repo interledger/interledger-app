3fe562606d043205a8a8b064e5cf0e007287f31d
Anto <me@example.com>
2025-10-26 23:49:44 +0200

    allow only UNFREEZE / TERMINATE if card is frozen

--- typescript/protea/app/components/CardView/CardActions.tsx
+++ typescript/protea/app/components/CardView/CardActions.tsx
@@ -30,74 +30,80 @@ export const CardActions = ({
   return (
     <div className='flex items-center space-x-4'>
       {/* Flip card */}
-      <button
-        className='flex items-center justify-center space-x-2 rounded-lg bg-blue-500 px-4 py-2 text-white transition-colors hover:bg-blue-600'
-        onClick={flip}
-      >
-        <Icon>{showBack ? 'flip_to_front' : 'flip_to_back'}</Icon>
-        <span>Flip</span>
-      </button>
+      {!isFrozen && (
+        <button
+          className='flex items-center justify-center space-x-2 rounded-lg bg-blue-500 px-4 py-2 text-white transition-colors hover:bg-blue-600'
+          onClick={flip}
+        >
+          <Icon>{showBack ? 'flip_to_front' : 'flip_to_back'}</Icon>
+          <span>Flip</span>
+        </button>
+      )}
 
       {/* View Dropdown */}
-      <Menu as='div' className='relative'>
-        {({ open }) => (
-          <>
-            <Menu.Button className='flex items-center justify-center space-x-2 rounded-lg bg-teal-600 px-4 py-2 text-white transition-colors hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2'>
-              <Icon>visibility</Icon>
-              <Icon className='text-lg'>
-                {open ? 'expand_less' : 'expand_more'}
-              </Icon>
-            </Menu.Button>
+      {!isFrozen && (
+        <Menu as='div' className='relative'>
+          {({ open }) => (
+            <>
+              <Menu.Button className='flex items-center justify-center space-x-2 rounded-lg bg-teal-600 px-4 py-2 text-white transition-colors hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2'>
+                <Icon>visibility</Icon>
+                <Icon className='text-lg'>
+                  {open ? 'expand_less' : 'expand_more'}
+                </Icon>
+              </Menu.Button>
 
-            <Transition
-              as={Fragment}
-              show={open}
-              enter='transition ease-out duration-100'
-              enterFrom='transform opacity-0 scale-95'
-              enterTo='transform opacity-100 scale-100'
-              leave='transition ease-in duration-75'
-              leaveFrom='transform opacity-100 scale-100'
-              leaveTo='transform opacity-0 scale-95'
-            >
-              <Menu.Items className='absolute left-0 z-10 mt-2 w-48 origin-top-left rounded-lg border border-gray-200 bg-white shadow-lg focus:outline-none'>
-                <div className='py-1'>
-                  {/* Details */}
-                  <Menu.Item>
-                    {({ active }) => (
-                      <button
-                        onClick={toggleSensitiveDataOn}
-                        className={`flex w-full items-center space-x-3 px-4 py-2 text-left text-sm ${
-                          active ? 'bg-teal-50 text-teal-900' : 'text-gray-700'
-                        }`}
-                      >
-                        <Icon className='text-teal-600'>numbers</Icon>
-                        <span>Details</span>
-                      </button>
-                    )}
-                  </Menu.Item>
+              <Transition
+                as={Fragment}
+                show={open}
+                enter='transition ease-out duration-100'
+                enterFrom='transform opacity-0 scale-95'
+                enterTo='transform opacity-100 scale-100'
+                leave='transition ease-in duration-75'
+                leaveFrom='transform opacity-100 scale-100'
+                leaveTo='transform opacity-0 scale-95'
+              >
+                <Menu.Items className='absolute left-0 z-10 mt-2 w-48 origin-top-left rounded-lg border border-gray-200 bg-white shadow-lg focus:outline-none'>
+                  <div className='py-1'>
+                    {/* Details */}
+                    <Menu.Item>
+                      {({ active }) => (
+                        <button
+                          onClick={toggleSensitiveDataOn}
+                          className={`flex w-full items-center space-x-3 px-4 py-2 text-left text-sm ${
+                            active
+                              ? 'bg-teal-50 text-teal-900'
+                              : 'text-gray-700'
+                          }`}
+                        >
+                          <Icon className='text-teal-600'>numbers</Icon>
+                          <span>Details</span>
+                        </button>
+                      )}
+                    </Menu.Item>
 
-                  {/* PIN */}
-                  <Menu.Item>
-                    {({ active }) => (
-                      <button
-                        onClick={toggleViewPin}
-                        className={`flex w-full items-center space-x-3 px-4 py-2 text-left text-sm ${
-                          active
-                            ? 'bg-purple-50 text-purple-900'
-                            : 'text-gray-700'
-                        }`}
-                      >
-                        <Icon className='text-purple-600'>password</Icon>
-                        <span>PIN</span>
-                      </button>
-                    )}
-                  </Menu.Item>
-                </div>
-              </Menu.Items>
-            </Transition>
-          </>
-        )}
-      </Menu>
+                    {/* PIN */}
+                    <Menu.Item>
+                      {({ active }) => (
+                        <button
+                          onClick={toggleViewPin}
+                          className={`flex w-full items-center space-x-3 px-4 py-2 text-left text-sm ${
+                            active
+                              ? 'bg-purple-50 text-purple-900'
+                              : 'text-gray-700'
+                          }`}
+                        >
+                          <Icon className='text-purple-600'>password</Icon>
+                          <span>PIN</span>
+                        </button>
+                      )}
+                    </Menu.Item>
+                  </div>
+                </Menu.Items>
+              </Transition>
+            </>
+          )}
+        </Menu>
+      )}
 
       {/* More Actions Dropdown */}
       <Menu as='div' className='relative'>
@@ -122,22 +128,26 @@ export const CardActions = ({
             >
               <Menu.Items className='absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-lg border border-gray-200 bg-white shadow-lg focus:outline-none'>
                 <div className='py-1'>
-                  {/* Change PIN */}
-                  <Menu.Item>
-                    {({ active }) => (
-                      <button
-                        onClick={toggleChangePin}
-                        className={`flex w-full items-center space-x-3 px-4 py-2 text-left text-sm ${
-                          active
-                            ? 'bg-purple-50 text-purple-900'
-                            : 'text-gray-700'
-                        }`}
-                      >
-                        <Icon className='text-purple-600'>password</Icon>
-                        <span>Change PIN</span>
-                      </button>
-                    )}
-                  </Menu.Item>
+                  {!isFrozen && (
+                    <>
+                      {/* Change PIN */}
+                      <Menu.Item>
+                        {({ active }) => (
+                          <button
+                            onClick={toggleChangePin}
+                            className={`flex w-full items-center space-x-3 px-4 py-2 text-left text-sm ${
+                              active
+                                ? 'bg-purple-50 text-purple-900'
+                                : 'text-gray-700'
+                            }`}
+                          >
+                            <Icon className='text-purple-600'>password</Icon>
+                            <span>Change PIN</span>
+                          </button>
+                        )}
+                      </Menu.Item>
+                    </>
+                  )}
 
                   {/* Freeze/Unfreeze */}
                   <Menu.Item>
