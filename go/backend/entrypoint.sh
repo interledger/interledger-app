9c1bbede4d700f24377eb648485f5c6ab84f852f
Stephan Butler <stephan.butler@deepsouthdevels.net>
2025-10-30 14:26:16 +0200

    feat(backend): simplistic WAIT_BEFORE_START environmental variable in entrypoint

--- go/backend/entrypoint.sh
+++ go/backend/entrypoint.sh
@@ -9,6 +9,13 @@ function usage {
     exit 1
 }
 
+# In some scenarios we may want to briefly delay the start of the application 
+# to allow sidecars or other services to start up first.
+if [[ ! -z "$WAIT_BEFORE_START" ]]; then
+	echo "Waiting for $WAIT_BEFORE_START seconds before starting..."
+	sleep $WAIT_BEFORE_START
+fi
+
 if [[ $1 = "test" ]]; then
 	echo "Running go test ./..."
 	go test ./...
