6995ba74e7a46cb595a87b658e41e7a0da376023
Radu-Cristian Popa <radu.popa@breakpointit.eu>
2025-10-30 12:41:08 +0200

    Improve card design

--- typescript/protea/app/components/CardView/CardViewFront.tsx
+++ typescript/protea/app/components/CardView/CardViewFront.tsx
@@ -1,9 +1,10 @@
+import clsx from 'clsx'
 import type { ComponentProps } from 'react'
 import { CardViewContainer } from './CardViewContainer'
 import { MasterCardLogo } from './MasterCardLogo'
 
 interface CardViewFrontProps extends ComponentProps<'div'> {
-  nameOnCard?: string
+  nameOnCard: string
 }
 
 export const CardViewFront = ({
@@ -12,15 +13,15 @@ export const CardViewFront = ({
   ...props
 }: CardViewFrontProps) => {
   return (
-    <CardViewContainer className={className} {...props}>
+    <CardViewContainer className={clsx(className, 'px-5 pt-4')} {...props}>
       <div className='flex h-full flex-col'>
         {/* Header with logos */}
         <div className='flex items-center justify-between text-sm'>
           <div className='flex items-center'>
             {/* Interledger Foundation SVG Logo */}
             <img
-              src='/interledger-foundation.svg'
-              alt='Interledger Foundation logo'
+              src='/interledger-card-logo.svg'
+              alt='Interledger Logo'
               className='h-8 max-w-[120px] object-contain opacity-90'
               onError={(e) => {
                 const target = e.target as HTMLImageElement
@@ -31,15 +32,13 @@ export const CardViewFront = ({
             />
           </div>
 
-          <div className='flex items-center justify-end'>
-            <span className='text-xs font-light tracking-wider text-black/80'>
-              debit
-            </span>
+          <div className='mt-1 flex items-center justify-end'>
+            <span className='text-xs tracking-wider text-charcoal'>debit</span>
           </div>
         </div>
 
         {/* EMV Chip */}
-        <div className='mt-8'>
+        <div className='ml-3.5 mt-4'>
           <div className='w-fit'>
             <img
               src='/emv_chip.svg'
@@ -50,17 +49,10 @@ export const CardViewFront = ({
         </div>
 
         {/* Footer with name and mastercard logo */}
-        <div className='mt-auto flex items-end justify-between'>
-          <div className='flex flex-col'>
-            {/* <div className='text-xs uppercase tracking-wide text-black/50'>
-              CARDHOLDER NAME
-            </div> */}
-            {nameOnCard && (
-              <span className='text-sm font-medium uppercase text-black'>
-                {nameOnCard}
-              </span>
-            )}
-          </div>
+        <div className='-mr-4 mt-auto flex items-center justify-between'>
+          <span className='mt-2 text-xs font-medium uppercase text-black'>
+            {nameOnCard}
+          </span>
           <MasterCardLogo />
         </div>
       </div>
