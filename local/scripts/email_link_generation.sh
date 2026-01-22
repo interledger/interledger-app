850d489b253c49d339b985d59c27e171aa4241ca
Anto <me@example.com>
2026-01-22 17:36:55 +0200

    fix email conf

--- local/scripts/email_link_generation.sh
+++ local/scripts/email_link_generation.sh
@@ -1,33 +1,31 @@
 #!/bin/bash
 
-PASSW_RECOVERY_PATH="/recovery"
-EMAIL_CONF_PATH="/verification"
+PASSW_RECOVERY_PATH="recovery"
+EMAIL_CONF_PATH="verification"
 
-if [[ -z "$1" ]]; then
-  echo "Usage: sh email_link_generation.sh <passowrd or recovery> <email>" >&2
-  exit 1
-elif [[ -z "$2" ]]; then
-  echo "Usage: sh email_link_generation.sh <passowrd or recovery> <email>" >&2
+if [[ -z "$1" || -z "$2" ]]; then
+  echo "Usage: sh email_link_generation.sh <password or recovery> <email>" >&2
   exit 1
 fi
 
-flow_id=$(docker exec local-postgres-1 psql -U postgres -d kratos -t -A -c "SELECT selfservice_recovery_flow_id FROM public.identity_recovery_tokens ORDER BY issued_at DESC LIMIT 1;")
-token=$(docker logs  local-kratos-1 2>&1 \
-  | grep $2 \
-  | sed -n 's/.*recovery_link_token=\([^ ]*\).*/\1/p' \
+FLOW_PATH=""
+if [[ "$1" == "recovery" ]]; then
+  FLOW_PATH=$PASSW_RECOVERY_PATH
+elif [[ "$1" == "password" ]]; then
+  FLOW_PATH=$EMAIL_CONF_PATH
+else
+  echo "Error: invalid first argument. Usage: sh email_link_generation.sh <password or recovery> <email>" >&2
+  exit 1
+fi
+
+# Fetch Flow ID and Token
+flow_id=$(docker exec local-postgres-1 psql -U postgres -d kratos -t -A -c "SELECT selfservice_${FLOW_PATH}_flow_id FROM public.identity_${FLOW_PATH}_tokens ORDER BY issued_at DESC LIMIT 1;")
+token=$(docker logs local-kratos-1 2>&1 \
+  | grep "$2" \
+  | sed -n "s/.*${FLOW_PATH}_link_token=\([^ ]*\).*/\1/p" \
   | tail -n 1)
 
-PATH=""
-if [[ "$1" == "recovery" ]]; then
-  PATH=$PASSW_RECOVERY_PATH
-elif [[ "$1" == "password" ]]; then
-  PATH=$EMAIL_CONF_PATH
-else
-  echo "Error: invalid first argument. Usage: sh email_link_generation.sh <passowrd or recovery> <email>" >&2
-  exit 1
-fi
-
+# Display Link
 echo "👇🏼 Link 👇🏼"
-echo "https://interledger.test/self-service$PATH?flow=$flow_id&token=$token"
+echo "https://interledger.test/self-service/$FLOW_PATH?flow=$flow_id&token=$token"
 echo "// Make sure you just requested the confirmation, the token is always the last token without considering the user"
-
