bc397a20150deeacdded2da19493613a827a1f95
Radu-Cristian Popa <radu.popa@breakpointit.eu>
2026-02-13 14:42:52 +0200

    Remove test list

--- go/backend/identities/ops/ops_test.go
+++ go/backend/identities/ops/ops_test.go
@@ -16,39 +16,6 @@ import (
 	"gitlab.com/fynbos/env"
 )
 
-func TestList(t *testing.T) {
-	ctx := context.Background()
-	db := db.MigrateTestDB(t, ctx)
-	b := ops.NewTestBackends(t, db)
-
-	env.SetEnv(t, "local")
-
-	walletID := uuid.NewString()
-	// Publicly visible
-
-	il, err := ops.List(ctx, b, walletID)
-	require.NoError(t, err)
-
-	assert.Len(t, il, 1)
-	assert.Equal(t, identities.PlatformTwitter, il[0].Platform)
-
-	il, err = ops.ListPublic(ctx, b, walletID)
-	require.NoError(t, err)
-
-	require.Len(t, il, 0)
-
-	il, err = ops.ListPublic(ctx, b, walletID)
-	require.NoError(t, err)
-
-	require.Len(t, il, 1)
-	assert.Equal(t, identities.PlatformTwitter, il[0].Platform)
-	assert.Equal(t, "@king_cold", il[0].Identifier)
-	assert.Equal(t, "", il[0].VerificationProof)
-	assert.Equal(t, walletID, il[0].WalletID)
-	assert.Equal(t, identities.StateVerified, il[0].State)
-	assert.True(t, il[0].Public)
-}
-
 func TestSearch(t *testing.T) {
 	ctx := context.Background()
 	db := db.MigrateTestDB(t, ctx)
