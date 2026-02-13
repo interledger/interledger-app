5c27c8763f0b160c37dbe1257e3c4f0939424b75
Bogdan Radu <bogdan.radu.b@gmail.com>
2026-02-13 13:29:21 +0000

    fix(geo): store by value instead of by pointer (#330)

--- go/geo/error.go
+++ go/geo/error.go
@@ -5,7 +5,6 @@ import "errors"
 var (
 	ErrInvalidFormat    = errors.New("invalid amount format")
 	ErrAssetMismatch    = errors.New("currency assets do not match")
-	ErrUnsupportedType  = errors.New("unsupported type for SetAmount")
 	ErrUnsupportedAsset = errors.New("unsupported asset")
 
 	ErrCountryNotFound = errors.New("country not found")
