8f09843cbde2d655ea08d67b2f03bc2c52708af9
Arpi <arpi@arpi-System>
2026-02-20 10:49:19 +0200

    interledger pay routes and menu items

---
+++ typescript/protea/app/routes/quick-pay.tsx
@@ -0,0 +1,53 @@
+import type {
+  ActionFunctionArgs,
+  LoaderFunctionArgs,
+  MetaFunction
+} from '@remix-run/node'
+import { json, redirect } from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
+import { route } from 'routes-gen'
+import type { ApplicationProps } from '~/components'
+import { Layouts } from '~/components'
+import { mergeMeta } from '~/lib/meta'
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const url = new URL(request.url)
+  console.log({ url })
+  const features = null
+
+  return json({
+    features
+  })
+}
+
+export const handle: ApplicationProps = {
+  layout: Layouts.Focus,
+  scaffold: {
+    header: { title: 'Interledger Pay' }
+  }
+}
+
+export const meta: MetaFunction = mergeMeta(() => [
+  {
+    title: 'Interledger Pay'
+  }
+])
+
+export default function Page() {
+  const { features } = useLoaderData<typeof loader>()
+
+  return (
+    <>
+      <p>{features}</p>
+    </>
+  )
+}
+
+export async function action({ request }: ActionFunctionArgs) {
+  const form = await request.formData()
+  const type = form.get('type') as string
+
+  console.log(type)
+
+  return null
+}
