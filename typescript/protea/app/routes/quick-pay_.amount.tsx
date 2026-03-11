189fa31f067877a0782263a30de66a5257617279
Arpi <arpi@arpi-System>
2026-03-11 12:27:06 +0200

    Dial pad page and functioanlity

--- typescript/protea/app/routes/quick-pay_.amount.tsx
+++ typescript/protea/app/routes/quick-pay_.amount.tsx
@@ -1,26 +1,52 @@
 import type {
-  ActionFunctionArgs,
   LoaderFunctionArgs,
   MetaFunction
 } from '@remix-run/node'
 import { json } from '@remix-run/node'
-import { useLoaderData } from '@remix-run/react'
+import { Link, useLoaderData } from '@remix-run/react'
+import { useEffect } from 'react'
 import type { ApplicationProps } from '~/components'
-import { Layouts } from '~/components'
+import { Button, GridColumn, Layouts, WalletGrid } from '~/components'
+import { DialPad, DialPadIds } from '~/components/QuickPay/Dialpad'
+import { useDialPadContext } from '~/lib/context/dialpad'
 import { mergeMeta } from '~/lib/meta'
+import { getSession } from '~/session.server'
+import { getUserSession } from '~/lib/kratos.server'
+import { QuickPaySessionError} from '~/lib/error.server'
 
 export async function loader({ request }: LoaderFunctionArgs) {
-  const url = new URL(request.url)
-  console.log({ url })
-  const features = null
-
+  let isLoggedIn
+  
+    try {
+      await getUserSession(request)
+      isLoggedIn = true
+  
+    } catch (err) {
+      isLoggedIn = false
+    }
+  
+  const session = await getSession(request.headers.get('Cookie'))
+  const walletAddressInfo = session.get('quickPay')
+  const assetCode = walletAddressInfo?.validWalletAddress?.assetCode
+  
+  if (walletAddressInfo === undefined || assetCode === undefined) {
+    throw json(
+      {
+        code: "QUICKPAY_SESSION_ERROR",
+        message: "Payment session expired."
+      },
+      { status: 400 }
+    )
+  }
   return json({
-    features
-  })
+    assetCode,
+    isLoggedIn
+  } as const)
 }
 
 export const handle: ApplicationProps = {
-  layout: Layouts.Focus,
+  layout: (match) =>
+    match.data?.isLoggedIn ? Layouts.Wallet : Layouts.Marketing,
   scaffold: {
     header: { title: 'Interledger Pay' }
   }
@@ -33,20 +59,63 @@ export const meta: MetaFunction = mergeMeta(() => [
 ])
 
 export default function Page() {
-  const { features } = useLoaderData<typeof loader>()
+  const { assetCode } = useLoaderData<typeof loader>()
+  const { amountValue, setAmountValue, setAssetCode } = useDialPadContext()
+
+  useEffect(() => {
+    setAssetCode(assetCode)
+  })
 
   return (
-    <>
-      <p>{features}</p>
-    </>
+    <WalletGrid>
+      <GridColumn
+        className='col-span-full mt-20 mx-auto'
+      >
+        <DialPad />
+        <div className="flex justify-center gap-2 mt-12 w-64">
+          <Link
+            to={`/quick-pay/request`}
+            onClick={(e: React.MouseEvent<HTMLElement>) => {
+              if (
+                amountValue.indexOf(DialPadIds.Dot) === -1 ||
+                amountValue.endsWith(DialPadIds.Dot)
+              ) {
+                setAmountValue(Number(amountValue).toFixed(2).toString())
+              }
+              if (Number(amountValue) === 0) e.preventDefault()
+            }}
+            className='min-w-28'
+          >
+            <Button
+              aria-label="request"
+              disabled={Number(amountValue) === 0}
+            >
+              Request
+            </Button>
+          </Link>
+          <Link
+            to={`/quick-pay/pay`}
+            onClick={(e: React.MouseEvent<HTMLElement>) => {
+              if (
+                amountValue.indexOf(DialPadIds.Dot) === -1 ||
+                amountValue.endsWith(DialPadIds.Dot)
+              ) {
+                setAmountValue(Number(amountValue).toFixed(2).toString())
+              }
+              if (Number(amountValue) === 0) e.preventDefault()
+            }}
+            className='min-w-28'
+          >
+            <Button
+              aria-label="pay"
+              disabled={Number(amountValue) === 0}
+            >
+              Pay
+            </Button>
+          </Link>
+        </div>
+      </GridColumn>
+    </WalletGrid>
   )
 }
 
-export async function action({ request }: ActionFunctionArgs) {
-  const form = await request.formData()
-  const type = form.get('type') as string
-
-  console.log(type)
-
-  return null
-}
