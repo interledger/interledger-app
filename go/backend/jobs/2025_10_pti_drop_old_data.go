6e1a6b144bf24de035728f1d9b8e023f6ec80bd5
Bogdan Radu <bogdan.radu.b@gmail.com>
2026-03-30 21:21:00 +0300

    feat(pti-drop-old-data): job update for old pti data removal

--- go/backend/jobs/2025_10_pti_drop_old_data.go
+++ go/backend/jobs/2025_10_pti_drop_old_data.go
@@ -41,13 +41,13 @@ func (a *Activity) PtiDropWalletOldDataActivity(ctx context.Context) (err error)
 
 	var wlas []WalletLinkedAccount
 
-	q := `select 
-			ws.id as wallet_id, 
-			la.id as linked_account_id 
-		from pti_users pti 
+	q := `select
+			ws.id as wallet_id,
+			la.id as linked_account_id
+		from pti_users pti
 		join wallets ws on
 			pti.wallet_id = ws.id
-		join linked_accounts la on 
+		join linked_accounts la on
 			ws.id = la.wallet_id and
 			la.provider = 'pti';`
 	if err = a.b.DB().SelectContext(ctx, &wlas, q); err != nil {
@@ -74,50 +74,41 @@ func (a *Activity) PtiDropWalletOldDataActivity(ctx context.Context) (err error)
 			goto rollback
 		}
 
-		// transactions table, in short
-		// this query forces linked_account_id into transactions as is not present there
-		qTransactions := `delete 
-							tons 
-						  from transactions tons
-						  join transfers ters on
-							tons.id = ters.transaction_id and 
-							tons.asset_code = 'USD' and 
-							ters.asset_code = 'USD'
-						  where 
-						  	tons.provider = 'pti' and 
-						  	ters.linked_acc_id = $1;`
-		if _, err := tx.Exec(qTransactions, wla.LinkedAccoutId); err != nil {
-			logger.Error("transactions table", err)
-			goto rollback
-		}
-
 		// transfers table
 		if _, err := tx.Exec("delete from transfers where linked_acc_id = $1 and asset_code = 'USD';", wla.LinkedAccoutId); err != nil {
 			logger.Error("transfers table", err)
 			goto rollback
 		}
 
-		// pti_transactions table
-		if _, err := tx.Exec("truncate pti_transactions;"); err != nil {
-			logger.Error("pti_transactions table", err)
+		// transactions table, in short
+		// this query forces linked_account_id into transactions as is not present there
+		qTransactions := `delete from transactions tons
+						  using transfers ters
+						  where tons.id = ters.transaction_id
+						    and tons.asset_code = 'USD'
+						    and ters.asset_code = 'USD'
+						    and tons.provider = 'pti'
+						    and ters.linked_acc_id = $1;`
+		if _, err := tx.Exec(qTransactions, wla.LinkedAccoutId); err != nil {
+			logger.Error("transactions table", err)
 			goto rollback
 		}
 
 		// wallet_kyc_status
-		if _, err := tx.Exec("update walet_kyc_status set status = 0 where wallet_id = $1;", wla.WalletId); err != nil {
+		if _, err := tx.Exec("update wallet_kyc_status set status = 0 where wallet_id = $1;", wla.WalletId); err != nil {
 			logger.Error("wallet_kyc_status table", err)
 			goto rollback
 		}
 
 		// linked_accounts
-		qLinkedAccounts := `delete 
+		qLinkedAccounts := `delete
 							from linked_accounts where
 								id = $1 and
 								wallet_id = $2 and
 								provider = 'pti' and
 								send_country = 'US' and
-								receive_country = 'US' and 
-								send_currency = 'USD' and 
+								receive_country = 'US' and
+								send_currency = 'USD' and
 								receive_currency = 'USD';`
 		if _, err := tx.Exec(qLinkedAccounts, wla.LinkedAccoutId, wla.WalletId); err != nil {
 			goto rollback
@@ -125,12 +116,24 @@ func (a *Activity) PtiDropWalletOldDataActivity(ctx context.Context) (err error)
 
 	}
 
-rollback:
-	err = tx.Rollback()
-	if err != nil {
-		logger.Error("failed to rollback", err) // ouch! the worst thing can happen
-		return err
+	// pti_transactions table
+	// if _, err = tx.Exec("truncate table pti_transactions;"); err != nil {
+	// 	logger.Error("pti_transactions table", err)
+	// 	goto rollback
+	// }
+
+	// pti_users table
+	if _, err = tx.Exec("truncate table pti_users;"); err != nil {
+		logger.Error("pti_users table", err)
+		goto rollback
 	}
 
-	return tx.Commit() // fingers crossed
+	return tx.Commit()
+
+rollback:
+	if rbErr := tx.Rollback(); rbErr != nil {
+		logger.Error("failed to rollback", rbErr) // ouch! the worst thing can happen
+		return rbErr
+	}
+	return err
 }
