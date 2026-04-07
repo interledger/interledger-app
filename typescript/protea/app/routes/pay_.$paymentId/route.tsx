d20cbef40a70fd88ac5b0d7bedda8b8a2b44a8c3
Anto <antoniu@interledger.foundation>
2026-04-07 17:13:09 +0300

    feat(protea): [WAL-507] Use @Ory Kratos Client instead of API calls (#412)

--- typescript/protea/app/routes/pay_.$paymentId/route.tsx
+++ typescript/protea/app/routes/pay_.$paymentId/route.tsx
@@ -27,7 +27,7 @@ import { jsonWithCSRF } from '~/lib/csrf.server'
 import type { ConnectError } from '~/lib/error.server'
 import { isConnectError } from '~/lib/error.server'
 import { grpc } from '~/lib/grpc.server'
-import { getUserSession } from '~/lib/kratos.server'
+import { getUserSession } from '~/lib/kratos/session.server'
 import { mergeMeta } from '~/lib/meta'
 import { PayStep, usePayStore } from '~/lib/usePayStore'
 import { useScaffoldStore } from '~/lib/useScaffoldStore'
@@ -107,8 +107,9 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
   // Only load the phone mask if we require otp
   if (payment.requiredActions.includes(7)) {
     phoneMask = await getUserSession(request).then((v) => {
-      const len = v.identity.traits.phone.length
-      return v.identity.traits.phone.substring(len - 4, len).padStart(len, '*')
+      const phone: string = v?.identity?.traits?.phone ?? ''
+      const len = phone.length
+      return phone.substring(len - 4, len).padStart(len, '*')
     })
   }
 
