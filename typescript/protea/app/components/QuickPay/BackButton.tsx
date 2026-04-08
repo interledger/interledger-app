a1d44e9cbad629e38b9966fd0ee4ff920b984272
Arpi <arpi@arpi-System>
2026-04-08 11:36:57 +0300

    Added Back navigation and logic

--- typescript/protea/app/components/QuickPay/BackButton.tsx
+++ typescript/protea/app/components/QuickPay/BackButton.tsx
@@ -1,9 +1,7 @@
-import { Link, useFetcher, useNavigate } from '@remix-run/react'
+import { Link, useFetcher, useNavigate } from 'react-router'
 import { flushSync } from 'react-dom'
-import type { QuickPaySession } from '~/lib/types'
-import { commitSession } from '~/session.server'
-import { redirect, type Session } from '@remix-run/node'
 import { useDialPadContext } from '~/lib/context/dialpad'
+import type { QuickPaySession } from '~/lib/types'
 import { clsx } from 'clsx'
 
 interface BackButtonProps {
@@ -34,22 +32,7 @@ export const BackButton = ({ title, to, clearSessionKeys, className, resetAmount
             navigate(to)
         }
     }
-    return <div className={clsx('w-full cursor-pointer hover:text-rose-600', className)}>
+    return <div className={clsx('w-full text-left cursor-pointer hover:text-rose-600', className)}>
         {clearSessionKeys || resetAmount ? <Link to={to} onClick={(e) => { handleBackClick(e, to, clearSessionKeys) }}>&laquo; {title}</Link> : <Link to={to}>&laquo; {title}</Link>}
     </div>
-}
-export const handleSessionUpdate = async (session: Session, intent: string, to: string, clearSessionKeys: Array<keyof QuickPaySession> | 'all') => {
-    if (intent === 'updateSession') {
-        let sessionData = session.get('quickPay')
-        if (clearSessionKeys === 'all') {
-            sessionData = undefined
-        } else {
-            for (const key of clearSessionKeys) { sessionData[key] = undefined }
-        }
-        session.set('quickPay', sessionData)
-
-        return redirect(to, {
-            headers: { 'Set-Cookie': await commitSession(session) }
-        })
-    }
 }
