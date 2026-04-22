739e2767ead9a3741f12e6f673eb9c9689314fc4
David Marian <david.pavel2906@gmail.com>
2026-04-22 15:43:09 +0300

    remove recovery middleware

--- go/backend/api/routes.go
+++ go/backend/api/routes.go
@@ -15,7 +15,6 @@ func NewRouter(uc user.Client, wc wallets.Client, gc gatehub.Client) http.Handle
 	r := chi.NewRouter()
 
 	r.Use(api_middleware.MakeRequestIDMiddleware())
-	r.Use(api_middleware.MakeRecoveryMiddleware())
 	r.Use(api_middleware.MakeUserMiddleware(uc))
 	r.Use(api_middleware.MakeWalletMiddleware(uc, wc))
 
