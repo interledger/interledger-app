a8954f5950e6736270941c0d8289595b93d0cebf
David Marian <david.pavel2906@gmail.com>
2026-04-23 16:03:58 +0300

    add nil guards for required clients in NewRouter

--- go/backend/api/routes.go
+++ go/backend/api/routes.go
@@ -12,8 +12,19 @@ import (
 )
 
 func NewRouter(uc user.Client, wc wallets.Client, gc gatehub.Client) http.Handler {
+	if uc == nil {
+		panic("api: user.Client is nil")
+	}
+	if wc == nil {
+		panic("api: wallets.Client is nil")
+	}
+	if gc == nil {
+		panic("api: gatehub.Client is nil")
+	}
+
 	r := chi.NewRouter()
 
+	// TODO: discuss adding a recovery middleware
 	r.Use(api_middleware.MakeRequestIDMiddleware())
 	r.Use(api_middleware.MakeUserMiddleware(uc))
 	r.Use(api_middleware.MakeWalletMiddleware(uc, wc))
