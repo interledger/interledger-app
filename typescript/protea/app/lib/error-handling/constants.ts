ec05527db84abd6c477fd0349d7bc9581d106876
darianm <darian@interledger.org>
2026-06-04 14:53:08 +0300

    test make workflow fail

--- typescript/protea/app/lib/error-handling/constants.ts
+++ typescript/protea/app/lib/error-handling/constants.ts
@@ -1,6 +1,6 @@
 export const ErrorCodes = {
   OTP_EXPIRED: 'OTP_EXPIRED',
-  USER_INVALIDATED: 'USER_INVALIDATED'
+    USER_INVALIDATED: 'USER_INVALIDATED'
 } as const
 
 export type ErrorCode = keyof typeof ErrorCodes
