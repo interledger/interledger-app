9c07e2889d6af3b7796e1570dc28744d85288aa5
Arpi <arpi@arpi-System>
2026-07-18 18:50:20 +0300

    Allow wallet address returnTo

--- typescript/protea/app/routes/me_.$.tsx
+++ typescript/protea/app/routes/me_.$.tsx
@@ -178,7 +178,13 @@ export default function Page() {
         </p>
       )}
 
-{!isUser &&  <ButtonRouter to={`/login?returnTo=${walletAddress.address}`}>Log in</ButtonRouter>}
+      {!isUser && (
+        <ButtonRouter
+          to={`/login?returnTo=${encodeURIComponent(walletAddress.address)}`}
+        >
+          Log in
+        </ButtonRouter>
+      )}
 
       {!isUser && (
         <Card>
