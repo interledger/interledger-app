61fc59aad57b051a45f636a335baf5faa5b7f6fd
Sorin Anton <antsorin@yahoo.com>
2026-07-31 18:08:02 +0300

    feat(zar-eur): formatting

--- go/backend/config/models.go
+++ go/backend/config/models.go
@@ -30,28 +30,28 @@ type StartConfig struct {
 	BlockedRegions       []string          `yaml:"blocked_regions"`
 	DeleteAccountEnabled bool              `yaml:"delete_account_enabled"`
 
-	DB         DBConfig         `yaml:"db"`
-	Kratos     KratosConfig     `yaml:"kratos"`
-	Temporal   TemporalConfig   `yaml:"temporal"`
-	Rafiki     RafikiConfig     `yaml:"rafiki"`
-	Gatehub    GatehubConfig    `yaml:"gatehub"`
-	Xago       XagoConfig       `yaml:"xago"`
-	PTI        PTIConfig        `yaml:"pti"`
-	Persona    PersonaConfig    `yaml:"persona"`
-	Twilio     TwilioConfig     `yaml:"twilio"`
-	Email      EmailConfig      `yaml:"email"`
-	Slack      SlackConfig      `yaml:"slack"`
-	Chimoney   ChimoneyConfig   `yaml:"chimoney"`
-	Admin      AdminConfig      `yaml:"admin"`
-	Mobile     MobileConfig     `yaml:"mobile"`
-	Vault      VaultConfig      `yaml:"vault"`
-	Sentry     SentryConfig     `yaml:"sentry"`
-	Smarty     SmartyConfig     `yaml:"smarty"`
-	Pusher     PusherConfig     `yaml:"pusher"`
-	Segment    SegmentConfig    `yaml:"segment"`
-	Agreements AgreementsConfig `yaml:"agreements"`
-	OTEL       OTELConfig       `yaml:"otel"`
-	Plaid      PlaidConfig      `yaml:"plaid"`
+	DB             DBConfig             `yaml:"db"`
+	Kratos         KratosConfig         `yaml:"kratos"`
+	Temporal       TemporalConfig       `yaml:"temporal"`
+	Rafiki         RafikiConfig         `yaml:"rafiki"`
+	Gatehub        GatehubConfig        `yaml:"gatehub"`
+	Xago           XagoConfig           `yaml:"xago"`
+	PTI            PTIConfig            `yaml:"pti"`
+	Persona        PersonaConfig        `yaml:"persona"`
+	Twilio         TwilioConfig         `yaml:"twilio"`
+	Email          EmailConfig          `yaml:"email"`
+	Slack          SlackConfig          `yaml:"slack"`
+	Chimoney       ChimoneyConfig       `yaml:"chimoney"`
+	Admin          AdminConfig          `yaml:"admin"`
+	Mobile         MobileConfig         `yaml:"mobile"`
+	Vault          VaultConfig          `yaml:"vault"`
+	Sentry         SentryConfig         `yaml:"sentry"`
+	Smarty         SmartyConfig         `yaml:"smarty"`
+	Pusher         PusherConfig         `yaml:"pusher"`
+	Segment        SegmentConfig        `yaml:"segment"`
+	Agreements     AgreementsConfig     `yaml:"agreements"`
+	OTEL           OTELConfig           `yaml:"otel"`
+	Plaid          PlaidConfig          `yaml:"plaid"`
 	WalletFeatures WalletFeaturesConfig `yaml:"wallet_features"`
 }
 
